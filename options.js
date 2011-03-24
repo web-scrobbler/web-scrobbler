d7c9fb5b56a200f9d6f635238485c3debe605bac
david-sabata <david.n.sabata@gmail.com>
2011-03-24 23:22:15 +0100

    options updated, added few settings

--- options.js
+++ options.js
@@ -1,29 +1,52 @@
-function saveOptions() {	
+// credentials -----------------------------------------------------------------
+function saveCredentials() {	
 	localStorage.username = document.getElementById("username").value;
 	localStorage.password = MD5(document.getElementById("password").value);
 	chrome.extension.sendRequest({type: "newSession"});
 	
 	//Update status box in options page to let user know their username and pass has been taken.
-	updateStatus("Will now scrobble using "+localStorage.username+".");
+	updateCredentialsStatus("Will now scrobble using "+localStorage.username+".");
 	return false;
 }
 
-function deleteOptions() {
-	localStorage.clear();
+function deleteCredentials() {
+	delete localStorage.username;
+      delete localStorage.password;
 	chrome.extension.sendRequest({type: "newSession"});
 	
-	updateStatus("Account deleted.");
+	updateCredentialsStatus("Account deleted.");
 }
 
 
-function updateStatus(msg) {
+function updateCredentialsStatus(msg) {
 	// Updates the status box
-	document.getElementById("status").style.display="block";
-	document.getElementById("status").innerHTML=msg;
+	document.getElementById("cred-status").style.display="block";
+	document.getElementById("cred-status").innerHTML=msg;
 }
 
 function getCurrentUser() {
 	if (localStorage.username != null) {
-		updateStatus('Using '+localStorage.username+' to scrobble.')
+		updateCredentialsStatus('Using '+localStorage.username+' to scrobble.')
 	}
 }
+
+
+
+// options ---------------------------------------------------------------------
+$(function(){
+   
+   // use immediate-change checkboxes
+   
+   $('#use-notifications').click(function(){
+      localStorage['useNotifications'] = this.checked ? 1 : 0;
+   });
+   
+   $('#use-youtube-inpage').click(function(){
+      localStorage['useYTInpage'] = this.checked ? 1 : 0;
+   });
+      
+   // preload options
+   $('#use-notifications').attr('checked', (localStorage['useNotifications'] == 1));   
+   $('#use-youtube-inpage').attr('checked', (localStorage['useYTInpage'] == 1));
+      
+});
