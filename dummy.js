8f7ef9ce2990c8a45e42a7258274b22539b6eadb
david-sabata <david.n.sabata@gmail.com>
2011-08-05 10:10:18 +0200

    testing permissions for <all_urls>, hope that once user confirms, they dont have to re-confirm on every update'

---
+++ dummy.js
@@ -0,0 +1,7 @@
+/**
+ * I do nothing.
+ * 
+ * I am here to be active on all websites, so once the users confirm 
+ * extension's permissions, they don't have to re-confirm on every update
+ * that adds a new website connector.
+ */
