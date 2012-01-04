4cfdf2f3ddcaa6acee717f1c15e330df0dfc9599
Jordan Perr <neutrino15@gmail.com>
2012-01-04 03:07:15 -0500

    added 10 second delay due to slow UI on Windows. Duplicate notification bug should be fixed

--- pandora.js
+++ pandora.js
@@ -32,6 +32,7 @@ function LFM_TRACK_DURATION() {
 /********* Connector: ***********/
 
 var LFM_lastTrack = "";
+var LFM_isWaiting = 0;
 
 function LFM_updateNowPlaying(){
 	// Acquire data from page
@@ -56,6 +57,7 @@ function LFM_updateNowPlaying(){
 			}
 		});
 	}	
+	LFM_isWaiting = 0;
 }
 
 // Run at startup
@@ -65,7 +67,10 @@ $(function(){
 	$(LFM_WATCHED_CONTAINER).live('DOMSubtreeModified', function(e) {
 		//console.log("Live watcher called");
 		if ($(LFM_WATCHED_CONTAINER).length > 0) {
-			LFM_updateNowPlaying();
+			if(LFM_isWaiting == 0){
+				LFM_isWaiting = 1;
+				setTimeout(LFM_updateNowPlaying, 10000);
+			}
 			return;    
 		}
 	});
@@ -74,6 +79,4 @@ $(function(){
 		chrome.extension.sendRequest({type: 'reset'});
 		return true;      
 	});
-
-	LFM_updateNowPlaying();
 });
