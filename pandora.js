cbb7f59754308a3dee6cb79a2cf006d439198285
Jordan Perr <neutrino15@gmail.com>
2012-01-03 21:10:16 -0500

    fixed 0 second bug in pandora

--- pandora.js
+++ pandora.js
@@ -39,13 +39,19 @@ function LFM_TRACK_DURATION() {
 var LFM_lastTrack = "";
 
 function LFM_updateNowPlaying(){
-	console.log("LFM_updateNowPlaying()");
+	// Acquire data from page
 	title = LFM_TRACK_TITLE();
 	artist = LFM_TRACK_ARTIST();
 	album = LFM_TRACK_ALBUM();
 	duration = LFM_TRACK_DURATION();
 	newTrack = title + " " + artist;
+	// Update scrobbler if necessary
 	if (newTrack != "" && newTrack != LFM_lastTrack){
+		if (duration == 0) {
+			// Nasty workaround for delayed duration visiblity with skipped tracks.
+			setTimeout(LFM_updateNowPlaying, 5000);
+			return 0;
+		}
 		console.log("submitting a now playing request. artist: "+artist+", title: "+title+", duration: "+duration);
 		LFM_lastTrack = newTrack;
 		chrome.extension.sendRequest({type: 'validate', artist: artist, track: title}, function(response) {
@@ -58,12 +64,12 @@ function LFM_updateNowPlaying(){
 	}	
 }
 
+// Run at startup
 $(function(){
-
 	console.log("Pandora module starting up");
 
 	$(LFM_WATCHED_CONTAINER).live('DOMSubtreeModified', function(e) {
-		console.log("Live watcher called");
+		//console.log("Live watcher called");
 		if ($(LFM_WATCHED_CONTAINER).length > 0) {
 			LFM_updateNowPlaying();
 			return;    
@@ -71,5 +77,4 @@ $(function(){
 	});
 
 	LFM_updateNowPlaying();
-
 });
