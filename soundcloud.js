2fd041f6aea42e9bf625e7fa52942549bb8b8f6a
Dustin Haffner <dh@xix.org>
2012-08-08 15:18:02 -0700

    update soundcloud.js

--- soundcloud.js
+++ soundcloud.js
@@ -5,12 +5,40 @@
 //
 $(function() {	
 	// The div.player enclosing the current song.
-	var current; 
+	var current, validated = false; 
+	
+	
+	var trackids = {};
+
+	var validate = function(song) {
+		chrome.extension.sendRequest(
+			{type: 'validate', artist: song.artist, track: song.track},
+			function(response) {
+				validated = response;
+				console.log(current, validated);
+				if (!current || validated)
+					return;
+					
+				if ($(current).hasClass('set')) {
+					$('li.playing', current).append('<li>(not recognized)</li>');
+				}
+				else {
+					var stats;
+					if (stats = $('span.stats', current)) {
+						//var notfound = $('<span>not found</span>');
+						stats.prepend('<span>not found</span>');
+						$('span.first').removeClass('first');
+						$('span:first-child').addClass('first')
+					}
+				}
+			});
+	};
 
 	var track = function(context) {
-		return ($(context).hasClass('set') && 
-		        $('li.playing .info a', context).get(0).text) ||
-		       $('.info-header h3 a', context).get(0).text;
+		if ($(context).hasClass('set'))
+			return $('li.playing .info a', context).text();
+		else
+			return $('.info-header h3 a, .info-header h1 em', context).text();
 	};
 	var artist = function(context) {
 		return $('.info-header a.user-name', context).get(0).text;
@@ -31,19 +59,15 @@ $(function() {
 	// Track current song by watching which 'a.play' elements get the
 	// 'playing' class added to them. Then the closest div.player enclosing
 	// this tag will contain elements that describe the current song.
-	var lastElement; 
-	var checkPlaying = function(element) {
+	var check = function(context) {
 		// Expect only one element to match 'a.playing'
-		if (!element)
-			element = $('a.playing').get(0);
 
-		if (!element || (lastElement && element == lastElement))
-			return;
-
-		lastElement = element;
-		current = $(element).closest('li.set').get(0) || $(element).closest('div.player').get(0);
-		if (current)
-		    update(song(current));
+		var parent;
+		parent = $(context).closest('li.set').get(0) ||
+                 $(context).closest('div.player').get(0);
+		if (!parent)
+			console.log('couldnt get player from', context);
+		return (current = parent);
 	};
 
 	// Prevent multiple update firings.
@@ -52,29 +76,36 @@ $(function() {
 		if (lock)
 			return;
 		lock = true;
-		chrome.extension.sendRequest(
-			{type: 'validate', artist: song.artist, track: song.track},
-			function(response) {
-				if (response) {
-					chrome.extension.sendRequest(
-						{type: 'nowPlaying', artist: response.artist,
-						 track: response.track, duration: duration.total});
-				}
-				else {
-					chrome.extension.sendRequest({type: 'nowPlaying', 
-												  duration: duration.total});
-				}
-				lock = false;
-			});
+		if (validated) {
+			chrome.extension.sendRequest(
+				{type: 'nowPlaying', artist: validated.artist,
+				 track: validated.track, duration: validated.total});
+		}
+		else {
+			chrome.extension.sendRequest({type: 'nowPlaying', 
+										  duration: song.duration});
+		}
+		setTimeout(function() { lock = false; }, 1000);
 	};
 
 	var timespan;
 	$('.timecodes .editable').live('DOMSubtreeModified', function() {
-		if (timespan == this)
-			return;
-		var text = $(this).text();
-		if (text === '0.01')
-			checkPlaying(timespan = this);
+		var text = $(this).text(), player;
+		if (text == '0.01' && timespan != this) {
+			timespan = this;
+			player = check(timespan);
+			if (player)
+				validate(song(player));
+		}
+		else if (validated && timespan == this) {
+			var length = $(this).siblings('span.duration').text();
+			var curr = parseFloat(text), full = parseFloat(length);
+			if (curr && full && (curr / full) >= .4) {
+				update(current);
+				timespan = current = null;
+				validated = false;
+			}
+		}
 	});
 
 	// Alternative to using DOMSubtreeModified; just use setInterval:
