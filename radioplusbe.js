e808162e33e5e9d7ed41e5f430bd9fb417465798
jonasle <jonas.lejeune@tass.be>
2013-05-22 11:34:51 +0200

    Update and rename radio+be.js to radioplusbe.js
    Finished up Radioplus.be connector, included unload-function

--- radioplusbe.js
+++ radioplusbe.js
@@ -1,3 +1,9 @@
+/**
+ * Connector for Belgian radio-streaming website Radioplus.be
+ * Made by Jonas
+ * Based loosely on the Google Music connector by Sharjeel Aziz
+ */
+
 // State for event handlers
 var state = 'init';
 
@@ -7,24 +13,30 @@ var clipTitle = '';
 // Timeout to scrobble track ater minimum time passes
 var scrobbleTimeout = null;
 
-// Glabal constant for the song container ....
+// Global constant for the song container ....
 var CONTAINER_SELECTOR = '.now-playing-hidden';
 
 
 $(function(){   
   $(CONTAINER_SELECTOR).live('DOMSubtreeModified', function(e) {
-
 		if ($(CONTAINER_SELECTOR).length > 0) {
 			updateNowPlaying();
 			return;    
 		}
-
    });
    
    console.log("Last.fm Scrobbler: starting Radioplus.be connector")
    
    // first load
    updateNowPlaying();
+   
+   $(window).unload(function() {
+
+      // reset the background scrobbler song data
+      chrome.extension.sendRequest({type: 'reset'});
+
+      return true;
+   });
 });
 
 /**
@@ -34,17 +46,18 @@ function updateNowPlaying(){
   var parsedInfo = parseInfo();
   artist   = parsedInfo['artist']; 	//global
   track    = parsedInfo['track'];	//global
-  duration = parsedInfo['duration']; 	//global
 	
-  if (artist == '' || track == '' || duration == 0) {return;}
+  if (artist == '' || track == '') {return;}
     
   // check if the same track is being played and we have been called again
   // if the same track is being played we return
-  if (clipTitle == track) {
+  if (clipTitle == track) 
+  {
     return;
   }
+
   clipTitle = track;
-    
+  
   chrome.extension.sendRequest({type: 'validate', artist: artist, track: track}, function(response) {
 	  if (response != false) {
       chrome.extension.sendRequest({type: 'nowPlaying', artist: artist, track: track});
@@ -62,43 +75,26 @@ function parseInfo() {
   var track    = '';
   var duration = 0;
   
-  var divContents = $("div.now-playing-hidden").text();
+  var divContents = $("h3.now-playing-hidden").text();
   var combinedValue = divContents.split("**");
   // Get artist and song names
   var artistValue = combinedValue[0];
   var trackParent = combinedValue[1];
-  var durationValue = $("span#duration").text();
     
   try {
     if (null != artistValue) {
       artist = artistValue.replace(/^\s+|\s+$/g,'');
     }
     if (null != trackParent) {
-      track = $("div.fade-out-content", trackParent).text();
-      track = track.replace(/^\s+|\s+$/g,'');
-    }
-    if (null != durationValue) {
-      duration = parseDuration(durationValue);
+      track = trackParent.replace(/^\s+|\s+$/g,'');
     }
   } catch(err) {
-    return {artist: '', track: '', duration: 0};
+    return {artist: '', track: ''};
   }
   
-  console.log("artist: " + artist + ", track: " + track + ", duration: " + duration);
+  //console.log("artist: " + artist + ", track: " + track);
 	
-  return {artist: artist, track: track, duration: duration};
-}
-
-function parseDuration(artistTitle) {
-	try {
-		match = artistTitle.match(/\d+:\d+/g)[0]
-
-		mins    = match.substring(0, match.indexOf(':'));
-		seconds = match.substring(match.indexOf(':')+1);
-		return parseInt(mins*60) + parseInt(seconds);
-	} catch(err){
-		return 0;
-	}
+  return {artist: artist, track: track};
 }
 
 
@@ -107,8 +103,7 @@ function parseDuration(artistTitle) {
  */ 
 function scrobbleTrack() {
    // stats
-   chrome.extension.sendRequest({type: 'trackStats', text: 'The Google Music song scrobbled'});
-   
+   chrome.extension.sendRequest({type: 'trackStats', text: 'The Radio+ song scrobbled'});
    // scrobble
    chrome.extension.sendRequest({type: 'submit'});
 }
@@ -120,24 +115,14 @@ function scrobbleTrack() {
  */ 
 chrome.extension.onRequest.addListener(
 	function(request, sender, sendResponse) {
-		switch(request.type) {
+	  switch(request.type) {
     	// called after track has been successfully marked as 'now playing' at the server
-      case 'nowPlayingOK':
-         /*
-        var min_time = (240 < (duration/2)) ? 240 : (duration/2); //The minimum time is 240 seconds or half the track's total length. Duration comes from updateNowPlaying()
-        
-				// cancel any previous timeout
-        if (scrobbleTimeout != null)
-      		clearTimeout(scrobbleTimeout);
-               
-       		// set up a new timeout
-        	scrobbleTimeout = setTimeout(function(){setTimeout(scrobbleTrack, 2000);}, min_time*1000); 
-            */
+        case 'nowPlayingOK':
           break;
             
         // not used yet
         case 'submitOK':
-       		break;
+		  break;
 
         // not used yet
         case 'submitFAIL':
