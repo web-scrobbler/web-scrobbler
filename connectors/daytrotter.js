646dcc4a3d20af4fb293f7b9cd1d49007bc41a06
tomkrall <tkforeverer@yahoo.com>
2013-07-17 15:17:27 -0400

    Update daytrotter.js
    Better style modified check, removed dead code, modular.

--- connectors/daytrotter.js
+++ connectors/daytrotter.js
@@ -7,40 +7,47 @@
 
 console.log('Entered daytrotter.js');
 
-var mediainfo;
-var track;
-var artist;
-var duration;
-var current;
-
-var _flg_Reported;
-
+var mediainfo, track, artist, duration;
+var album = "Daytrotter Session";
 var container_duration = 'div#jp_interface_1.jp-interface div.jp-duration';
 var container_currenttime = 'div#jp_interface_1.jp-interface div.jp-current-time';
 var container_mediainfo	= 'div#jp_playlist_1.jp-playlist.single ul.nof li a';
-var container_playbutton = 'a.jp-play';		//'div#jp_playlist_1.jp-playlist.single ul.nof.jp-controls li a.jp-pause';
 var container_pausebutton = 'a.jp-pause';	//'div#jp_playlist_1.jp-playlist.single ul.nof.jp-controls li a.jp-play';
-
-var _flg_IsPlaying = false;
 var _flg_IsPaused = false;
-var _flg_NeedsToBeReported = 0;
-var _flg_HasBeenReported = 0;
 
 function IsPaused(){
 	return !_flg_IsPaused;
 }
 
-$(function(){
+function UpdateMediaInfo( _mediainfo, _artist, _track, _duration) {
+	//console.log("updating media info");
+	mediainfo = _mediainfo;
+	artist = _artist;
+	track = _track;
+	duration = _duration;
+}
 
+function PostMediaInfo (){
+	//console.log("posting media info");
+	chrome.runtime.sendMessage({type: 'validate', artist: artist, track: track}, function(response) {
+		if (response != false) {
+			chrome.runtime.sendMessage({type: 'nowPlaying', artist: artist, track: track, duration: duration});
+		} else { // on failure send nowPlaying 'unknown song'
+			chrome.runtime.sendMessage({type: 'nowPlaying', duration: duration});
+		}
+	});
+}
+
+
+$(function(){
 	console.log("Daytrotter module starting up");
 
-	/*********************************************************************************************************************************
-
-		Monitor the duration container for changes to the track. When the duration changes and is set, it is definitely a new track.
-
-	**********************************************************************************************************************************/
+	/***********************************************************************************************
+	*
+	*	Monitor the duration container for changes to the track.
+	*
+	***********************************************************************************************/
 	$(container_duration).live('DOMSubtreeModified', function(e) {
-
 		var t_mediainfo = $(container_mediainfo).text();
 		var tempSplit = t_mediainfo.split(" - ", 2);
 		var t_artist = tempSplit[0];
@@ -48,47 +55,24 @@ $(function(){
 		var t_duration = $(container_duration).text();
 		var t_current = $(container_currenttime).text();
 
+		// new duration and track/artist info = new song
 		if (t_duration != duration && t_duration != "00:00" && t_duration != "" && (t_artist != artist || t_track != track))
 		{
 			if (track != t_track || artist != t_artist)
 			{
 				//console.log ("Polling Temp Track Info - PLAYING[%s] - DISPLAY[%s] - ARTIST[%s] - TRACK[%s] - CURRENT[%s] - DURATION[%s]",
 				//			!IsPaused(), t_mediainfo, t_artist, t_track, t_current, t_duration);
-
-				mediainfo = t_mediainfo;
-				artist = t_artist;
-				track = t_track;
-				duration = t_duration;
-
-
-				if ( !IsPaused() )
-				{
-					//console.log("posting track info");
-					
-					chrome.runtime.sendMessage({type: 'validate', artist: artist, track: track}, function(response) {
-						if (response != false) {
-							chrome.runtime.sendMessage({type: 'nowPlaying', artist: artist, track: track, duration: duration});
-						} else { // on failure send nowPlaying 'unknown song'
-
-							//if validation failed, consider trying to determine if it is from an "official" playlist and parsing out the media info again to validate.
-							//
-							//	To be done later
-							//
-
-							chrome.runtime.sendMessage({type: 'nowPlaying', duration: duration});
-						}
-					});
-				}
+				UpdateMediaInfo(t_mediainfo, t_artist, t_track, t_duration);
+				PostMediaInfo();
 			}
 		}
 	});
 
-	/*********************************************************************************************************************************
-
-		Monitor the pause button to keep track of the "pause" status. It is shown while the play button is hidden. (track is playing)
-
-	**********************************************************************************************************************************/
-
+	/***********************************************************************************************
+	*
+	*	Monitor the pause button to keep track of the "pause" status. 
+	*
+	***********************************************************************************************/
 	var pauseElement = $(container_pausebutton).get(0); 
 
 	var pauseObserver = new WebKitMutationObserver(function (mutations) {
@@ -99,11 +83,7 @@ $(function(){
 	pauseObserver.observe(pauseElement, pauseConfig);
 
 	function attrModified(mutation) {
-	 	var name = mutation.attributeName,
-	    newValue = mutation.target.getAttribute(name),
-	    oldValue = mutation.oldValue;
-
-		if (name == "style"  &&  newValue == "display: none;")
+		if( mutation.target.style.display == 'none')
 		{	
 			//console.log("pause button hidden"); 
 			_flg_IsPaused = false;
@@ -115,14 +95,13 @@ $(function(){
 		}
 	}
 
-	/*********************************************************************************************************************************
-
-		Unload
-
-	**********************************************************************************************************************************/
+	/***********************************************************************************************
+	*
+	*	Unload
+	*
+	***********************************************************************************************/
 	$(window).unload(function() {     
-		console.log('unload');
-	
+		//console.log('unload');
 		chrome.runtime.sendMessage({type: 'reset'});
 		return true;      
 	});
