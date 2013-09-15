ad1d05feaf4b1f025de1acc06b9fc35de36a4bf8
david-sabata <david.n.sabata@gmail.com>
2013-09-15 23:16:50 +0200

    reformating, tiny optimizations and comments

--- inject.js
+++ inject.js
@@ -28,205 +28,201 @@
  */
 var connectors = [
 
-   {
-      label: "YouTube",
-      matches: ["*://www.youtube.com/watch*", "*://www.youtube.com/user/*"],
-      js: ["connectors/youtube.js"]
-   },
+    {
+        label: "YouTube",
+        matches: ["*://www.youtube.com/watch*", "*://www.youtube.com/user/*"],
+        js: ["connectors/youtube.js"]
+    },
 
-   {
-      label: "TTNET Müzik",
-      matches: ["*://www.ttnetmuzik.com.tr/*"],
-      js: ["connectors/ttnet.js"]
-   },
+    {
+        label: "TTNET Müzik",
+        matches: ["*://www.ttnetmuzik.com.tr/*"],
+        js: ["connectors/ttnet.js"]
+    },
 
-   {
-      label: "Thesixtyone",
-      matches: ["*://www.thesixtyone.com/*"],
-      js: ["connectors/61.js"]
-   },
+    {
+        label: "Thesixtyone",
+        matches: ["*://www.thesixtyone.com/*"],
+        js: ["connectors/61.js"]
+    },
 
-   {
-      label: "Google Play Music",
-      matches: ["*://play.google.com/music/*"],
-      js: ["connectors/googlemusic.js"]
-   },
+    {
+        label: "Google Play Music",
+        matches: ["*://play.google.com/music/*"],
+        js: ["connectors/googlemusic.js"]
+    },
 
-   {
-      label: "MySpace",
-      matches: ["*://myspace.com/*"],
-      js: ["connectors/myspace.js"]
-   },
+    {
+        label: "MySpace",
+        matches: ["*://myspace.com/*"],
+        js: ["connectors/myspace.js"]
+    },
 
-   {
-      label: "Pitchfork",
-      matches: ["*://pitchfork.com/*", "*://www.pitchfork.com/*"],
-      js: ["connectors/pitchfork.js"]
-   },
+    {
+        label: "Pitchfork",
+        matches: ["*://pitchfork.com/*", "*://www.pitchfork.com/*"],
+        js: ["connectors/pitchfork.js"]
+    },
 
-   {
-      label: "Fizy",
-      matches: ["*://fizy.com/*", "*://fizy.org/*"],
-      js: ["connectors/fizy.js"]
-   },
+    {
+        label: "Fizy",
+        matches: ["*://fizy.com/*", "*://fizy.org/*"],
+        js: ["connectors/fizy.js"]
+    },
 
-   {
-      label: "Virgin Radio Turkiye",
-      matches: ["*://*.virginradioturkiye.com/*", "*://*.radioeksen.com/*"],
-      js: ["connectors/virginradiotr.js"]
-   },
+    {
+        label: "Virgin Radio Turkiye",
+        matches: ["*://*.virginradioturkiye.com/*", "*://*.radioeksen.com/*"],
+        js: ["connectors/virginradiotr.js"]
+    },
 
-   {
-      label: "Ghostly Discovery",
-      matches: ["http://ghostly.com/discovery/play"],
-      js: ["connectors/ghostly.js"]
-   },
+    {
+        label: "Ghostly Discovery",
+        matches: ["http://ghostly.com/discovery/play"],
+        js: ["connectors/ghostly.js"]
+    },
 
-   {
-      label: "Bandcamp",
-      matches: ["*://*.bandcamp.com/*"],
-      js: ["connectors/bandcamp.js"]
-   },
+    {
+        label: "Bandcamp",
+        matches: ["*://*.bandcamp.com/*"],
+        js: ["connectors/bandcamp.js"]
+    },
 
-   {
-      label: "Jango",
-      matches: ["*://www.jango.com/*"],
-      js: ["connectors/jango.js", "connectors/jango-dom-inject.js"]
-   },
+    {
+        label: "Jango",
+        matches: ["*://www.jango.com/*"],
+        js: ["connectors/jango.js", "connectors/jango-dom-inject.js"]
+    },
 
-   {
-      label: "Pandora",
-      matches: ["*://www.pandora.com/*"],
-      js: ["connectors/pandora.js"]
-   },
+    {
+        label: "Pandora",
+        matches: ["*://www.pandora.com/*"],
+        js: ["connectors/pandora.js"]
+    },
 
-   {
-      label: "Deezer",
-      matches: ["*://www.deezer.com/*"],
-      js: ["connectors/deezer.js"]
-   },
+    {
+        label: "Deezer",
+        matches: ["*://www.deezer.com/*"],
+        js: ["connectors/deezer.js"]
+    },
 
-   {
-      label: "SoundCloud",
-      matches: ["*://soundcloud.com/*"],
-      js: ["connectors/soundcloud.js"]
-   },
+    {
+        label: "SoundCloud",
+        matches: ["*://soundcloud.com/*"],
+        js: ["connectors/soundcloud.js"]
+    },
 
-   {
-      label: "Amazon",
-      matches: ["*://www.amazon.com/gp/dmusic/mp3/player*", "*://www.amazon.de/gp/dmusic/mp3/player*", "*://www.amazon.es/gp/dmusic/mp3/player*"],
-      js: ["connectors/amazon.js"]
-   },
+    {
+        label: "Amazon",
+        matches: ["*://www.amazon.com/gp/dmusic/mp3/player*", "*://www.amazon.de/gp/dmusic/mp3/player*", "*://www.amazon.es/gp/dmusic/mp3/player*"],
+        js: ["connectors/amazon.js"]
+    },
 
-   { // DEAD?
-      label: "Z-Music",
-      matches: ["*://z-music.org/*"],
-      js: ["connectors/zmusic.js"]
-   },
+    { // DEAD?
+        label: "Z-Music",
+        matches: ["*://z-music.org/*"],
+        js: ["connectors/zmusic.js"]
+    },
 
-   {
-      label: "VK",
-      matches: ["*://vk.com/*"],
-      js: ["connectors/vk.js"]
-   },
+    {
+        label: "VK",
+        matches: ["*://vk.com/*"],
+        js: ["connectors/vk.js"]
+    },
 
-   {
-      label: "Zvooq",
-      matches: ["*://zvooq.ru/*"],
-      js: ["connectors/zvooq.js"]
-   },
+    {
+        label: "Zvooq",
+        matches: ["*://zvooq.ru/*"],
+        js: ["connectors/zvooq.js"]
+    },
 
-   {
-      label: "Weborama",
-      matches: ["*://www.weborama.fm/*"],
-      js: ["connectors/weborama.js"],
-      allFrames: true
-   },
+    {
+        label: "Weborama",
+        matches: ["*://www.weborama.fm/*"],
+        js: ["connectors/weborama.js"],
+        allFrames: true
+    },
 
-   {
-      label: "22 Tracks",
-      matches: ["*://22tracks.com/*"],
-      js: ["connectors/22tracks.js"]
-   },
+    {
+        label: "22 Tracks",
+        matches: ["*://22tracks.com/*"],
+        js: ["connectors/22tracks.js"]
+    },
 
-   {
-      label: "Megalyrics",
-      matches: ["*://megalyrics.ru/*"],
-      js: ["connectors/megalyrics.js"],
-      allFrames: true
-   },
+    {
+        label: "Megalyrics",
+        matches: ["*://megalyrics.ru/*"],
+        js: ["connectors/megalyrics.js"],
+        allFrames: true
+    },
 
-   {
-      label: "iHeart",
-      matches: ["*://*.iheart.com/*"],
-      js: ["connectors/iheart.js"]
-   },
+    {
+        label: "iHeart",
+        matches: ["*://*.iheart.com/*"],
+        js: ["connectors/iheart.js"]
+    },
 
-   {
-      label: "Indie Shuffle",
-      matches: ["*://www.indieshuffle.com/*"],
-      js: ["connectors/indieshuffle.js"]
-   },
+    {
+        label: "Indie Shuffle",
+        matches: ["*://www.indieshuffle.com/*"],
+        js: ["connectors/indieshuffle.js"]
+    },
 
-   {
-      label: "Tuba.FM",
-      matches: ["*://fm.tuba.pl/*"],
-      js: ["connectors/tubafm.js"]
-   },
+    {
+        label: "Tuba.FM",
+        matches: ["*://fm.tuba.pl/*"],
+        js: ["connectors/tubafm.js"]
+    },
 
-   {
-      label: "Spotify",
-      matches: ["https://play.spotify.com/*"],
-      js: ["connectors/spotify.js"]
-   },
+    {
+        label: "Spotify",
+        matches: ["https://play.spotify.com/*"],
+        js: ["connectors/spotify.js"]
+    },
 
-   {
-      label: "Grooveshark",
-      matches: ["*://grooveshark.com/*"],
-      js: ["connectors/grooveshark.js"]
-   },
+    {
+        label: "Grooveshark",
+        matches: ["*://grooveshark.com/*"],
+        js: ["connectors/grooveshark.js"]
+    },
 
-   {
-      label: "Plug.dj",
-      matches: ["http://plug.dj/*"],
-      js: ["connectors/plugdj.js"]
-   },
+    {
+        label: "Plug.dj",
+        matches: ["http://plug.dj/*"],
+        js: ["connectors/plugdj.js"]
+    },
 
-   {
-      label: "Turntable",
-      matches: ["*://turntable.fm/*"],
-      js: ["connectors/turntable.js"]
-   },
+    {
+        label: "Turntable",
+        matches: ["*://turntable.fm/*"],
+        js: ["connectors/turntable.js"]
+    },
 
-   {
-      label: "Slacker (iframe)",
-      matches: ["*://www.slacker.com/webplayer/index_embed.vm"],
-      js: ["connectors/slacker.js"]
-   },
+    {
+        label: "Slacker (iframe)",
+        matches: ["*://www.slacker.com/webplayer/index_embed.vm"],
+        js: ["connectors/slacker.js"]
+    },
 
-   {
-      label: "Slacker (main page)",
-      matches: ["*://www.slacker.com/*"],
-      js: ["connectors/slacker2.js"]
-   },
+    {
+        label: "Slacker (main page)",
+        matches: ["*://www.slacker.com/*"],
+        js: ["connectors/slacker2.js"]
+    },
 
-   {
-      label: "Daytrotter",
-      matches: ["*://www.daytrotter.com/*"],
-      js: ["connectors/daytrotter.js"]
-   },
+    {
+        label: "Daytrotter",
+        matches: ["*://www.daytrotter.com/*"],
+        js: ["connectors/daytrotter.js"]
+    },
 
-   {
-      label: "AOL Radio",
-      matches: ["*://aolradio.slacker.com/*"],
-      js: ["connectors/aolradio.js"]
-   }
+    {
+        label: "AOL Radio",
+        matches: ["*://aolradio.slacker.com/*"],
+        js: ["connectors/aolradio.js"]
+    }
 
 ];
-
-
-
-
 
 
 /**
@@ -239,8 +235,10 @@ var connectors = [
 function createPattern(input) {
     if (typeof input !== 'string') return null;
     var match_pattern = '^'
-      , regEscape = function(s) {return s.replace(/[[^$.|?*+(){}\\]/g, '\\$&');}
-      , result = /^(\*|https?|file|ftp|chrome-extension):\/\//.exec(input);
+        , regEscape = function (s) {
+            return s.replace(/[[^$.|?*+(){}\\]/g, '\\$&');
+        }
+        , result = /^(\*|https?|file|ftp|chrome-extension):\/\//.exec(input);
 
     // Parse scheme
     if (!result) return null;
@@ -274,70 +272,74 @@ function createPattern(input) {
  * @returns boolean
  */
 function isConnectorEnabled(label) {
-   var disabledArray = JSON.parse(localStorage.disabledConnectors);
-   return (disabledArray.indexOf(label) === -1);
+    var disabledArray = JSON.parse(localStorage.disabledConnectors);
+    return (disabledArray.indexOf(label) === -1);
 }
-
 
 
 /**
  * Injects connectors to tabs upon page loading
  */
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-   // wait for the Loaded event
-   if (changeInfo.status !== 'complete')
-      return;
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // wait for the Loaded event
+    if (changeInfo.status !== 'complete')
+        return;
 
-   // run first available connector
-   var anyMatch = !connectors.every(function(connector) {
-      var matchOk = false;
+    // run first available connector
+    var anyMatch = !connectors.every(function (connector) {
+        var matchOk = false;
 
-      connector.matches.forEach(function(match) {
-         matchOk = matchOk || createPattern(match).test(tab.url);
-      });
+        connector.matches.forEach(function (match) {
+            matchOk = matchOk || createPattern(match).test(tab.url);
+        });
 
-      if (matchOk === true) {
-         console.log('connector ' + connector.label + ' matched for ' + tab.url);
-         setActionIcon(ACTION_SITE_RECOGNIZED, tabId);
+        if (matchOk === true) {
+            console.log('connector ' + connector.label + ' matched for ' + tab.url);
+            setActionIcon(ACTION_SITE_RECOGNIZED, tabId);
 
-          // Ping the content page to see if the script is already in place.
-          // In the future, connectors will have unified interface, so they will all support
-          // the 'ping' request. Right now only YouTube supports this, because it
-          // is the only site that uses ajax navigation via History API (which is quite hard to catch).
-          // Other connectors will work as usual.
-          chrome.tabs.sendMessage(tabId, { type: 'ping' }, function(response) {
-              if (!response) {
-                  console.log('-- loaded for the first tine, injecting the scripts');
+            if (!isConnectorEnabled(connector.label)) {
+                setActionIcon(ACTION_SITE_DISABLED, tabId);
+                return false; // break forEach
+            }
 
-                  if (isConnectorEnabled(connector.label)) {
-                      // inject all scripts and jQuery, use slice to avoid mutating
-                      var scripts = connector.js.slice(0);
-                      scripts.unshift(JQUERY_PATH);
+            // Ping the content page to see if the script is already in place.
+            // In the future, connectors will have unified interface, so they will all support
+            // the 'ping' request. Right now only YouTube supports this, because it
+            // is the only site that uses ajax navigation via History API (which is quite hard to catch).
+            // Other connectors will work as usual.
+            //
+            // Sadly there is no way to silently check if the script has been already injected
+            // so we will see an error in the background console on load of every supported page
+            chrome.tabs.sendMessage(tabId, { type: 'ping' }, function (response) {
+                // if the message was sent to a non existing script or the script
+                // does not implement the 'ping' message, we get response==undefined;
+                if (!response) {
+                    console.log('-- loaded for the first time, injecting the scripts');
 
-                      scripts.forEach(function(jsFile){
-                          var injectDetails = {
-                              file: jsFile,
-                              allFrames: connector.allFrames ? connector.allFrames : false
-                          };
-                          chrome.tabs.executeScript(tabId, injectDetails);
-                      });
-                  } else {
-                      setActionIcon(ACTION_SITE_DISABLED, tabId);
-                  }
-              }
-              else {
-                  console.log('-- subsequent ajax navigation, the scripts are already injected');
-              }
+                    // inject all scripts and jQuery, use slice to avoid mutating
+                    var scripts = connector.js.slice(0);
+                    scripts.unshift(JQUERY_PATH);
 
-          });
+                    scripts.forEach(function (jsFile) {
+                        var injectDetails = {
+                            file: jsFile,
+                            allFrames: connector.allFrames ? connector.allFrames : false
+                        };
+                        chrome.tabs.executeScript(tabId, injectDetails);
+                    });
+                }
+                else {
+                    console.log('-- subsequent ajax navigation, the scripts are already injected');
+                }
+            });
 
-      }
+        }
 
-      return !matchOk;
-   });
+        return !matchOk;
+    });
 
-   // hide page action if there is no match
-   if (!anyMatch) {
-      chrome.pageAction.hide(tabId);
-   }
+    // hide page action if there is no match
+    if (!anyMatch) {
+        chrome.pageAction.hide(tabId);
+    }
 });
