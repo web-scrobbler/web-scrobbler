ea7cf1608a0b4c800a7ec8b8306cfc148f29cb9d
Rui Jiang <me@ruijiang.me>
2014-02-04 00:38:48 -0500

    Music Unlimited Connector
    First version completed! Not optimal, but at least it works, for now...

--- connectors/musicunlimited.js
+++ connectors/musicunlimited.js
@@ -1,15 +1,21 @@
+//Music Unlimited Connector
+//Provides basic scrobbling, does NOT support pause or like
+//Author: Rui Jiang
+//Inspired by Google Music Connector
+
 // Used only to remember last song title
 var currentTrack = '';
-var currentDuration = '';
 
-// Glabal constant for the song container ....
+// Glabal constant for the song information
 var CONTAINER_SELECTOR = '.GEKKVSQBL-';
 var TRACK_SELECTOR = '.GEKKVSQBN-';
 var ARTIST_SELECTOR = '.GEKKVSQBAY';
 var ALBUM_SELECTOR = '.GEKKVSQBF-';
 var DURATION_SELECTOR = '#PlayerDuration';
+var POSITION_SELECTOR = '#PlayerPosition';
 
 $(function(){
+	//Whenever a new song starts playing, the container that holds media information should changes, thus triggering updateNowPlaying()
 	$(CONTAINER_SELECTOR).live('DOMSubtreeModified', function(e) {
 		if (!newTrack()) {
 			return;
@@ -18,6 +24,8 @@ $(function(){
 		return;
 	});
 
+	//This is the only way that I could think of to allow scrobbling the first song being played after the application launches, by monitoring the player duration
+	//If anyone comes up with a better way around it, please feel free to improve it
 	$(DURATION_SELECTOR).live('DOMSubtreeModified', function(e) {
 		if (!newTrack()) {
 			return;
@@ -26,42 +34,38 @@ $(function(){
 		return;
 	});
 
-	//setup unload handler
+	//Setup unload handler
 	$(window).unload(function() {
 		chrome.runtime.sendMessage({type: 'reset'});
 		return true;      
 	});
 });
 
-/**
- * Called every time we load a new song
- */
+
+//Called every time we load a new song
 function updateNowPlaying(){
     var parsedInfo = parseInfo();
-    artist   = parsedInfo['artist']; 	//global
-    track    = parsedInfo['track'];	//global
+    artist   = parsedInfo['artist'];
+    track    = parsedInfo['track'];
     album    = parsedInfo['album'];
-    duration = parsedInfo['duration']; 	//global
+    duration = parsedInfo['duration'];
 
     if (artist == '' || track == '' || duration == 0) {return;}
 
     chrome.runtime.sendMessage({type: 'validate', artist: artist, track: track}, function(response) {
-      if (response != false) {
-		console.log("Scrobbling track: artist: " + artist + ", track: " + track + ", album: " + album + ", duration: " + duration);
-        chrome.extension.sendMessage({type: 'nowPlaying', artist: artist, track: track, album: album, duration: duration});
-      }
-      // on failure send nowPlaying 'unknown song'
-      else {
-        chrome.runtime.sendMessage({type: 'nowPlaying', duration: duration});
-      }
+		if (response != false) {
+			console.log("Scrobbling track: artist: " + artist + ", track: " + track + ", album: " + album + ", duration: " + duration);
+			chrome.extension.sendMessage({type: 'nowPlaying', artist: artist, track: track, album: album, duration: duration});
+		}
+		// on failure send nowPlaying 'unknown song'
+		else {
+			chrome.runtime.sendMessage({type: 'nowPlaying', duration: duration});
+		}
     });
 }
 
 function parseInfo() {
-    var artist   = '';
-    var track    = '';
-    var album    = '';
-    var duration = 0;
+    var artist, track, album, duration;
 
     // Get artist and song names
     var rawArtistValue = $(ARTIST_SELECTOR).children().first().attr("title");
@@ -89,11 +93,11 @@ function parseInfo() {
     return {artist: artist, track: track, album: album, duration: duration};
 }
 
+//Converts duration from string form to seconds
 function parseDuration(artistTitle) {
 	try {
-		match = artistTitle.match(/\d+:\d+/g)[0]
-
-		mins    = match.substring(0, match.indexOf(':'));
+		match = artistTitle.match(/\d+:\d+/g)[0];
+		mins = match.substring(0, match.indexOf(':'));
 		seconds = match.substring(match.indexOf(':')+1);
 		return parseInt(mins*60) + parseInt(seconds);
 	} catch(err){
@@ -101,38 +105,19 @@ function parseDuration(artistTitle) {
 	}
 }
 
+//Detects if a new song is indeed being played
 function newTrack() {
     var rawTrackValue = $(TRACK_SELECTOR).children().first().attr("title");
     var rawDurationValue = $(DURATION_SELECTOR).text();
-	if (currentTrack == rawTrackValue || currentDuration == rawDurationValue) {
+	if (currentTrack == rawTrackValue || '' == rawDurationValue || '0:00' == rawDurationValue) {
+		return false;
+	}
+	var rawPositionValue = $(POSITION_SELECTOR).text();
+	//Due to weird behavior of the application where currentTrack gets erased every time you click something on the page
+	if ('' == currentTrack && '0:00' != rawPositionValue) {
+		currentTrack = rawTrackValue;
 		return false;
 	}
 	currentTrack = rawTrackValue;
-	currentDuration = rawDurationValue
 	return true;
 }
-
-/**
- * Simply request the scrobbler.js to submit song previusly specified by calling updateNowPlaying()
- */
-/*function scrobbleTrack() {
-   // stats
-   chrome.runtime.sendMessage({type: 'trackStats', text: 'The Google Music song scrobbled'});
-
-   // scrobble
-   chrome.runtime.sendMessage({type: 'submit'});
-}*/
-
-/**
- * Listen for requests from scrobbler.js
- */
-/*chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		switch(request.type) {
-            // background calls this to see if the script is already injected
-            case 'ping':
-                sendResponse(true);
-                break;
-		}
-	}
-);*/
