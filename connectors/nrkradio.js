b5833eb3477f1db7dfc14203910cad8b7e9cd7b4
Erlend Wiig <erlend.wiig@gmail.com>
2014-02-20 10:06:21 +0100

    added comments, cleaning up formatting

--- connectors/nrkradio.js
+++ connectors/nrkradio.js
@@ -8,15 +8,12 @@ function updateNowPlaying() {
 		var artist = newTrack.substring(0, indexOfSeperator).trim();
 		var song = newTrack.substring(indexOfSeperator+1, newTrack.length);
 
-
 		chrome.runtime.sendMessage({type: 'validate', artist: artist, track: song}, function(response) {
-		// on success send nowPlaying song
-			var duration = 180;
+			 var duration = 180; //no duration information available in radio.nrk.no
 		     if (response != false) {
 		        var song = response;
 		        chrome.runtime.sendMessage({type: 'nowPlaying', artist: song.artist, track: song.track, duration: duration});
 		     }
-		     // on failure send nowPlaying 'unknown song'
 		     else {
 		        chrome.runtime.sendMessage({type: 'nowPlaying', duration: duration});
 		     }
@@ -26,35 +23,29 @@ function updateNowPlaying() {
 }
 
 var addListener = function() {
-		$("#live").live("DOMSubtreeModified", function(e) {
-	    	if ($(".active.music").length > 0)
-	    	{		
-		    	setTimeout(updateNowPlaying, 500);
-			}
-		});
+	$("#live").live("DOMSubtreeModified", function(e) {
+    	if ($(".active.music").length > 0)
+    	{		
+	    	setTimeout(updateNowPlaying, 500);
+		}
+	});
 }
-
 
 $(function() {
     $(window).unload(function() {
 		chrome.runtime.sendMessage({type: 'reset'});
 		return true;
     });
-
-    
 });
 
 chrome.runtime.onMessage.addListener(
    function(request, sender, sendResponse) {
          switch(request.type) {
-
-             // background calls this to see if the script is already injected
              case 'ping':
                  sendResponse(true);
                  break;
          }
    }
 );
-
 
 addListener();
