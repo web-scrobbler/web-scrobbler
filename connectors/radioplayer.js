190d534471451dfb80dafb9cd26fa51d14359e4b
david-sabata <david.n.sabata@gmail.com>
2014-02-23 19:34:41 +0100

    cleanup radioplayer.co.uk

--- connectors/radioplayer.js
+++ connectors/radioplayer.js
@@ -2,7 +2,7 @@
  * Connector for RadioPlayer enabled streams (http://www.radioplayer.co.uk/)
  * Made by Jiminald
  * Based loosely on the Google Music connector by Sharjeel Aziz
- * 
+ *
  * To update the inject list, Open a RadioPlayer, view the A-Z list, and run the following command below
  * function onlyUnique(value, index, self) { return self.indexOf(value) === index; } var list = new Array(); $('.overlay-item-link').each(function(i, v){ var url = $(v).attr('href'); url = url.substring(7); url = url.substring(0, url.indexOf('/')); list.push('*://'+url+'/*'); }); list = list.filter(onlyUnique); console.log(JSON.stringify(list));
  */
@@ -11,7 +11,7 @@
 var state = 'init';
 
 // Used only to remember last song title
-var clipTitle = '';  
+var clipTitle = '';
 
 // Timeout to scrobble track ater minimum time passes
 var scrobbleTimeout = null;
@@ -20,17 +20,17 @@ var scrobbleTimeout = null;
 var CONTAINER_SELECTOR = '#live-strip';
 
 
-$(function(){   
+$(function(){
   $(CONTAINER_SELECTOR).live('DOMSubtreeModified', function(e) {
 		if ($(CONTAINER_SELECTOR).length > 0) {
 			updateNowPlaying();
-			return;    
+			return;
 		}
    });
-   
+
    // first load
    updateNowPlaying();
-   
+
    $(window).unload(function() {
 
       // reset the background scrobbler song data
@@ -42,24 +42,24 @@ $(function(){
 
 /**
  * Called every time we load a new song
- */ 
+ */
 function updateNowPlaying(){
   var parsedInfo = parseInfo($(".scrolling-text").text());
   artist   = parsedInfo['artist']; 	//global
   track    = parsedInfo['track']; //global
   duration = parsedInfo['duration'];	//global
-	
+
   if (artist == '' || track == '') {return;}
-    
+
   // check if the same track is being played and we have been called again
   // if the same track is being played we return
-  if (clipTitle == track) 
+  if (clipTitle == track)
   {
     return;
   }
 
   clipTitle = track;
-  
+
   chrome.runtime.sendMessage({type: 'validate', artist: artist, track: track}, function(response) {
 	  if (response != false) {
       chrome.runtime.sendMessage({type : 'nowPlaying', artist: artist, track: track, duration: duration});
@@ -75,24 +75,24 @@ function parseInfo(artistTitle) {
   var artist   = '';
   var track    = '';
   var duration = 90; // As there is no time, we default to 90 seconds (Saves 2 alerts popping up on track change)
-   
+
   artistTitle = artistTitle.replace(/\(\d+:\d+\)(.)*?/g , "");
-  
-  // Figure out where to split; use " - " rather than "-" 
+
+  // Figure out where to split; use " - " rather than "-"
   if (artistTitle.indexOf(' - ') > -1) {
     track = artistTitle.substring(0, artistTitle.indexOf(' - '));
     artist = artistTitle.substring(artistTitle.indexOf(' - ') + 3);
   } else if (artistTitle.indexOf('-') > -1) {
     track = artistTitle.substring(0, artistTitle.indexOf('-'));
-    artist = artistTitle.substring(artistTitle.indexOf('-') + 1);      
+    artist = artistTitle.substring(artistTitle.indexOf('-') + 1);
   } else if (artistTitle.indexOf(':') > -1) {
     track = artistTitle.substring(0, artistTitle.indexOf(':'));
-    artist = artistTitle.substring(artistTitle.indexOf(':') + 1);   
+    artist = artistTitle.substring(artistTitle.indexOf(':') + 1);
   } else {
     // can't parse
     return {artist:'', track:'', duration: duration};
   }
-  
+
   artist = artist.replace(/^\s+|\s+$/g,'');
   track = track.replace(/^\s+|\s+$/g,'');
 
@@ -102,35 +102,3 @@ function parseInfo(artistTitle) {
 }
 
 
-/**
- * Simply request the scrobbler.js to submit song previusly specified by calling updateNowPlaying()
- */ 
-function scrobbleTrack() {
-   // stats
-   chrome.runtime.sendMessage({type: 'trackStats', text: 'The RadioPlayer song scrobbled'});
-   // scrobble
-   // chrome.runtime.sendMessage({type: 'submit'});
-}
-
-
-
-/**
- * Listen for requests from scrobbler.js
- */ 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-	  switch(request.type) {
-    	// called after track has been successfully marked as 'now playing' at the server
-        case 'nowPlayingOK':
-          break;
-            
-        // not used yet
-        case 'submitOK':
-		  break;
-
-        // not used yet
-        case 'submitFAIL':
-          break; 
-    }
-	}
-);
