d22ae42093f8a7dee43ad38e088d69621e2a0d45
Erlend Wiig <erlend.wiig@gmail.com>
2014-03-03 21:18:17 +0100

    made sure a track is only processed once. Also not trying to validate empty records. And replacing plusses with commas in artist for more matches

--- connectors/nrkradio.js
+++ connectors/nrkradio.js
@@ -4,20 +4,24 @@ function updateNowPlaying() {
 	var newTrack = $(".active.music > .fnn-title").text();
 
 	if (newTrack != lastTrack) {
+		lastTrack = newTrack;
 		var indexOfSeperator = newTrack.indexOf("-");
 		var artist = newTrack.substring(0, indexOfSeperator).trim();
-		var song = newTrack.substring(indexOfSeperator+1, newTrack.length);
+		var song = newTrack.substring(indexOfSeperator+1, newTrack.length).trim();
+        if(artist.length > 0 && song.length > 0)
+		{
+			var artistsSeperatorReplaced = artist.replace(/\s\+/g, ",");
+			chrome.runtime.sendMessage({type: 'validate', artist: artistsSeperatorReplaced, track: song}, function(response) {
+			     if (response != false) {
+			        var song = response;
+			        chrome.runtime.sendMessage({type: 'nowPlaying', artist: song.artist, track: song.track, duration: song.duration/1000});
+			     }
+			     else {
+			        chrome.runtime.sendMessage({type: 'nowPlaying', duration: 0});
+			     }
 
-		chrome.runtime.sendMessage({type: 'validate', artist: artist, track: song}, function(response) {
-		     if (response != false) {
-		        var song = response;
-		        chrome.runtime.sendMessage({type: 'nowPlaying', artist: song.artist, track: song.track, duration: song.duration/1000});
-		     }
-		     else {
-		        chrome.runtime.sendMessage({type: 'nowPlaying', duration: 0});
-		     }
-		     lastTrack = newTrack;
-		});
+			});
+		}
 	};
 }
 
