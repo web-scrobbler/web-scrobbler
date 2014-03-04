1b458eeacb347fda00e70dd27e10a4e8a639ca4c
j4cknife <j4cknife@users.noreply.github.com>
2014-03-04 15:31:56 -0500

    Update plex.js

--- connectors/plex.js
+++ connectors/plex.js
@@ -1,445 +1,133 @@
-/**
- * All connectors are defined here, instead of manifest.
- *
- * Matching connector is injected to the page after document_end event.
- *
- * Do not include jQuery - it is included by default.
- *
- *
- * Supported fields:
- *
- *    label
- *          - label to be shown in options to enable/disable the connector
- *          - be careful with renaming, as connector disable state depends on the label
- *
- *    matches
- *          - array of positive matches in format described in Chrome Ext. Dev. guide
- *          - connectors are processed in order and the first match is used; you can use
- *            this behaviour to emulate exclude matches
- *
- *    js
- *          - array of paths of files to be executed
- *          - all executions happen on or after 'document_end'
- *
- *    allFrames (optional)
- *          - boolean value representing InjectDetails.allFrames
- *          - FALSE by default
- *
- */
-var connectors = [
-    {
-        label: "Baidu Music",
-        matches: ["*://play.baidu.com/*"],
-        js: ["connectors/baidu.js"]
-    },
+// State for event handlers
+var state = 'init';
 
-    {
-        label: "YouTube",
-        matches: ["*://www.youtube.com/watch*", "*://www.youtube.com/user/*"],
-        js: ["connectors/youtube.js"]
-    },
+// Used only to remember last song title
+var clipTitle = '';
 
-    {
-        label: "TTNET Müzik",
-        matches: ["*://www.ttnetmuzik.com.tr/*"],
-        js: ["connectors/ttnet.js"]
-    },
+// Timeout to scrobble track ater minimum time passes
+var scrobbleTimeout = null;
 
-    {
-        label: "Thesixtyone",
-        matches: ["*://www.thesixtyone.com/*"],
-        js: ["connectors/61.js"]
-    },
+// Glabal constant for the song container ....
+var CONTAINER_SELECTOR = 'div#plex.application';
 
-    {
-        label: "Google Play Music",
-        matches: ["*://play.google.com/music/*"],
-        js: ["connectors/googlemusic.js"]
-    },
 
-    {
-        label: "MySpace",
-        matches: ["*://myspace.com/*"],
-        js: ["connectors/myspace.js"]
-    },
+$(function(){
+	$(CONTAINER_SELECTOR).live('DOMSubtreeModified', function(e) {
 
-    {
-        label: "Pitchfork",
-        matches: ["*://pitchfork.com/*", "*://www.pitchfork.com/*"],
-        js: ["connectors/pitchfork.js"]
-    },
+		if ($(CONTAINER_SELECTOR).length > 0) {
+			updateNowPlaying();
+			return;
+		}
 
-    {
-        label: "Fizy",
-        matches: ["*://fizy.com/*", "*://fizy.org/*"],
-        js: ["connectors/fizy.js"]
-    },
+   });
 
-    {
-        label: "Virgin Radio Turkiye",
-        matches: ["*://*.virginradioturkiye.com/*", "*://*.radioeksen.com/*"],
-        js: ["connectors/virginradiotr.js"]
-    },
+   //console.log("Last.fm Scrobbler: starting Google Music connector")
 
-    {
-        label: "Ghostly Discovery",
-        matches: ["http://ghostly.com/discovery/play"],
-        js: ["connectors/ghostly.js"]
-    },
-
-    {
-        label: "Bandcamp",
-        matches: ["*://*.bandcamp.com/*"],
-        js: ["connectors/bandcamp.js"]
-    },
-
-    {
-        label: "Jango",
-        matches: ["*://www.jango.com/*"],
-        js: ["connectors/jango.js", "connectors/jango-dom-inject.js"]
-    },
-
-    {
-        label: "Pandora",
-        matches: ["*://www.pandora.com/*"],
-        js: ["connectors/pandora.js"]
-    },
-
-    {
-        label: "Deezer",
-        matches: ["*://www.deezer.com/*"],
-        js: ["connectors/deezer.js"]
-    },
-
-    {
-        label: "SoundCloud",
-        matches: ["*://soundcloud.com/*"],
-        js: ["connectors/soundcloud.js"]
-    },
-
-    {
-        label: "Amazon",
-        matches: ["*://www.amazon.com/gp/dmusic/mp3/player*", "*://www.amazon.de/gp/dmusic/mp3/player*", "*://www.amazon.es/gp/dmusic/mp3/player*", "*://www.amazon.co.uk/gp/dmusic/mp3/player*"],
-        js: ["connectors/amazon.js"]
-    },
-
-    { // DEAD?
-        label: "Z-Music",
-        matches: ["*://z-music.org/*"],
-        js: ["connectors/zmusic.js"]
-    },
-
-    {
-        label: "VK",
-        matches: ["*://vk.com/*"],
-        js: ["connectors/vk.js"]
-    },
-
-    {
-        label: "Zvooq",
-        matches: ["*://zvooq.ru/*"],
-        js: ["connectors/zvooq.js"]
-    },
-
-    {
-        label: "Weborama",
-        matches: ["*://www.weborama.fm/*"],
-        js: ["connectors/weborama.js"],
-        allFrames: true
-    },
-
-    {
-        label: "22 Tracks",
-        matches: ["*://22tracks.com/*"],
-        js: ["connectors/22tracks.js"]
-    },
-
-    {
-        label: "Megalyrics",
-        matches: ["*://megalyrics.ru/*"],
-        js: ["connectors/megalyrics.js"],
-        allFrames: true
-    },
-
-    {
-        label: "iHeart",
-        matches: ["*://*.iheart.com/*"],
-        js: ["connectors/iheart.js"]
-    },
-
-    {
-        label: "Indie Shuffle",
-        matches: ["*://www.indieshuffle.com/*"],
-        js: ["connectors/indieshuffle.js"]
-    },
-
-    {
-        label: "Tuba.FM",
-        matches: ["*://fm.tuba.pl/*"],
-        js: ["connectors/tubafm.js"]
-    },
-
-    {
-        label: "Spotify",
-        matches: ["https://play.spotify.com/*"],
-        js: ["connectors/spotify.js"]
-    },
-
-    {
-        label: "Grooveshark",
-        matches: ["*://grooveshark.com/*"],
-        js: ["connectors/grooveshark.js"]
-    },
-
-    {
-        label: "Plug.dj",
-        matches: ["http://plug.dj/*"],
-        js: ["connectors/plugdj.js"]
-    },
-
-    {
-        label: "Turntable",
-        matches: ["*://turntable.fm/*"],
-        js: ["connectors/turntable.js"]
-    },
-
-    {
-        label: "Slacker (iframe)",
-        matches: ["*://www.slacker.com/webplayer/index_embed.vm"],
-        js: ["connectors/slacker.js"]
-    },
-
-    {
-        label: "Slacker (main page)",
-        matches: ["*://www.slacker.com/*"],
-        js: ["connectors/slacker2.js"]
-    },
-
-    {
-        label: "Daytrotter",
-        matches: ["*://www.daytrotter.com/*"],
-        js: ["connectors/daytrotter.js"]
-    },
-
-    {
-        label: "AOL Radio",
-        matches: ["*://aolradio.slacker.com/*"],
-        js: ["connectors/aolradio.js"]
-    },
-
-    {
-        label: "HillyDilly",
-        matches: ["*://www.hillydilly.com/*"],
-        js: ["connectors/hillydilly.js"]
-    },
-
-    {
-        label: "Xbox Music",
-        matches: ["*://music.xbox.com/*"],
-        js: ["connectors/xboxmusic.js"]
-    },
-
-    {
-        label: "8tracks",
-        matches: ["*://8tracks.com/*"],
-        js: ["connectors/8tracks.js"]
-    },
-
-    {
-        label: "Moje Polskie Radio",
-        matches: ["*://moje.polskieradio.pl/station/*"],
-        js: ["connectors/mojepolskieradio.js"]
-    },
-
-    {
-        label: "Nova Planet",
-        matches: ['*://www.novaplanet.com/radionova/player'],
-        js: ["connectors/novaplanet.js"]
-    },
-
-    {
-        label: "Radio+ Belgium",
-        matches: ["*://www.radioplus.be/*"],
-        js: ["connectors/radioplusbe.js"]
-    },
-
-    {
-        label: "Songza",
-        matches: ["*://songza.com/*"],
-        js: ["connectors/songza.js"]
-    },
-
-    {
-        label: "Douban Programme",
-        matches: ["http://music.douban.com/programme/*"],
-        js: ["connectors/douban-programme.js"]
-    },
-
-    {
-        label: "Focus@Will",
-        matches: ["*://www.focusatwill.com/*"],
-        js: ["connectors/focusatwill.js"]
-    },
-
-    {
-        label: "Le Tourne Disque",
-        matches: ["*://www.letournedisque.com/*"],
-        js: ["connectors/letournedisque.js"]
-    },
-
-    {
-        label: "Rdio",
-        matches: ["*://www.rdio.com/*"],
-        js: ["connectors/rdio.js"]
-    },
-
-    {
-        label: "Reddit Playlister",
-        matches: ["*://redditplayer.phoenixforgotten.com/*"],
-        js: ["connectors/redditplayer.js"]
-    },
-
-    {
-      label: "Sullen-Ural",
-      matches: ["*://sullen-ural.ru/*", "*://*.sullen-ural.ru/*"],
-      js: ["connectors/sullen-ural.js"]
-    },
-
-    {
-      label: "Digitally Imported",
-      matches: ["*://www.di.fm/*"],
-      js: ["connectors/difm.js"]
-    },
-
-	{
-      label: "PLEX",
-      matches: ["*://*32400/web/*"],
-      js: ["connectors/plex.js"]
-    },
-	
-    {
-      label: "Beats Music",
-      matches: ["*://*.beatsmusic.com/*"],
-      js: ["connectors/beats.js"]
-    }
-];
-
-
-/**
- * Creates regex from single match pattern
- *
- * @author lacivert
- * @param {String} input
- * @returns RegExp
- */
-function createPattern(input) {
-    if (typeof input !== 'string') return null;
-    var match_pattern = '^'
-        , regEscape = function (s) {
-            return s.replace(/[[^$.|?*+(){}\\]/g, '\\$&');
-        }
-        , result = /^(\*|https?|file|ftp|chrome-extension):\/\//.exec(input);
-
-    // Parse scheme
-    if (!result) return null;
-    input = input.substr(result[0].length);
-    match_pattern += result[1] === '*' ? 'https?://' : result[1] + '://';
-
-    // Parse host if scheme is not `file`
-    if (result[1] !== 'file') {
-        if (!(result = /^(?:\*|(\*\.)?([^\/*]+))/.exec(input))) return null;
-        input = input.substr(result[0].length);
-        if (result[0] === '*') {    // host is '*'
-            match_pattern += '[^/]+';
-        } else {
-            if (result[1]) {         // Subdomain wildcard exists
-                match_pattern += '(?:[^/]+\.)?';
-            }
-            // Append host (escape special regex characters)
-            match_pattern += regEscape(result[2]);// + '/';
-        }
-    }
-    // Add remainder (path)
-    match_pattern += input.split('*').map(regEscape).join('.*');
-    match_pattern += '$';
-
-    return new RegExp(match_pattern);
-}
-
-
-/**
- * @param {String} label
- * @returns boolean
- */
-function isConnectorEnabled(label) {
-    var disabledArray = JSON.parse(localStorage.disabledConnectors);
-    return (disabledArray.indexOf(label) === -1);
-}
-
-
-/**
- * Injects connectors to tabs upon page loading
- */
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    // wait for the Loaded event
-    if (changeInfo.status !== 'complete')
-        return;
-
-    // run first available connector
-    var anyMatch = !connectors.every(function (connector) {
-        var matchOk = false;
-
-        connector.matches.forEach(function (match) {
-            matchOk = matchOk || createPattern(match).test(tab.url);
-        });
-
-        if (matchOk === true) {
-            console.log('connector ' + connector.label + ' matched for ' + tab.url);
-            setActionIcon(ACTION_SITE_RECOGNIZED, tabId);
-
-            if (!isConnectorEnabled(connector.label)) {
-                setActionIcon(ACTION_SITE_DISABLED, tabId);
-                return false; // break forEach
-            }
-
-            // Ping the content page to see if the script is already in place.
-            // In the future, connectors will have unified interface, so they will all support
-            // the 'ping' request. Right now only YouTube supports this, because it
-            // is the only site that uses ajax navigation via History API (which is quite hard to catch).
-            // Other connectors will work as usual.
-            //
-            // Sadly there is no way to silently check if the script has been already injected
-            // so we will see an error in the background console on load of every supported page
-            chrome.tabs.sendMessage(tabId, { type: 'ping' }, function (response) {
-                // if the message was sent to a non existing script or the script
-                // does not implement the 'ping' message, we get response==undefined;
-                if (!response) {
-                    console.log('-- loaded for the first time, injecting the scripts');
-
-                    // inject all scripts and jQuery, use slice to avoid mutating
-                    var scripts = connector.js.slice(0);
-                    scripts.unshift(JQUERY_PATH);
-
-                    scripts.forEach(function (jsFile) {
-                        var injectDetails = {
-                            file: jsFile,
-                            allFrames: connector.allFrames ? connector.allFrames : false
-                        };
-                        chrome.tabs.executeScript(tabId, injectDetails);
-                    });
-                }
-                else {
-                    console.log('-- subsequent ajax navigation, the scripts are already injected');
-                }
-            });
-
-        }
-
-        return !matchOk;
-    });
-
-    // hide page action if there is no match
-    if (!anyMatch) {
-        chrome.pageAction.hide(tabId);
-    }
+   // first load
+   updateNowPlaying();
 });
+
+/**
+ * Called every time we load a new song
+ */
+function updateNowPlaying(){
+    var parsedInfo = parseInfo();
+    artist   = parsedInfo['artist']; 	//global
+    track    = parsedInfo['track'];	//global
+    album    = parsedInfo['album'];
+    duration = parsedInfo['duration']; 	//global
+
+    if (artist == '' || track == '' || duration == 0) {return;}
+
+    // check if the same track is being played and we have been called again
+    // if the same track is being played we return
+    if (clipTitle == track) {
+	return;
+    }
+    clipTitle = track;
+
+    chrome.runtime.sendMessage({type: 'validate', artist: artist, track: track}, function(response) {
+      if (response != false) {
+          chrome.extension.sendMessage({type: 'nowPlaying', artist: artist, track: track, album: album, duration: duration});
+      }
+      // on failure send nowPlaying 'unknown song'
+      else {
+         chrome.runtime.sendMessage({type: 'nowPlaying', duration: duration});
+      }
+    });
+}
+
+
+function parseInfo() {
+    var artist   = '';
+    var track    = '';
+    var album    = '';
+    var duration = 0;
+
+    // Get artist and song names
+    var artistValue = $("a.now-playing-title").text();
+    var trackValue = $("span.now-playing-subtitle").text();
+    var albumValue = $("h2.album-title").text();
+    var durationValue = $("span.player-duration").text();
+
+    try {
+        if (null != artistValue) {
+            artist = artistValue.replace(/^\s+|\s+$/g,'');
+        }
+        if (null != trackValue) {
+            track = trackValue.replace(/^\s+|\s+$/g,'');
+        }
+        if (null != albumValue) {
+            album = albumValue.replace(/^\s+|\s+$/g,'');
+        }
+        if (null != durationValue) {
+            duration = parseDuration(durationValue);
+        }
+    } catch(err) {
+        return {artist: '', track: '', duration: 0};
+    }
+
+    //console.log("artist: " + artist + ", track: " + track + ", album: " + album + ", duration: " + duration);
+
+    return {artist: artist, track: track, album: album, duration: duration};
+}
+
+function parseDuration(artistTitle) {
+	try {
+		match = artistTitle.match(/\d+:\d+/g)[0]
+
+		mins    = match.substring(0, match.indexOf(':'));
+		seconds = match.substring(match.indexOf(':')+1);
+		return parseInt(mins*60) + parseInt(seconds);
+	} catch(err){
+		return 0;
+	}
+}
+
+
+/**
+ * Simply request the scrobbler.js to submit song previusly specified by calling updateNowPlaying()
+ */
+function scrobbleTrack() {
+   // stats
+   chrome.runtime.sendMessage({type: 'trackStats', text: 'The Google Music song scrobbled'});
+
+   // scrobble
+   chrome.runtime.sendMessage({type: 'submit'});
+}
+
+
+
+/**
+ * Listen for requests from scrobbler.js
+ */
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		switch(request.type) {
+            // background calls this to see if the script is already injected
+            case 'ping':
+                sendResponse(true);
+                break;
+    }
+	}
+);
