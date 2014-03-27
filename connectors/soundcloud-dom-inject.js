b74da80c44e595a2bd4325bc722724ce5abde2f9
Jeppe Hasseriis <j.hasseriis@gmail.com>
2014-03-28 00:18:52 +0100

    Updated soundcloud-dom-inject script to post messages on PLAY and PAUSE.

--- connectors/soundcloud-dom-inject.js
+++ connectors/soundcloud-dom-inject.js
@@ -1,5 +1,25 @@
-require(['event-bus'], function(bus) {
+/**
+ * Last.fm Scrobbler for Chrome - Soundcloud connector
+ * @author Jeppe Hasseriis - https://github.com/cenobitedk
+ *
+ * This script runs in the context of soundcloud itself. 
+ * It listens to the page event bus, and propagates these 
+ * events to the extension via 'postMessage'.
+ */
+
+(function() {
+  require(['event-bus'], function(bus) {
     bus.on('audio:play', function(e) {
-        console.log('playing: ' + e.sound.attributes.title);
-    })
-});
+      window.postMessage({
+        type: 'SC_PLAY',
+        metadata: e.sound.attributes
+      }, '*');
+    });
+    bus.on('audio:pause', function(e) {
+      window.postMessage({
+        type: 'SC_PAUSE',
+        metadata: e.sound.attributes
+      }, '*');
+    });
+  });
+}());
