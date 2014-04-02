30ee8f1122fcfdfab97d3a61454925a6ba7ae632
Jeppe Hasseriis <j.hasseriis@gmail.com>
2014-04-02 08:32:05 +0200

    WIP: disable most functionality and only enable what we need right now.

--- connectors/soundcloud.js
+++ connectors/soundcloud.js
@@ -1,142 +1,183 @@
-/**
- * Find first occurence of possible separator in given string
- * and return separator's position and size in chars or null
- */
-function findSeparator(str) {
-   // care - minus vs hyphen
-   var separators = [' - ', ' – ', '-', '–', ':'];
+// /**
+//  * Find first occurence of possible separator in given string
+//  * and return separator's position and size in chars or null
+//  */
+// function findSeparator(str) {
+//    // care - minus vs hyphen
+//    var separators = [' - ', ' – ', '-', '–', ':'];
 
-   for (i in separators) {
-      var sep = separators[i];
-      var index = str.indexOf(sep);
-      if (index > -1)
-         return { index: index, length: sep.length };
-   }
+//    for (i in separators) {
+//       var sep = separators[i];
+//       var index = str.indexOf(sep);
+//       if (index > -1)
+//          return { index: index, length: sep.length };
+//    }
 
-   return null;
-}
+//    return null;
+// }
 
-/**
- * Parse given string into artist and track, assume common order Art - Ttl
- * @return {artist, track}
- */
-function parseInfo(artistTitle) {
-   var artist = '';
-   var track = '';
+// /**
+//  * Parse given string into artist and track, assume common order Art - Ttl
+//  * @return {artist, track}
+//  */
+// function parseInfo(artistTitle) {
+//    var artist = '';
+//    var track = '';
 
-   var separator = findSeparator(artistTitle);
-   if (separator == null)
-      return { artist: '', track: '' };
+//    var separator = findSeparator(artistTitle);
+//    if (separator == null)
+//       return { artist: '', track: '' };
 
-   artist = artistTitle.substr(0, separator.index);
-   track = artistTitle.substr(separator.index + separator.length);
+//    artist = artistTitle.substr(0, separator.index);
+//    track = artistTitle.substr(separator.index + separator.length);
 
-   return cleanArtistTrack(artist, track);
-}
+//    return cleanArtistTrack(artist, track);
+// }
 
 
-/**
- * Clean non-informative garbage from title
- */
-function cleanArtistTrack(artist, track) {
+// /**
+//  * Clean non-informative garbage from title
+//  */
+// function cleanArtistTrack(artist, track) {
 
-   // Do some cleanup
-   artist = artist.replace(/^\s+|\s+$/g,'');
-   track = track.replace(/^\s+|\s+$/g,'');
+//    // Do some cleanup
+//    artist = artist.replace(/^\s+|\s+$/g,'');
+//    track = track.replace(/^\s+|\s+$/g,'');
 
-   // Strip crap
-   track = track.replace(/\s*\*+\s?\S+\s?\*+$/, ''); // **NEW**
-   track = track.replace(/\s*\[[^\]]+\]$/, ''); // [whatever]
-   track = track.replace(/\s*\([^\)]*version\)$/i, ''); // (whatever version)
-   track = track.replace(/\s*\.(avi|wmv|mpg|mpeg|flv)$/i, ''); // video extensions
-   track = track.replace(/\s*(of+icial\s*)?(music\s*)?video/i, ''); // (official)? (music)? video
-   track = track.replace(/\s*\(\s*of+icial\s*\)/i, ''); // (official)
-   track = track.replace(/\s*\(\s*[0-9]{4}\s*\)/i, ''); // (1999)
-   track = track.replace(/\s+\(\s*(HD|HQ)\s*\)$/, ''); // HD (HQ)
-   track = track.replace(/\s+(HD|HQ)\s*$/, ''); // HD (HQ)
-   track = track.replace(/\s*video\s*clip/i, ''); // video clip
-   track = track.replace(/\s+\(?live\)?$/i, ''); // live
-   track = track.replace(/\(\s*\)/, ''); // Leftovers after e.g. (official video)
-   track = track.replace(/^(|.*\s)"(.*)"(\s.*|)$/, '$2'); // Artist - The new "Track title" featuring someone
-   track = track.replace(/^(|.*\s)'(.*)'(\s.*|)$/, '$2'); // 'Track title'
-   track = track.replace(/^[\/\s,:;~-]+/, ''); // trim starting white chars and dash
-   track = track.replace(/[\/\s,:;~-]+$/, ''); // trim trailing white chars and dash
+//    // Strip crap
+//    track = track.replace(/\s*\*+\s?\S+\s?\*+$/, ''); // **NEW**
+//    track = track.replace(/\s*\[[^\]]+\]$/, ''); // [whatever]
+//    track = track.replace(/\s*\([^\)]*version\)$/i, ''); // (whatever version)
+//    track = track.replace(/\s*\.(avi|wmv|mpg|mpeg|flv)$/i, ''); // video extensions
+//    track = track.replace(/\s*(of+icial\s*)?(music\s*)?video/i, ''); // (official)? (music)? video
+//    track = track.replace(/\s*\(\s*of+icial\s*\)/i, ''); // (official)
+//    track = track.replace(/\s*\(\s*[0-9]{4}\s*\)/i, ''); // (1999)
+//    track = track.replace(/\s+\(\s*(HD|HQ)\s*\)$/, ''); // HD (HQ)
+//    track = track.replace(/\s+(HD|HQ)\s*$/, ''); // HD (HQ)
+//    track = track.replace(/\s*video\s*clip/i, ''); // video clip
+//    track = track.replace(/\s+\(?live\)?$/i, ''); // live
+//    track = track.replace(/\(\s*\)/, ''); // Leftovers after e.g. (official video)
+//    track = track.replace(/^(|.*\s)"(.*)"(\s.*|)$/, '$2'); // Artist - The new "Track title" featuring someone
+//    track = track.replace(/^(|.*\s)'(.*)'(\s.*|)$/, '$2'); // 'Track title'
+//    track = track.replace(/^[\/\s,:;~-]+/, ''); // trim starting white chars and dash
+//    track = track.replace(/[\/\s,:;~-]+$/, ''); // trim trailing white chars and dash
 
-   return {artist: artist, track: track};
-}
+//    return {artist: artist, track: track};
+// }
 
 
 (function() {
-    var options;
-    var current = {};
+  // Inject script to extract events from the Soundcloud API event-bus.
+  var s = document.createElement('script');
+  s.src = chrome.extension.getURL('connectors/soundcloud-dom-inject.js');
+  s.onload = function() {
+      this.parentNode.removeChild(this);
+  };
+  (document.head||document.documentElement).appendChild(s);
 
-    var song = function(ptitle) {
-        var title = ptitle.split(' by ');
-        var parsedInfo = parseInfo(title[0]);
 
-        if (parsedInfo.artist === '') {
-            parsedInfo.track = title[0];
-            parsedInfo.artist = title[1];
-        }
+  
+  
+  function updateNowPlaying(metadata) {
 
-        return {
-            artist: parsedInfo.artist,
-            track: parsedInfo.track,
-            duration: duration(title[0])
-        };
-    };
+  }
 
-    var duration = function(title) {
-        node = $('.sound.playing').not('.playlist').find('.timeIndicator__total');
-        if (node.length) {
-            // time is given as h.m.s
-            var digits = node[0].innerHTML.split(/\D/),
-                seconds = 0, length, digit;
 
-            length = digits.length;
-            if (length > 3)
-                return;
+  function eventHandler(e) {
+    switch (e.data.type) {
+      case 'SC_PLAY':
+        //updateNowPlaying(evt.data.metadata);
+        console.log(x + ' PLAY', e.data.metadata);
+        break;
 
-            while (digits.length) {
-                d = digits.pop();
-                seconds += parseInt(d, 10) * Math.pow(60, length - digits.length - 1);
-            }
-            return seconds;
-        }
-        // if unknown duration, assume 2 minutes
-        return 120;
-    };
+      case 'SC_PAUSE':
+        //console.log('PAUSE', e.data.metadata.title);
+        //chrome.runtime.sendMessage({type: 'pause'});
+        break;
 
-    $(document).ready(function() {
-        var current_title = '';
-        $('title').live('DOMSubtreeModified', function() {
-            var title = $(this).text();
-            if (title.indexOf(' by ') < 0 || current_title === title)
-                return;
-            current_title = title;
-            setTimeout(function () {
-                var s = song(title);
-                chrome.runtime.sendMessage({type: 'reset'});
-                chrome.runtime.sendMessage({type: 'validate',
-                                            artist: s.artist,
-                                             track: s.track},
-                function(response) {
-                    current.validated = response;
-                    if (response !== false) {
-                        chrome.runtime.sendMessage({type: 'nowPlaying',
-                                                    artist: response.artist,
-                                                     track: response.track,
-                                                  duration: s.duration});
-                    }
+      default:
+        break;
+    }
+  }
 
-                    else {
-                        chrome.runtime.sendMessage({type: 'nowPlaying',
-                                                  duration: s.duration});
-                    }
-                });
-            }, 500);
-        });
-    });
+
+
+
+
+  // Listen to messages from the script.
+  window.addEventListener('message', eventHandler);
+
+    // var options;
+    // var current = {};
+
+    // var song = function(ptitle) {
+    //     var title = ptitle.split(' by ');
+    //     var parsedInfo = parseInfo(title[0]);
+
+    //     if (parsedInfo.artist === '') {
+    //         parsedInfo.track = title[0];
+    //         parsedInfo.artist = title[1];
+    //     }
+
+    //     return {
+    //         artist: parsedInfo.artist,
+    //         track: parsedInfo.track,
+    //         duration: duration(title[0])
+    //     };
+    // };
+
+    // var duration = function(title) {
+    //     node = $('.sound.playing').not('.playlist').find('.timeIndicator__total');
+    //     if (node.length) {
+    //         // time is given as h.m.s
+    //         var digits = node[0].innerHTML.split(/\D/),
+    //             seconds = 0, length, digit;
+
+    //         length = digits.length;
+    //         if (length > 3)
+    //             return;
+
+    //         while (digits.length) {
+    //             d = digits.pop();
+    //             seconds += parseInt(d, 10) * Math.pow(60, length - digits.length - 1);
+    //         }
+    //         return seconds;
+    //     }
+    //     // if unknown duration, assume 2 minutes
+    //     return 120;
+    // };
+
+    // $(document).ready(function() {
+
+    //     // var current_title = '';
+    //     // $('title').live('DOMSubtreeModified', function() {
+    //     //     var title = $(this).text();
+    //     //     if (title.indexOf(' by ') < 0 || current_title === title)
+    //     //         return;
+    //     //     current_title = title;
+    //     //     setTimeout(function () {
+    //     //         var s = song(title);
+    //     //         chrome.runtime.sendMessage({type: 'reset'});
+    //     //         chrome.runtime.sendMessage({type: 'validate',
+    //     //                                     artist: s.artist,
+    //     //                                      track: s.track},
+    //     //         function(response) {
+    //     //             current.validated = response;
+    //     //             if (response !== false) {
+    //     //                 chrome.runtime.sendMessage({type: 'nowPlaying',
+    //     //                                             artist: response.artist,
+    //     //                                              track: response.track,
+    //     //                                           duration: s.duration});
+    //     //             }
+
+    //     //             else {
+    //     //                 chrome.runtime.sendMessage({type: 'nowPlaying',
+    //     //                                           duration: s.duration});
+    //     //             }
+    //     //         });
+    //     //     }, 500);
+    //     // });
+    // });
 
     $(window).unload(function() {
         chrome.runtime.sendMessage({type: 'reset'});
