b3a264aca6e168efe1a5f9e8ebe23249ad08e302
JEHR <jehr@dr.dk>
2014-05-01 14:58:35 +0200

    Added/changed protection for double attachment. Don't use .off() since this interfere with the soundcloud playback controls. Linted.

--- connectors/soundcloud-dom-inject.js
+++ connectors/soundcloud-dom-inject.js
@@ -2,24 +2,30 @@
  * Last.fm Scrobbler for Chrome - Soundcloud connector
  * @author Jeppe Hasseriis - https://github.com/cenobitedk
  *
- * This script runs in the context of soundcloud itself. 
- * It listens to the page event bus, and propagates these 
+ * This script runs in the context of soundcloud itself.
+ * It listens to the page event bus, and propagates these
  * events to the extension via 'postMessage'.
  */
 
+window._ATTACHED = window._ATTACHED || false;
+
 (function() {
-  require(['event-bus'], function(bus) {
-    bus.off('audio:play').on('audio:play', function(e) {
-      window.postMessage({
-        type: 'SC_PLAY',
-        metadata: e.sound.attributes
-      }, '*');
+    // Exit if already attached.
+    if (window._ATTACHED) return;
+    // Attach event listeners to the event-bus.
+    require(['event-bus'], function(bus) {
+        bus.on('audio:play', function(e) {
+            window.postMessage({
+                type: 'SC_PLAY',
+                metadata: e.sound.attributes
+            }, '*');
+        });
+        bus.on('audio:pause', function(e) {
+            window.postMessage({
+                type: 'SC_PAUSE',
+                metadata: e.sound.attributes
+            }, '*');
+        });
     });
-    bus.off('audio:pause').on('audio:pause', function(e) {
-      window.postMessage({
-        type: 'SC_PAUSE',
-        metadata: e.sound.attributes
-      }, '*');
-    });
-  });
+    window._ATTACHED = true;
 }());
