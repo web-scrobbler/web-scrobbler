cee611c1cd5830ae14c06bd0e2d1936ae2ce517c
david-sabata <david.n.sabata@gmail.com>
2014-05-18 23:36:31 +0200

    add package.json and basic Gruntfile to run JShint; make defines.js pass the lint

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* global module, require */
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		jshint: {
+			all: ['Gruntfile.js', 'defines.js'], // intentionally does not contain all files yet
+			options: {
+				jshintrc: true,
+				reporter: require('jshint-stylish')
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.registerTask('lint', ['jshint']);
+	grunt.registerTask('default', ['lint']);
+};
