332f5205028bbe70e18661b186d83cb7576b473a
Home Alone <mailbox@homealone.name>
2014-05-20 14:29:19 +0400

    yandex music duration fix + repeat playing support added

--- connectors/yandex.js
+++ connectors/yandex.js
@@ -1,41 +1,50 @@
-var lastTrack = null;
-var $r = chrome.runtime.sendMessage;
+var lastTrack = '';
+var startTime = 0;
 
-function scrobble(e) {
+function parseDuration (duration) {
+    var res = /(\d+):(\d+)/.exec(duration);
+    // note: not found how songs which longer than 1 hour are displayed
+    return parseInt(res[1], 10) * 60 + parseInt(res[2], 10);
+}
+
+function scrobble () {
     var artist = $(".js-player-artist").text();
     var title = $(".js-player-title").text();
+    var duration = parseDuration($(".b-jambox__ftime").text());
 
-    if (lastTrack != artist + " " + title) {
-        lastTrack = artist + " " + title;
+    var now = new Date().getTime();
+    var isNewPlaying = (lastTrack != artist + ' ' + title) || /* new artist-title pair or */
+            ((lastTrack == artist + ' ' + title) && ((now - startTime) > 1000 * duration)); /* repeated playing of same track */
+    
+    if (isNewPlaying) {
+        lastTrack = artist + ' ' + title;
+        startTime = now;
 
-        $r({type: 'validate', artist: artist, track: title}, function(response) {
-            if (response != false) {
-                $r({
+        chrome.runtime.sendMessage({type: 'validate', artist: artist, track: title}, function (response) {
+            if (response !== false) {
+                chrome.runtime.sendMessage({
                     type: 'nowPlaying',
                     artist: response.artist,
                     track: response.track,
-                    duration: Math.floor(response.duration / 1000)
+                    duration: duration
                 });
-            } else {
-                $r({type: 'nowPlaying'});
-            }
+            } else
+                chrome.runtime.sendMessage({type: 'nowPlaying'}); // unidentified music
         });
     }
 }
 
-$(function() {
-    $(window).unload(function() {
-        // reset the background scrobbler song data
-        chrome.runtime.sendMessage({type: 'reset'});
-        return true;
+$(function () {
+    $(window).unload(function () {
+	// reset the background scrobbler song data
+	chrome.runtime.sendMessage({type: 'reset'});
+	return true;
     });
 
-    if ($(".b-jambox__controls").length > 0) {
-        $(".b-jambox__controls").bind("DOMSubtreeModified", function(e) {
-            if ($(".b-jambox__button.b-jambox__play.b-jambox__playing").length > 0)
-                setTimeout(scrobble, 500);
-        });
-    }
+    $(".b-jambox__controls").bind("DOMSubtreeModified", function () {
+        if ($(".b-jambox__button.b-jambox__play.b-jambox__playing").length > 0)
+            setTimeout(scrobble, 1000);
+    });
 });
 
 
@@ -43,7 +52,7 @@ $(function() {
  * Listen for requests from scrobbler.js
  */
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
         switch(request.type) {
 
             // background calls this to see if the script is already injected
