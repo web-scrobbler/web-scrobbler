cff50df753df2b3ca85bb77db215358df08a8b0a
Malachi Soord <inverse.chi@gmail.com>
2014-06-22 12:14:18 +0200

    Made options.js jshint valid

--- options/options.js
+++ options/options.js
@@ -1,3 +1,5 @@
+/* global authorize, connectors, isConnectorEnabled */
+'use strict';
 
 // options ---------------------------------------------------------------------
 $(function(){
@@ -5,32 +7,32 @@ $(function(){
    // use immediate-change checkboxes
 
    $('#use-notifications').click(function(){
-      localStorage['useNotifications'] = this.checked ? 1 : 0;
+      localStorage.useNotifications = this.checked ? 1 : 0;
       updateDisabled();
    });
    
    $('#use-notifications-nowplaying').click(function(){
-      localStorage['useNotificationsNowPlaying'] = this.checked ? 1 : 0;
+      localStorage.useNotificationsNowPlaying = this.checked ? 1 : 0;
    });
 
    $('#use-notifications-scrobbled').click(function(){
-      localStorage['useNotificationsScrobbled'] = this.checked ? 1 : 0;
+      localStorage.useNotificationsScrobbled = this.checked ? 1 : 0;
    });
 
    $('#auto-hide-notifications').click(function(){
-      localStorage['autoHideNotifications'] = this.checked ? 1 : 0;
+      localStorage.autoHideNotifications = this.checked ? 1 : 0;
    });
 
    $('#use-autocorrect').click(function(){
-      localStorage['useAutocorrect'] = this.checked ? 1 : 0;
+      localStorage.useAutocorrect = this.checked ? 1 : 0;
    });
 
    // preload options
-   $('#use-notifications').attr('checked', (localStorage['useNotifications'] == 1));
-   $('#use-notifications-nowplaying').attr('checked', (localStorage['useNotificationsNowPlaying'] == 1));
-   $('#use-notifications-scrobbled').attr('checked', (localStorage['useNotificationsScrobbled'] == 1));
-   $('#auto-hide-notifications').attr('checked', (localStorage['autoHideNotifications'] == 1));
-   $('#use-autocorrect').attr('checked', (localStorage['useAutocorrect'] == 1));
+   $('#use-notifications').attr('checked', (localStorage.useNotifications == 1));
+   $('#use-notifications-nowplaying').attr('checked', (localStorage.useNotificationsNowPlaying == 1));
+   $('#use-notifications-scrobbled').attr('checked', (localStorage.useNotificationsScrobbled == 1));
+   $('#auto-hide-notifications').attr('checked', (localStorage.autoHideNotifications == 1));
+   $('#use-autocorrect').attr('checked', (localStorage.useAutocorrect == 1));
       
    // disable subitems
    function updateDisabled() {
@@ -65,7 +67,7 @@ $(function(){
 function toggleInitState() {
    var checkedState = true;
 
-   $('input[id^="conn"]').each(function(index, connector) {
+   $('input[id^="conn"]').each(function() {
       if (!$(this).is(':checked')) {
          checkedState = false;
          return false;
@@ -94,7 +96,7 @@ function createConnectors() {
                     
       checkbox.attr('checked', isConnectorEnabled(connector.label));
       
-      checkbox.click(function(){ 
+      checkbox.click(function(){
          var box = $(this);
          var disabledArray = JSON.parse(localStorage.disabledConnectors);
          
