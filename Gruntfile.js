67babc06fa127a6a6011c0ae02b0f7ddbe960569
Malachi Soord <inverse.chi@gmail.com>
2014-06-28 19:22:59 +0200

    Updated spaces to tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,46 +3,46 @@
 /* global module, require */
 module.exports = function(grunt) {
 
-    var jsFiles = ['Gruntfile.js', 'defines.js', 'inject.js', 'options/options.js', 'connectors/archive.js'];// intentionally does not contain all files yet
+	var jsFiles = ['Gruntfile.js', 'defines.js', 'inject.js', 'options/options.js', 'connectors/archive.js'];// intentionally does not contain all files yet
 
-    grunt.initConfig({
-        jshint: {
-            all: jsFiles,
-            options: {
-                jshintrc: true,
-                reporter: require('jshint-stylish')
-            }
-        },
-        compress: {
-            main: {
-                options: {
-                    archive: 'web-scrobbler.zip',
-                    pretty: true
-                },
-                expand: true,
-                src: ['*.*', 'connectors/**', 'options/**', 'vendor/**']
-            }
-        },
-        lintspaces: {
-            all: {
-                src: [
-                    jsFiles
-                ],
+	grunt.initConfig({
+		jshint: {
+			all: jsFiles,
+			options: {
+				jshintrc: true,
+				reporter: require('jshint-stylish')
+			}
+		},
+		compress: {
+			main: {
+				options: {
+					archive: 'web-scrobbler.zip',
+					pretty: true
+				},
+				expand: true,
+				src: ['*.*', 'connectors/**', 'options/**', 'vendor/**']
+			}
+		},
+		lintspaces: {
+			all: {
+				src: [
+					jsFiles
+				],
 
-                options: {
-                    editorconfig: '.editorconfig',
-                    ignores: [
-                        'js-comments'
-                    ]
-                }
-            }
-        }
-    });
+				options: {
+					editorconfig: '.editorconfig',
+					ignores: [
+						'js-comments'
+					]
+				}
+			}
+		}
+	});
 
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-lintspaces');
-    grunt.registerTask('lint', ['jshint']);
-    grunt.registerTask('default', ['lint', 'lintspaces']);
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-lintspaces');
+	grunt.registerTask('lint', ['jshint']);
+	grunt.registerTask('default', ['lint', 'lintspaces']);
 };
