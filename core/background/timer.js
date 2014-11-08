8c09dee86bfe238accedccf991b17cd2792d8ee4
david-sabata <david.n.sabata@gmail.com>
2014-11-08 21:52:08 +0100

    prevent Timer from triggering againg on unpausing

--- core/background/timer.js
+++ core/background/timer.js
@@ -12,7 +12,8 @@ define([], function() {
 			target = null, // target seconds
 			pausedOn = null, // marks pause time in seconds
 			startedOn = null, // marks start time in seconds
-			spentPaused = 0; // sum of paused time in seconds
+			spentPaused = 0, // sum of paused time in seconds
+			hasTriggered = false; // already triggered callback?
 
 		/**
 		 * Returns current time in seconds
@@ -23,7 +24,10 @@ define([], function() {
 
 		function setTrigger(seconds) {
 			clearTrigger();
-			timeoutId = setTimeout(callback, seconds * 1000);
+			timeoutId = setTimeout(function() {
+				callback();
+				hasTriggered = true;
+			}, seconds * 1000);
 		}
 
 		/**
@@ -70,7 +74,10 @@ define([], function() {
 			if (startedOn !== null && pausedOn !== null) {
 				spentPaused += now() - pausedOn;
 				pausedOn = null;
-				setTrigger(target - this.getElapsed());
+
+				if (!hasTriggered) {
+					setTrigger(target - this.getElapsed());
+				}
 			}
 		};
 
@@ -78,6 +85,10 @@ define([], function() {
 		 * Update time for this timer before callback is triggered.
 		 * Already elapsed time is not modified and callback
 		 * will be triggered immediately if the new time is less than elapsed.
+		 *
+		 * Intentionally does not check if the callback was already triggered.
+		 * This allows to update the timer after it went out once and still
+		 * be able to properly trigger the callback for the new timeout.
 		 */
 		this.update = function(seconds) {
 			// only if timer was started
@@ -99,6 +110,13 @@ define([], function() {
 		};
 
 		/**
+		 * Checks if current timer has already triggered its callback
+		 */
+		this.hasTriggered = function() {
+			return hasTriggered;
+		};
+
+		/**
 		 * Reset timer
 		 */
 		this.reset = function() {
@@ -107,6 +125,7 @@ define([], function() {
 			pausedOn = null;
 			spentPaused = 0;
 			callback = null;
+			hasTriggered = false;
 
 			clearTrigger();
 		};
