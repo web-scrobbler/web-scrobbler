8412bc7308c16221f2230b21292343b5e9f944ac
Ostah <ostah@outlook.com>
2014-12-07 00:40:55 +0100

    no empty lines (really ? :D )

--- connectors/rmfon.js
+++ connectors/rmfon.js
@@ -19,18 +19,15 @@ $(function()
 		if (current !== last)
 		{
 			last = current;
-			
 			var duration = 3*60;
 			var current_time = stripMinutes($('.playlist-line.playlist-current').find('.time').text());
 			var next_time = stripMinutes($('.playlist-line.playlist-current').next().find('.time').text());
-			
 			if(current_time < next_time){
 				duration = (next_time - current_time)*60;
 			}
 			else{
 				duration = (next_time + (current_time-60))*60;
 			}
-			
 			$r({type: 'validate', artist: $( '#now-artist' ).text(), track: $( '#now-title' ).text(), duration: duration}, function(response) {
 				if (response) {
 					$r({type: 'nowPlaying', duration: duration, artist: response.artist, track: response.track});
