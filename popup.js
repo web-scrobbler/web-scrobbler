5766ebb07aec9a4c275b2f7ebc53210859d77d5f
Joe Crawford <joe@artlung.com>
2014-12-10 11:23:11 -0800

    Altering to be same as master
    I am a Github idiot today.

--- popup.js
+++ popup.js
@@ -1,59 +1,125 @@
-$(function(){
-   /*
-   var o = '<pre>';
-   for (var x in chrome.extension.getBackgroundPage().song) {
-      o += x + ' : ' + chrome.extension.getBackgroundPage().song[x] + '\n';
+$(function() {
+   'use strict';
+
+   // trick to get current tab ID
+   chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+   }, function(tabs) {
+      var tab = tabs[0];
+      onTabReady(tab.id);
+   });
+
+   /**
+    * Tab ID is resolved callback
+    */
+   function onTabReady(tabId) {
+      // ask background for current song info
+      chrome.runtime.sendMessage({
+         type: 'v2.getSong',
+         tabId: tabId
+      }, function(response) {
+         // false means legacy mode
+         if (response === false) {
+            legacy();
+         } else {
+            onSongLoaded(tabId, response);
+         }
+      });
    }
-   o += '</pre>';
-   $(o).appendTo('body');
-   */
 
-   if (chrome.extension.getBackgroundPage().song.artist != '')
-      $('#artist').val(chrome.extension.getBackgroundPage().song.artist);
-
-   if (chrome.extension.getBackgroundPage().song.track != '')
-      $('#track').val(chrome.extension.getBackgroundPage().song.track);
-
-
-
-   $('#submit').click(function(){
-      $(this).attr('disabled', true);
-      $(this).siblings().remove();
-
-      var artist = $('#artist').val();
-      var track = $('#track').val();
-      var res = chrome.extension.getBackgroundPage().validate( artist, track );
-
-      if (res == false) {
-         $(this).attr('disabled', false);
-         $(this).siblings().remove();
-         $(this).parent().append('<span class="note">Not valid</span>');
+   /**
+    * Song data from background script are loaded
+    */
+   function onSongLoaded(tabId, song) {
+      // no current song - should not happen, because page action with popup shows
+      // only when there is a song that can be corrected
+      if (song === null) {
+         return;
       }
-      else {
-         // did the content script recognize at least the duration?
-         if (!chrome.extension.getBackgroundPage().song.duration)
-            chrome.extension.getBackgroundPage().song.duration = res.duration;
 
-         // fill other data
-         chrome.extension.getBackgroundPage().song.artist = res.artist;
-         chrome.extension.getBackgroundPage().song.track = res.track;
-         //chrome.extension.getBackgroundPage().song.album = res.album;
+      $('#artist').val(song.processed.artist || song.parsed.artist);
+      $('#track').val(song.processed.track || song.parsed.track);
 
-         // make the connection to last.fm service to notify
-         chrome.extension.getBackgroundPage().nowPlaying();
+      $('#submit').click(function () {
+         $(this).attr('disabled', true);
+         $(this).siblings().remove();
 
-         // The minimum time is 240 seconds or half the track's total length
-         // minus the time that already past
-         var past = parseInt(new Date().getTime() / 1000.0) - chrome.extension.getBackgroundPage().song.startTime;
-         var min_time = Math.min(240, chrome.extension.getBackgroundPage().song.duration / 2) - past;
+         var data = {
+            artist: $('#artist').val(),
+            track: $('#track').val()
+         };
 
-         // Set up the timer to scrobble
-         chrome.extension.getBackgroundPage().scrobbleTimeout = chrome.extension.getBackgroundPage().setTimeout(chrome.extension.getBackgroundPage().submit, min_time * 1000);
+         chrome.runtime.sendMessage({
+            type: 'v2.correctSong',
+            tabId: tabId,
+            data: data
+         });
 
+         // show green tick even if the song may not be valid - we have no way of knowing yet
          $('#form').fadeOut(0);
          $('#valid').fadeIn(0);
+      });
+   }
+
+
+   /**
+    * Legacy code for cases when legacy controller is loaded and active
+    */
+   function legacy() {
+      var popupApi = chrome.extension.getBackgroundPage().popupApi;
+      var song = popupApi.getSong();
+
+      if (song.artist !== '') {
+         $('#artist').val(song.artist);
       }
 
-   });
+      if (song.track !== '') {
+         $('#track').val(song.track);
+      }
+
+      $('#submit').click(function () {
+         $(this).attr('disabled', true);
+         $(this).siblings().remove();
+
+         var artist = $('#artist').val();
+         var track = $('#track').val();
+
+         var validateCB = function (res) {
+            if (res === false) {
+               $(this).attr('disabled', false);
+               $(this).siblings().remove();
+               $(this).parent().append('<span class="note">Not valid</span>');
+            } else {
+               // did the content script recognize at least the duration?
+               if (!song.duration) {
+                  song.duration = res.duration;
+               }
+
+               // fill other data
+               song.artist = res.artist;
+               song.track = res.track;
+               //chrome.extension.getBackgroundPage().song.album = res.album;
+
+               // make the connection to last.fm service to notify
+               popupApi.nowPlaying(song);
+
+               // The minimum time is 240 seconds or half the track's total length
+               // minus the time that already past
+               var past = parseInt(new Date().getTime() / 1000.0) - song.startTime;
+               var min_time = Math.min(240, song.duration / 2) - past;
+
+               // Set up the timer to scrobble
+               popupApi.planSubmit(min_time * 1000);
+
+               $('#form').fadeOut(0);
+               $('#valid').fadeIn(0);
+            }
+         };
+
+         popupApi.validate(artist, track, validateCB);
+      });
+   }
+
 
 });
