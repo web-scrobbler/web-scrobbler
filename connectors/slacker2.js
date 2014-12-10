7ff54d0113c31ec094ca99f3f91a46d6b4230daa
Joe Crawford <joe@artlung.com>
2014-12-10 10:58:14 -0800

    Fix for Slacker Interface Change
    Addressses Issue #511 -- and also accounts for users who can switch back
    to old interface.

--- connectors/slacker2.js
+++ connectors/slacker2.js
@@ -1,3 +1,7 @@
+/*jslint sloppy: true */
+/*global LFM_WATCHED_CONTAINER:true, $ */
+/*member length, text, split, log, chrome, runtime, sendMessage, live, unload,
+artist, track, duration, album, type, hasClass */
 /*
  * Chrome-Last.fm-Scrobbler Slacker.com Connector
  * 
@@ -9,22 +13,39 @@
 /********* Configuration: ***********/
 
 // changes to the DOM in this container will trigger an update.
-LFM_WATCHED_CONTAINER = "div#track-metadata";
+var SITE_VERSION;
+var SITE_VERSION_4 = 4;
+var SITE_VERSION_6 = 6;
+
+function LFM_WATCHED_CONTAINER() {
+ return (SITE_VERSION === SITE_VERSION_6) ? 'section.mini' : 'div#track-metadata';
+}
 
 function LFM_IS_A_SONG() {
-	return !$("#mini-track-name").hasClass("noClick");
+	if (SITE_VERSION === SITE_VERSION_6) {
+		return ($('ul[id=tuning]:visible li a.disabled:visible').length === 0);
+	} else if (SITE_VERSION === SITE_VERSION_4) {
+		return !$('#mini-track-name').hasClass('noClick');
+	}
 }
+
 
 // function that returns title of current song
 function LFM_TRACK_TITLE() {
-	return $("#mini-track-name").text();
+	if (SITE_VERSION === SITE_VERSION_6) {
+		return $('.metadata:visible span.link:eq(1)').text();
+	} else if (SITE_VERSION === SITE_VERSION_4) {
+		return $('#mini-track-name').text();
+	}
 }
-
 
 // function that returns artist of current song
 function LFM_TRACK_ARTIST() {
-	return $("#mini-artist-name").text();
-}
+	if (SITE_VERSION === SITE_VERSION_6) {
+		return $('.metadata:visible span.link:eq(0)').text();
+	} else if (SITE_VERSION === SITE_VERSION_4) {
+		return $('#mini-artist-name').text();
+	}}
 
 // function that returns artist of current song
 function LFM_TRACK_ALBUM() {
@@ -34,15 +55,22 @@ function LFM_TRACK_ALBUM() {
 // function that returns duration of current song in seconds
 // called at begining of song
 function LFM_TRACK_DURATION() {
-	durationArr = $('#progress-total').text().split(":");
+	var durationArr;
+	if (SITE_VERSION === SITE_VERSION_6) {
+		durationArr = $('#progressContainer span:first').text().split(':');
+	} else if (SITE_VERSION === SITE_VERSION_4) {
+		durationArr = $('#progress-total').text().split(':');
+	}
 	return parseInt(durationArr[0], 10)*60 + parseInt(durationArr[1], 10);
 }
 
 
 /********* Connector: ***********/
 
-var LFM_lastTrack = "";
+var LFM_lastTrack = '';
 var LFM_isWaiting = 0;
+
+var title, artist, duration, album, newTrack, isASong;
 
 function LFM_updateNowPlaying(){
 	// Acquire data from page
@@ -50,16 +78,22 @@ function LFM_updateNowPlaying(){
 	artist = LFM_TRACK_ARTIST();
 	duration = LFM_TRACK_DURATION();
 	album = LFM_TRACK_ALBUM();
-	newTrack = title + " " + artist;
+	newTrack = title + '' + artist;
 	isASong = LFM_IS_A_SONG();
+	console.log('=====================================================================');
+	console.log(title, artist, duration, album, newTrack, isASong);
+	console.log('=====================================================================');
+
 	// Update scrobbler if necessary
-	if (isASong && newTrack != " " && newTrack != LFM_lastTrack){
+	if (isASong && newTrack != ' ' && newTrack != LFM_lastTrack){
 		if (duration === 0) {
 			// Nasty workaround for delayed duration visiblity with skipped tracks.
 			setTimeout(LFM_updateNowPlaying, 5000);
 			return 0;
 		}
-		console.log("submitting a now playing request. artist: "+artist+", title: "+title+", duration: "+duration);
+		console.log('=====================================================================');
+		console.log('submitting a now playing request. artist: '+ artist + ', title: '+ title +', duration: ' + duration);
+		console.log('=====================================================================');
 		LFM_lastTrack = newTrack;
 		chrome.runtime.sendMessage({type: 'validate', artist: artist, track: title}, function(response) {
 			if (response !== false) {
@@ -78,10 +112,17 @@ function LFM_updateNowPlaying(){
 
 // Run at startup
 $(function(){
-	console.log("Slacker module starting up");
-
-	$(LFM_WATCHED_CONTAINER).live('DOMSubtreeModified', function(e) {
-		//console.log("Live watcher called");
+	console.log('=====================================================================');
+	console.log('Slacker LAST.FM Scrobbler module starting up');
+	if ($('section').length > 0) {
+		SITE_VERSION = SITE_VERSION_6;
+	} else {
+		SITE_VERSION = SITE_VERSION_4;
+	}
+	console.log('Slacker LAST.FM Scrobbler for Site Version ' + SITE_VERSION);
+	console.log('=====================================================================');
+	$(LFM_WATCHED_CONTAINER()).live('DOMSubtreeModified', function() {
+		
 		if ($(LFM_WATCHED_CONTAINER).length > 0) {
 			if(LFM_isWaiting === 0){
 				LFM_isWaiting = 1;
