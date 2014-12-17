8cc02779798b10c445aaf52270ac29399d08f401
Jan Baykara <jan@baykara.co.uk>
2014-12-17 10:37:33 +0000

    Fix connector for BBC Radio Player's new Playlister/NowPlaying functionality

--- connectors/bbcradioplayer.js
+++ connectors/bbcradioplayer.js
@@ -15,16 +15,10 @@ var DEFAULT_TIMEOUT = 90;
 var config = {childList: true, subtree: true, attributes: true};
 
 // BBC RadioPlayer stations show a Now Playing box inside the container
-// The box becomes invisible when the track stops, so we need to check for visibility
-
-var CONTAINER = '#programme-info';
-var NP_ELEMENT = '#realtime:visible';
-var ARTIST_ELEMENT = '#artists';
-
-// Track names are usually just text, but if they are too long, the div contains child
-// elements that include an ellipsis, which we want to remove.
-var TRACK_ELEMENT = '#track';
-var TRACK_CHILD_ELEMENT = ':not(.more-indicator)';
+var CONTAINER = '.programme-details-wrapper ';
+var NP_ELEMENT = '#now-playing .playlister ';
+var ARTIST_ELEMENT = NP_ELEMENT+'.track .artist';
+var TRACK_ELEMENT = NP_ELEMENT+'.track .title';
 
 /**
  * Call the specified function when the container contents change.
@@ -63,17 +57,7 @@ chrome.runtime.onMessage.addListener(
 function updateTrack() {
     if ($(NP_ELEMENT).length > 0) {
         artist = $(ARTIST_ELEMENT).text();
-        track = '';
-
-        if ($(TRACK_ELEMENT).children().length > 0) {
-            $(TRACK_ELEMENT).children().each(function() {
-                if ($(this).is(TRACK_CHILD_ELEMENT)) {
-                    track += $(this).text();
-                }
-            });
-        } else {
-            track = $(TRACK_ELEMENT).text();
-        }
+        track = $(TRACK_ELEMENT).text();
 
         scrobbleTrack($.trim(artist), $.trim(track), DEFAULT_TIMEOUT);
     } else {
