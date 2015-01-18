f9c417df33be413601aea2e466721aeaf96357b0
Malachi Soord <inverse.chi@gmail.com>
2015-01-18 12:47:29 +0100

    Added support for bop.fm

---
+++ connectors/v2/bopfm.js
@@ -0,0 +1,10 @@
+
+Connector.playerSelector = '#bop-player';
+
+Connector.artistSelector = '#bop-player div.current-song-section > div.song-info > div.artist > a';
+
+Connector.trackSelector = '#bop-player div.current-song-section > div.song-info > div.title > a';
+
+Connector.isPlaying = function() {
+	return $('body').hasClass('song-playing');
+};
