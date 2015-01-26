8473c3ef306ad2c5c1e1def866f0cac7dcb5cd0a
Anton Romanov <romanton@amazon.com>
2015-01-26 15:55:17 -0800

    Add support for listen.tidalhifi.com, closes #485

---
+++ connectors/v2/tidal.js
@@ -0,0 +1,5 @@
+Connector.playerSelector = '#wimp';
+Connector.trackSelector = 'div.player__text > a';
+Connector.artistSelector = 'div.player__text > div > a';
+Connector.playButtonSelector  = 'button.play-controls__play';
+Connector.currentTimeSelector = 'span.progress-progress';
