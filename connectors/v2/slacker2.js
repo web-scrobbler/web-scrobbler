e5dacb02be7445f70aae6aeec50c1843bc278ae9
Paszt <nonya@paszt.net>
2015-05-03 23:27:06 -0400

    Update Slacker (main page) to V2

---
+++ connectors/v2/slacker2.js
@@ -0,0 +1,17 @@
+﻿
+Connector.playerSelector = '#bar';
+
+Connector.artistSelector = '.item.current [data-bind="text: artistName"]';
+
+Connector.trackSelector = '.item.current [data-bind="text: name"]';
+
+Connector.currentTimeSelector = '#progressContainer span:first';
+
+Connector.playButtonSelector = '#transport li.pause';
+
+/** @returns {number|null} track length in seconds */
+Connector.getDuration = function () {
+    'use strict';
+    return Connector.stringToSeconds($(Connector.currentTimeSelector).text() || '') +
+           Connector.stringToSeconds($('#progressContainer span:last').text() || '');
+};
