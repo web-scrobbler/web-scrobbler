7f645bd30b94a6415e404cc23f4f51c8db3b05f9
Malachi Soord <inverse.chi@gmail.com>
2015-05-12 22:12:47 +0200

    Fixed indentation

--- connectors/v2/musicload.js
+++ connectors/v2/musicload.js
@@ -7,10 +7,9 @@ Connector.playerSelector = '.player-info-wrapper';
 Connector.artistSelector = '.player-info-wrapper > .info > .title > span';
 
 Connector.getTrack = function() {
-  return $('.player-info-wrapper > .info > .title').contents().get(0).nodeValue;
+	return $('.player-info-wrapper > .info > .title').contents().get(0).nodeValue;
 };
 
 Connector.isPlaying = function() {
-  return ($('.control.play').css('display') == 'none');
-
+	return ($('.control.play').css('display') == 'none');
 };
