a33d351c87aceb937326601331d2d847cf007191
Jan Baykara <jan@baykara.co.uk>
2015-05-14 12:47:58 +0100

    More precise MusicBrainz querying

--- core/background/pipeline/musicbrainz-coverartarchive.js
+++ core/background/pipeline/musicbrainz-coverartarchive.js
@@ -2,6 +2,10 @@
 
 define([], function() {
 
+	// NB:
+	// Could potentially split `getMusicBrainzId()` and `getCoverArtArchive()` into consecutive pipeline modules,
+	// if we can come up with other uses for the MusicBrainz data.
+
 	var service = {
 		getCoverArt: function(song, cb) {
 			// Only query APIs if no cover art can be found
@@ -10,38 +14,61 @@ define([], function() {
 				cb();
 			} else {
 				console.log('Looking for album artwork via MusicBrainz');
-				// Try to ID the song via the MusicBrainz API
-				service.getMusicBrainzId(song, cb, function() {
-					// then Check for CoverArtArchive imagery against that MusicBrainz ID
-					service.getCoverArtArchive(song, cb);
-				});
+
+				// Search both, just incase there's artwork for the album, but not for the song/single
+				var endpoints = ['release','release-group'];
+
+				var coverArtSearch = function() {
+					if (endpoints.length < 1) {
+						return cb();
+					}
+
+					var endpoint = endpoints.shift();
+					// Try to ID the song via the MusicBrainz API
+					service.getMusicBrainzId(endpoint, song, coverArtSearch, function() {
+						// then Check for CoverArtArchive imagery against that MusicBrainz ID
+						service.getCoverArtArchive(song, coverArtSearch, cb);
+					});
+				};
+
+				coverArtSearch();
 			}
 		},
 
-		// NB:
-		// Could potentially split `getMusicBrainzId()` and `getCoverArtArchive()` into consecutive pipeline modules,
-		// if we can come up with other uses for the MusicBrainz data.
+		/* Get track or album MusicBrainz ID
+			* Search API docs:
+				http://musicbrainz.org/doc/Development/XML_Web_Service/Version_2/Search
+			* Query syntax docs:
+				https://lucene.apache.org/core/4_3_0/queryparser/org/apache/lucene/queryparser/classic/package-summary.html#package_description
+		*/
+		getMusicBrainzId: function(endpoint, song, onFailure, onSuccess) {
+			$.get('http://musicbrainz.org/ws/2/' + endpoint + '?fmt=json&query=' +
+					'title:'+
+					'+"' + song.getTrack() + '"^3 ' + // bias towards the exact string
+					song.getTrack() + ' ' + // and individual words
 
-		getMusicBrainzId: function(song, onFailure, onSuccess) {
-			// Get MusicBrainz ID
-			// Search API docs: http://musicbrainz.org/doc/Development/XML_Web_Service/Version_2/Search
-			$.get('http://musicbrainz.org/ws/2/release?query=' +
-				'title:' + song.getTrack() +
-				' AND ' +
-				'artist:' + song.getArtist() +
-				'&fmt=json&limit=1'
-			)
+					'artistname:'+
+					'+"' + song.getArtist() + '"^4' +
+					song.getArtist() + ' ' +
+				)
 				.done(function(musicbrainz) {
-					var MBID = musicbrainz.releases[0].id;
+					if (musicbrainz.count === 0) {
+						return onFailure();
+					}
+
+					var results = musicbrainz[endpoint + 's'];
+					var MBID = results[0].id;
 					song.metadata.musicBrainzId = MBID;
+
 					(typeof onSuccess === 'function' ? onSuccess : onFailure)(MBID);
 				})
 				.fail(onFailure);
 		},
 
+		// Requires song.metadata.musicBrainzId to have been previously retrieved
 		getCoverArtArchive: function(song, onFailure, onSuccess) {
-			// Look for CoverArtArchive.org images with this ID
 			var coverArtUrl = 'http://coverartarchive.org/release/' + song.metadata.musicBrainzId + '/front';
+
 			$.ajax({
 					url: coverArtUrl,
 					type: 'HEAD' // Check if the CoverArt is actually a useable HTTP resource; prevents Chrome errors
@@ -49,6 +76,7 @@ define([], function() {
 				.done(function() {
 					console.log('Found album artwork via MusicBrainz');
 					song.metadata.artistThumbUrl = coverArtUrl;
+
 					(typeof onSuccess === 'function' ? onSuccess : onFailure)(coverArtUrl);
 				})
 				.fail(onFailure);
