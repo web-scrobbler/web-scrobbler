f6b0b89965de8922c90d65ca69e4f8c4a23f10a6
Jan Baykara <jan@baykara.co.uk>
2015-05-20 15:49:37 +0100

    Satisfy Grunt Gruntington

--- connectors/v2/bbcradioplayer.js
+++ connectors/v2/bbcradioplayer.js
@@ -5,17 +5,17 @@ var nowPlayingSelector = '#now-playing .playlister';
 
 Connector.playerSelector = '.programme-details-wrapper';
 
-Connector.artistSelector = nowPlayingSelector+' .track .artist';
+Connector.artistSelector = nowPlayingSelector + ' .track .artist';
 
-Connector.trackSelector = nowPlayingSelector+' .track .title';
+Connector.trackSelector = nowPlayingSelector + ' .track .title';
 
 Connector.getUniqueID = function() {
-	return $("#data-uid").text();
+	return $('#data-uid').text();
 };
 
 // Data provided rounds duration UP to next minute... Is usually longer than the last.fm version data.
 Connector.getDuration = function () {
-	return $("#data-end").text() - $("#data-start").text();
+	return $('#data-end').text() - $('#data-start').text();
 };
 
 Connector.isPlaying = function() {
