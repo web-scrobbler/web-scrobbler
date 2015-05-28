2e46e325932965c8682d540c052e5496efa3949a
Paszt <nonya@paszt.net>
2015-05-28 02:46:14 -0400

    correct for lintspaces

--- connectors/v2/amazon.js
+++ connectors/v2/amazon.js
@@ -4,19 +4,19 @@
 
 Connector.playerSelector = '#mp3Player';
 
-Connector.getArtist = function () {
+Connector.getArtist = function() {
 	return $('.currentSongDetails .artistLink').attr('title') || null;
 };
 
-/** prefer titles from playlist but in case user has navigated away from the playlist, 
- *  fallback to the (possibly truncated) values in the bottom now playing pane. 
+/** prefer titles from playlist but in case user has navigated away from the playlist,
+ *  fallback to the (possibly truncated) values in the bottom now playing pane.
  */
-Connector.getTrack = function () {
+Connector.getTrack = function() {
 	return $('tr.selectable.currentlyPlaying td.titleCell').attr('title') ||
 		   $('.currentSongDetails .title').text() || null;
 };
 
-Connector.getAlbum = function () {
+Connector.getAlbum = function() {
 	return $('tr.selectable.currentlyPlaying td.albumCell').attr('title') ||
 		   $('.nowPlayingDetail img.albumImage').attr('title') || null;
 };
@@ -26,11 +26,11 @@ Connector.currentTimeSelector = '#currentTime';
 Connector.playButtonSelector = 'span.mp3MasterPlay.icon-play';
 
 /** @returns {number|null} track length in seconds */
-Connector.getDuration = function () {
+Connector.getDuration = function() {
 	return Connector.stringToSeconds($('#currentDuration').text()) || null;
 };
 
-Connector.getUniqueID = function () {
+Connector.getUniqueID = function() {
 	var optionsHref = $('.buttonOption.main[title=Options]').attr('href');
 	if (optionsHref) {
 		return optionsHref.replace(/[\w|\W]+adriveId=/, '');
