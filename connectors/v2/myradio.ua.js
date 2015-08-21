814edc56807b4574ecedd0f5099834079e2286af
Alexander Georgievskiy <galeksandrp@gmail.com>
2015-08-22 01:43:20 +0300

    Add myradio.ua

--- connectors/v2/myradio.ua.js
+++ connectors/v2/myradio.ua.js
@@ -7,21 +7,21 @@ Connector.playerSelector = '.cur-blk';
 Connector.separators = [' -             '];
 
 Connector.getArtistTrack = function () {
-    var text = $('.name').text().substring(12);
-    text = text.substring(0, text.length-1);
-    var separator = this.findSeparator(text);
+	var text = $('.name').text().substring(12);
+	text = text.substring(0, text.length - 1);
+	var separator = this.findSeparator(text);
 
-    var artist = null;
-    var track = null;
+	var artist = null;
+	var track = null;
 
-    if (separator !== null) {
-        artist = text.substr(0, separator.index);
-        track = text.substr(separator.index + separator.length);
-    }
+	if (separator !== null) {
+		artist = text.substr(0, separator.index);
+		track = text.substr(separator.index + separator.length);
+	}
 
-    return {artist: artist, track: track};
+	return {artist: artist, track: track};
 };
 
-Connector.isPlaying = function() {
+Connector.isPlaying = function () {
 	return $('#play').hasClass('pause');
 };
