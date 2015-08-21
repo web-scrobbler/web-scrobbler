4ec2f02b456e6f469c4da6360dde7bec9595dd8b
Alexander Georgievskiy <galeksandrp@gmail.com>
2015-08-21 22:49:17 +0300

    Add myradio.ua

---
+++ connectors/v2/myradio.ua.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '.cur-blk .clr';
+
+Connector.artistTrackSelector = '.cur-blk .clr > .name';
+
+Connector.playButtonSelector = '#play';
