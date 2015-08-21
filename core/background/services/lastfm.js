349713067dbe48fc033fdfbc1d1aac1ee4f4768c
Jan Baykara <janbaykara@gmail.com>
2015-08-21 16:12:29 +0100

    Undo accidental line removal.

--- core/background/services/lastfm.js
+++ core/background/services/lastfm.js
@@ -361,6 +361,7 @@ define([
 
 			var params = {
 				method: 'track.scrobble',
+				'timestamp[0]': song.metadata.startTimestamp,
 				'track[0]': song.processed.track || song.parsed.track,
 				'artist[0]': song.processed.artist || song.parsed.artist,
 				api_key: config.apiKey,
