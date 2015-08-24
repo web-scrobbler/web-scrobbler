491a7ef2820502bdec10e4e02eed460a9b0a9cb5
Alexander Georgievskiy <galeksandrp@gmail.com>
2015-08-24 06:08:23 +0300

    Fix artist name yandexradio

--- connectors/v2/yandexradio.js
+++ connectors/v2/yandexradio.js
@@ -3,8 +3,8 @@
 /* global Connector */
 
 Connector.playerSelector = '.page-station__bar';
-Connector.trackSelector = '.slider__items > div:nth-child(3) .track__title a';
-Connector.artistSelector = '.slider__items > div:nth-child(3) .track__artists a';
+Connector.trackSelector = '.slider__items > div:nth-child(3) .track__title';
+Connector.artistSelector = '.slider__items > div:nth-child(3) .track__artists';
 Connector.isPlaying = function() {
 	return $('body').hasClass('body_state_playing');
 };
