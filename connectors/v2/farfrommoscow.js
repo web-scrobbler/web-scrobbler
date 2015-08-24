a6f26363c23adaf8d4a4f30022a08c15496eb5fd
Alexander Georgievskiy <galeksandrp@gmail.com>
2015-08-24 05:00:47 +0300

    Fix #547 add farfrommoscow

--- connectors/v2/farfrommoscow.js
+++ connectors/v2/farfrommoscow.js
@@ -2,12 +2,27 @@
 
 /* global Connector */
 
-Connector.playerSelector = '.player-name';
-
-Connector.artistSelector = '#playing h2';
-
-Connector.trackSelector = '#playing h3';
+Connector.playerSelector = '.global-content';
 
 Connector.isPlaying = function () {
-	return !$('#controls a[title=Play] i').is(':visible');
+	return $('.player-buttonPause').length ? true : $('.Artist-Mini-Playing').length;
+};
+
+Connector.getArtistTrack = function () {
+	var artist = $('.AR-wrap:has(.Artist-Mini-Playing) .AR-name').length ? $('.AR-wrap:has(.Artist-Mini-Playing) .AR-name').text() : $(':has(.Artist-Mini-Playing) > div > .Artist-link').text();
+	var track = $('.Artist-Mini-Playing').attr('title');
+	if (artist.length) {return {artist: artist, track: track};}
+
+	var text = $('.player:has(.player-buttonPause) .player-name span').text();
+	var separator = this.findSeparator(text);
+
+	artist = null;
+	track = null;
+
+	if (separator !== null) {
+		artist = text.substr(0, separator.index).trim();
+		track = text.substr(separator.index + separator.length).trim();
+	}
+
+	return {artist: artist, track: track};
 };
