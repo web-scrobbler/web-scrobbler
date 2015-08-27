a5ac76262c615ecbc93565ad9d130dda0a501771
Malachi Soord <inverse.chi@gmail.com>
2015-08-27 22:44:18 +0200

    Linty lint

--- core/background/scrobblers/librefm.js
+++ core/background/scrobblers/librefm.js
@@ -4,15 +4,15 @@
  * Module for all communication with libre.fm
  */
 define([
-    'scrobblers/baseScrobbler'
+	'scrobblers/baseScrobbler'
 ], function (BaseScrobbler) {
 
-    var LibreFM = new BaseScrobbler({
-        storage: 'LibreFm',
-        apiUrl: 'https://libre.fm/2.0/',
-        apiKey: '',
-        apiSecret: ''
-    });
+	var LibreFM = new BaseScrobbler({
+		storage: 'LibreFm',
+		apiUrl: 'https://libre.fm/2.0/',
+		apiKey: '',
+		apiSecret: ''
+	});
 
-    return LibreFM;
+	return LibreFM;
 });
