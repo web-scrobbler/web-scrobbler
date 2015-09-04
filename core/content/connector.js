c1f32029f6ee040b913da8cf03da70f60cabac5b
Jan Baykara <jan@baykara.co.uk>
2015-09-04 11:01:21 +0100

    Placate jshint being a little bitch.

--- core/content/connector.js
+++ core/content/connector.js
@@ -340,12 +340,12 @@ var BaseConnector = window.BaseConnector || function () {
 					return newPlaylistTrack.startTime === someTrack.startTime;
 				});
 
-				newDuration = this.currentPlaylist[trackIndex+1] // Find time to next track
-							? this.currentPlaylist[trackIndex+1].startTime - newPlaylistTrack.startTime
-							: ( this.getDuration() // If you can't, it must be the last track, sooo...
-								? this.getDuration() - newPlaylistTrack.startTime // Find time to end of whole media
-								: 20 // or... if !duration, just wing it and give it 20 seconds.
-							)
+				newDuration = this.currentPlaylist[trackIndex+1] ? // Find time to next track
+								this.currentPlaylist[trackIndex+1].startTime - newPlaylistTrack.startTime
+								: ( this.getDuration() ? // If you can't, it must be the last track, sooo...
+									this.getDuration() - newPlaylistTrack.startTime // Find time to end of whole media
+									: 20 // or... if !duration, just wing it and give it 20 seconds.
+								);
 
 				// Append #NN (indexOf) to UID, to prevent scrobble issues due to repeated UID
 				newUID = this.getUniqueID()+'#'+trackIndex;
