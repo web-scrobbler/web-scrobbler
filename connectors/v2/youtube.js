8397c5ed882e5a7d5259f1bc51460de520e50430
Jan Baykara <jan@baykara.co.uk>
2015-09-04 15:15:46 +0100

    Better numbering recognition

--- connectors/v2/youtube.js
+++ connectors/v2/youtube.js
@@ -193,8 +193,8 @@ function cleansePlaylistLine(maybeTrack) {
 	maybeTrack = maybeTrack.replace(/\s*[\[\(\{]__TIMESTAMP__[\]\)\}]/gi,''); // [00:00] (e.g. https://www.youtube.com/watch?v=YKkOxFoE5yo)
 	maybeTrack = maybeTrack.replace('__TIMESTAMP__','');
 	maybeTrack = maybeTrack.replace(/\s*&[a-z]+;\s*/gi,''); // remove HTML entity codes (e.g. https://www.youtube.com/watch?v=VFOF47nalCY)
-	maybeTrack = maybeTrack.replace(/\s*[\[\(]*[0-9]+[\.:\-\]\)]+\s*/i,''); // numbering in middle of str [1] 1) (e.g. https://www.youtube.com/watch?v=RW3li2XWgoM)
 	maybeTrack = maybeTrack.replace(/^\s*[-:=]\s*/gi,''); // HH:MM - Track
+	maybeTrack = maybeTrack.replace(/\s*[\[\(\{\-]*\s*[0-9]{1,2}\s*[\.\-:=\)\]\}]{1,3}\s*/i,''); // numbering  1.  1 -  (1) etc. (e.g. https://www.youtube.com/watch?v=Y7QQS5V3cnI)
 
 	return maybeTrack;
 }
