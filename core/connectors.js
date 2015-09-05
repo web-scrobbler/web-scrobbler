1e86f269e6ffee834b1356d3463cc0beb741d5ca
Alexander Georgievskiy <galeksandrp@gmail.com>
2015-09-05 12:36:54 +0300

    Add v2-aolradio

--- core/connectors.js
+++ core/connectors.js
@@ -242,7 +242,8 @@ define(function() {
 		{
 			label: 'AOL Radio',
 			matches: ['*://aolradio.slacker.com/*'],
-			js: ['connectors/aolradio.js']
+			js: ['connectors/v2/aolradio.js'],
+			version: 2
 		},
 
 		{
