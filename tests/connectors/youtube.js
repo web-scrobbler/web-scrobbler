eb74f871ab91aef3f231c76523280119bb432041
Jan Baykara <jan@baykara.co.uk>
2015-09-06 15:52:00 +0100

    Add Youtube and BBCRadioPlayer testing

--- tests/connectors/youtube.js
+++ tests/connectors/youtube.js
@@ -1,14 +1,11 @@
 module.exports = function(driver, connector, next) {
-	var url = 'http://www.bbc.co.uk/radio/player/bbc_6music';
 
+	var url = 'https://www.youtube.com/watch?v=YqeW9_5kURI';
 	before('should load '+url, function(done) {
 		siteSpec.shouldLoad(driver, url, done);
 	});
 	it('should load page: '+url, function(done) { done(); })
-
 	describe('Loaded website', function() {
-		connectorSpec.shouldRecogniseATrack(driver, {timeout: 120, optional: true});
+		connectorSpec.shouldRecogniseATrack(driver);
 	});
-
-	after(function() { next(); });
 };
--- tests/generic_test_components/connector.js
+++ tests/generic_test_components/connector.js
@@ -1,15 +1,34 @@
 var webdriver = require('selenium-webdriver');
 
-module.exports.shouldRecogniseATrack = function(driver, timeout, optional) {
-
-	it('should recognise a playing song', !optional ? function(done) {
+module.exports.shouldRecogniseATrack = function(driver, opts) {
+	var opts = opts || {};
+	it(opts.comment ? opts.comment : 'should recognise a playing song', !opts.optional ? function(done) {
 
 		helpers.listenFor(driver, 'connector_state_changed', function(res) {
 			return done();
 		}, function(res) {
 			return done(new Error('Connector did not send any track data to core :('));
-		}, timeout || 50);
+		}, opts.timeout || 50);
 
 	} : null);
 
+};
+
+module.exports.loadPlayListen = function(driver, next, url, btnSelector) {
+	before('should load '+url, function(done) {
+		siteSpec.shouldLoad(driver, url, done);
+	});
+
+	it('should load page: '+url, function(done) { done(); })
+
+	describe('Loaded website', function() {
+		before('Play a track', function() {
+			return thisPage.promiseClick(driver, {css: btnSelector});
+		})
+		it('should play a song', function(done) { done(); })
+
+		connectorSpec.shouldRecogniseATrack(driver);
+	});
+
+	after(function() { next(); });
 };
