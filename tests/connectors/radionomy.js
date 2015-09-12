de94afd9bd33eef2df087ac9d65f50597906f8f9
Jan Baykara <jan@baykara.co.uk>
2015-09-12 14:02:42 +0100

    Minor test tweaks, re-enable youtube playlist testing

--- tests/connectors/radionomy.js
+++ tests/connectors/radionomy.js
@@ -2,7 +2,8 @@ module.exports = function(driver, connector, next) {
 
 	connectorSpec.loadPlayListen(driver, next,
 		'https://www.radionomy.com/en/radio/foofightersfanloopradio/index',
-		'.radioPlayBtn'
+		'.radioPlayBtn',
+		{ timeout: 120 } // adverts
 	);
 
 };
--- tests/connectors/xiami.js
+++ tests/connectors/xiami.js
@@ -1,8 +1,17 @@
 module.exports = function(driver, connector, next) {
 
-	connectorSpec.loadPlayListen(driver, next,
-		'http://www.xiami.com/play',
-		'#J_nextBtn'
-	);
+	console.warn("            Xiami is not globally accessible - cannot reliably test for load");
+	next();
+
+	/* TODO:
+	 * "Not allowed in your country"
+	 - we need tests that can recognise stuff.
+	 - graceful failure, not outright. Try { } catch { }
+	*/
+
+	// connectorSpec.loadPlayListen(driver, next,
+	// 	'http://www.xiami.com/play',
+	// 	'#J_nextBtn'
+	// );
 
 };
