78f09bc25fd13f710ee3f6e8544dea6d7bbb20a5
Jan Baykara <jan@baykara.co.uk>
2015-09-12 11:47:28 +0100

    Add advert blocking to test browser, to reduce wait-times

--- tests/helpers/chromeSpoofing.js
+++ tests/helpers/chromeSpoofing.js
@@ -3,8 +3,10 @@ var chromedriver = require('selenium-webdriver/chrome');
 global.test = require('selenium-webdriver/testing');
 var chromeOptions = new chromedriver.Options();
 var extPath = '--load-extension=' + helpers.getPath(__dirname,'.././');
+var adblockPath = helpers.getPath(__filename, 'adblockplus.crx');
 console.log('Extension load path: ' + extPath);
 
+chromeOptions.addExtensions(adblockPath);
 chromeOptions.addArguments([extPath, '--log-level=0', '--test-type']);
 chromeOptions.setLoggingPrefs({browser: 'ALL'});
 
