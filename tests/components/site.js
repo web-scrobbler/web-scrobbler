3401ea9fd3bb71c5105ed54f85b823fba0905074
Jan Baykara <jan@baykara.co.uk>
2015-09-12 19:03:17 +0100

    Better validation of track data, highlight lack of trackArt & uniqueID

--- tests/components/site.js
+++ tests/components/site.js
@@ -1,12 +1,13 @@
 module.exports.shouldLoad = function(driver, url, done, loadOpts) {
 	var pageLoad = true;
+	var loadTimeout = loadOpts ? loadOpts.timeout : null;
 
 	if(url) {
-		/*#*/ console.log("Loading "+url);
+		// /*#*/ console.log("Loading "+url);
 		pageLoad = false;
-		helpers.getAndWait(driver, url, loadOpts.timeout)
+		helpers.getAndWait(driver, url, loadTimeout)
 		.then(function() {
-			console.log("LOADED "+url)
+			// console.log("LOADED "+url)
 			pageLoad = true;
 		})
 		.thenCatch(function(err) {
@@ -17,25 +18,25 @@ module.exports.shouldLoad = function(driver, url, done, loadOpts) {
 	driver.wait(function() { return pageLoad; })
 	.then(function() {
 		helpers.alertCheck(driver).then(function() {
-			console.log('Alert check done!\nStarting waitforload');
-			helpers.waitForLoad(driver, loadOpts.timeout)
+			// console.log('Alert check done!\nStarting waitforload');
+			helpers.waitForLoad(driver, loadTimeout)
 			.then(function() {
-				console.log('Wait for load done!\nInjecting test capture.');
+				// console.log('Wait for load done!\nInjecting test capture.');
 				helpers.injectTestCapture(driver).then(function() {
 					helpers.waitForExtensionLoad(driver, {count: 0})
 					.then(function(result) {
-						/*#*/ console.info('		Extension loaded!');
+						// /*#*/ console.info('		Extension loaded!');
 						//expect(result).to.be.true;
 						if(!result) return done(new Error('Extension load error!'));
 						// cb();
 						done();
 					}, function(err) {
-						/*#*/ console.warn('Extension error: ', err);
+						// /*#*/ console.warn('Extension error: ', err);
 						return done(err);
 					});
 				});
 			}, function(err) {
-				/*#*/ console.warn('Driver Timeout!', err);
+				// /*#*/ console.warn('Driver Timeout!', err);
 				return done(err);
 			});
 		});
