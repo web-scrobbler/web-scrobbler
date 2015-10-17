9c0dd8d59481cee93ee71867ba6954fc84029c89
Malachi Soord <inverse.chi@gmail.com>
2015-10-17 16:28:11 +0200

    Remove unused console.log

--- core/background/main.js
+++ core/background/main.js
@@ -309,7 +309,6 @@ require([
 		LibreFM.getSession(function(sessionID) {
 			if (sessionID !== 'undefined') {
 				scrobblerNotifications.get(function(data) {
-					console.log(data);
 					if (typeof data.librefm === 'undefined') {
 						Notifications.showAuthenticate(LibreFM.getAuthUrl.bind(LibreFM));
 						data.librefm = true;
