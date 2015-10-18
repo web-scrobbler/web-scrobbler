752bc3238612b627c9b40279c6917ded54ebac99
Malachi Soord <inverse.chi@gmail.com>
2015-10-18 20:14:32 +0200

    Added getunique function

--- connectors/v2/chorus.js
+++ connectors/v2/chorus.js
@@ -11,3 +11,7 @@ Connector.trackSelector = '.playing-song-title';
 Connector.isPlaying = function () {
 	return $('.player-play.player-button .fa.fa-pause').is(':visible');
 };
+
+Connector.getUniqueID = function () {
+	return $('.track-item.active.playing').prop('id');
+};
