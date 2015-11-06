55da4b144bdde2b78711462131d396203a4d8db9
Dmitry Pursanov <dpursanov@live.com>
2015-11-06 22:14:04 +0400

    Added v2 for music.microsoft.com (Groove Music) connector and turned it ON

---
+++ connectors/v2/groovemusic.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '#player';
+
+Connector.trackArtImageSelector = '#player .imgWrapper .img';
+
+Connector.artistSelector = '#player .secondaryMetadata a:first';
+
+Connector.trackSelector = '#player .primaryMetadata a:first';
+
+Connector.getAlbum = function() {
+	var album = $('#player .imgWrapper .img').attr('alt');
+	album = album.substr(album.indexOf("-")+1).trim();
+	return album || null;
+};
+
+Connector.currentTimeSelector = '.playerDurationText .playerDurationTextOnGoing';
+
+Connector.playButtonSelector = '#player button.iconPlayerPlay';
+
+Connector.isPlaying = function () {
+	return $("#player button.iconPlayerPlay").length === 0;
+};
+
