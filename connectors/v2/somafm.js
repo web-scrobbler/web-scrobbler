5c167271834ea0f84500662077e4aaa197df1525
Malachi Soord <inverse.chi@gmail.com>
2016-01-04 21:26:01 +0100

    Change somafm play button selector

--- connectors/v2/somafm.js
+++ connectors/v2/somafm.js
@@ -5,7 +5,4 @@
 Connector.playerSelector = '.player-container';
 Connector.artistSelector = '#list > div:nth-child(1) > div:nth-child(2) > p';
 Connector.trackSelector = '#list > div:nth-child(1) > div:nth-child(3) > p';
-
-Connector.isPlaying = function() {
-	return $('#playBtn').hasClass('ng-hide');
-};
+Connector.playButtonSelector = '#playBtn';
