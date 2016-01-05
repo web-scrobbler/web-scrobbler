3d13859ac3e14abc51278490657430cc538f6bd6
alexesprit <alex.esprit@gmail.com>
2016-01-06 00:22:45 +0300

    Fix Tradiio connector
    getTrack function fails because coverart is not displayed
    on Tradiio page anymore. Remove it.

--- connectors/v2/tradiio.js
+++ connectors/v2/tradiio.js
@@ -10,12 +10,6 @@ Connector.trackSelector = '.leftmusicplayer .musicname a:first';
 
 Connector.playButtonSelector = '.leftmusicplayer .section-controls .btn-play';
 
-Connector.getDuration = function() {
-	return Connector.stringToSeconds($('.leftmusicplayer .songDuration').text()) || null;
-};
-
 Connector.currentTimeSelector = '.leftmusicplayer .songDuration.s-progress';
 
-Connector.getTrackArt = function () {
-	return $('.leftmusicplayer .cover-small img').attr('src').replace('-square.jpg', '-normal.jpg');
-};
+Connector.durationSelector = '.leftmusicplayer .s-total';
