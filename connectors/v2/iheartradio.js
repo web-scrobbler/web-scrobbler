b1e23dec59ec72d07f8c28f75b0b092998bbecc8
Eric Cogan <ccrwas1elc@CCCNYC02ELC.local>
2016-01-12 10:53:37 -0500

    Support for fullscreen player

--- connectors/v2/iheartradio.js
+++ connectors/v2/iheartradio.js
@@ -1,8 +1,7 @@
 'use strict';
 
 /* global Connector */
-
-Connector.playerSelector = '.player-info';
+Connector.playerSelector = '.player-controls'; //playerSelector and isPlaying required
 Connector.playButtonSelector = '.player-controls .play';
 Connector.durationSelector = '.player-duration-duration';
 
@@ -15,23 +14,36 @@ Connector.isPlaying = function() {
 };
 
 Connector.getTrackArt = function() {
-	return 'http:' + $('.player-left .player-art img').attr('src');
+	if ($('body').attr('data-modal-name')) {
+		return 'https:' + $('.dialog-fullscreen-player-art').attr('src') || null;
+	} else {
+		return 'https:' + $('.player-left .player-art img').attr('src') || null;
+	}
 };
 
-Connector.getArtist = function() {
-	return $('.player-left .player-artist').text() ||
-		$('.player-left .player-artist').attr('title') || null;
+Connector.getArtist = function() { //grab artist and then check for fullscreen player and use page title if found
+	var artist = $('.player-left .player-artist').attr('title') || null;
+	if ($('body').attr('data-modal-name')) {
+		Connector.playerSelector = '.dialog-fullscreen-player-now-playing';
+		var artist = $('title').text().split('-');
+		return artist[1].trim();
+	}
+	return artist;
 };
 
-Connector.getTrack = function() {
+Connector.getTrack = function() { //if is fullscreen player fallback to track title in fullscreen player
 	return $('.player-left .player-song').text() ||
-		$('.player-left .player-song').attr('title') || null;
+		$('.dialog-fullscreen-player-now-playing .player-song').text() || null;
 };
 
 Connector.getUniqueID = function() {
-	return $('.player-left .player-song').attr('href');
+	return $('.player-left .player-song').attr('href') || $('.dialog-fullscreen-player-now-playing .type-secondary a').attr('href') || null;
 };
 
-Connector.getCurrentTime = function() {
-	return $('.player-duration-position').text();
+Connector.isStateChangeAllowed = function() { //this errors out
+	try {
+		return !Connector.getTrack().startsWith('Thanks for listening to');
+	} catch (e) {
+		console.log(e.message);
+	}
 };
