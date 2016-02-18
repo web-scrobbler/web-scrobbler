10c33d398fce00703c0662bf7e2d15c267f486a9
alexesprit <alex.esprit@gmail.com>
2016-02-18 21:07:02 +0300

    Use artistTrackSelector instead of functions

--- connectors/v2/dubtrack.js
+++ connectors/v2/dubtrack.js
@@ -6,16 +6,7 @@ Connector.playerSelector = '#player-controller';
 
 Connector.trackArtImageSelector = '.imgEl > img';
 
-
-Connector.getTrack = function() {
-	var res = /.*\s-\s(.*)/.exec($('.currentSong').text());
-	return  res[1];
-};
-
-Connector.getArtist = function () {
-	var res = /(.*)\s-\s/.exec($('.currentSong').text());
-	return res[1];
-};
+Connector.artistTrackSelector = '.currentSong';
 
 Connector.isPlaying = function () {
 	return $('.progressBg').width();
