32739de5064ed428cef4cf1207c82fbf2ae22857
Puthali H.B <Puthali.H.B@microfocus.com>
2016-03-06 13:45:35 -0800

    Fix jshint and linter errors.

--- connectors/v2/alexa.js
+++ connectors/v2/alexa.js
@@ -25,41 +25,41 @@
 })();
 
 var isPlayingLiveRadio = function() {
-  if($('#d-secondary-control-left .disabled').size() == 1 && $('#d-secondary-control-right .disabled').size() == 1) {
-    return 'true';
-  } else {
-    return 'false';
-  }
-}
+	if ($('#d-secondary-control-left .disabled').size() == 1 && $('#d-secondary-control-right .disabled').size() == 1) {
+		return 'true';
+	} else {
+		return 'false';
+	}
+};
 
 Connector.getArtist = function() {
-  if(isPlayingLiveRadio() === 'true') {
-    var songTitle =  $('.d-queue-info .song-title').text();
-    if(songTitle.indexOf("-") == -1) {
-      //Maybe ad or program, so ignore
-      return null;
-    }
-    var results = songTitle.split("-");
-    return results[0].trim();
-  }else {
-    return $('#d-info-text .d-sub-text-1').text();
-  }
+	if (isPlayingLiveRadio() === 'true') {
+		var songTitle =  $('.d-queue-info .song-title').text();
+		if (songTitle.indexOf('-') == -1) {
+			//Maybe ad or program, so ignore
+			return null;
+		}
+		var results = songTitle.split('-');
+		return results[0].trim();
+	} else {
+		return $('#d-info-text .d-sub-text-1').text();
+	}
 };
 
 Connector.getTrack = function() {
-  if(isPlayingLiveRadio() === 'true') {
-    var songTitle =  $('.d-queue-info .song-title').text();
-    if(songTitle.indexOf("-") == -1) {
-      //Maybe ad or program, so ignore
-      return null;
-    }
-    var results = songTitle.split("-");
-    return results[1].trim();
-  }else {
-    return $('#d-info-text .d-main-text').text();
-  }
-}
+	if (isPlayingLiveRadio() === 'true') {
+		var songTitle =  $('.d-queue-info .song-title').text();
+		if (songTitle.indexOf('-') == -1) {
+			//Maybe ad or program, so ignore
+			return null;
+		}
+		var results = songTitle.split('-');
+		return results[1].trim();
+	} else {
+		return $('#d-info-text .d-main-text').text();
+	}
+};
 
 Connector.isPlaying = function () {
-	return $('#d-primary-control .play').size() == 0;
+	return $('#d-primary-control .play').size() === 0;
 };
