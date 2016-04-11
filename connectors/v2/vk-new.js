555c1f16026d11a823f0ae8609bee34096bac03a
Andrey Zvorygin <umount.dev.brain@ya.ru>
2016-04-11 15:00:06 +0300

    Tab fix

--- connectors/v2/vk-new.js
+++ connectors/v2/vk-new.js
@@ -5,17 +5,17 @@
 Connector.playerSelector = '#top_audio_player';
 
 Connector.getArtist = function() {
-  return $('[data-is-current="1"]').data('performer');
+	return $('[data-is-current="1"]').data('performer');
 };
 
 Connector.getTrack = function() {
-  return $('[data-is-current="1"]').data('title');
+	return $('[data-is-current="1"]').data('title');
 };
 
 Connector.getDuration = function() {
-  return $('[data-is-current="1"]').data('duration');
+	return $('[data-is-current="1"]').data('duration');
 };
 
 Connector.isPlaying = function() {
-  return $('#top_audio_player').hasClass('top_audio_player_playing');
+	return $('#top_audio_player').hasClass('top_audio_player_playing');
 };
