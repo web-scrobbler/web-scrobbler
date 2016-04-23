d369cc4c4f788f3333d6d4cede6addbc1befbac7
Geraint White <geraintwhite@gmail.com>
2016-04-23 11:27:57 +0100

    Remove [genre] from the beginning of the title
    Fixes parsing of Monstercat video titles (example https://www.youtube.com/watch?v=8c0M7p49Acs)

--- connectors/v2/youtube.js
+++ connectors/v2/youtube.js
@@ -64,6 +64,8 @@ Connector.isStateChangeAllowed = function() {
 
 Connector.getArtistTrack = function () {
 	var text = $.trim($(Connector.artistTrackSelector).text());
+	
+	text = text.replace(/^\[[^\]]+\]\s*-*\s*/i, ''); // remove [genre] from the beginning of the title
 
 	var separator = Connector.findSeparator(text);
 
