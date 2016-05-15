beb85bdde740f65a01d216073195fbae207ba384
Andrey Zvorygin <umount.dev.brain@ya.ru>
2016-05-15 11:25:40 +0300

    New player params

--- connectors/v2/vk-new.js
+++ connectors/v2/vk-new.js
@@ -5,15 +5,15 @@
 Connector.playerSelector = '#top_audio_player';
 
 Connector.getArtist = function() {
-	return $('[data-is-current="1"]').data('performer');
+	return $('[data-is-current="1"]').data('audio')[4];
 };
 
 Connector.getTrack = function() {
-	return $('[data-is-current="1"]').data('title');
+	return $('[data-is-current="1"]').data('audio')[3];
 };
 
 Connector.getDuration = function() {
-	return $('[data-is-current="1"]').data('duration');
+	return $('[data-is-current="1"]').data('audio')[5];
 };
 
 Connector.isPlaying = function() {
