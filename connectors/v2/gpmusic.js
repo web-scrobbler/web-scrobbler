8737d0daa4fee3bb0efe4b1c83462950e02d38b9
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-05-31 02:52:05 +0300

    Fix #963

---
+++ connectors/v2/gpmusic.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '.app-layout';
+
+Connector.artistSelector = '.text .artist';
+
+Connector.trackSelector = '.text .title';
+
+Connector.playButtonSelector = '.controls .icon-play-outline';
