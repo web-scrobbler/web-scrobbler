a63e8ecca400793a9cbc6963b39621ccf05be0f5
Oğuzhan Türk <stkyoht@hotmail.com>
2016-07-27 12:18:29 +0300

    Update trtturku.js

--- connectors/v2/trtturku.js
+++ connectors/v2/trtturku.js
@@ -14,14 +14,8 @@ Connector.getArtist = function() {
   return toTitleCase($('#besa_mp3_play_area')[0].getElementsByTagName('table')[0].getElementsByTagName('td')[3].innerHTML.trim());
 }
 
-//Connector.artistSelector = '#now_playing .title_artist .a:first';
-
 Connector.getTrack = function() {
   return toTitleCase($('#besa_mp3_play_area')[0].getElementsByTagName('table')[0].getElementsByTagName('td')[5].innerHTML.trim());
 }
-
-//Connector.trackSelector = '#now_playing .title_artist .t:first';
-
-//Connector.albumSelector = '#now_playing .album .detail:first';
 
 Connector.playButtonSelector = '.jwplay';
