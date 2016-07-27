22455aaf4cec754f83bae99898b6c72957cbb354
Oğuzhan Türk <stkyoht@hotmail.com>
2016-07-27 12:25:32 +0300

    getCurrentTime added
    Additionally, tab characters has been updated.

--- connectors/v2/trtturku.js
+++ connectors/v2/trtturku.js
@@ -5,17 +5,17 @@
 
 function toTitleCase(str)
 {
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
 Connector.playerSelector = '.jwplayer';
 
 Connector.getArtist = function() {
-  return toTitleCase($('#besa_mp3_play_area')[0].getElementsByTagName('table')[0].getElementsByTagName('td')[3].innerHTML.trim());
+	return toTitleCase($('#besa_mp3_play_area')[0].getElementsByTagName('table')[0].getElementsByTagName('td')[3].innerHTML.trim());
 }
 
 Connector.getTrack = function() {
-  return toTitleCase($('#besa_mp3_play_area')[0].getElementsByTagName('table')[0].getElementsByTagName('td')[5].innerHTML.trim());
+	return toTitleCase($('#besa_mp3_play_area')[0].getElementsByTagName('table')[0].getElementsByTagName('td')[5].innerHTML.trim());
 }
 
 Connector.playButtonSelector = '.jwplay';
@@ -23,3 +23,7 @@ Connector.playButtonSelector = '.jwplay';
 Connector.isPlaying = function () {
 	return $('.jwplay.jwtoggle').length == 1;
 };
+
+Connector.getCurrentTime = function() {
+	return $('.jwgroup.jwleft')[0].children[3].innerHTML;
+}
