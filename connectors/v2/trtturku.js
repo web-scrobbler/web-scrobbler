8dee5b9de795623b7d91443e2db04ec3ae632ace
Oğuzhan Türk <stkyoht@hotmail.com>
2016-07-27 15:36:25 +0300

    Trailing spaces again

--- connectors/v2/trtturku.js
+++ connectors/v2/trtturku.js
@@ -7,13 +7,13 @@
 
 function toTitleCase(str) {
 	str = str.toLowerCase().split(' ');
-	
+
 	for(var i = 0; i < str.length; ++i){
 		str[i] = str[i].split('');
 		str[i][0] = str[i][0] == 'i' ? 'İ' : str[i][0].toUpperCase();
 		str[i] = str[i].join('');
 	}
-	
+
 	return str.join(' ');
 }
 
