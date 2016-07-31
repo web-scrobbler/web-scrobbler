0e44409f2a3a7adfcb251da5be984746768524be
Wojtek Witkowski <hi@wojtek.im>
2016-07-31 03:06:11 +0200

    Fix for mislabeled elements

--- connectors/v2/noonpacific.js
+++ connectors/v2/noonpacific.js
@@ -3,9 +3,6 @@
 /* global Connector */
 
 Connector.playerSelector = '.sidebar .audio-player';
-
-Connector.artistSelector = '.sidebar .audio-player__artist p';
-
-Connector.trackSelector = '.sidebar .audio-player__song p';
-
+Connector.artistSelector = '.sidebar .audio-player__song p'; // yeah, these are the other way around in the app...
+Connector.trackSelector = '.sidebar .audio-player__artist p'; // yeah, these are the other way around in the app...
 Connector.playButtonSelector = '.sidebar .audio-player__play .fa-play';
