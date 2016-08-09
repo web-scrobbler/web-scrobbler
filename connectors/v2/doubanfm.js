3b9f542503ac385312f007b68d76572adcb4ce4c
mao <myj226@gmail.com>
2016-08-09 13:50:12 +0800

    Fix lintspaces

--- connectors/v2/doubanfm.js
+++ connectors/v2/doubanfm.js
@@ -14,10 +14,10 @@ Connector.playButtonSelector = '.fr .icon-pause';
 
 Connector.getDuration = function () {
   var text = $('.fr .time').text();
-	return Connector.stringToSeconds(text.substring(1)) || null;
+  return Connector.stringToSeconds(text.substring(1)) || null;
 };
 
 Connector.isPlaying = function () {
-	var classString = $('.fr').children('svg').attr('class');
+  var classString = $('.fr').children('svg').attr('class');
   return /(^|\s)icon-pause(\s|$)/.test(classString);
 };
