1f9e48965dbe212ce45a744e9f6f723e9f2381a4
mao <myj226@gmail.com>
2016-08-09 14:01:43 +0800

    Fix lintspaces & remove playButtonSelector

--- connectors/v2/doubanfm.js
+++ connectors/v2/doubanfm.js
@@ -10,14 +10,12 @@ Connector.trackSelector =	'.fullplayer .titles .title a';
 
 Connector.artistSelector = '.fullplayer .link.artist-name';
 
-Connector.playButtonSelector = '.fr .icon-pause';
-
 Connector.getDuration = function () {
-  var text = $('.fr .time').text();
-  return Connector.stringToSeconds(text.substring(1)) || null;
+	var text = $('.fr .time').text();
+	return Connector.stringToSeconds(text.substring(1)) || null;
 };
 
 Connector.isPlaying = function () {
-  var classString = $('.fr').children('svg').attr('class');
-  return /(^|\s)icon-pause(\s|$)/.test(classString);
+	var classString = $('.fr').children('svg').attr('class');
+	return /(^|\s)icon-pause(\s|$)/.test(classString);
 };
