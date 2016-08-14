a8c99b74f478311f7d6837067c890e8ac8eb5656
alexesprit <alex.esprit@gmail.com>
2016-08-14 22:12:31 +0300

    Process current time and duration in Yandex connector

--- connectors/v2/yandex.js
+++ connectors/v2/yandex.js
@@ -8,6 +8,10 @@ Connector.artistSelector = '.track_type_player .track__artists';
 
 Connector.trackSelector = '.track_type_player .track__title';
 
+Connector.currentTimeSelector = '.progress__left';
+
+Connector.durationSelector = '.progress__right';
+
 Connector.isPlaying = function () {
 	return $('.player-controls__btn_play').hasClass('player-controls__btn_pause');
 };
