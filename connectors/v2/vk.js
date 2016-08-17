07eeffbecc133a2fff3d43715ad4ff13dbe5b77f
alexesprit <alex.esprit@gmail.com>
2016-08-17 18:17:14 +0300

    Decode HTML entities in VK connector

--- connectors/v2/vk.js
+++ connectors/v2/vk.js
@@ -8,16 +8,32 @@ function updateTrackInfo() {
 	trackInfo = JSON.parse(localStorage.getItem('audio_v10_track'));
 }
 
+/**
+ * Decodes HTML entities in given text string
+ * @param  {String} str String with HTML entities
+ * @return {String}     Decoded string
+ */
+var decodeHtmlEntity = function(str) {
+	if (str === null) {
+		return null;
+	}
+
+	return str.replace(/&#(\d+);/g, function(match, dec) {
+		return String.fromCharCode(dec);
+	});
+};
+
+
 Connector.artistTrackSelector = '.top_audio_player_title';
 Connector.playerSelector = '#top_audio_player';
 
 Connector.getArtist = function () {
 			updateTrackInfo();
-			return trackInfo[4] || null;
+			return decodeHtmlEntity(trackInfo[4]);
 		};
 Connector.getTrack = function () {
 			updateTrackInfo();
-			return trackInfo[3] || null;
+			return decodeHtmlEntity(trackInfo[3]);
 		};
 Connector.getCurrentTime = function () {
 			updateTrackInfo();
