b5cf0e160bc2f3980f13540f0d386119828dd969
alexesprit <alex.esprit@gmail.com>
2016-08-30 19:46:50 +0300

    Run tests one time each

--- tests/connectorsTests.js
+++ tests/connectorsTests.js
@@ -5,7 +5,6 @@ require('node-define');
 var fs = require('fs');
 var path = require('path');
 var driver = require('./helpers/chromeSpoofing').getDriver();
-var connectors = require('../core/connectors');
 
 var async = global.async = require('async');
 global.connectorSpec = require('./components/connector');
@@ -16,6 +15,23 @@ function getConnectorTestFilePath(connector) {
 	return path.join(__dirname, 'connectors', testFileName);
 }
 
+function getConnectorsList() {
+	var connectors = require('../core/connectors');
+	var uniqueConnectors = [];
+	var sortedConnectors = connectors.filter(function(item) {
+		var filePath = getConnectorTestFilePath(item);
+		if (uniqueConnectors.indexOf(filePath) != -1) {
+			return false;
+		}
+		uniqueConnectors.push(filePath);
+		return true;
+	}).sort(function(a, b) {
+		return a.label.localeCompare(b.label);
+	});
+
+	return sortedConnectors;
+}
+
 describe('Web-Scrobbler Extension', function() {
 	before(function(done) {
 		driver.sleep(1000).then(function() {
@@ -23,17 +39,14 @@ describe('Web-Scrobbler Extension', function() {
 		});
 	});
 
-	var sortedConnectors = connectors.slice(0);
-	sortedConnectors.sort(function(a, b) {
-		return a.label.localeCompare(b.label);
-	});
+	var connectors = getConnectorsList();
 
 	/**
 	 * Loop through all the connectors currently enabled,
 	 * look for explicitly defined tests first,
 	 * falling back to a generic test if none are found.
 	*/
-	async.each(sortedConnectors, function(connector, next) {
+	async.each(connectors, function(connector, next) {
 		describe('Connector: ' + connector.label, function() {
 			var connectorFilePath = getConnectorTestFilePath(connector);
 			if (fs.existsSync(connectorFilePath)) {
