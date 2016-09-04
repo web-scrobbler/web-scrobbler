196180dbdb91415f9a9bf8b344bbac54c01fc944
alexesprit <alex.esprit@gmail.com>
2016-09-04 10:16:17 +0300

    Describe messages sent to tests

--- core/content/testReporter.js
+++ core/content/testReporter.js
@@ -3,21 +3,26 @@
 /* exported testReporter */
 
 /**
- * Log messages to console with prepended message. Also dispatches a JS event
- * to interact with tests
- * @param msg {String} message to log
- * @param [obj] {Object} object to dump with message
- * @param [err] {Boolean} TRUE if the message is an error
-*/
+ * Dispatch a JS event to interact with tests
+ * @param  {String} msg Message to send to tests
+ * @param  {Object} obj Object to send to tests
+ * @param  {Boolean} err True if the message is an error
+ *
+ * Messages:
+ *  'connector_injected' - connector is injected
+ *  'connector_state_changed' - state of connector is changed
+ *     data: currentState
+ */
 var testReporter = function(msg, obj, err) {
-	if(msg) {
-		obj = obj || '';
-		if(err) {
-			console.error('WEB-SCROBBLER-ERROR: ' + msg, obj);
-			msg = 'ERROR: ' + msg;
-		} else {
-			console.log('WEB-SCROBBLER-INFO: ' + msg, obj);
-		}
-		document.dispatchEvent(new CustomEvent('web-scrobbler-test-response', {detail: {detail: msg, data: obj}}));
+	if (err) {
+		console.error('WEB-SCROBBLER-ERROR: ' + msg, obj);
+	} else {
+		console.log('WEB-SCROBBLER-INFO: ' + msg, obj);
 	}
+	document.dispatchEvent(new CustomEvent('web-scrobbler-test-response', {
+		detail: {
+			detail: msg,
+			data: obj
+		}}
+	));
 };
