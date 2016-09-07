920ffba805cbd290b0ff814da2a7a1e15e26af17
ViRGiL7 <virgil7g@gmail.com>
2016-09-07 15:14:16 +0400

    Basic Mail.ru Music support
    Bad spaces in title and album.

---
+++ connectors/v2/mailrumusic.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '.l-music__player';
+
+Connector.trackSelector = '.l-music__player__song__name'
+
+Connector.artistSelector = '.l-music__player__song__author.sp-music-booster';
+
+Connector.playButtonSelector = '.l-music__player.playing';
