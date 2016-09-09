a9176af6de8327ce877d12f6e76bb1e7340bb745
ViRGiL7 <virgil7g@gmail.com>
2016-09-09 11:18:50 +0400

    TrackArt getter override

--- connectors/v2/mailrumusic.js
+++ connectors/v2/mailrumusic.js
@@ -17,3 +17,13 @@ Connector.getArtist = function() {
 Connector.isPlaying = function () {
 	return $('.l-music__player').hasClass('playing');
 };
+
+Connector.getTrackArt = function () {
+	var coverArtURL = $('.l-music__player__song__cover').css('background-image');
+	var regexp = /https?%3A\S*jpg/i;
+	var coverArtURLClean = regexp.exec(coverArtURL);
+	var coverArtURLstring = coverArtURLClean.toString();
+	coverArtURLstring = coverArtURLstring.replace(/%3a/gi, ':');
+	coverArtURLstring = coverArtURLstring.replace(/%2f/gi, '/');
+	return coverArtURLstring;
+};
