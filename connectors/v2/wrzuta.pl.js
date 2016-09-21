184736b361739b09d9f02db8aef2f6f459f9c648
alexesprit <alex.esprit@gmail.com>
2016-09-21 09:03:14 +0300

    Fix WRZUTA connector

--- connectors/v2/wrzuta.pl.js
+++ connectors/v2/wrzuta.pl.js
@@ -2,10 +2,25 @@
 
 /* global Connector */
 
-Connector.playerSelector = '.npp-skin';
+Connector.playerSelector = '#content';
 
-Connector.artistTrackSelector = '.title';
+Connector.getArtistTrack = function() {
+	var text = $('.playlist-position.active a.js-file-link').attr('title');
+	var separator = this.findSeparator(text);
+
+	var artist = null;
+	var track = null;
+
+	if (separator !== null) {
+		artist = text.substr(0, separator.index);
+		track = text.substr(separator.index + separator.length);
+	}
+
+	return {artist: artist, track: track};
+};
+
+Connector.durationSelector = '.playlist-position.active .position-time';
 
 Connector.isPlaying = function () {
-	return $('.play').hasClass('playing');
+	return $('.npp-container').hasClass('playing');
 };
