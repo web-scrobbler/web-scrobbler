5bd97bfb27267cdfaca3b90811bd462d09891bf2
alexesprit <alex.esprit@gmail.com>
2016-10-02 10:50:24 +0300

    Implement `getUniqueID` for Yandex connector

--- connectors/v2/yandex.js
+++ connectors/v2/yandex.js
@@ -12,6 +12,14 @@ Connector.currentTimeSelector = '.progress__left';
 
 Connector.durationSelector = '.progress__right';
 
+Connector.getUniqueID = function() {
+	var trackUrl = $('.track_type_player .track__title').attr('href');
+	if (trackUrl) {
+		return trackUrl.split('/').pop();
+	}
+	return null;
+};
+
 Connector.isPlaying = function () {
 	return $('.player-controls__btn_play').hasClass('player-controls__btn_pause');
 };
