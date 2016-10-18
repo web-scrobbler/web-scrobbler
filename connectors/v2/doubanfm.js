5d0108c735eda2f66b1981f59990a380b6d6af8e
mao <myj226@gmail.com>
2016-10-18 16:01:41 +0800

    Update douban fm connector

--- connectors/v2/doubanfm.js
+++ connectors/v2/doubanfm.js
@@ -2,20 +2,40 @@
 
 /* global Connector */
 
-Connector.playerSelector = '.fullplayer';
+(function() {
+		var playerObserver = new MutationObserver(function() {
+		if (document.querySelector('.player-wrapper')) {
+			playerObserver.disconnect();
+			var actualObserver = new MutationObserver(Connector.onStateChanged);
+			actualObserver.observe(document.querySelector('.player-wrapper'), {
+				childList: true,
+				subtree: true,
+				attributes: true,
+				characterData: true
+			});
+		}
+	});
+
+	playerObserver.observe(document.body, {
+		childList: true,
+		subtree: true,
+		attributes: false,
+		characterData: false
+	});
+})();
 
 Connector.trackArtImageSelector = '.playing-cover img';
 
-Connector.trackSelector = '.fullplayer .titles .title a';
+Connector.trackSelector = '.player-wrapper .middle>div:nth-child(2) a';
 
-Connector.artistSelector = '.fullplayer .link.artist-name';
+Connector.artistSelector = '.player-wrapper .link.artist-name';
 
 Connector.getDuration = function () {
-	var text = $('.fr .time').text();
+	var text = $('.time').text();
 	return Connector.stringToSeconds(text.substring(1)) || null;
 };
 
 Connector.isPlaying = function () {
-	var classString = $('.fr').children('svg').attr('class');
+	var classString = $('.buttons .icon').eq(2).attr('class');
 	return /(^|\s)icon-pause(\s|$)/.test(classString);
 };
