dcf39735169df6b4694c4686659f23a766614dc6
alexesprit <alex.esprit@gmail.com>
2016-10-20 15:41:39 +0300

    Add track art to TuneIn connector

--- connectors/v2/tunein.js
+++ connectors/v2/tunein.js
@@ -6,6 +6,8 @@ Connector.playerSelector = '.container';
 
 Connector.artistTrackSelector = '.line1._navigateNowPlaying';
 
+Connector.trackArtImageSelector = '.album.logo';
+
 Connector.isPlaying = function () {
 	return $('#tuner').hasClass('playing');
 };
