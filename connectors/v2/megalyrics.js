cf42db326ed494c63a7caefc2015e071960b2b14
alexesprit <alex.esprit@gmail.com>
2016-10-21 08:06:46 +0300

    Add current time and duration to Megalyrics connector

--- connectors/v2/megalyrics.js
+++ connectors/v2/megalyrics.js
@@ -6,6 +6,13 @@ Connector.playerSelector = '#player';
 
 Connector.artistTrackSelector = '#pr_title';
 
+Connector.currentTimeSelector = '#pr_position';
+
+Connector.getDuration = function() {
+	var timeStr = $('#pr_time').text();
+	return timeStr.split('/')[1];
+};
+
 Connector.isPlaying = function () {
 	return $('#pr_play').hasClass('playing');
 };
