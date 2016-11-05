7ad81017bedbb4759df8bbf16f8ef51c594d59ca
alexesprit <alex.esprit@gmail.com>
2016-11-06 00:31:08 +0300

    Fix playing state recognition

--- connectors/v2/sullen-ural.js
+++ connectors/v2/sullen-ural.js
@@ -6,19 +6,22 @@ Connector.playerSelector = $('#tracks-async').length ? '#tracks-async' : ($('.ba
 
 Connector.getArtistTrack = function () {
 	var text = $('.play:contains("ll")').attr('download');
-	var separator = this.findSeparator(text);
+	if (text) {
+		var separator = this.findSeparator(text);
 
-	var artist = null;
-	var track = null;
+		var artist = null;
+		var track = null;
 
-	if (separator !== null) {
-		artist = text.substr(0, separator.index);
-		track = text.substr(separator.index + separator.length);
+		if (separator !== null) {
+			artist = text.substr(0, separator.index);
+			track = text.substr(separator.index + separator.length);
+		}
+
+		return {artist: artist, track: track};
 	}
-
-	return {artist: artist, track: track};
+	return {artist: null, track: null};
 };
 
 Connector.isPlaying = function () {
-	return $('.play:contains("ll")').length;
+	return $('.play:contains("ll")').length > 0;
 };
