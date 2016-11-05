d9f7edd951b7c69db4623ade722f71b751a65143
alexesprit <alex.esprit@gmail.com>
2016-11-05 20:28:13 +0300

    Use strict equality in Pitchfork connector

--- connectors/v2/pitchfork.js
+++ connectors/v2/pitchfork.js
@@ -7,7 +7,7 @@ Connector.artistSelector = '.track-title .artist';
 Connector.trackSelector = '.track-title .title';
 
 Connector.isPlaying = function () {
-	return $('.playback-button > div').attr('title') == 'Pause Track';
+	return $('.playback-button > div').attr('title') === 'Pause Track';
 };
 
 (function() {
