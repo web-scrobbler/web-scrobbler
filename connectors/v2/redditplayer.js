6e662be8cf52db89036bfb5875cf22af522ff1f4
alexesprit <alex.esprit@gmail.com>
2016-11-05 14:06:01 +0300

    Add current time/duration processing for Reddit Playlister

--- connectors/v2/redditplayer.js
+++ connectors/v2/redditplayer.js
@@ -2,6 +2,9 @@
 
 /* global Connector */
 
+var INFO_CURRENT_TIME = 1;
+var INFO_DURATION = 2;
+
 Connector.playerSelector = 'body';
 
 Connector.getArtistTrack = function () {
@@ -19,6 +22,27 @@ Connector.getArtistTrack = function () {
 	return {artist: artist, track: track};
 };
 
+Connector.getCurrentTime = function() {
+	return getTimeInfo(INFO_CURRENT_TIME);
+};
+
+Connector.getDuration = function() {
+	return getTimeInfo(INFO_DURATION);
+};
+
 Connector.isPlaying = function () {
 	return $('.playing').length > 0;
 };
+
+Connector.isStateChangeAllowed = function() {
+	return Connector.getCurrentTime() > 0;
+};
+
+function getTimeInfo(field) {
+	var pattern = /(.+)\s-\s(.+)/gi;
+	var songInfo = pattern.exec($('.elapsed').text());
+	if (songInfo) {
+		return  Connector.stringToSeconds(songInfo[field]);
+	}
+	return 0;
+}
