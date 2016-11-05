e9be537666c154e7d3b9ca1bd2537150a776752c
alexesprit <alex.esprit@gmail.com>
2016-11-05 14:00:19 +0300

    Fix formatting of Reddit Playlister connector

--- connectors/v2/redditplayer.js
+++ connectors/v2/redditplayer.js
@@ -5,19 +5,19 @@
 Connector.playerSelector = 'body';
 
 Connector.getArtistTrack = function () {
-		var text = $('#portablecontrols h3').text().replace(' [ buffering... ]', '');
-		var separator = this.findSeparator(text);
+	var text = $('#portablecontrols h3').text().replace(' [ buffering... ]', '');
+	var separator = this.findSeparator(text);
 
-		var artist = null;
-		var track = null;
+	var artist = null;
+	var track = null;
 
-		if (separator !== null) {
-			artist = text.substr(0, separator.index);
-			track = text.substr(separator.index + separator.length);
-		}
+	if (separator !== null) {
+		artist = text.substr(0, separator.index);
+		track = text.substr(separator.index + separator.length);
+	}
 
-		return {artist: artist, track: track};
-	};
+	return {artist: artist, track: track};
+};
 
 Connector.isPlaying = function () {
 	return $('.playing').length > 0;
