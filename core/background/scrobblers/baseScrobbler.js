4b27bfbc0d9f8bd4fb4c6d5c84836cf5bd381a5d
Malachi Soord <inverse.chi@gmail.com>
2016-11-10 22:21:24 +0100

    Fix logging and improve unbind

--- core/background/scrobblers/baseScrobbler.js
+++ core/background/scrobblers/baseScrobbler.js
@@ -155,11 +155,11 @@ define([
 					} else {
 						cb(response.session);
 					}
-				})
+				}.bind(this))
 				.fail(function (jqxhr, textStatus, error) {
 					console.error(this.label + ' auth.getSession failed: ' + error + ', ' + textStatus);
 					cb(null);
-				});
+				}.bind(this));
 		},
 
 		/**
