c3b8c637008032bd1b13af83adc2259e6bae7480
alexesprit <alex.esprit@gmail.com>
2016-11-10 18:50:47 +0300

    Remove redundant implementation of `getArtistTrack` function
    These connectors should use defauilt implementation.

--- connectors/v2/zen-audio-player.js
+++ connectors/v2/zen-audio-player.js
@@ -20,19 +20,4 @@ Connector.isPlaying = function() {
 	return $('.plyr').hasClass('plyr--playing');
 };
 
-Connector.getArtistTrack = function () {
-	var text = $(Connector.artistTrackSelector).text();
-
-	var separator = Connector.findSeparator(text);
-
-	if (separator === null || text.length === 0) {
-		return {artist: null, track: null};
-	}
-
-	var artist =  text.substr(0, separator.index);
-	var track = text.substr(separator.index + separator.length);
-
-	return {artist: artist, track: track};
-};
-
 Connector.filter = YoutubeFilter;
