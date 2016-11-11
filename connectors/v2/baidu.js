14d6dad3991c6a5f37cc801550bbdf4fc1475d75
alexesprit <alex.esprit@gmail.com>
2016-11-11 05:02:17 +0300

    Add current time and duration processing to Baidu

--- connectors/v2/baidu.js
+++ connectors/v2/baidu.js
@@ -8,6 +8,10 @@ Connector.artistSelector = '.artist';
 
 Connector.trackSelector = '.songname';
 
+Connector.currentTimeSelector = '.curTime';
+
+Connector.durationSelector = '.totalTime';
+
 Connector.isPlaying = function () {
 	return !$('.play').hasClass('stop');
 };
