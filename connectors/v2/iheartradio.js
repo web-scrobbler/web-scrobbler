770253f31de8675f61e490b8ba167a142afb5e00
alexesprit <alex.esprit@gmail.com>
2016-11-28 02:07:30 +0300

    Fix iHeartRadio connector

--- connectors/v2/iheartradio.js
+++ connectors/v2/iheartradio.js
@@ -1,38 +1,23 @@
 'use strict';
 
 /* global Connector */
-Connector.playerSelector = '.player-controls'; //playerSelector and isPlaying required
+
+Connector.playerSelector = '#player';
+
 Connector.playButtonSelector = '.player-controls .play';
-Connector.durationSelector = '.player-duration-duration';
+
+Connector.artistSelector = '.player-artist';
+
+Connector.trackSelector = '.player-song-text';
 
 Connector.isPlaying = function() {
-	return $('.player-controls .icon-play').length !== 1;
+	return $('.player-controls-center .play').hasClass('playing');
 };
 
-Connector.getTrackArt = function() {
-	if ($('body').attr('data-modal-name')) {
-		return 'https:' + $('.dialog-fullscreen-player-art').attr('src').split('?')[0] || null;
-	} else {
-		return 'https:' + $('.player-left .player-art img').attr('src').split('?')[0] || null;
-	}
-};
-
-Connector.getArtist = function() {
-	return $('.dialog-fullscreen-player-now-playing h2 a').text() || $('.player-left .player-artist').text() || null;
-};
-
-Connector.getTrack = function() {
-	return $('.dialog-fullscreen-player-now-playing .player-song').text() || $('.player-left .player-song').text() || null;
-};
-
-Connector.getUniqueID = function() {
-	return $('.player-left .player-song').attr('href') || $('.dialog-fullscreen-player-now-playing .type-secondary a').attr('href') || null;
-};
-
-Connector.isStateChangeAllowed = function() { //this errors on page load at first
-	try {
+Connector.isStateChangeAllowed = function() {
+	let track = Connector.getTrack();
+	if (track) {
 		return !Connector.getTrack().startsWith('Thanks for listening to');
-	} catch (e) {
-		console.log(e.message);
 	}
+	return false;
 };
