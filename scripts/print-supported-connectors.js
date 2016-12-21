3886d6711f132402ee3d5616d3a3e849c269c195
alexesprit <alex.esprit@gmail.com>
2016-12-21 09:23:41 +0300

    Add script that lists all supported connectors
    Used for easy update of list of supported connectors on Chrome Web store and AMO.

---
+++ scripts/print-supported-connectors.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('node-define');
+
+function printSupportedConnectors() {
+	let connectors = require('../core/connectors').sort(function(a, b) {
+		return a.label.localeCompare(b.label);
+	});
+	for (let connector of connectors) {
+		console.log(connector.label);
+	}
+}
+
+printSupportedConnectors();
