792bca508a737b12c098749a772bc2d237f67142
alexesprit <alex.esprit@gmail.com>
2016-12-22 12:02:45 +0300

    Add ability to edit song metadata

--- popups/info.js
+++ popups/info.js
@@ -3,34 +3,31 @@
 $(document).ready(function() {
 	console.log(chrome.extension.getBackgroundPage());
 
-	// trick to get current tab ID
-	chrome.tabs.query({
-		active: true,
-		lastFocusedWindow: true
-	}, function(tabs) {
-		var tab = tabs[0];
-		onTabReady(tab.id);
-	});
+	let isEditModeEnabled = false;
 
-	/**
-	* Tab ID is resolved callback
-	*/
-	function onTabReady(tabId) {
-		console.log('onTabReady(' + tabId + ')');
-		// ask background for current song info
-		chrome.runtime.sendMessage({
-			type: 'v2.getSong',
-			tabId: tabId
-		}, function(response) {
-			onSongLoaded(tabId, response);
+	function getCurrentTab() {
+		return new Promise((resolve) => {
+			chrome.tabs.query({
+				active: true,
+				lastFocusedWindow: true
+			}, function(tabs) {
+				resolve(tabs[0].id);
+			});
+		});
+	}
+
+	function sendMessageToCurrentTab(type, data) {
+		return getCurrentTab().then((tabId) => {
+			return new Promise((resolve) => {
+				chrome.runtime.sendMessage({type, data, tabId}, resolve);
+			});
 		});
 	}
 
 	/**
 	* Song data from background script are loaded
 	*/
-	function onSongLoaded(tabId, song) {
-		console.log('onSongLoaded(' + tabId + ', song)');
+	function onSongLoaded(song) {
 		console.log(song);
 		// no current song - should not happen, because page action with popup shows
 		// only when there is a song that can be corrected
@@ -38,6 +35,43 @@ $(document).ready(function() {
 			return;
 		}
 
+		configHeart(song.metadata.userloved);
+		configEditControls();
+		fillMetadataLabels(song);
+	}
+
+	function setEditMode(flag) {
+		isEditModeEnabled = flag;
+
+		$('#info').attr('data-hide', isEditModeEnabled);
+		$('#edit').attr('data-hide', !isEditModeEnabled);
+
+		$('#edit-link').text(isEditModeEnabled ? 'Submit' : 'Edit');
+	}
+
+	function updateMetadataLabels() {
+		let isSongMetadataChanged = false;
+
+		for (let field of ['artist', 'track', 'album']) {
+			let inputData = $(`#${field}-input`).val();
+			let labelText = $(`#${field}`).text();
+
+			if (!inputData) {
+				// Don't allow to submit empty results.
+				$(`#${field}-input`).val(labelText);
+				continue;
+			}
+
+			if (labelText !== inputData) {
+				$(`#${field}`).text(inputData);
+				isSongMetadataChanged = true;
+			}
+		}
+
+		return isSongMetadataChanged;
+	}
+
+	function fillMetadataLabels(song) {
 		let artist = song.processed.artist || song.parsed.artist;
 		let track = song.processed.track || song.parsed.track;
 		let album = song.processed.album || song.parsed.album;
@@ -52,23 +86,58 @@ $(document).ready(function() {
 		$('#album-art').css('background-image', `url("${albumArt}")`);
 		configHeart(song.metadata.userloved);
 
-		// UI listeners
-		$('#love').on('click', function() {
-			var currentLoveStatus = $('#love').attr('last-fm-loved') === 'true';
-			var desiredLoveStatus = !currentLoveStatus;
+		for (let field of ['artist', 'track', 'album']) {
+			$(`#${field}-input`).val($(`#${field}`).text());
+		}
+	}
 
-			chrome.runtime.sendMessage({
-				type: 'v2.toggleLove',
-				data: { shouldBeLoved: desiredLoveStatus },
-				tabId: tabId
-			}, function() {
-				configHeart(desiredLoveStatus);
-			});
+	function configEditControls() {
+		$('#edit-link').on('click', () => {
+			if (isEditModeEnabled) {
+				setEditMode(false);
+				correctSongInfo();
+			} else {
+				setEditMode(true);
+			}
+		});
+		$('#edit input').keypress((e) => {
+			let isEnterKey = e.keyCode === 13;
+			if (isEnterKey) {
+				setEditMode(false);
+				correctSongInfo();
+			}
 		});
 	}
 
 	function configHeart(userloved) {
 		$('#love').attr('last-fm-loved', userloved);
 		$('#love').attr('title', userloved ? 'unlove song' : 'love song');
+		// UI listeners
+		$('#love').on('click', function() {
+			var currentLoveStatus = $('#love').attr('last-fm-loved') === 'true';
+			var desiredLoveStatus = !currentLoveStatus;
+
+			sendMessageToCurrentTab('v2.toggleLove',  {
+				shouldBeLoved: desiredLoveStatus
+			}).then(() => {
+				configHeart(desiredLoveStatus);
+			});
+		});
 	}
+
+	function correctSongInfo() {
+		if (updateMetadataLabels()) {
+			sendMessageToCurrentTab('v2.correctSong', {
+				artist: $('#artist-input').val(),
+				track: $('#track-input').val(),
+				album: $('#album-input').val()
+			});
+		}
+	}
+
+	function main() {
+		sendMessageToCurrentTab('v2.getSong').then(onSongLoaded);
+	}
+
+	main();
 });
