9e86b7b6b1fc1485188d07f65538b879f21e3a4d
alexesprit <alex.esprit@gmail.com>
2016-12-26 00:20:24 +0300

    Add core test runner

--- tests/runner.js
+++ tests/runner.js
@@ -4,12 +4,27 @@ const path = require('path');
 const Mocha = require('mocha');
 const options = require('./helpers/options');
 
+function getTestMode() {
+	let args = process.argv.slice(2);
+	if (args.length === 1 && args[0] === 'core') {
+		return 'core';
+	}
+	return 'connectors';
+}
+
 function createMocha() {
 	var mocha = new Mocha({
 		timeout: 120000,
 		reporter: options.get('debug') ? 'spec' : 'tap'
 	});
-	mocha.addFile(path.join(__dirname, 'connectorsTests.js'));
+	switch (getTestMode()) {
+		case 'core':
+			mocha.addFile(path.join(__dirname, 'coreTests.js'));
+			break;
+		case 'connectors':
+			mocha.addFile(path.join(__dirname, 'connectorsTests.js'));
+			break;
+	}
 	return mocha;
 }
 
