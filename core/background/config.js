c93006d15d1f413bbac9f47f9dd1b5c550b46442
alexesprit <alex.esprit@gmail.com>
2016-12-25 20:16:31 +0300

    Move back initialization of default options values

--- core/background/config.js
+++ core/background/config.js
@@ -1,13 +1,34 @@
 'use strict';
 define(function() {
-	return {
-		/**
-		 * @param {String} label
-		 * @returns boolean
-		 */
-		isConnectorEnabled: function(label) {
-			var disabledArray = JSON.parse(localStorage.disabledConnectors);
-			return (disabledArray.indexOf(label) === -1);
+	/**
+	 * Setup default options values.
+	 * This function is called on module init.
+	 */
+	function setupDefaultConfigValues() {
+		// use notifications by default
+		if (typeof localStorage.useNotifications === 'undefined') {
+			localStorage.useNotifications = 1;
 		}
+
+		// no disabled connectors by default
+		if (typeof localStorage.disabledConnectors === 'undefined') {
+			localStorage.disabledConnectors = JSON.stringify([]);
+		}
+	}
+
+	/**
+	 * Check if connector is enabled.
+	 * @param  {stringify}  label Label of connector
+	 * @return {Boolean} True if connector is enabled; false otherwise
+	 */
+	function isConnectorEnabled(label) {
+		let disabledArray = JSON.parse(localStorage.disabledConnectors);
+		return (disabledArray.indexOf(label) === -1);
+	}
+
+	setupDefaultConfigValues();
+
+	return {
+		isConnectorEnabled
 	};
 });
