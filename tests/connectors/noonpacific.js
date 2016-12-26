9c7019dce2b52c13c15ec2ddedb7dc89e21fab8f
alexesprit <alex.esprit@gmail.com>
2016-12-26 14:21:02 +0300

    Extend Noonpacific test
    Add tests cases for collections.

--- tests/connectors/noonpacific.js
+++ tests/connectors/noonpacific.js
@@ -2,7 +2,19 @@
 
 module.exports = function(driver, connectorSpec) {
 	connectorSpec.shouldBehaveLikeMusicSite(driver, {
-		url: 'http://noonpacific.com/',
+		url: 'https://noonpacific.com/',
 		playButtonSelector: '.audio-player__play .fa-play'
 	});
+
+	connectorSpec.shouldBehaveLikeMusicSite(driver, {
+		url: 'https://collection.noonpacific.com/slowtide/#?track=Beachwood%20Sparks%20-%20%20Forget%20the%20Song',
+		playButtonSelector: '#player .mb2.butt-circle'
+	});
+
+	connectorSpec.shouldBehaveLikeMusicSite(driver, {
+		url: 'https://collection.noonpacific.com/spacejams/',
+		playButtonSelector: '.controls .play'
+	});
+
+
 };
