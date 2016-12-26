21472844d1a1368b471154100370e4ca547bef69
alexesprit <alex.esprit@gmail.com>
2016-12-26 21:26:34 +0300

    Beatport: Use MetadataFilter to modify track title

--- connectors/v2/beatport-www.js
+++ connectors/v2/beatport-www.js
@@ -1,19 +1,12 @@
 'use strict';
 
-/* global Connector */
+/* global Connector, MetadataFilter */
 
 Connector.playerSelector = '.player-container';
 
 Connector.artistSelector = '.player-track-name-artist-standard .track-artist';
 
-Connector.getTrack = function() {
-	var trackName = $('.player-track-name-artist-standard .primary-title').text();
-	var remixedBy = $('.player-track-name-artist-standard .remixed').text();
-	if (remixedBy === 'Original Mix') {
-		return trackName;
-	}
-	return trackName + ' (' + remixedBy + ')';
-};
+Connector.trackSelector = '.player-track-name-artist-standard .primary-title';
 
 Connector.getUniqueID = function() {
 	var trackUrl = $('.player-current-track-container a').attr('href');
@@ -24,3 +17,16 @@ Connector.getUniqueID = function() {
 Connector.isPlaying = function() {
 	return $('.player-controls .play-button').hasClass('pause');
 };
+
+Connector.filter = new MetadataFilter({
+	all: MetadataFilter.trim, track: removeOriginalMix
+});
+
+function removeOriginalMix(track) {
+	let remixedBy = $('.player-track-name-artist-standard .remixed').text();
+	if (remixedBy === 'Original Mix') {
+		return track;
+	}
+
+	return `${track} (${remixedBy})`;
+}
