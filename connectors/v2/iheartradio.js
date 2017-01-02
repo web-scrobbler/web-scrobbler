c96fb9472f9d73e2fb9716b0e93179dbdb8a965a
alexesprit <alex.esprit@gmail.com>
2017-01-02 09:54:07 +0300

    iHeartRadio: Fix playing state recognition

--- connectors/v2/iheartradio.js
+++ connectors/v2/iheartradio.js
@@ -4,15 +4,11 @@
 
 Connector.playerSelector = '#player';
 
-Connector.playButtonSelector = '.player-controls .play';
+Connector.playButtonSelector = '.player-center [aria-labelledby="play"]';
 
 Connector.artistSelector = '.player-artist';
 
 Connector.trackSelector = '.player-song-text';
-
-Connector.isPlaying = function() {
-	return $('.player-controls-center .play').hasClass('playing');
-};
 
 Connector.isStateChangeAllowed = function() {
 	let track = Connector.getTrack();
