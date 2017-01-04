b1c1fdd2ca6fdb9272a9ec20d2e5e78e505f8d97
alexesprit <alex.esprit@gmail.com>
2017-01-04 12:01:36 +0300

    Nightwave Plaza: Fix test

--- tests/connectors/nightwaveplaza.js
+++ tests/connectors/nightwaveplaza.js
@@ -2,6 +2,7 @@
 
 module.exports = function(driver, connectorSpec) {
 	connectorSpec.shouldBehaveLikeMusicSite(driver, {
-		url: 'https://plaza.one/'
+		url: 'https://plaza.one/',
+		playButtonSelector: '.player-play'
 	});
 };
