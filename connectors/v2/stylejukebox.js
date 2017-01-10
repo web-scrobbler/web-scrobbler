de1d3abb942a7e06c092d0d048593e84708f5def
Ali Essam <ali.essam.arafa@gmail.com>
2017-01-10 16:26:48 +0200

    Fix lint issues

--- connectors/v2/stylejukebox.js
+++ connectors/v2/stylejukebox.js
@@ -14,8 +14,8 @@ Connector.durationSelector = '.seeking-time > div:last-child';
 Connector.playerSelector = 'audio-player';
 
 Connector.getTrackArt = function () {
-  var src = $('.playerAlbumArt > img').attr('src');
-  return src.split('/').pop() === 'defaultAlbum.jpg' ? null : src;
+	var src = $('.playerAlbumArt > img').attr('src');
+	return src.split('/').pop() === 'defaultAlbum.jpg' ? null : src;
 };
 
 Connector.isPlaying = function () {
