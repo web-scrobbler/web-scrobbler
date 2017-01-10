b85b3f6ab37c7e5e2cbd3701fede031c417b172c
Ali Essam <ali.essam.arafa@gmail.com>
2017-01-10 16:22:36 +0200

    Ignore stylejukebox default album cover

--- connectors/v2/stylejukebox.js
+++ connectors/v2/stylejukebox.js
@@ -13,7 +13,10 @@ Connector.durationSelector = '.seeking-time > div:last-child';
 
 Connector.playerSelector = 'audio-player';
 
-Connector.trackArtImageSelector = '.playerAlbumArt > img';
+Connector.getTrackArt = function () {
+  var src = $('.playerAlbumArt > img').attr('src');
+  return src.split('/').pop() === 'defaultAlbum.jpg' ? null : src;
+};
 
 Connector.isPlaying = function () {
 	// Check pause button visibility instead of play button invisibility,
