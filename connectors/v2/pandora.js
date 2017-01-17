188391aa809ae3a413067e7572a6cb4c6f7c1b1e
alexesprit <alex.esprit@gmail.com>
2017-01-17 06:25:51 +0300

    Pandora: Add support for new website
    Resolved #1068.
    Resolves #1194.

--- connectors/v2/pandora.js
+++ connectors/v2/pandora.js
@@ -2,33 +2,68 @@
 
 /* global Connector */
 
-Connector.playerSelector = '#playbackControl';
+function setupNewPandoraPlayer() {
+	Connector.playerSelector = '.region-bottomBar';
 
-Connector.trackArtImageSelector = '.playerBarArt';
+	Connector.artistSelector = '.Tuner__Audio__TrackDetail__artist';
 
-Connector.albumSelector = 'a.playerBarAlbum';
+	Connector.trackSelector = '.Tuner__Audio__TrackDetail__title';
 
-Connector.artistSelector = 'a.playerBarArtist';
+	Connector.durationSelector = 'span[data-qa="remaining_time"]';
 
-Connector.trackSelector = 'a.playerBarSong';
+	Connector.trackArtImageSelector = '.Tuner__Audio__TrackDetail__img img';
 
-Connector.playButtonSelector = 'div.playButton';
-
-Connector.getDuration = function () {
-	return getElapsedTime() + getRemainingTime();
-};
-
-Connector.isStateChangeAllowed = function() {
-	return getElapsedTime() > 0;
-};
-
-function getElapsedTime() {
-	let timeStr = $('div.elapsedTime').text();
-	return Connector.stringToSeconds(timeStr);
+	Connector.isPlaying = function() {
+		// #ic_play_7a43ed1f23aeda3fadb7bdaa3bb07cbe
+		// #ic_pause_829b1bd4f7297ce64c54560a4a499300
+		let playButtonHref = $('.PlayButton use').attr('xlink:href');
+		return playButtonHref.startsWith('#ic_pause');
+	};
 }
 
-function getRemainingTime() {
-	// Remove 'minus' sign
-	let timeStr = $('div.remainingTime').text().substring(1);
-	return Connector.stringToSeconds(timeStr);
+function setupPandoraPlayer() {
+	Connector.playerSelector = '#playbackControl';
+
+	Connector.trackArtImageSelector = '.playerBarArt';
+
+	Connector.albumSelector = 'a.playerBarAlbum';
+
+	Connector.artistSelector = 'a.playerBarArtist';
+
+	Connector.trackSelector = 'a.playerBarSong';
+
+	Connector.playButtonSelector = 'div.playButton';
+
+	Connector.isStateChangeAllowed = function() {
+		return getElapsedTime() > 0;
+	};
+
+	Connector.getDuration = function () {
+		return getElapsedTime() + getRemainingTime();
+	};
+
+	function getElapsedTime() {
+		let timeStr = $('div.elapsedTime').text();
+		return Connector.stringToSeconds(timeStr);
+	}
+
+	function getRemainingTime() {
+		// Remove 'minus' sign
+		let timeStr = $('div.remainingTime').text().substring(1);
+		return Connector.stringToSeconds(timeStr);
+	}
 }
+
+function isNewPandoraPlayer() {
+	return $('#playbackControl').length === 0;
+}
+
+function setupConnector() {
+	if (isNewPandoraPlayer()) {
+		setupNewPandoraPlayer();
+	} else {
+		setupPandoraPlayer();
+	}
+}
+
+setupConnector();
