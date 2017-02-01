ddebf8a57bcfc9d5f237d228ae2d55018e3cf248
reubot <reuben_p@yahoo.com>
2017-02-01 14:36:36 +1000

    freegal.com library service
    initial work on freegal.com library service

---
+++ connectors/v2/freegal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '#custom-jw-player-wrapper';
+
+Connector.artistTrackSelector = '#jw-player-text';
+
+Connector.playButtonSelector = '#jw-play-button';
