b1700490709bdfd8a625e436182a0aa8e7ce6b6e
Michal Čaplygin <michal.caplygin@etnetera.cz>
2017-04-04 11:18:29 +0200

    playButtonSelector, broader playerSelector

--- connectors/v2/spotify-open.js
+++ connectors/v2/spotify-open.js
@@ -6,7 +6,7 @@
  * The connector for new version of Spotify (open.spotify.com).
  */
 
-Connector.playerSelector = '.now-playing-bar';
+Connector.playerSelector = '.nowPlayingBar-container';
 
 Connector.getArtist = function() {
 	let artists = $('.track-info__artists a').toArray();
@@ -17,8 +17,8 @@ Connector.trackSelector = '.track-info__name a';
 
 Connector.trackArtImageSelector = '.now-playing__cover-art .cover-art-image-loaded';
 
-Connector.playButtonSelector = '.control-button[class^="spoticon-pause-"]';
+Connector.playButtonSelector = '.control-button[class*="spoticon-play-"]';
 
-Connector.currentTimeSelector = '.player-controls__progress-bar > div:first-child';
+Connector.currentTimeSelector = '.player-controls__progress-time:first-child';
 
-Connector.durationSelector = '.player-controls__progress-bar > div:last-child';
+Connector.durationSelector = '.player-controls__progress-time:last-child';
