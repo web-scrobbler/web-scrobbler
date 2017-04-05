12d64a075bf59528330b7d03fa5b891d3cb74133
alexesprit <alex.esprit@gmail.com>
2017-04-05 07:18:32 +0300

    Improve Kollekt.fm test

--- tests/connectors/kollekt.js
+++ tests/connectors/kollekt.js
@@ -1,8 +1,12 @@
 'use strict';
 
-module.exports = function(driver, connectorSpec) {
-	// Auth is required
-	connectorSpec.shouldLoadWebsite(driver, {
+module.exports = (driver, spec) => {
+	spec.shouldLoadWebsite(driver, {
 		url: 'https://kollekt.fm/'
+	});
+
+	spec.shouldBehaveLikeMusicSite(driver, {
+		url: 'https://community.kollekt.fm/playlist/16885',
+		playButtonSelector: '.fa-play'
 	});
 };
