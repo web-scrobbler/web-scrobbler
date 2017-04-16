b8924b9a7a3a792a00921171349229969455b2b9
alexesprit <alex.esprit@gmail.com>
2017-04-16 12:30:03 +0300

    Fix 163 Music connector
    Closes #1284.

--- connectors/163music.js
+++ connectors/163music.js
@@ -2,28 +2,24 @@
 
 /* global Connector */
 
+const trackRegEx = new RegExp(String.fromCharCode(160), 'g');
+
 Connector.playerSelector = '.m-playbar';
 
 Connector.trackArtImageSelector = '.head.j-flag img';
 
 Connector.getTrack = function() {
-	var track = $('.fc1').text();
-	var re = new RegExp(String.fromCharCode(160), 'g');
-	return track.replace(re, ' ');
+	let track = $('.fc1').text();
+	return track.replace(trackRegEx, ' ');
 };
 
 Connector.getArtist = function () {
-	return $('.by').children('span').attr('title') || null;
+	return $('.by span').attr('title');
 };
 
 Connector.playButtonSelector = '.btns .ply';
 
-Connector.currentTimeSelector = '.j-flag.time em';
-
-Connector.getDuration = function () {
-	let timeStr = $('#g_player .time').text();
-	return Connector.stringToSeconds(timeStr.substr(timeStr.length - 4));
-};
+Connector.timeInfoSelector = '.j-flag.time';
 
 Connector.isPlaying = function () {
 	return $('.btns .ply').hasClass('pas');
