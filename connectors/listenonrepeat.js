cd0d1a10c4d2a1134fc430b4fa6eb0530cdc49c8
Malachi Soord <inverse.chi@gmail.com>
2017-04-26 21:46:21 +0200

    Fix

--- connectors/listenonrepeat.js
+++ connectors/listenonrepeat.js
@@ -1,12 +1,19 @@
 'use strict';
 
-/* global Connector */
+/* global Connector Util */
 
 Connector.playerSelector = '.player-controls';
 
-Connector.artistTrackSelector = '.player-controls .video-title';
+Connector.artistTrackSelector = '.player-controls > .video-title:first';
 
 Connector.playButtonSelector = '.controls > button:nth-child(4)';
+
+Connector.getArtistTrack = function () {
+	var text = $(Connector.artistTrackSelector).text();
+
+	console.log(text);
+	return Util.processYoutubeVideoTitle(text);
+};
 
 Connector.isPlaying = function() {
 	return $('button:contains(Pause Video)').length > 0;
