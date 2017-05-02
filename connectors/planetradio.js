0265157916966c2cedc27c0645d0febe04180835
argymeg <argymeg@gmail.com>
2017-05-02 19:36:16 +0100

    Added planetradio.co.uk connector

---
+++ connectors/planetradio.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '#app';
+
+Connector.artistSelector = '.text-right > span:nth-child(2)';
+
+Connector.trackSelector = '.text-right > span:nth-child(3)';
+
+Connector.trackArtImageSelector = 'div.now-playing-block:nth-child(3) > div:nth-child(1)';
+
+Connector.playButtonSelector = '.play';
