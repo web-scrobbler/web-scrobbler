bff0886e30d36f36ad4eeaed3411731d8b67366c
argymeg <argymeg@gmail.com>
2017-05-04 21:44:55 +0100

    Added roxx.gr connector

---
+++ connectors/roxx.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '#rj-player';
+
+Connector.artistSelector = '#trackInfo .artist';
+
+Connector.trackSelector = '#trackInfo .track';
+
+Connector.playButtonSelector = '.jp-play';
