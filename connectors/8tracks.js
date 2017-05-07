df5ddfbd712df11d35c201c4d4497cff44e430b0
alexesprit <alex.esprit@gmail.com>
2017-05-07 22:42:09 +0300

    8tracks: update current state on page navigation

--- connectors/8tracks.js
+++ connectors/8tracks.js
@@ -2,10 +2,8 @@
 
 /* global Connector */
 
-
 if ($('#play_on_youtube')) {
-
-	Connector.playerSelector = '#play_area';
+	Connector.playerSelector = '#main';
 
 	Connector.artistSelector = '#now_playing .title_artist .a';
 
@@ -16,9 +14,7 @@ if ($('#play_on_youtube')) {
 	Connector.isPlaying = function() {
 		return $('#mix_youtube').hasClass('playing');
 	};
-
 } else {
-
 	Connector.playerSelector = '#player';
 
 	Connector.artistSelector = '#now_playing .title_artist .a:first';
@@ -28,11 +24,15 @@ if ($('#play_on_youtube')) {
 	Connector.albumSelector = '#now_playing .album .detail:first';
 
 	Connector.playButtonSelector = '#player_play_button';
-
 }
 
 Connector.trackArtImageSelector = '#player_mix img';
 
 Connector.getUniqueID = function() {
-	return $('.track_details').attr('id').replace('track_details_', '');
+	let trackId = $('.track_details').attr('id');
+	if (trackId) {
+		return trackId.replace('track_details_', '');
+	}
+
+	return null;
 };
