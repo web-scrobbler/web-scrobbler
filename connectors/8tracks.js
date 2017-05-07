2886aa8d0ec444c867520432f4fcb1ce47d215b3
alexesprit <alex.esprit@gmail.com>
2017-05-07 23:09:08 +0300

    8tracks: recognize website types correctly

--- connectors/8tracks.js
+++ connectors/8tracks.js
@@ -2,37 +2,50 @@
 
 /* global Connector */
 
-if ($('#play_on_youtube')) {
+function setupConnector() {
+	setupCommonProperties();
+	if (isYoutubePlayer()) {
+		setupYoutubePlayer();
+	} else {
+		setup8tracksPlayer();
+	}
+}
+
+function isYoutubePlayer() {
+	return $('body').hasClass('international');
+}
+
+function setupYoutubePlayer() {
 	Connector.playerSelector = '#main';
 
+	Connector.isPlaying = function() {
+		return $('#mix_youtube').hasClass('playing');
+	};
+}
+
+function setup8tracksPlayer() {
+	Connector.playerSelector = '#player';
+
+	Connector.playButtonSelector = '#player_play_button';
+}
+
+function setupCommonProperties() {
 	Connector.artistSelector = '#now_playing .title_artist .a';
 
 	Connector.trackSelector = '#now_playing .title_artist .t';
 
 	Connector.albumSelector = '#now_playing .album .detail';
 
-	Connector.isPlaying = function() {
-		return $('#mix_youtube').hasClass('playing');
+	Connector.trackArtImageSelector = '#player_mix img';
+
+	Connector.getUniqueID = function() {
+		let trackId = $('.track_details').attr('id');
+		if (trackId) {
+			return trackId.replace('track_details_', '');
+		}
+
+		return null;
 	};
-} else {
-	Connector.playerSelector = '#player';
-
-	Connector.artistSelector = '#now_playing .title_artist .a:first';
-
-	Connector.trackSelector = '#now_playing .title_artist .t:first';
-
-	Connector.albumSelector = '#now_playing .album .detail:first';
-
-	Connector.playButtonSelector = '#player_play_button';
 }
 
-Connector.trackArtImageSelector = '#player_mix img';
-
-Connector.getUniqueID = function() {
-	let trackId = $('.track_details').attr('id');
-	if (trackId) {
-		return trackId.replace('track_details_', '');
-	}
-
-	return null;
-};
+setupConnector();
