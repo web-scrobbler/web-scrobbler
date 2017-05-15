93cdf363ec894ff42e62d7ce260c2d3331f77cb5
alexesprit <alex.esprit@gmail.com>
2017-05-15 20:18:25 +0300

    RadioTunes: fix playing state recognition
    Closes #1314.

--- connectors/radiotunes.js
+++ connectors/radiotunes.js
@@ -6,6 +6,4 @@ Connector.playerSelector = '#row-player-controls';
 
 Connector.artistTrackSelector = '.title-container .title';
 
-Connector.isPlaying = function () {
-	return $('#ctl-play .icon').hasClass('icon-stop');
-};
+Connector.playButtonSelector = '.icon-play';
