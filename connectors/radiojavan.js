2ff43752078e15833c8e3936fc1a74970d5511a1
Farhad Ahmadi <ahm.farhad@gmail.com>
2017-06-03 14:30:48 +0800

    Add Radiojavan support
    radiojavan.com is a free and popular online Persian music player.

---
+++ connectors/radiojavan.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* global Connector */
+
+Connector.playerSelector = '#player_controls';
+
+Connector.artistTrackSelector = '#song_info_panel span.song_name';
+
+Connector.artistSelector = '#song_info_panel span.artist_name';
+
+Connector.trackSelector = '#song_info_panel span.song_name';
+
+Connector.currentTimeSelector = '#mp3_position';
+
+Connector.durationSelector = '#mp3_duration';
+
+Connector.playButtonSelector = '#mp3_play .fa-play';
+
+Connector.albumSelector = '#album_panel .hide-for-small-only .album_name';
+
+Connector.trackArtImageSelector = '#artwork_panel img';
