add19fc2497173bdf57b37753db75e19ffe75aa4
alexesprit <alex.esprit@gmail.com>
2017-06-12 17:52:52 +0300

    Megalyrics: use 'timeInfoSelector'

--- connectors/megalyrics.js
+++ connectors/megalyrics.js
@@ -6,12 +6,7 @@ Connector.playerSelector = '#player';
 
 Connector.artistTrackSelector = '#pr_title';
 
-Connector.currentTimeSelector = '#pr_position';
-
-Connector.getDuration = function() {
-	var timeStr = $('#pr_time').text();
-	return timeStr.split('/')[1];
-};
+Connector.timeInfoSelector = '#pr_time';
 
 Connector.isPlaying = function () {
 	return $('#pr_play').hasClass('playing');
