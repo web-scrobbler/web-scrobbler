b1c14d1909de875cb8666835a7b97cf5d7a131dd
alexesprit <alex.esprit@gmail.com>
2017-07-16 11:46:02 +0300

    Obozrevatel: add support for new player

--- connectors/obozrevatel.js
+++ connectors/obozrevatel.js
@@ -9,6 +9,8 @@ function setupConnector() {
 		setupRadioPlayer();
 	} else if (isMusicPlayer()) {
 		setupMusicPlayer();
+	} else if (isNewMusicPlayer()) {
+		setupNewMusicPlayer();
 	}
 }
 
@@ -16,8 +18,28 @@ function isRadioPlayer() {
 	return $('.cur-blk').length > 0;
 }
 
+function isNewMusicPlayer() {
+	return $('#file_0').length > 0;
+}
+
 function isMusicPlayer() {
 	return $('.cnt-song-lst').length > 0;
+}
+
+function setupNewMusicPlayer() {
+	Connector.playerSelector = '#file_0';
+
+	Connector.isPlaying = () => $('#file_0').hasClass('jp-state-playing');
+
+	Connector.artistSelector = '#file_0 .name a:first';
+
+	Connector.trackSelector = '#file_0 .song-name';
+
+	Connector.getUniqueID = () => $('#file_0 .song-name').attr('href');
+
+	Connector.currentTimeSelector = '.jp-current-time';
+
+	Connector.durationSelector = '.jp-duration';
 }
 
 function setupMusicPlayer() {
