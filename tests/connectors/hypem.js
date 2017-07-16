cfc4a3995007cbf9891f6b0b324d663957579040
alexesprit <alex.esprit@gmail.com>
2017-07-16 15:38:06 +0300

    Update Hype Machine test

--- tests/connectors/hypem.js
+++ tests/connectors/hypem.js
@@ -5,4 +5,8 @@ module.exports = function(driver, connectorSpec) {
 		url: 'http://hypem.com/artist/Violet+Days+x+Win+and+Woo',
 		playButtonSelector: '.tools .playdiv'
 	});
+	connectorSpec.shouldBehaveLikeMusicSite(driver, {
+		url: 'http://hypem.com/premiere/the+stevens',
+		playButtonSelector: '.icon-play'
+	});
 };
