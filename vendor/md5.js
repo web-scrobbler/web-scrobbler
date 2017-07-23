f0809c00b0e1b35a6eb0258f15c494892c2448f5
alexesprit <alex.esprit@gmail.com>
2017-07-23 12:19:37 +0300

    Calculate MD5 hash properly
    MD5 module encodes an input string to UTF-8 incorrectly, e.g. when an
    emoji char is in the input string. Use `encodeURIComponent` and
    `unescape` function to convert the string to UTF-8 properly.
    
    Closes #1358.

--- vendor/md5.js
+++ vendor/md5.js
@@ -6,7 +6,7 @@
 **/
 define(function() {
 	return function (string) {
- 
+
 		function RotateLeft(lValue, iShiftBits) {
 			return (lValue<<iShiftBits) | (lValue>>>(32-iShiftBits));
 		}
@@ -91,29 +91,7 @@ define(function() {
 		};
 
 		function Utf8Encode(string) {
-			string = string.replace(/\r\n/g,"\n");
-			var utftext = "";
-
-			for (var n = 0; n < string.length; n++) {
-
-				var c = string.charCodeAt(n);
-
-				if (c < 128) {
-					utftext += String.fromCharCode(c);
-				}
-				else if((c > 127) && (c < 2048)) {
-					utftext += String.fromCharCode((c >> 6) | 192);
-					utftext += String.fromCharCode((c & 63) | 128);
-				}
-				else {
-					utftext += String.fromCharCode((c >> 12) | 224);
-					utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-					utftext += String.fromCharCode((c & 63) | 128);
-				}
-
-			}
-
-			return utftext;
+			return unescape(encodeURIComponent(string));
 		};
 
 		var x=Array();
