032a599ba86f5604bb69f915f4c1eb5a30b86dba
alexesprit <alex.esprit@gmail.com>
2017-07-23 18:38:35 +0300

    Update Deezer test

--- tests/connectors/deezer.js
+++ tests/connectors/deezer.js
@@ -2,8 +2,7 @@
 
 module.exports = function(driver, connectorSpec) {
 	// Auth is required
-	connectorSpec.shouldBehaveLikeMusicSite(driver, {
-		url: 'http://www.deezer.com/artist/1',
-		playButtonSelector: '.list-actions .btn-play'
+	connectorSpec.shouldLoadWebsite(driver, {
+		url: 'http://www.deezer.com/artist/1'
 	});
 };
