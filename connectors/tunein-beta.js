fba8341ef712dd0aedf414c6697417b25a7383b2
alexesprit <alex.esprit@gmail.com>
2017-08-07 00:04:28 +0300

    Fix ESLint issues

--- connectors/tunein-beta.js
+++ connectors/tunein-beta.js
@@ -1,11 +1,9 @@
 'use strict';
 
-/* global Connector */
-
 Connector.playerSelector = '.player__playerContainer___JEJ2U';
+
 Connector.artistTrackSelector = '#playerTitle';
+
 Connector.trackArtSelector = '#playerArtwork';
 
-Connector.isPlaying = function () {
-	return $('#playerActionButton').hasClass('playing');
-};
+Connector.isPlaying = () => $('#playerActionButton').hasClass('playing');
