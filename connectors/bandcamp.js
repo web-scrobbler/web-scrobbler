9478c1dcd2259088e934e75fbce9699972f67796
alexesprit <alex.esprit@gmail.com>
2017-08-15 22:29:04 +0300

    Bandcamp: look for separators surrounded by space symbols
    Closes #676.

--- connectors/bandcamp.js
+++ connectors/bandcamp.js
@@ -7,6 +7,8 @@
 // wire audio element to fire state changes
 $('audio').bind('playing pause timeupdate', Connector.onStateChanged);
 
+const separators = [' - ', ' | '];
+
 function getArtist() {
 	let artist = $('.detail_item_link_2').text() ||
 				$('span[itemprop=byArtist]').text() ||
@@ -23,40 +25,38 @@ function getTrack() {
 				$('.trackTitle').first().text() ||
 				$('.collection-item-container.playing').find('.fav-track-static').first().text() ||
 				$('.waypoint-item-title').text() ||
-				$('.track_info .title') ||
-				null;
+				$('.track_info .title');
 	return track;
 }
 
-function artistIsletious() {
-	// album page: true if all tracks contain a hyphen or vertical bar (pipe symbol)
-	// example of pipe usage: http://tigersmilkrecords.bandcamp.com/album/peru-maravilloso-vintage-latin-tropical-cumbia
+function isArtistLetious() {
+	// Album page: true if all tracks contain a hyphen or vertical bar.
+	// Example: http://tigersmilkrecords.bandcamp.com/album/peru-maravilloso-vintage-latin-tropical-cumbia
 	if ($('meta[property="og:type"]').attr('content') === 'album') {
 		let allDashed = true;
 		$('.track_list span[itemprop="name"]').each(function() {
-			if (!/\||-/.test($(this).text())) {
+			if (!Util.findSeparator($(this).text(), separators)) {
 				allDashed = false;
 				return false;
 			}
 		});
 		return allDashed;
 	}
-	// discover & song pages: true if artist name is 'letious' or 'letious Artists'
-	// and track contains a hyphen or vertical bar.
+
+	// Discover & song pages: true if artist name is 'letious' or
+	// 'letious Artists' and track contains a hyphen or vertical bar.
 	// Also takes into account misspelling of letious as letios
-	//    http://krefeld8ung.bandcamp.com/album/krefeld-8ung-vol-1
-	return (/^letiou?s(\sArtists)?$/.test(getArtist()) && /\||-/.test(getTrack()));
+	// Example: http://krefeld8ung.bandcamp.com/album/krefeld-8ung-vol-1
+	return /^letiou?s(\sArtists)?$/.test(getArtist()) &&
+		Util.findSeparator(getTrack(), separators) !== null;
 }
 
-/* @returns {{artist, track}} */
 Connector.getArtistTrack = () => {
-	let artist = getArtist(),
-		track = getTrack(),
-		separatorIndex;
-	if (artistIsletious()) {
-		separatorIndex = Math.max(track.indexOf('-'), track.indexOf('|'));
-		artist = track.substring(0, separatorIndex);
-		track = track.substring(separatorIndex + 1);
+	let artist = getArtist();
+	let track = getTrack();
+
+	if (isArtistLetious()) {
+		return Util.splitArtistTrack(track, separators);
 	}
 	return { artist, track };
 };
@@ -64,8 +64,7 @@ Connector.getArtistTrack = () => {
 Connector.getAlbum = () => {
 	let album = $('.detail_item_link').text() ||
 				$('h2.trackTitle').text() ||
-				$('[itemprop="inAlbum"] [itemprop="name"]').text() ||
-				null;
+				$('[itemprop="inAlbum"] [itemprop="name"]').text();
 	return album;
 };
 
@@ -73,8 +72,8 @@ Connector.playButtonSelector = 'div.playbutton:not(.playing)';
 
 Connector.getTrackArt = () => {
 	return $('#tralbumArt > a > img').attr('src') ||
-	$('#detail_gallery_container img').attr('src') ||
-	$('.discover-detail-inner img').attr('src');
+		$('#detail_gallery_container img').attr('src') ||
+		$('.discover-detail-inner img').attr('src');
 };
 
 Connector.getCurrentTime = () => {
