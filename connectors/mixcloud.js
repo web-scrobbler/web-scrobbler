a0f60abc828e38117b896f2c947d1ae033f46812
alexesprit <alex.esprit@gmail.com>
2017-09-15 06:15:29 +0300

    Fix Mixcloud connector

--- connectors/mixcloud.js
+++ connectors/mixcloud.js
@@ -1,8 +1,8 @@
 'use strict';
 
-Connector.playerSelector = '.player';
+Connector.playerSelector = '.player-wrapper';
 
-Connector.artistSelector = '.current-artist span';
+Connector.artistSelector = '.current-artist';
 
 Connector.trackSelector = '.current-track';
 
@@ -11,5 +11,13 @@ Connector.isPlaying = () => $('.player-control').hasClass('pause-state');
 Connector.isStateChangeAllowed = () => {
 	// Mixcloud player hides artist and track elements while seeking the stream,
 	// and we should not update state in such case.
-	return Connector.getArtist() && Connector.getTrack();
+	return Connector.getArtist() || Connector.getTrack();
 };
+
+Connector.filter = MetadataFilter.getTrimFilter().append({
+	artist: removeByPrefix
+});
+
+function removeByPrefix(text) {
+	return text.replace('by ', '');
+}
