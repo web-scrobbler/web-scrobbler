c65954b21b4613fa91ec3423572c3f733ba08c6c
alexesprit <alex.esprit@gmail.com>
2017-09-17 19:11:00 +0300

    Remove support for old Pandora UI

--- connectors/pandora.js
+++ connectors/pandora.js
@@ -1,74 +1,29 @@
 'use strict';
 
-function setupNewPandoraPlayer() {
-	Connector.playerSelector = '.region-bottomBar';
+Connector.playerSelector = '.region-bottomBar';
 
-	Connector.artistSelector = '.Tuner__Audio__TrackDetail__artist';
+Connector.artistSelector = '.Tuner__Audio__TrackDetail__artist';
 
-	Connector.trackSelector = '.Tuner__Audio__TrackDetail__title';
+Connector.trackSelector = '.Tuner__Audio__TrackDetail__title';
 
-	Connector.durationSelector = 'span[data-qa="remaining_time"]';
+Connector.durationSelector = 'span[data-qa="remaining_time"]';
 
-	Connector.trackArtSelector = '.Tuner__Audio__TrackDetail__img img';
+Connector.trackArtSelector = '.Tuner__Audio__TrackDetail__img img';
 
-	Connector.getTrackArt = () => {
-		let trackArtUrl = $('.Tuner__Audio__TrackDetail__img img').attr('src');
-		if (trackArtUrl) {
-			return trackArtUrl.replace('90W_90H', '500W_500H');
-		}
-
-		return null;
-	};
-
-	Connector.isPlaying = () => {
-		let playButtonHref = $('.PlayButton use').attr('xlink:href');
-		return playButtonHref.includes('pause');
-	};
-
-	Connector.isScrobblingAllowed = () => {
-		return $('.Tuner__Audio__TrackDetail__title--ad').length === 0;
-	};
-}
-
-function setupPandoraPlayer() {
-	Connector.playerSelector = '#playbackControl';
-
-	Connector.trackArtSelector = '.playerBarArt';
-
-	Connector.albumSelector = 'a.playerBarAlbum';
-
-	Connector.artistSelector = 'a.playerBarArtist';
-
-	Connector.trackSelector = 'a.playerBarSong';
-
-	Connector.playButtonSelector = 'div.playButton';
-
-	Connector.getDuration = () => {
-		return getElapsedTime() + getRemainingTime();
-	};
-
-	function getElapsedTime() {
-		let timeStr = $('div.elapsedTime').text();
-		return Util.stringToSeconds(timeStr);
+Connector.getTrackArt = () => {
+	let trackArtUrl = $('.Tuner__Audio__TrackDetail__img img').attr('src');
+	if (trackArtUrl) {
+		return trackArtUrl.replace('90W_90H', '500W_500H');
 	}
 
-	function getRemainingTime() {
-		// Remove 'minus' sign
-		let timeStr = $('div.remainingTime').text().substring(1);
-		return Util.stringToSeconds(timeStr);
-	}
-}
+	return null;
+};
 
-function isNewPandoraPlayer() {
-	return $('#playbackControl').length === 0;
-}
+Connector.isPlaying = () => {
+	let playButtonHref = $('.PlayButton use').attr('xlink:href');
+	return playButtonHref.includes('pause');
+};
 
-function setupConnector() {
-	if (isNewPandoraPlayer()) {
-		setupNewPandoraPlayer();
-	} else {
-		setupPandoraPlayer();
-	}
-}
-
-setupConnector();
+Connector.isScrobblingAllowed = () => {
+	return $('.Tuner__Audio__TrackDetail__title--ad').length === 0;
+};
