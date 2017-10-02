99b1f9eb212d9c55aae30ed4897df97ad7106731
andrewc2 <andrewc2@users.noreply.github.com>
2017-10-02 08:57:53 -0400

    Adding album art to SXM connector

--- connectors/siriusxm-player.js
+++ connectors/siriusxm-player.js
@@ -5,3 +5,5 @@ Connector.playerSelector = '#mainRow';
 Connector.artistTrackSelector = '.np-track-artist';
 
 Connector.playButtonSelector = '.play .RegularPlay01';
+
+Connector.trackArtSelector = '.np-track-art img';
