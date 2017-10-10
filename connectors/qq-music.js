2362ad9ae6eca6a0b2df5dc9210159dde7173bc6
xyauhideto <xyauhideto@gmail.com>
2017-10-10 07:30:16 +0200

    QQ Music: Get unique ID; prefer selector

--- connectors/qq-music.js
+++ connectors/qq-music.js
@@ -2,9 +2,16 @@
 
 Connector.playerSelector = '#opbanner';
 
-Connector.getArtist = () => $('#sim_song_info .js_singer').attr('title');
+Connector.artistSelector = '#singer_name a';
 
-Connector.getTrack = () => $('#sim_song_info .js_song').attr('title');
+Connector.trackSelector = '#song_name a';
+
+Connector.getUniqueID = () => {
+	let text = $('#song_name a').attr('href');
+	return text.split('/').pop().split('.html').shift();
+};
+
+Connector.albumSelector = '#album_name a';
 
 Connector.isPlaying = () => $('#btnplay').hasClass('btn_big_play--pause');
 
