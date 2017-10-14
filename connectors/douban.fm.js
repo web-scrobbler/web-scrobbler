3f53165f6dfcaab39d10937a9117e0ed14a51717
xyauhideto <xyauhideto@gmail.com>
2017-10-15 00:59:05 +0200

    Douban.fm: Add mobile player

--- connectors/douban.fm.js
+++ connectors/douban.fm.js
@@ -1,11 +1,47 @@
 'use strict';
 
-Connector.playerSelector = '.player-wrapper';
+function setupConnector() {
+	if (isMobilePlayer()) {
+		setupMobilePlayer();
+	} else {
+		setupDesktopPlayer();
+	}
+}
 
-Connector.trackArtSelector = '.playing-cover img';
+function isMobilePlayer() {
+	return $('#appbar').length > 0;
+}
 
-Connector.trackSelector = '.middle>div:nth-child(2)>a';
+function setupDesktopPlayer() {
+	Connector.playerSelector = '.player-wrapper';
 
-Connector.artistSelector = '.middle>div:nth-child(3) a';
+	Connector.trackArtSelector = '.playing-cover img';
 
-Connector.playButtonSelector = 'label[title="播放"]';
+	Connector.trackSelector = '.middle>div:nth-child(2)>a';
+
+	Connector.artistSelector = '.middle>div:nth-child(3) a';
+
+	Connector.playButtonSelector = 'label[title="播放"]';
+}
+
+function setupMobilePlayer() {
+	Connector.playerSelector = '#app';
+
+	Connector.playButtonSelector = 'svg>path';
+
+	Connector.trackSelector = 'h1 a';
+
+	Connector.getArtist = () => {
+		let text = $('h3>span').text();
+		return text.split('Icon Angle right').shift();
+	};
+
+	Connector.trackArtSelector = '#app > div > div:nth-child(2) > div:nth-child(3)';
+
+	Connector.getUniqueID = () => {
+		let text = $('h1 > a').attr('href');
+		return text.split('/').slice(-2).shift();
+	};
+}
+
+setupConnector();
