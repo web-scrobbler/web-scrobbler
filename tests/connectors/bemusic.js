4bbd341c8fcf8c1ad59e2a4cc58ed5d15cc9d71a
alexesprit <alex.esprit@gmail.com>
2017-10-16 22:08:58 +0300

    Fix BeMusic tests

--- tests/connectors/bemusic.js
+++ tests/connectors/bemusic.js
@@ -2,8 +2,8 @@
 
 module.exports = function(driver, connectorSpec) {
 	connectorSpec.shouldBehaveLikeMusicSite(driver, {
-		url: 'http://youtubify.vebto.com/album/12/Twenty+One+Pilots/Blurryface',
-		playButtonSelector: '.actions .icon.icon-play'
+		url: 'http://bemusic.vebto.com/album/12/Twenty+One+Pilots/Blurryface',
+		playButtonSelector: '.play-button'
 	});
 
 	connectorSpec.shouldBehaveLikeMusicSite(driver, {
@@ -14,5 +14,10 @@ module.exports = function(driver, connectorSpec) {
 	connectorSpec.shouldBehaveLikeMusicSite(driver, {
 		url: 'http://s2music.com/album/147455/Boards+of+Canada/Geogaddi',
 		playButtonSelector: '.actions .icon.icon-play'
+	});
+
+	connectorSpec.shouldBehaveLikeMusicSite(driver, {
+		url: 'http://ma5onic.com/PlayerApp/album/3043/Twenty+One+Pilots/Blurryface',
+		playButtonSelector: '.play-button'
 	});
 };
