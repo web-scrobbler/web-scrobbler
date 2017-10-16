219b9ea4ba33bc2e0cd78ff46533bcb249425c48
alexesprit <alex.esprit@gmail.com>
2017-10-16 22:09:10 +0300

    BeMusic: support for new UI

--- connectors/bemusic.js
+++ connectors/bemusic.js
@@ -1,37 +1,68 @@
 'use strict';
 
 /**
- * This connector is for BeMusic music streaming engine.
- * The connector currently covers Youtubify and GrooveMP3 music services.
+ * This connector is for BeMusic music streaming engine.s
  */
 
-Connector.playerSelector = '#player-controls';
+setupConnector();
 
-Connector.getArtist = () => {
-	let artist = $('.current-track .info .artist').first();
-	return artist.get(0).firstChild.textContent;
-};
-
-Connector.trackSelector = '.current-track .info .track-name';
-
-Connector.trackArtSelector = '.current-track img';
-
-Connector.durationSelector = '.track-length';
-
-Connector.currentTimeSelector = '.elapsed-time';
-
-Connector.isPlaying = () => {
-	return $('#player-controls .icon-pause').is(':visible');
-};
-
-Connector.getUniqueID = () => {
-	try {
-		let lastTrack = JSON.parse(localStorage.getItem('last-track'));
-		if (lastTrack) {
-			return lastTrack.value.id.toString();
-		}
-	} catch (e) {
+function setupConnector() {
+	if (isOldBeMusic()) {
+		setupOldBeMusicPlayer();
+	} else {
+		setupNewBeMusicPlayer();
 	}
+	setupCommonProps();
+}
 
-	return null;
-};
+function isOldBeMusic() {
+	return $('body').attr('ng-app') !== undefined;
+}
+
+function setupNewBeMusicPlayer() {
+	Connector.playerSelector = 'player-controls';
+
+	Connector.getArtist = () => {
+		let artist = $('.current-track .artist-name span').first();
+		return artist.get(0).firstChild.textContent;
+	};
+
+	Connector.isPlaying = () => {
+		return $('player-controls .icon-pause').is(':visible');
+	};
+}
+
+function setupOldBeMusicPlayer() {
+	Connector.playerSelector = '#player-controls';
+
+	Connector.getArtist = () => {
+		let artist = $('.current-track .info .artist').first();
+		return artist.get(0).firstChild.textContent;
+	};
+
+	Connector.isPlaying = () => {
+		return $('#player-controls .icon-pause').is(':visible');
+	};
+
+	Connector.getUniqueID = () => {
+		try {
+			let lastTrack = JSON.parse(localStorage.getItem('last-track'));
+			if (lastTrack) {
+				return lastTrack.value.id.toString();
+			}
+		} catch (e) {
+		}
+
+		return null;
+	};
+}
+
+function setupCommonProps() {
+	Connector.trackSelector = '.current-track .track-name';
+
+	Connector.trackArtSelector = '.current-track img';
+
+	Connector.durationSelector = '.track-length';
+
+	Connector.currentTimeSelector = '.elapsed-time';
+}
