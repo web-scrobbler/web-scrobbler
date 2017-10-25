dfbae4302026cb460d6daaf356c023e8a6afd638
Carsten Petersen <carpet@taox.net>
2017-10-25 09:39:49 +0200

    Updated "fritz" and "radioeins" connectors
    ### Simplified "fritz" connector
    
    The "is playing" status can simply be determined from a certain tag's class names.
    
    ### Two types of "radioeins" players exist
    
    The player page randomly provides one player (jwplayer) or another (lsplayer). The old (rather complicated) player is still supported. The new player has an element.

--- connectors/fritz.js
+++ connectors/fritz.js
@@ -6,23 +6,4 @@ Connector.artistSelector = 'div.teasertext > h3 > a > span.manualteasertitle';
 
 Connector.trackSelector = 'div.teasertext > h3 > h3 > span.manualteasertitle';
 
-Connector.isPlaying = () => {
-	let state = retrieveJwplayerState();
-	return (typeof state === 'string') && (state.toUpperCase() === 'PLAYING');
-};
-
-function retrieveJwplayerState() {
-	let scriptContent = 'if (typeof jwplayer === "function") $("body").attr("tmp_state", typeof jwplayer().getState === "function" ? jwplayer().getState() : "");\n';
-
-	let script = document.createElement('script');
-	script.id = 'tmpScript';
-	script.appendChild(document.createTextNode(scriptContent));
-	(document.body || document.head || document.documentElement).appendChild(script);
-
-	let ret = $('body').attr('tmp_state');
-
-	$('body').removeAttr('tmp_state');
-	$('#tmpScript').remove();
-
-	return ret;
-}
+Connector.isPlaying = () => $('.player').hasClass('player_playing');
