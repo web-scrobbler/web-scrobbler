083ebd84029de72d971170a73ad566ae345cd61f
alexesprit <alex.esprit@gmail.com>
2017-11-02 21:06:15 +0300

    Remove unused script file

--- popups/error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-$(() => {
-	$('#reauth').click(() => {
-		// FIXME: Replace to `chrome.runtime.getURL`.
-		let url = chrome.extension.getURL('/options/options.html#accounts');
-		chrome.tabs.create({ url });
-	});
-});
