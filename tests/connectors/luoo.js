f7f9e7a1255ce864defc8db959ec067563ad9bd7
xyauhideto <xyauhideto@gmail.com>
2017-11-12 08:38:20 +0100

    luooMusic: Add support for essay, musician, search
    Also add time info, unique ID, track art

--- tests/connectors/luoo.js
+++ tests/connectors/luoo.js
@@ -1,7 +1,21 @@
 'use strict';
 
 module.exports = function(driver, connectorSpec) {
+	// /vol or /music
 	connectorSpec.shouldBehaveLikeMusicSite(driver, {
 		url: 'http://www.luoo.net/music/854'
+	});
+	// essay
+	connectorSpec.shouldBehaveLikeMusicSite(driver, {
+		url: 'http://www.luoo.net/essay/915'
+	});
+	// musician
+	connectorSpec.shouldBehaveLikeMusicSite(driver, {
+		url: 'http://www.luoo.net/musician/'
+	});
+	// search
+	connectorSpec.shouldBehaveLikeMusicSite(driver, {
+		url: 'http://www.luoo.net/search/?q=love&t=single',
+		playButtonSelector: '.icon-search-play'
 	});
 };
