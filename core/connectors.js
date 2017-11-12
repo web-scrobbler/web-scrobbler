1fc22621bcd7d4e8e6c5fdc212230939e16daf2b
alexesprit <alex.esprit@gmail.com>
2017-11-12 11:34:21 +0300

    Add Discogs support
    Closes #1474.

--- core/connectors.js
+++ core/connectors.js
@@ -969,6 +969,11 @@ define(function() {
 	},
 
 	{
+		label: 'Discogs',
+		matches: ['*://www.discogs.com/*'],
+		js: ['connectors/youtube-embed.js'],
+		allFrames: true
+	}, {
 		label: 'Kuwo Music',
 		matches: [
 			'*://kuwo.cn/*',
