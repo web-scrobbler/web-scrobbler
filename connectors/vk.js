2d6c4ddfbcda0c634566882bdc60e37913236372
alexesprit <alex.esprit@gmail.com>
2017-11-19 01:59:19 +0300

    VK: bind connector object to event listener

--- connectors/vk.js
+++ connectors/vk.js
@@ -3,35 +3,40 @@
 let currentState = {};
 let isPlaying = false;
 
-(function() {
-	// FIXME: Replace to `chrome.runtime.getURL`.
-	let scriptUrl = chrome.extension.getURL('connectors/vk-dom-inject.js');
-	Util.injectScriptIntoDocument(scriptUrl);
-
-	$(window).on('message', ({ originalEvent: event }) => {
-		if (event.data.sender !== 'web-scrobbler') {
-			return;
-		}
-
-		switch (event.data.type) {
-			case 'start':
-				isPlaying = true;
-				break;
-			case 'stop':
-			case 'pause':
-				isPlaying = false;
-				break;
-		}
-
-		currentState = event.data.trackInfo;
-		currentState.isPlaying = isPlaying;
-
-		Connector.onStateChanged();
-	});
-})();
+setupMessageListener();
 
 Connector.filter = new MetadataFilter({
 	all: [MetadataFilter.decodeHtmlEntities, MetadataFilter.trim]
 });
 
 Connector.getCurrentState = () => currentState;
+
+function setupMessageListener() {
+	// FIXME: Replace to `chrome.runtime.getURL`.
+	let scriptUrl = chrome.extension.getURL('connectors/vk-dom-inject.js');
+	Util.injectScriptIntoDocument(scriptUrl);
+
+	window.addEventListener('message', onMessage.bind(Connector));
+}
+
+function onMessage(event) {
+	if (event.data.sender !== 'web-scrobbler') {
+		return;
+	}
+
+	switch (event.data.type) {
+		case 'start':
+			isPlaying = true;
+			break;
+		case 'stop':
+		case 'pause':
+			isPlaying = false;
+			break;
+	}
+
+	currentState = event.data.trackInfo;
+	currentState.isPlaying = isPlaying;
+
+	this.onStateChanged();
+	console.log(Connector);
+}
