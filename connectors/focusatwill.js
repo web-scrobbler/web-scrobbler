56ee16b14d0b59643c268c10be1460f009b06764
alexesprit <alex.esprit@gmail.com>
2017-12-26 15:54:47 +0300

    Fix Focus@Will connector
    Closes #1521.

--- connectors/focusatwill.js
+++ connectors/focusatwill.js
@@ -1,9 +1,18 @@
 'use strict';
 
-Connector.playerSelector = '#scroller';
+Connector.playerSelector = '#root';
 
-Connector.getArtist = () => $('.artist').text().substring(4);
+Connector.artistSelector = '._2H2od > div:nth-child(2)';
 
-Connector.trackSelector = '.track';
+Connector.trackSelector = '._2H2od > div:nth-child(1)';
 
-Connector.isPlaying = () => $('.play.stopped').hasClass('playing');
+Connector.isPlaying = () => $('.icon-stop').length > 0;
+
+Connector.filter = new MetadataFilter({
+	all: MetadataFilter.trim,
+	artist: removeByPrefix
+});
+
+function removeByPrefix(text) {
+	return text.replace('By: ', '');
+}
