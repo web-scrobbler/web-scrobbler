c99b1a748dc93ab0ce3a5b343ff1140bad77192e
Schaaf, Martin <mschaaf@datameer.com>
2017-12-31 19:21:11 +0100

    Add connector for Perisonic frontend to Subsonic

---
+++ connectors/perisonic.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Connector.artistSelector = '#title h3';
+
+Connector.trackSelector = '#title p';
+
+Connector.trackArtSelector = '#coverart img';
+
+Connector.isPlaying = () => $('#playpause').hasClass('pause');
+
+$('#title').bind("DOMSubtreeModified", Connector.onStateChanged);
+
