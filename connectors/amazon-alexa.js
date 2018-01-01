cd41e24388f2e082b0a956cb0b07fa6ede6a7ecc
Ben Scobie <admin@benscobie.com>
2018-01-01 13:14:05 +0000

    Remove unneeded check and fix CS errors

--- connectors/amazon-alexa.js
+++ connectors/amazon-alexa.js
@@ -1,7 +1,8 @@
 'use strict';
 
 Connector.playerSelector = '#d-content';
-Connector.remainingTimeSelector  = '.d-np-time-display.remaining-time';
+Connector.albumSelector = '#d-info-text .d-sub-text-2';
+Connector.remainingTimeSelector = '.d-np-time-display.remaining-time';
 Connector.currentTimeSelector = '.d-np-time-display.elapsed-time';
 
 Connector.getDuration = () => {
@@ -26,19 +27,17 @@ Connector.getArtistTrack = () => {
 	return { artist, track };
 };
 
-Connector.albumSelector = '#d-info-text .d-sub-text-2';
-
 Connector.isPlaying = () => {
-	let songProgress = $('.d-np-progress-slider .d-slider-container .d-slider-track').attr('aria-valuenow');
 	let duration = Connector.getDuration();
 
-	// The app doesn't update the progress bar or time remaining straight away (and starts counting down from an hour). This is a workaround to avoid detecting an incorrect duration time.
-	if (duration > 3600 || songProgress == 100) {
+	// The app doesn't update the remaining and elapsed times straight away when changing songs. The remaining time rolls over and starts counting down from an hour.
+	// This is a workaround to avoid detecting an incorrect duration time.
+	if (duration > 3600) {
 		return false;
 	}
-	
+
 	return $('#d-primary-control .play').size() === 0;
-}
+};
 
 function isPlayingLiveRadio() {
 	return $('#d-secondary-control-left .disabled').size() === 1 &&
