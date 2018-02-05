a1b1ae276d2068eb6511298eb68de5602c2755f7
Carsten Petersen <carpet@taox.net>
2018-02-05 22:11:08 +0100

    Fixed lint errors

--- src/connectors/deutschlandfunknova.js
+++ src/connectors/deutschlandfunknova.js
@@ -6,10 +6,10 @@ Connector.artistTrackSelector = '.playing-title';
 
 Connector.getArtistTrack = () => {
 	let text = $(Connector.artistTrackSelector).text();
-	let m = text.match(/"(.*?)" von (.*?) · /);
+	let m = text.match(/"(.*?)" von (.*?)\u2003\u00B7\u2003/);
 	if (m && (m.length === 3)) {
 		// Sometimes the title box displays something like '"undefined" von undefined'
-		if (m[2].toLowerCase() == "undefined") {
+		if (m[2].toLowerCase() === 'undefined') {
 			return null;
 		}
 		return { artist: m[2], track: m[1] };
