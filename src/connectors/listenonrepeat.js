a03c4d3c75b29605474e08d7387725cf7ce35d64
alexesprit <alex.esprit@gmail.com>
2018-02-12 20:04:23 +0300

    ListenOnRepeat: don't get unique ID
    Youtube video URL isn't changed on track change until the page is refreshed.

--- src/connectors/listenonrepeat.js
+++ src/connectors/listenonrepeat.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const videoIdRegExp = /v=?([^#&?]*).*/;
-
 Connector.playerSelector = '.player-controls';
 
 Connector.filter = MetadataFilter.getYoutubeFilter();
@@ -14,17 +12,3 @@ Connector.getArtistTrack = () => {
 Connector.isPlaying = () => {
 	return $('button:contains(Pause Video)').length > 0;
 };
-
-Connector.getUniqueID = () => {
-	let videoUrl = $('meta[property="og:url"]').attr('content');
-	return getYoutubeVideoIdFromUrl(videoUrl);
-};
-
-function getYoutubeVideoIdFromUrl(videoUrl) {
-	let match = videoUrl.match(videoIdRegExp);
-	if (match) {
-		return match[1];
-	}
-
-	return null;
-}
