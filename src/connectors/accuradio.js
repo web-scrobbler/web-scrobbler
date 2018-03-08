19360e6556cbe80c64ccd0e3cbcd808da3058333
alexesprit <alex.esprit@gmail.com>
2018-03-08 18:03:39 +0300

    AccuRadio: get track info properly
    Fixes #1593.

--- src/connectors/accuradio.js
+++ src/connectors/accuradio.js
@@ -2,11 +2,13 @@
 
 Connector.playerSelector = '#playerContents';
 
-Connector.artistSelector = '#songartist';
+Connector.artistSelector = '#songartist div:first-child, #songartist';
 
-Connector.trackSelector = '#songtitle';
+Connector.getArtist = () => getScrollableText('#songartist');
 
-Connector.albumSelector = '#songalbum';
+Connector.getTrack = () => getScrollableText('#songtitle');
+
+Connector.getAlbum = () => getScrollableText('#songalbum');
 
 Connector.getTrackArt = () => {
 	let trackArtUrl = $('#albumArtImg').attr('src');
@@ -14,3 +16,7 @@ Connector.getTrackArt = () => {
 };
 
 Connector.isPlaying = () => $('#playerPauseButton').length > 0;
+
+function getScrollableText(selector) {
+	return $(`${selector} div`).first().text() || $(selector).text();
+}
