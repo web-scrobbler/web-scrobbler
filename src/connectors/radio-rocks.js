f31584c5727b31145eb6bc2676a34a72939b4adc
Dmytro Matsiuk <dmmmat@gmail.com>
2018-05-15 16:40:10 +0300

    change selector

--- src/connectors/radio-rocks.js
+++ src/connectors/radio-rocks.js
@@ -1,10 +1,9 @@
 'use strict';
 
-Connector.playerSelector = '.jp-gui';
+Connector.playerSelector = '#jp_container_1';
 
 Connector.artistSelector = '#singer0';
 
 Connector.trackSelector = '#song0';
 
 Connector.isPlaying = () => $('#jp_container_1').hasClass('jp-state-playing');
-
