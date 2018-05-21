c889b7ad014f615ea7b0534dbf2f8714633554e6
Malachi Soord <inverse.chi@gmail.com>
2018-05-21 11:42:55 +0200

    Fix

--- src/connectors/somafm.js
+++ src/connectors/somafm.js
@@ -8,5 +8,5 @@ Connector.trackSelector = '.list-body > div:nth-child(1) > div:nth-child(3) > p'
 
 Connector.isPlaying = () => {
 	let $button = $('.controls-container > div > button:nth-child(1)');
-    return $button.attr('ng-click') === 'stop()';
+	return $button.attr('ng-click') === 'stop()';
 };
