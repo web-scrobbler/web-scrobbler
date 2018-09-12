32a8bc1bc31b0bae4b558ae72c1fc64493e3542f
Stefan Markovic <zmstefan@gmail.com>
2018-09-13 00:20:05 +0200

    new deezer layout support added

--- src/connectors/deezer.js
+++ src/connectors/deezer.js
@@ -1,20 +1,44 @@
 'use strict';
 
-Connector.playerSelector = '#page_sidebar';
+if($('.page-player').length){
+	Connector.playerSelector = '.page-player';
+} else {
+	Connector.playerSelector = '#page_sidebar';
+}
 
 Connector.getArtist = () => {
-	let artists = $('.player-track-artist').children().toArray();
-	return Util.joinArtists(artists);
+	if($('.track-title .track-link:eq(1)').length){
+		return $('.track-title .track-link:eq(1)').text();		
+	} else{
+		let artists = $('.player-track-artist').children().toArray();
+		return Util.joinArtists(artists);
+	}	
 };
 
-Connector.trackSelector = '.player-track-title';
+if($('.track-title .track-link:eq(0)').length){
+	Connector.trackSelector = '.track-title .track-link:eq(0)';	
+} else {
+	Connector.trackSelector = '.player-track-title';
+}
 
-Connector.currentTimeSelector = '.player-progress .progress-time';
+if($('.slider-counter-current').length){
+	Connector.currentTimeSelector = '.slider-counter-current';	
+} else {
+	Connector.currentTimeSelector = '.player-progress .progress-time';
+}
 
-Connector.durationSelector = '.player-progress .progress-length';
+if($('.slider-counter-max').length){
+	Connector.durationSelector = '.slider-counter-max';	
+} else {
+	Connector.durationSelector = '.player-progress .progress-length';
+}
 
-Connector.trackArtSelector = '.player-cover img';
+if($('.picture-img.active').length){
+	Connector.trackArtSelector = '.picture-img.active';	
+} else {
+	Connector.trackArtSelector = '.player-cover img';
+}
 
 Connector.filter = MetadataFilter.getRemasteredFilter().extend(MetadataFilter.getDoubleTitleFilter());
 
-Connector.isPlaying = () => $('#player .svg-icon-pause').length > 0;
+Connector.isPlaying = () => $('#player .svg-icon-pause').length > 0 || $('.page-player .svg-icon-pause').length > 0;
