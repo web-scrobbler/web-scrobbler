b1b1920d069e09be975e17b366f3c597e25ea024
o1111111o <jcm940308@gmail.com>
2018-09-19 03:24:35 +0900

    Implement Naver Connector

--- src/core/connectors.js
+++ src/core/connectors.js
@@ -959,6 +959,12 @@ define(function() {
 		matches: ['*://www.idagio.com/*'],
 		js: ['connectors/idagio.js'],
 	},
+	{
+		label: 'Naver',
+		matches: ['*://playerui.music.naver.com*'],
+		js: ['connectors/naver.js'],
+	},
+
 
 	{
 		label: 'Ma5onic Music Player',
