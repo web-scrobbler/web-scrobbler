c6a99fe1ad24e1a640aca84767fc3317ff0e1320
NamJinWoo <cjsencks@naver.com>
2018-09-23 20:08:09 +0900

    soribada.js ,spacing fixed

--- src/connectors/soribada.js
+++ src/connectors/soribada.js
@@ -1,34 +1,20 @@
 'use strict';
 
-
-
 Connector.playerSelector = '#footer .player';
-
-
 
 Connector.artistSelector = '.info .pado_tit_wrap span:nth-child(2)';
 
-
-
 Connector.trackSelector = '.pado_tit_wrap strong:first';
-
-
 
 Connector.currentTimeSelector = '#progress .now';
 
-
-
 Connector.durationSelector = '#progress .total';
-
-
 
 Connector.isPlaying = () => {
 
 	return $('#controller .pause').length !== 0;
 
 };
-
-
 
 Connector.getTrackArt = () => {
 
