9bb5222442eabec414f5676e6cd25c9835629918
KwonDae <goodday94@cnu.ac.kr>
2018-09-25 19:59:17 +0900

    remove space of olleh.js

--- src/connectors/olleh.js
+++ src/connectors/olleh.js
@@ -1,19 +1,12 @@
 'use strict';
-
  Connector.playerSelector = '#n11_player';
-
  Connector.artistSelector = '.full_info.in.artist';
-
  Connector.trackSelector = '.full_info.in.song';
-
  Connector.currentTimeSelector = '.time_1';
-
  Connector.durationSelector = '.time_2';
-
  Connector.isPlaying = () => {
 	return $('.client_player.pbtn_pause').length !== 0;
 };
-
  Connector.getTrackArt = () => {
 	return `http:${$('.album_view').find('img').attr('src')}`;
 };
