193ed7a831b5799d73815790af6aeb6f466fc384
KwonDae <goodday94@cnu.ac.kr>
2018-09-26 20:39:23 +0900

    remove space

--- src/connectors/olleh.js
+++ src/connectors/olleh.js
@@ -1,12 +1,19 @@
 'use strict';
- Connector.playerSelector = '#n11_player';
- Connector.artistSelector = '.full_info.in.artist';
- Connector.trackSelector = '.full_info.in.song';
- Connector.currentTimeSelector = '.time_1';
- Connector.durationSelector = '.time_2';
- Connector.isPlaying = () => {
-	return $('.client_player.pbtn_pause').length !== 0;
+
+Connector.playerSelector = '#n11_player';
+
+Connector.artistSelector = '.full_info .in .artist';
+
+Connector.trackSelector = '.full_info .in .song';
+
+Connector.currentTimeSelector = '.time_1';
+
+Connector.durationSelector = '.time_2';
+
+Connector.isPlaying = () => {
+	return $('.client_player .pbtn_pause').length !== 0;
 };
- Connector.getTrackArt = () => {
+
+Connector.getTrackArt = () => {
 	return `http:${$('.album_view').find('img').attr('src')}`;
 };
