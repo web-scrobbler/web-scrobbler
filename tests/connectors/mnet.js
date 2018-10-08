affb028176d95e67463a96716fd89beaafa1e395
JungTaepyo <jtp9688@naver.com>
2018-10-08 17:43:56 +0900

    implement mnet connector

--- tests/connectors/mnet.js
+++ tests/connectors/mnet.js
@@ -2,6 +2,6 @@
 
 module.exports = function(driver, connectorSpec) {
 	connectorSpec.shouldContainPlayerElement(driver, {
-		url: ''http://www.mnet.com/player/aod'
+		url: 'http://www.mnet.com/player/aod/'
 	});
 };
