036f3fee8308c26b22a0bfe19ded752071d6ccbc
Stéphane Bruckert <contact@stephanebruckert.com>
2018-10-17 00:48:42 +0100

    Get current time and album

--- src/connectors/warp-aphextwin.js
+++ src/connectors/warp-aphextwin.js
@@ -2,12 +2,19 @@
 
 Connector.playerSelector = '.player';
 
-Connector.playButtonSelector = '.track-controls .player-button.play-track';
-
 Connector.trackSelector = '.current-track .js-current-track-title';
 
 Connector.durationSelector = '.current-track .js-current-track-duration';
 
-Connector.getArtist = function() {
+Connector.currentTimeSelector = '.current-time.js-current-time';
+
+Connector.playButtonSelector = '.track-controls .js-play-track';
+
+Connector.getArtist = () => {
 	return $('.current-track .js-current-track-artist').text().replace(' - ', '');
+};
+
+Connector.getAlbum = () => {
+	let releasePath = $(Connector.trackSelector).attr('href');
+	return $(`.track-list .track-release a[href="${releasePath}"]`).attr('title');
 };
