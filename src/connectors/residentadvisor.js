5e1ea9ae2f25b25cfde45b8ccc0bf61a866d14c4
Stéphane Bruckert <contact@stephanebruckert.com>
2018-10-20 16:36:49 +0100

    Normalize categories

--- src/connectors/residentadvisor.js
+++ src/connectors/residentadvisor.js
@@ -1,64 +1,100 @@
 'use strict';
 
+const domain = 'https://www.residentadvisor.net';
+
 Connector.playerSelector = '.content-list';
 
 Connector.playButtonSelector = '.controls .play';
 
 function getTrackContainer() {
-  let track = $('.play.paused').parent().parent().parent();
+  let track = $('.play.paused').parents().eq(2);
   if (track.is('article')) {
-    return track; // recommended or single track
+    return track; // recommended
   } else {
-    return track.parent(); // archive track
+    return track.parent(); // archive track or single track
   }
 }
 
-function isTrackInRecommended() {
+function isTrackRecommended() {
   return getTrackContainer().is('article');
+}
+
+function isArchivedTrack() {
+  return getTrackContainer().is('ul');
 }
 
 function isTrackSingle() {
   return getTrackContainer().is('div');
 }
 
+function isDjTrack() {
+  return false;
+}
+
+function isChartTrack() {
+  return false;
+}
+
 Connector.getTrackArt = () => {
   let artPath;
-  if (isTrackInRecommended()) {
+  if (isTrackRecommended()) {
     artPath = getTrackContainer().find('a img').attr('src'); // /images/cover/tr-929172.jpg
   } else if (isTrackSingle()) {
     artPath = getTrackContainer().find('.fl .pr16 img').attr('src');
-  } else {
+  } else if (isDjTrack()) {
+
+  } else if (isChartTrack()) {
+
+  } else if (isArchivedTrack()) {
     artPath = getTrackContainer().find('li:first-child .pr8 a img').attr('src');
   }
-  return 'https://www.residentadvisor.net' + artPath;
+  return domain + artPath;
 }
 
 Connector.getTrack = () => {
-  if (isTrackInRecommended()) {
+  if (isTrackRecommended()) {
     return Util.splitArtistTrack(getTrackContainer().find('div a').text()).track;
   } else if (isTrackSingle()) {
     return Util.splitArtistTrack($('#sectionHead h1').text()).track;
-  } else {
+  } else if (isDjTrack()) {
+
+  } else if (isChartTrack()) {
+
+  } else if (isArchivedTrack()) {
     return getTrackContainer().find('li:last-child .pr8 a.f24').text();
   }
 }
 
 Connector.getArtist = () => {
-  if (isTrackInRecommended()) {
+  if (isTrackRecommended()) {
     return Util.splitArtistTrack(getTrackContainer().find('div a').text()).artist;
   } else if (isTrackSingle()) {
     return Util.splitArtistTrack($('#sectionHead h1').text()).artist;
-  } else {
+  } else if (isDjTrack()) {
+
+  } else if (isChartTrack()) {
+
+  } else if (isArchivedTrack()) {
     return getTrackContainer().find('li:last-child .pr8 div.f24').first().text();
   }
 }
 
 Connector.getUniqueId = () => {
-  return getTrackContainer().find('.play.player').attr('data-trackid');
+  if (isTrackRecommended()) {
+    return getTrackContainer().find('.play.player').attr('data-trackid');
+  } else if (isTrackSingle()) {
+    return getTrackContainer().find('.play.player').attr('data-trackid');
+  } else if (isDjTrack()) {
+
+  } else if (isChartTrack()) {
+
+  } else if (isArchivedTrack()) {
+
+  }
 }
 
 Connector.isPlaying = () => $('.play.paused').length > 0;
 
 Connector.isTrackArtDefault = (trackArtUrl) => {
-  return trackArtUrl === 'https://www.residentadvisor.net/images/cover/blank.jpg'
+  return trackArtUrl === domain + '/images/cover/blank.jpg'
 }
