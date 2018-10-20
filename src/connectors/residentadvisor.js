1763802a2c7a2b374409644692e7a32d6b68824c
Stéphane Bruckert <contact@stephanebruckert.com>
2018-10-20 14:35:38 +0100

    Single tracks

--- src/connectors/residentadvisor.js
+++ src/connectors/residentadvisor.js
@@ -7,7 +7,7 @@ Connector.playButtonSelector = '.controls .play';
 function getTrackContainer() {
   let track = $('.play.paused').parent().parent().parent();
   if (track.is('article')) {
-    return track; // recommended track
+    return track; // recommended or single track
   } else {
     return track.parent(); // archive track
   }
@@ -17,10 +17,16 @@ function isTrackInRecommended() {
   return getTrackContainer().is('article');
 }
 
+function isTrackSingle() {
+  return getTrackContainer().is('div');
+}
+
 Connector.getTrackArt = () => {
   let artPath;
   if (isTrackInRecommended()) {
     artPath = getTrackContainer().find('a img').attr('src'); // /images/cover/tr-929172.jpg
+  } else if (isTrackSingle()) {
+    artPath = getTrackContainer().find('.fl .pr16 img').attr('src');
   } else {
     artPath = getTrackContainer().find('li:first-child .pr8 a img').attr('src');
   }
@@ -30,6 +36,8 @@ Connector.getTrackArt = () => {
 Connector.getTrack = () => {
   if (isTrackInRecommended()) {
     return Util.splitArtistTrack(getTrackContainer().find('div a').text()).track;
+  } else if (isTrackSingle()) {
+    return Util.splitArtistTrack($('#sectionHead h1').text()).track;
   } else {
     return getTrackContainer().find('li:last-child .pr8 a.f24').text();
   }
@@ -38,6 +46,8 @@ Connector.getTrack = () => {
 Connector.getArtist = () => {
   if (isTrackInRecommended()) {
     return Util.splitArtistTrack(getTrackContainer().find('div a').text()).artist;
+  } else if (isTrackSingle()) {
+    return Util.splitArtistTrack($('#sectionHead h1').text()).artist;
   } else {
     return getTrackContainer().find('li:last-child .pr8 div.f24').first().text();
   }
