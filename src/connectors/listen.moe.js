9c35b4955e6d6759d2ef9ebfbf2aa58bef263451
Calvin_c64 <angrytuberslp@gmail.com>
2018-10-25 18:47:37 +0200

    Fixing Recognition Problems

--- src/connectors/listen.moe.js
+++ src/connectors/listen.moe.js
@@ -2,8 +2,14 @@
 
 Connector.playerSelector = '#app';
 
-Connector.artistSelector = '.playerContainer.columns.mini .player .player-song-artist';
+Connector.getArtistTrack = () => {
+	//Tag fix, because most of the songs are not recognized because they have a weird naming scheme: title [ANIME-name]
+	return $('.player-song-title')[0].innerText.split(' [')[0];
+};
 
-Connector.trackSelector = '.playerContainer.columns.mini .player .player-song-title';
+Connector.getArtist = () => {
+	//Tag fix, because else not recognized most of the time!
+	return $('.player-song-artist-container')[0].innerText.replace(" ,", ",");
+};
 
 Connector.isPlaying = () => $('.icon-music-pause-a') !== null;
