fa9b7e0c5a9f2ab66c6eef441dc022e5fb5f322d
Calvin_c64 <angrytuberslp@gmail.com>
2018-10-26 11:41:33 +0200

    Update listen.moe.js

--- src/connectors/listen.moe.js
+++ src/connectors/listen.moe.js
@@ -6,12 +6,10 @@ Connector.artistSelector = '.playerContainer.columns.mini .player .player-song-a
 Connector.trackSelector = '.playerContainer.columns.mini .player .player-song-title';
 
 Connector.getArtistTrack = () => {
-	//Tag fix, because most of the songs are not recognized because they have a weird naming scheme: title [ANIME-name]
 	return $('.player-song-title')[0].innerText.split(' [')[0];
 };
 
 Connector.getArtist = () => {
-	//Tag fix, because else not recognized most of the time!
 	return $('.player-song-artist-container')[0].innerText.replace(" ,", ",");
 };
 
