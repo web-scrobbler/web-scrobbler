621ff37f7e28aa6dcef6956b653af59271195afa
Malachi Soord <inverse.chi@gmail.com>
2018-11-04 13:55:41 +0100

    Add back support for main page player

--- src/connectors/radioparadise.js
+++ src/connectors/radioparadise.js
@@ -1,15 +1,35 @@
 'use strict';
 
-Connector.playerSelector = '.content-wrapper';
+if ($('body').hasClass('ng-tns-0-0')) {
+	setupNewPlayer();
+} else {
+	setupMainPlayer();
+}
 
-Connector.artistSelector = '.song_info_display div:first-child b';
 
-Connector.trackSelector = '.song_info_display div:nth-child(2) b';
+function setupNewPlayer() {
+	Connector.playerSelector = '.content-wrapper';
 
-Connector.albumSelector = '.song_info_display div:nth-child(3) b';
+	Connector.artistSelector = '.song_info_display div:first-child b';
 
-Connector.isPlaying = () => $('#play-button').hasClass('active');
+	Connector.trackSelector = '.song_info_display div:nth-child(2) b';
 
-Connector.getTrackArt = () => {
-	return `${$('#info .cover a').find('img').attr('src')}`;
-};
+	Connector.albumSelector = '.song_info_display div:nth-child(3) b';
+
+	Connector.isPlaying = () => $('#play-button').hasClass('active');
+
+	Connector.getTrackArt = () => {
+		return `${$('#info .cover a').find('img').attr('src')}`;
+	};
+}
+
+function setupMainPlayer() {
+
+	Connector.playerSelector = '#header';
+
+	Connector.artistTrackSelector = '#nowplaying_title > b';
+
+	Connector.trackArtSelector = '#nowplaying_title > img';
+
+	Connector.isPlaying = () => $('#play_button').hasClass('button_active');
+}
