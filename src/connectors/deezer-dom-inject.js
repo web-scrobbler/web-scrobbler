14d6575dd2b33a11371d317271aa513fa43f22cf
Lutz Brückner <irie@gmx.de>
2018-11-12 10:03:28 +0100

    uses ART_NAME instead of ARTISTS for consistent and much simpler code

--- src/connectors/deezer-dom-inject.js
+++ src/connectors/deezer-dom-inject.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const DEEZER_MAIN_ARTIST = '0';
-const DEEZER_FEATURED_ARTIST = '5';
-
 initConnector();
 
 function initConnector() {
@@ -39,69 +36,17 @@ function getState() {
 	const item = player.getCurrentSong();
 
 	return {
-		artist: lastfmifyArtists(item),
-		title: lastfmifyTitle(item),
+		artist: item.ART_NAME,
+		title: item.SNG_TITLE,
 		album: item.ALB_TITLE,
 		duration: player.getDuration(),
 		currentTime: player.getPosition(),
 		uniqueId: item.SNG_ID,
 		isPlaying: player.isPlaying(),
-		trackArt: getTrackArt(item.ALB_PICTURE),
-		artists: item.ARTISTS
+		trackArt: getTrackArt(item.ALB_PICTURE)
 	};
 }
 
 function getTrackArt(pic) {
 	return `https://e-cdns-images.dzcdn.net/images/cover/${pic}/264x264-000000-80-0-0.jpg`;
-}
-
-function lastfmifyArtists(item) {
-	let artists = item.ARTISTS;
-	artists = getArtistsnamesByRole(item, DEEZER_MAIN_ARTIST);
-	if (artists.length <= 1) {
-		return item.ART_NAME;
-	}
-
-	return getArtistsList(artists);
-}
-
-function lastfmifyTitle(item) {
-	let title = item.SNG_TITLE;
-	if (item.VERSION !== '') {
-		title = `${title} ${item.VERSION}`;
-	}
-
-	// don't add featured artists from the item object to the title
-	// if there is already feature information included
-	if (title.search(/\s[([]?[f|F](eat\.|eaturing)\s([^ ]+)/i) !== -1) {
-		return title;
-	}
-
-	let artists = getArtistsnamesByRole(item, DEEZER_FEATURED_ARTIST);
-	if (artists.length === 0) {
-		return title;
-	}
-
-	return `${title} (feat. ${getArtistsList(artists)})`;
-}
-
-function getArtistsnamesByRole(item, roleId) {
-	return item.ARTISTS.filter((a) => a.ROLE_ID === roleId) // 0: main artist; 5: featured artist
-		.sort((a, b) => a.ARTISTS_SONG_ORDER < b.ARTISTS_SONG_ORDER) // respect ordering
-		.map((a) => a.ART_NAME) // array of artistnames from array of items
-		.filter(removeDuplicateArtists);
-}
-
-function removeDuplicateArtists(artist, index, artists) {
-	// make sure an artistname is not a comma- or ampersand-seperated list
-	// of artistnames which are already included in the artists array
-	let dupes = artist.split(/\s&\s|\sand\s/i);
-	return dupes.length === 1 || !dupes.every((a) => artists.includes(a));
-}
-
-function getArtistsList(artists) {
-	// concat artists with ',' but last artist with '&'
-	return artists.reduce(function concatArtists(string, artist, index, arr) {
-		return string + (arr.length === index + 1 ? ' & ' : ', ') + artist;
-	});
 }
