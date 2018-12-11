748821d90d1e31aa61a8e5e4c11198610d87204c
Calvin <calvinturbo@gmail.com>
2018-12-11 13:50:24 +0100

    Fix merge issue

--- src/connectors/radiozenders.js
+++ src/connectors/radiozenders.js
@@ -3,8 +3,8 @@
 Connector.playerSelector = '#jp_container_1';
 
 Connector.getArtistTrack = function() {
-    let artistTrack = $('#player-station-info').text();
-    return Util.splitArtistTrack(artistTrack, [' - '], true);
+	let artistTrack = $('#player-station-info').text();
+	return Util.splitArtistTrack(artistTrack, [' - '], true);
 };
 
 Connector.isPlaying = () => $('.player-equalizer').is(':visible');
