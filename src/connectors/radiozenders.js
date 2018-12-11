ba7692b6444a402521f79710a6348ef4a1415982
Calvin <calvinturbo@gmail.com>
2018-12-11 13:43:41 +0100

    Radiozenders.FM connector file

---
+++ src/connectors/radiozenders.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Connector.playerSelector = '#jp_container_1';
+
+Connector.getArtistTrack = function() {
+    let artistTrack = $('#player-station-info').text();
+    return Util.splitArtistTrack(artistTrack, [' - '], true);
+};
+
+Connector.isPlaying = () => $('.player-equalizer').is(':visible');
