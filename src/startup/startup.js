738ff5a40b4f98446463812364c67b19f7572782
Malachi Soord <inverse.chi@gmail.com>
2018-12-13 22:31:10 +0100

    Wire up

--- src/startup/startup.js
+++ src/startup/startup.js
@@ -3,34 +3,45 @@
 require([
 	'config',
 	'wrapper/chrome',
+	'storage/chrome-storage',
 	'vendor/showdown.min'
-], function(config, chrome, showdown, ) {
-
+], function(config, chrome, ChromeStorage, showdown) {
 	$('#opt-in').click(function() {
-		window.close();
+		update(false);
 	});
 
 	$('#opt-out').click(function() {
-		window.close();
+		update(true);
 	});
 
 	let errorHandler = function(ex) {
 		console.log(ex.message);
 	}
 
+	function update(value) {
+		const options = ChromeStorage.getStorage(ChromeStorage.OPTIONS)
+
+		options.get().then((data) => {
+			data.disableGa = value
+			options.set(data)
+		});
+
+		window.close();
+	}
+
 	chrome.runtime.getPackageDirectoryEntry(function(root) {
 		root.getFile('PRIVACY.md', {}, function(fileEntry) {
-		  fileEntry.file(function(file) {
-			var reader = new FileReader();
-			reader.onloadend = function(e) {
+			fileEntry.file(function(file) {
+				var reader = new FileReader();
+				reader.onloadend = function(e) {
 
-				let converter = new showdown.Converter();
-				let content = converter.makeHtml(this.result);
-				$('.privacy-policy').html(content);
-			};
-			reader.readAsText(file);
-		  }, errorHandler);
+					let converter = new showdown.Converter();
+					let content = converter.makeHtml(this.result);
+					$('.privacy-policy').html(content);
+				};
+				reader.readAsText(file);
+			}, errorHandler);
 		}, errorHandler);
-	  });
+	});
 
 });
