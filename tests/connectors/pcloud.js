7bb410a69b82b7706ad01d92c2c60e6627e711ab
Russ <russ666999@gmail.com>
2018-12-18 22:42:47 +0200

    pCloud connector test fixed

--- tests/connectors/pcloud.js
+++ tests/connectors/pcloud.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function(driver, connectorSpec) {
-	connectorSpec.shouldBehaveLikeMusicSite(driver, {
-		url: 'https://my.pcloud.com/',
-		playButtonSelector: '#audioplayer[style] .playbackcontrol .pauseplay.play'
-	});
+    // Auth is required
+    connectorSpec.shouldLoadWebsite(driver, {
+        url: 'https://my.pcloud.com'
+    });
 };
