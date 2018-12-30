176dc86270076ad9dd754484f899e2f26046709f
Calvin <calvinturbo@gmail.com>
2018-12-30 14:59:17 +0100

    Wrong scrobbling for Q-Music radiostation

--- src/connectors/radiozenders.js
+++ src/connectors/radiozenders.js
@@ -1,10 +1,15 @@
 'use strict';
 
+var reverse = false;
+if ($('.player-station-title').text().includes('Qmusic')) {
+	reverse = true;
+}
+
 Connector.playerSelector = '#jp_container_1';
 
 Connector.getArtistTrack = function() {
 	let artistTrack = $('#player-station-info').text();
-	return Util.splitArtistTrack(artistTrack, [' - '], true);
+	return Util.splitArtistTrack(artistTrack, [' - '], reverse);
 };
 
 Connector.playButtonSelector = '.player-equalizer';
