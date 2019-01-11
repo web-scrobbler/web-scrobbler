9ef6f8e1c629cdef3e9964dfeddf935b9f8200ea
Peter Josling <peterjosling@google.com>
2019-01-11 09:31:03 +0000

    Add support for playapplemusic.com
    Reuses existing MusicKitJS connector.

--- src/core/connectors.js
+++ src/core/connectors.js
@@ -1244,6 +1244,10 @@ define(function() {
 		matches: ['*://music.zacharyseguin.ca/*'],
 		js: ['connectors/musickit.js'],
 	}, {
+		label: 'PlayAppleMusic.com',
+		matches: ['*://playapplemusic.com/*'],
+		js: ['connectors/musickit.js'],
+	}, {
 		label: '1001tracklists',
 		matches: ['*://www.1001tracklists.com/tracklist/*'],
 		js: ['connectors/1001tracklists.js'],
