3fb78baa0cc740a733fc72ec53d811146ffb9c43
KwonDae <goodday94@cnu.ac.kr>
2019-01-22 15:13:16 -0500

    add UniqueId

--- src/connectors/xiami.js
+++ src/connectors/xiami.js
@@ -8,6 +8,8 @@ Connector.artistSelector = '.music .info .singers';
 
 Connector.trackArtSelector = '.music .cover-link .active img';
 
+Connector.getUniqueID = () => $('.play-bar .content .title').attr('href').split('?')[0].split('/song/')[1];
+
 Connector.timeInfoSelector = '.audio-progress .range .bar .handle';
 
 Connector.isPlaying = () => {
