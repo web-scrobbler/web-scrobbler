92e8bcaed6fd50e47ecaf7c2d9b5ad41e54b4274
Rodrigo Leite <rodrigolf.dev@gmail.com>
2019-01-29 14:57:09 -0200

    Add funkwhale connector

---
+++ src/connectors/funkwhale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Connector.playerSelector = '.player';
+
+Connector.trackSelector = '.small.header.discrete.link.track';
+
+Connector.artistSelector = '.meta > .artist';
+
+Connector.albumSelector = '.meta > .album';
