ba9eba86afd812e316ea967901177b823c2293c7
Alexander <lapseofreason17@gmail.com>
2019-02-05 05:35:21 +0300

    improve code quality

--- src/connectors/r-a-d.io.js
+++ src/connectors/r-a-d.io.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
-Connector.artistTrackSelector = '#np'
+Connector.artistTrackSelector = '#np';
 
 Connector.playButtonSelector = '#stream-play';
 
-Connector.playerSelector = '.dynamic-row > .col-md-6'
+Connector.playerSelector = '.dynamic-row > .col-md-6';
