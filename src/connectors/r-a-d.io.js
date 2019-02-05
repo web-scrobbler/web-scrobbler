ce2e8749675385d7b77d37bedaec6b3100e01f45
ExpandingShapes <>
2019-02-05 23:11:44 +0300

    add maooings for total duration and current time

--- src/connectors/r-a-d.io.js
+++ src/connectors/r-a-d.io.js
@@ -5,3 +5,7 @@ Connector.artistTrackSelector = '#np';
 Connector.playButtonSelector = '#stream-play';
 
 Connector.playerSelector = '.dynamic-row > .col-md-6';
+
+Connector.durationSelector = '#progress-length';
+
+Connector.currentTimeSelector = '#progress-current';
