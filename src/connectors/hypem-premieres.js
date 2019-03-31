ff14a2195414d24926518ae5bf85968e8ee416cd
Jacob MacDonald <jaccarmac@gmail.com>
2019-03-31 05:06:55 -0500

    Add album selector for Hype Machine premieres. (#1947)

--- src/connectors/hypem-premieres.js
+++ src/connectors/hypem-premieres.js
@@ -4,6 +4,8 @@ Connector.playerSelector = '.hype-player';
 
 Connector.artistSelector = '#album-header-artist';
 
+Connector.albumSelector = '#album-header-title';
+
 Connector.trackSelector = 'li.active .title';
 
 Connector.trackArtSelector = 'img#album-big';
