d074de509c5bef73f6884aa98ab18cd6b6814d84
alexesprit <alex.esprit@gmail.com>
2019-05-05 20:58:14 +0300

    Extend tests of connectors list
    Test if JS files are exist.

--- tests/core/connectors.js
+++ tests/core/connectors.js
@@ -1,34 +1,63 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 const expect = require('chai').expect;
 const connectors = require('../../src/core/connectors');
 
-const propTypes = {
+const PROP_TYPES = {
 	allFrames: 'boolean',
 	matches: 'array',
 	label: 'string',
 	js: 'array',
 };
-const requiredProps = ['label', 'js'];
+const REQUIRED_PROPS = ['label', 'js'];
+
+function testProps(entry) {
+	for (const prop of REQUIRED_PROPS) {
+		if (!entry[prop]) {
+			throw new Error(`Missing property: ${prop}`);
+		}
+	}
+
+	for (const prop in entry) {
+		const type = PROP_TYPES[prop];
+		if (!type) {
+			throw new Error(`Unknown property: ${prop}`);
+		}
+
+		expect(entry[prop]).to.be.a(type);
+	}
+}
+
+function testPaths(entry) {
+	if (!entry.js) {
+		throw new Error('Missing property: js');
+	}
+
+	if (!Array.isArray(entry.js)) {
+		throw new Error('Invalid property type: js');
+	}
+
+	for (const f of entry.js) {
+		const jsPath = path.join(__dirname, '../../src', f);
+		try {
+			fs.statSync(jsPath);
+		} catch (e) {
+			throw new Error(`File is missing: ${f}`);
+		}
+	}
+}
 
 function runTests() {
-	for (let entry of connectors) {
+	for (const entry of connectors) {
 		it(`should have valid properties for ${entry.label}`, () => {
-			for (let prop of requiredProps) {
-				if (!entry[prop]) {
-					throw new Error(`Missing property: ${prop}`);
-				}
-			}
+			testProps(entry);
+		});
 
-			for (let prop in entry) {
-				let type = propTypes[prop];
-				if (!type) {
-					throw new Error(`Unknown property: ${prop}`);
-				}
-
-				expect(entry[prop]).to.be.a(type);
-			}
-
+		it(`should have js files for ${entry.label}`, () => {
+			testPaths(entry);
 		});
 	}
 }
