2c78c2eafba84fe181db87635205a22158c660ee
alexesprit <alex.esprit@gmail.com>
2019-05-06 23:29:24 +0300

    Improve TuneIn connector
    Some TuneIn stations have reversed "Artist-Track" format. The connector forces swapping artist-track values for such stations.
    
    Closes #1970.

--- src/connectors/tunein.js
+++ src/connectors/tunein.js
@@ -1,9 +1,35 @@
 'use strict';
 
+/**
+ * Stations have Artist-Track info reversed.
+ * Each entry is a part of station URL.
+ * @type {Array}
+ */
+const STATIONS_SWAP = [
+	'Cat-Country-987', '885-FM-So-Cal'
+];
+
 Connector.playerSelector = '.player__playerContainer___JEJ2U';
 
-Connector.artistTrackSelector = '#playerTitle';
+Connector.getArtistTrack = () => {
+	const artistTrackText = $('#playerTitle').text();
+	const swap = shouldSwapArtistTrack();
+
+	return Util.splitArtistTrack(artistTrackText, null, { swap });
+};
 
 Connector.trackArtSelector = '#playerArtwork';
 
 Connector.isPlaying = () => $('#Group-6').length > 0;
+
+function shouldSwapArtistTrack() {
+	const stationUrl = $('.nowPlaying__link___2FTVw').attr('href');
+
+	for (const station of STATIONS_SWAP) {
+		if (stationUrl.includes(station)) {
+			return true;
+		}
+	}
+
+	return false;
+}
