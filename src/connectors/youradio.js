6f72a9e04072117cdc47f041facb247d2860b7d4
alexesprit <alex.esprit@gmail.com>
2019-05-17 14:44:10 +0300

    Use track art selector instead of function

--- src/connectors/youradio.js
+++ src/connectors/youradio.js
@@ -9,8 +9,3 @@ Connector.artistSelector = '.yr2-navbar-player-metadata-info-artist';
 Connector.trackSelector = '.yr2-navbar-player-metadata-info-song';
 
 Connector.trackArtSelector = '.yr2-navbar-player-metadata-cover';
-
-Connector.getTrackArt = () => {
-	// Popup does not accepts URL without protocol
-	return `https:${$(Connector.trackArtSelector).attr('src')}`;
-};
