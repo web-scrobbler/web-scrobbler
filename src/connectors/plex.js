8250249445be6a3dc4d0d84d33ceedf1d36c657a
alexesprit <alex.esprit@gmail.com>
2019-05-31 14:35:20 +0300

    Update plex connector
    Move support for legacy UI back.
    
    Closes #1990.

--- src/connectors/plex.js
+++ src/connectors/plex.js
@@ -1,45 +1,79 @@
 'use strict';
 
-const playerBarSelector = '[class^=ControlsContainer-controlsContainer]';
+initConnector();
 
-Connector.playerSelector = '[class^=AudioVideoPlayerView-container]';
-
-Connector.artistSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title] > a:nth-child(1)`;
-
-Connector.trackSelector = `${playerBarSelector} a[class*=MetadataPosterTitle-singleLineTitle]`;
-
-// for watch-it-later videos
-Connector.artistTrackSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title]`;
-
-Connector.applyFilter(MetadataFilter.getYoutubeFilter());
-
-Connector.getTrack = () => {
-	if (Connector.getArtist()) {
-		return $(Connector.trackSelector).text();
+function initConnector() {
+	if (isNewUI()) {
+		initPropsForNewUI();
+	} else {
+		initPropsForLegacyUI();
 	}
-	return null;
-};
+}
 
-Connector.albumSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title] > a:nth-child(3)`;
+function isNewUI() {
+	return $('[class^=AudioVideoPlayerView-container]').length > 0;
+}
 
-/*
- * Don't use `playerBarSelector` to get track art
- * in both normal and expanded players.
- */
-Connector.trackArtSelector = '[class^=PosterCardImg-imageContainer] div';
+function initPropsForNewUI() {
+	const playerBarSelector = '[class^=ControlsContainer-controlsContainer]';
 
-Connector.timeInfoSelector = `${playerBarSelector} [class*=DurationRemaining-container]`;
+	Connector.playerSelector = '[class^=AudioVideoPlayerView-container]';
 
-Connector.getTrackArt = () => {
-	let trackArtEl = $(`${playerBarSelector} [class^=PosterCardImg-imageContainer] div`);
-	if (trackArtEl.length === 0) {
-		trackArtEl = $('[class^=AudioVideoFullPlayer] [class^=PosterCardImg-imageContainer] div');
-	}
+	Connector.artistSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title] > a:nth-child(1)`;
 
-	const elStyle = trackArtEl.css('background-image');
-	return Util.extractUrlFromCssProperty(elStyle);
-};
+	Connector.trackSelector = `${playerBarSelector} a[class*=MetadataPosterTitle-singleLineTitle]`;
 
-Connector.isPlaying = () => {
-	return $(`${playerBarSelector} [data-qa-id="pauseButton"]`).length > 0;
-};
+	// for watch-it-later videos
+	Connector.artistTrackSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title]`;
+
+	Connector.applyFilter(MetadataFilter.getYoutubeFilter());
+
+	Connector.getTrack = () => {
+		if (Connector.getArtist()) {
+			return $(Connector.trackSelector).text();
+		}
+		return null;
+	};
+
+	Connector.albumSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title] > a:nth-child(3)`;
+
+	/*
+	 * Don't use `playerBarSelector` to get track art
+	 * in both normal and expanded players.
+	 */
+	Connector.trackArtSelector = '[class^=PosterCardImg-imageContainer] div';
+
+	Connector.timeInfoSelector = `${playerBarSelector} [class*=DurationRemaining-container]`;
+
+	Connector.getTrackArt = () => {
+		let trackArtEl = $(`${playerBarSelector} [class^=PosterCardImg-imageContainer] div`);
+		if (trackArtEl.length === 0) {
+			trackArtEl = $('[class^=AudioVideoFullPlayer] [class^=PosterCardImg-imageContainer] div');
+		}
+
+		const elStyle = trackArtEl.css('background-image');
+		return Util.extractUrlFromCssProperty(elStyle);
+	};
+
+	Connector.isPlaying = () => {
+		return $(`${playerBarSelector} [data-qa-id="pauseButton"]`).length > 0;
+	};
+}
+
+function initPropsForLegacyUI() {
+	Connector.playerSelector = '#plex';
+
+	Connector.artistSelector = '.grandparent-title';
+
+	Connector.trackSelector = '.title-container .item-title';
+
+	Connector.currentTimeSelector = '.player-position';
+
+	Connector.durationSelector = '.player-duration';
+
+	Connector.isPlaying = () => $('.player .play-btn').hasClass('hidden');
+
+	Connector.getTrackArt = () => $('.player .media-poster').data('imageUrl');
+
+	Connector.getAlbum = () => $('.player .media-poster').data('parentTitle');
+}
