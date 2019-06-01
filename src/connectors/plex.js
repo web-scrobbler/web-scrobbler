2a1c2a7a2d09243a0c39aa2b741d5ed958b3b3de
alexesprit <alex.esprit@gmail.com>
2019-06-01 14:38:52 +0300

    Fix plex connector
    Update selectors to cover multiple UI versions.
    Drop legacy UI completely.
    
    Closes #1990.

--- src/connectors/plex.js
+++ src/connectors/plex.js
@@ -1,76 +1,43 @@
 'use strict';
 
-initConnector();
+const titleContainer = '[class*=PlayerControlsMetadata]';
 
-function initConnector() {
-	if (isNewUI()) {
-		initPropsForNewUI();
-	} else {
-		initPropsForLegacyUI();
+Connector.playerSelector = '[class^=AudioVideoPlayerView-container]';
+
+Connector.trackSelector = `${titleContainer} a[class*=MetadataPosterTitle]`;
+
+Connector.albumSelector = `${titleContainer} [class*=MetadataPosterTitle-title] > a:nth-child(3)`;
+
+Connector.artistSelector = `${titleContainer} [class*=MetadataPosterTitle-title] > a:nth-child(1)`;
+
+Connector.timeInfoSelector = `${Connector.playerSelector} [class*=DurationRemaining-container]`;
+
+// @ifndef FIREFOX
+// NOTE: Blob URLs used in Plex aren't supported by Firefox 67 and older.
+Connector.getTrackArt = () => {
+	let trackArtEl = $(`${Connector.playerSelector} [class^=PosterCardImg-imageContainer] div`);
+	if (trackArtEl.length === 0) {
+		trackArtEl = $('[class^=AudioVideoFullPlayer] [class^=PosterCardImg-imageContainer] div');
 	}
-}
 
-function isNewUI() {
-	return $('[class^=AudioVideoPlayerView-container]').length > 0;
-}
+	const elStyle = trackArtEl.css('background-image');
+	return Util.extractUrlFromCssProperty(elStyle);
+};
+// @endif
 
-function initPropsForNewUI() {
-	const playerBarSelector = '[class^=ControlsContainer-controlsContainer]';
+Connector.isPlaying = () => {
+	return $(`${Connector.playerSelector} [data-qa-id="pauseButton"]`).length > 0
+		|| $(`${Connector.playerSelector} [class^=plex-icon-player-pause]`).length > 0;
+};
 
-	Connector.playerSelector = '[class^=AudioVideoPlayerView-container]';
+// For watch-it-later videos
+Connector.artistTrackSelector = `${Connector.playerSelector} [class*=MetadataPosterTitle-title]`;
 
-	Connector.artistSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title] > a:nth-child(1)`;
+Connector.applyFilter(MetadataFilter.getYoutubeFilter());
 
-	Connector.trackSelector = `${playerBarSelector} a[class*=MetadataPosterTitle-singleLineTitle]`;
-
-	// for watch-it-later videos
-	Connector.artistTrackSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title]`;
-
-	Connector.applyFilter(MetadataFilter.getYoutubeFilter());
-
-	Connector.getTrack = () => {
-		if (Connector.getArtist()) {
-			return $(Connector.trackSelector).text();
-		}
-		return null;
-	};
-
-	Connector.albumSelector = `${playerBarSelector} [class*=MetadataPosterTitle-title] > a:nth-child(3)`;
-
-	Connector.timeInfoSelector = `${playerBarSelector} [class*=DurationRemaining-container]`;
-
-	// @ifndef FIREFOX
-	// NOTE: Blob URLs used in Plex aren't supported by Firefox 67 and older.
-	Connector.getTrackArt = () => {
-		let trackArtEl = $(`${playerBarSelector} [class^=PosterCardImg-imageContainer] div`);
-		if (trackArtEl.length === 0) {
-			trackArtEl = $('[class^=AudioVideoFullPlayer] [class^=PosterCardImg-imageContainer] div');
-		}
-
-		const elStyle = trackArtEl.css('background-image');
-		return Util.extractUrlFromCssProperty(elStyle);
-	};
-	// @endif
-
-	Connector.isPlaying = () => {
-		return $(`${playerBarSelector} [data-qa-id="pauseButton"]`).length > 0;
-	};
-}
-
-function initPropsForLegacyUI() {
-	Connector.playerSelector = '#plex';
-
-	Connector.artistSelector = '.grandparent-title';
-
-	Connector.trackSelector = '.title-container .item-title';
-
-	Connector.currentTimeSelector = '.player-position';
-
-	Connector.durationSelector = '.player-duration';
-
-	Connector.isPlaying = () => $('.player .play-btn').hasClass('hidden');
-
-	Connector.getTrackArt = () => $('.player .media-poster').data('imageUrl');
-
-	Connector.getAlbum = () => $('.player .media-poster').data('parentTitle');
-}
+Connector.getTrack = () => {
+	if (Connector.getArtist()) {
+		return $(Connector.trackSelector).text();
+	}
+	return null;
+};
