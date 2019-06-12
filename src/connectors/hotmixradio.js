f786fe47cc4064271cf414ad5e8f37fc7b4ed25d
alexesprit <alex.esprit@gmail.com>
2019-06-12 12:36:27 +0300

    Update hotmixradio connector
    Use track art selector instead of function.
    Remove unused artist selector.

--- src/connectors/hotmixradio.js
+++ src/connectors/hotmixradio.js
@@ -2,16 +2,11 @@
 
 Connector.playerSelector = '#new_ajax-player';
 
-Connector.artistSelector = '#hotmixPlayerSongArtist .ajax';
-
 Connector.getArtist = () => getScrollableText('#hotmixPlayerSongArtist');
 
 Connector.getTrack = () => getScrollableText('#hotmixPlayerSongTitle');
 
-Connector.getTrackArt = () => {
-	let trackArtUrl = $('#hotmixPlayerSongCover').attr('src');
-	return trackArtUrl !== null ? `${trackArtUrl}` : null;
-};
+Connector.trackArtSelector = '#hotmixPlayerSongCover';
 
 Connector.isPlaying = () => $('#play-pause .fa-pause').length > 0;
 
