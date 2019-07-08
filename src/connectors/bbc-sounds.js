64fbc32ff23999270155004ba2776e40d0a41807
alexesprit <alex.esprit@gmail.com>
2019-07-08 06:52:39 +0300

    Update bbc-sounds connector
    Remove redundant functions.
    Fix an issue when the connector didn't recognized now playing track after page is loaded.

--- src/connectors/bbc-sounds.js
+++ src/connectors/bbc-sounds.js
@@ -6,8 +6,4 @@ Connector.artistSelector = '.sc-c-track__artist';
 
 Connector.trackSelector = '.sc-c-track__title';
 
-// TODO: remove these functions
-
-Connector.getArtist = () => $(Connector.artistSelector).first().text();
-
-Connector.getTrack = () => $(Connector.trackSelector).first().text();
+Connector.onReady = Connector.onStateChanged;
