8c2d56ca16818e41028ef09a1ee1867e414255e0
alexesprit <alex.esprit@gmail.com>
2019-07-15 19:30:16 +0300

    Merge hooks module into main module

--- src/core/background/main.js
+++ src/core/background/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/* @ifndef DEBUG
+chrome.runtime.onInstalled.addListener((event) => {
+	if ('install' !== event.reason) {
+		return;
+	}
+
+	chrome.tabs.create({
+		url: '/startup/startup.html'
+	});
+});
+/* @endif */
+
 /**
  * Background script entry point.
  */
