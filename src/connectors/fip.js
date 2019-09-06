bb8a9ba56c6649d020f5f85d140484ddea6ec37e
Romain Briand <contact@romain-briand.fr>
2019-09-06 22:04:42 +0200

    update FIP connector

--- src/connectors/fip.js
+++ src/connectors/fip.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const filter = new MetadataFilter({
+	artist: removeYearFromArtist
+});
+
 Connector.playerSelector = '.player';
 
 Connector.artistSelector = '.playing-now .now-info-subtitle';
@@ -13,3 +17,10 @@ Connector.trackArtSelector = '.playing-now .playing-now-cover img';
 Connector.isPlaying = () => {
 	return $('.playing-now button.live-button').hasClass('pause');
 };
+
+Connector.applyFilter(filter);
+
+function removeYearFromArtist(text) {
+	let regexp = new RegExp(/\s+\(\d{4}\)(?=[^\s+(\d{4})]*$)/gm);
+	return text.replace(regexp, '');
+}
