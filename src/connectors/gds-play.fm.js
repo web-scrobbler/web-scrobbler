4ae98358fe0e804eaead171aaa45eb06ad4c209d
rxuriguera <rxuriguera@gmail.com>
2019-09-30 17:25:21 +0200

    Add support for GDS.FM

---
+++ src/connectors/gds-play.fm.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Connector.playerSelector = '.meta-wrap';
+
+Connector.artistSelector = '.meta-artist';
+
+Connector.trackSelector = '.meta-title';
+
+Connector.playButtonSelector = '[src="/assets/images/play.gif"]';
+
+Connector.onReady = Connector.onStateChanged;
