57ab66136f78b13d6b85ae31221697f20330225b
James Carter <jamescdcarter@gmail.com>
2019-10-16 21:30:57 +0100

    fixed time + duration selectors

--- src/connectors/poolside.js
+++ src/connectors/poolside.js
@@ -4,5 +4,5 @@ Connector.playButtonSelector = '.middle.paused';
 Connector.playerSelector = '.inner-size';
 Connector.trackSelector = '.current-track a:first';
 Connector.artistSelector = '.current-track a:last';
-Connector.currentTimeSelector = '.timer span:first-child';
-Connector.durationSelector = '.timer span:first-child';
+Connector.currentTimeSelector = '.timer span:first';
+Connector.durationSelector = '.timer span:last';
