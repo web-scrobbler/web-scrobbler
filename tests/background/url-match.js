663ec28f09283f67d5488a977e16dd102b73d570
alexesprit <alex.esprit@gmail.com>
2019-10-26 15:59:44 +0300

    Add more tests

--- tests/background/url-match.js
+++ tests/background/url-match.js
@@ -8,21 +8,40 @@ const expect = require('chai').expect;
 const UrlMatch = require('../../src/core/background/util/url-match');
 
 const URL_MATCH_DATA = [{
+	description: 'should return false for invalid result',
+	pattern: 1,
+	urls: ['http://example.com/'],
+	expected: false,
+}, {
+	description: 'should return false for non-matched pattern',
+	pattern: '*://test.com/*',
+	urls: ['http://example.com/', 'http://www.test.com/page'],
+	expected: false,
+}, {
+	description: 'should return false for input with invalid scheme',
+	pattern: '*://test.com/*',
+	urls: ['wtf://example.com/'],
+	expected: false,
+}, {
 	description: 'should match exact pattern',
 	pattern: 'http://example.com/',
-	urls: ['http://example.com/']
+	urls: ['http://example.com/'],
+	expected: true,
 }, {
 	description: 'should match for all schemes',
 	pattern: '*://example.com/',
-	urls: ['http://example.com/', 'https://example.com/']
+	urls: ['http://example.com/', 'https://example.com/'],
+	expected: true,
 }, {
 	description: 'should match for all subdomains',
 	pattern: '*://*.example.com/',
-	urls: ['http://www.example.com/', 'https://just.example.com/']
+	urls: ['http://www.example.com/', 'https://just.example.com/'],
+	expected: true,
 }, {
 	description: 'should match for all hosts',
 	pattern: '*://*/player',
-	urls: ['http://example.com/player', 'https://whatever.com/player']
+	urls: ['http://example.com/player', 'https://whatever.com/player'],
+	expected: true,
 }, {
 	description: 'should match for all paths',
 	pattern: '*://example.com/*',
@@ -30,7 +49,8 @@ const URL_MATCH_DATA = [{
 		'http://example.com/path1',
 		'https://example.com/path2',
 		'http://example.com/'
-	]
+	],
+	expected: true,
 }, {
 	description: 'should match for all tlds',
 	pattern: '*://example.*/',
@@ -38,7 +58,8 @@ const URL_MATCH_DATA = [{
 		'http://example.com/',
 		'http://example.org/',
 		'http://example.co.uk/',
-	]
+	],
+	expected: true,
 }, {
 	description: 'should match URL for all schemes, tlds and paths',
 	pattern: '*://example.*/*',
@@ -46,17 +67,19 @@ const URL_MATCH_DATA = [{
 		'http://example.com/',
 		'https://example.com/path',
 		'https://example.co.uk/',
-	]
+	],
+	expected: true,
 }];
 
 function runTests() {
 	for (let data of URL_MATCH_DATA) {
-		let { description, pattern, urls } = data;
+		let { description, pattern, urls, expected } = data;
 
-		describe(`Pattern: ${pattern}`, function() {
+		describe(`Pattern: ${pattern}`, () => {
 			for (let url of urls) {
-				it(`${description}: ${url}`, function() {
-					expect(UrlMatch.test(url, pattern)).to.be.true;
+				it(`${description}: ${url}`, () => {
+					const actual = UrlMatch.test(url, pattern);
+					expect(expected).to.be.equal(actual);
 				});
 
 			}
