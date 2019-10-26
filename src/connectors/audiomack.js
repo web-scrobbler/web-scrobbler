c9bf475b66eff584da9b4321d5bed4d133a48e72
alexesprit <alex.esprit@gmail.com>
2019-10-26 11:21:31 +0300

    Improve audiomack connector
    Add featuring artists to the track title.
    Don't scrobble playlist names.

--- src/connectors/audiomack.js
+++ src/connectors/audiomack.js
@@ -1,10 +1,30 @@
 'use strict';
 
+const albumSelector = '.player__album-text > a';
+const trackSelector = '.player .player__title';
+const albumLabelSelector = '.player__album-text';
+const featArtistSelector = '.player .player__featuring';
+
 Connector.artistSelector = '.player .player__artist';
 
-Connector.albumSelector = '.player__album-text > a';
+Connector.getAlbum = () => {
+	if (isAlbum()) {
+		return Util.getTextFromSelectors(albumSelector);
+	}
 
-Connector.trackSelector = '.player .player__title';
+	return null;
+};
+
+Connector.getTrack = () => {
+	const track = Util.getTextFromSelectors(trackSelector);
+	const featArtist = Util.getTextFromSelectors(featArtistSelector);
+
+	if (featArtist) {
+		return `${track} (${featArtist})`;
+	}
+
+	return track;
+};
 
 Connector.playerSelector = '.player';
 
@@ -15,3 +35,16 @@ Connector.isPlaying = () => {
 Connector.currentTimeSelector = '.waveform__elapsed';
 
 Connector.durationSelector = '.waveform__duration';
+
+/**
+ * Check if an album is playing.
+ * @return {Boolean} Check result
+ */
+function isAlbum() {
+	const label = Util.getTextFromSelectors(albumLabelSelector);
+	if (label) {
+		return label.includes('Album');
+	}
+
+	return false;
+}
