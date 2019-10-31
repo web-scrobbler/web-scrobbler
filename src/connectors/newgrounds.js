b0753fef9c67e70cfc1229d8a197545c21d7e66f
Ydeu <sukurma98@gmail.com>
2019-10-31 20:37:00 +0100

    comments removed

--- src/connectors/newgrounds.js
+++ src/connectors/newgrounds.js
@@ -1,28 +1,17 @@
 'use strict';
 
-// argument is id of div element
 Connector.playerSelector = '#_ngHiddenAudioPlayer';
 
-// argument is id of button element
 Connector.playButtonSelector = '#global-audio-player-play';
 
-// argument is id of button element
 Connector.pauseButtonSelector = '#global-audio-player-pause';
 
-// argument is class name in div element
-// if does not works, maybe use #_ngHiddenAudioPlayerDetails id which contains info about artist and song name
 Connector.trackArtSelector = '.ng-apg-media-icon';
 
-// argument is class name in <a/> element
-// if does not works, maybe use #_ngHiddenAudioPlayerDetails id which contains info about artist and song name
 Connector.artistSelector = '.ng-apg-media-artist';
 
-// argument is class name in <a/> element
-// if does not works, maybe use #_ngHiddenAudioPlayerDetails id which contains info about artist and song name
 Connector.trackSelector = '.ng-apg-media-title';
 
-// argument is id of div element
 Connector.currentTimeSelector = '#global-audio-player-progress';
 
-// argument is id of div element
 Connector.durationSelector = '#global-audio-player-duration';
