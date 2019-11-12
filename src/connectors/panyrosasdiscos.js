105aa3a5aea01d806c408b2fc9d5bf6110263ccd
Jacob MacDonald <jaccarmac@gmail.com>
2019-11-12 00:01:20 -0600

    Use Connector.getTrackInfo.

--- src/connectors/panyrosasdiscos.js
+++ src/connectors/panyrosasdiscos.js
@@ -4,24 +4,18 @@ Connector.playerSelector = '#interfaceMI_0';
 
 Connector.pauseButtonSelector = '#statusMI_0 .mjp-playing';
 
-const splitAlbumData = () => {
+Connector.trackSelector = '#T_mp3j_0';
+
+Connector.getTrackInfo = () => {
 	const text = Util.getTextFromSelectors('.entry-title');
 	const pattern = /(pyr[0-9]+) (.+) – (.+)/g;
 	const matches = [...text.matchAll(pattern)][0];
 	return {
-		catalog: matches[1],
+		uniqueID: `${matches[1]}#${$('.mp3j_A_current').attr('id')}`,
 		artist: matches[2],
 		album: matches[3],
 	};
 };
-
-Connector.getArtist = () => splitAlbumData().artist;
-
-Connector.getAlbum = () => splitAlbumData().album;
-
-Connector.trackSelector = '#T_mp3j_0';
-
-Connector.getUniqueID = () => `${splitAlbumData().catalog}#${$('.mp3j_A_current').attr('id')}`;
 
 Connector.trackArtSelector = '.entry-content > p > a > img';
 
