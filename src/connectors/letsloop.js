275da8d70d16d1c6da6fccff1869b05d22476363
alexesprit <alex.esprit@gmail.com>
2019-12-05 20:00:49 +0300

    Improve letsloop connector
    Add support for track art.

--- src/connectors/letsloop.js
+++ src/connectors/letsloop.js
@@ -5,3 +5,5 @@ Connector.playerSelector = '#player';
 Connector.artistSelector = '#current-song-title .sub a';
 
 Connector.trackSelector = '#current-song-title .main a';
+
+Connector.trackArtSelector = '#player-image';
