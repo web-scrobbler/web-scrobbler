2b47b5d8bd202f12b36756183dbd5eb6f193f90d
Suyeon <tndus970120@gmail.com>
2019-12-13 19:51:01 +0900

    Implement flo.js connector

---
+++ src/connectors/flo.js
@@ -0,0 +1,15 @@
+'use strict';
+//# id 접근 . 클래스접근
+Connector.playerSelector = '#app .player_ct mini';
+
+Connector.artistSelector = '.track_info .artist';
+
+Connector.trackSelector = '.track_info .title';
+
+Connector.currentTimeSelector = '.time_current';
+
+Connector.durationSelector = '.time_all';
+
+Connector.pauseButtonSelector = '.control_area .icon-player btn-player-play';
+
+Connector.trackArtSelector = '.playbar_ct .thumb';
