b990e195bef7f5f22ef82bcb7c22f416079400ca
alexesprit <alex.esprit@gmail.com>
2019-12-21 20:30:14 +0300

    Improve kodi connector
    Get track info from local player.
    Add track art.
    
    Resolves #2202.

--- src/connectors/kodi.js
+++ src/connectors/kodi.js
@@ -1,13 +1,72 @@
 'use strict';
 
-Connector.playerSelector = '#player-kodi';
+const trackArtSelector = '.playing-thumb';
 
-Connector.artistSelector = `${Connector.playerSelector} .playing-subtitle`;
+const fieldSelectors = {
+	artist: '.playing-subtitle',
+	track: '.playing-title',
+	currentTime: 'currentTimeSelector',
+	duration: '.playing-time-duration'
+};
 
-Connector.trackSelector = `${Connector.playerSelector} .playing-title`;
+const kodiPlayerSelector = '#player-kodi';
+const localPlayerSelector = '#player-local';
 
-Connector.currentTimeSelector = `${Connector.playerSelector} .playing-time-current`;
+Connector.playerSelector = '#player-wrapper';
 
-Connector.durationSelector = `${Connector.playerSelector} .playing-time-duration`;
+Connector.getTrackInfo = () => {
+	const context = getCurrentContext();
+	const trackInfo = {};
 
-Connector.isPlaying = () => $('body').hasClass('kodi-playing');
+	for (const field in fieldSelectors) {
+		const selector = fieldSelectors[field];
+		const fullSelector = `${context} ${selector}`;
+
+		trackInfo[field] = Util.getTextFromSelectors(fullSelector);
+	}
+
+	const trackArtFullSelector = `${context} ${trackArtSelector}`;
+	trackInfo.trackArt = getTrackArtUrl(trackArtFullSelector);
+
+	return trackInfo;
+};
+
+Connector.isPlaying = () => {
+	const body = $('body');
+
+	if (body.hasClass('active-player-local')) {
+		return body.hasClass('local-playing');
+	}
+	if (body.hasClass('active-player-kodi')) {
+		return body.hasClass('kodi-playing');
+	}
+
+	throw new Error('Unknown context');
+};
+
+function getCurrentContext() {
+	const body = $('body');
+
+	if (body.hasClass('active-player-local')) {
+		return localPlayerSelector;
+	}
+	if (body.hasClass('active-player-kodi')) {
+		return kodiPlayerSelector;
+	}
+
+	throw new Error('Unknown context');
+}
+
+/*
+ * `#player-local .playing-thumb` contains invalid path in `src` attribute.
+ * Use a custom function to extract URL from CSS property only.
+ */
+function getTrackArtUrl(selector) {
+	const element = Util.queryElements(selector);
+	if (element) {
+		const propertyValue = element.css('background-image');
+		return Util.extractUrlFromCssProperty(propertyValue);
+	}
+
+	return null;
+}
