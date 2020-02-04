4ba94f7bddb1c0da7ef4f05cc387fd5a9788f653
tallaghi <tallaghi@gmail.com>
2020-02-04 18:45:32 -0500

    update relisten.js
    change let to const

--- src/connectors/relisten.js
+++ src/connectors/relisten.js
@@ -12,14 +12,14 @@ function setupConnector() {
 	Connector.trackSelector = '.player .content .info .song-title';
 
 	Connector.getAlbum = () => {
-		let artistAlbum = $('.player .content .info .band-title').text();
-		let albumName = artistAlbum.substring(artistAlbum.indexOf('–') + 1, artistAlbum.length);
+		const artistAlbum = $('.player .content .info .band-title').text();
+		const albumName = artistAlbum.substring(artistAlbum.indexOf('–') + 1, artistAlbum.length);
 		return albumName;
 	};
 
 	Connector.getArtist = () => {
-		let artistAlbum = $('.player .content .info .band-title').text();
-		let artistName = artistAlbum.substring(0, artistAlbum.indexOf('–'));
+		const artistAlbum = $('.player .content .info .band-title').text();
+		const artistName = artistAlbum.substring(0, artistAlbum.indexOf('–'));
 		return artistName;
 	};
 	Connector.isPlaying = () => {
