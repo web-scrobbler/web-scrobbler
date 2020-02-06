c05745d16b92cfb7ed01b16ef1395bcd96f5876c
tallaghi <tallaghi@gmail.com>
2020-02-05 20:04:36 -0500

    Update relisten.js
    formatting updates as per inverse's recommendation

--- src/connectors/relisten.js
+++ src/connectors/relisten.js
@@ -1,29 +1,24 @@
 'use strict';
 
-function setupConnector() {
-	Connector.playButtonSelector = '.playpause i';
+Connector.currentTimeSelector = '.player div.timing:not(.duration) div:last-child';
 
-	Connector.currentTimeSelector = '.player div.timing:not(.duration) div:last-child';
+Connector.durationSelector = '.player div.timing.duration div:last-child';
 
-	Connector.durationSelector = '.player div.timing.duration div:last-child';
+Connector.playerSelector = '.navigation > .player';
 
-	Connector.playerSelector = '.navigation > .player';
+Connector.trackSelector = '.player .content .info .song-title';
 
-	Connector.trackSelector = '.player .content .info .song-title';
+Connector.getAlbum = () => {
+	const artistAlbum = $('.player .content .info .band-title').text();
+	const albumName = artistAlbum.substring(artistAlbum.indexOf('–') + 1, artistAlbum.length);
+	return albumName;
+};
 
-	Connector.getAlbum = () => {
-		const artistAlbum = $('.player .content .info .band-title').text();
-		const albumName = artistAlbum.substring(artistAlbum.indexOf('–') + 1, artistAlbum.length);
-		return albumName;
-	};
-
-	Connector.getArtist = () => {
-		const artistAlbum = $('.player .content .info .band-title').text();
-		const artistName = artistAlbum.substring(0, artistAlbum.indexOf('–'));
-		return artistName;
-	};
-	Connector.isPlaying = () => {
-		return $('.playpause .fas.fa-pause').length;
-	};
-}
-setupConnector();
+Connector.getArtist = () => {
+	const artistAlbum = $('.player .content .info .band-title').text();
+	const artistName = artistAlbum.substring(0, artistAlbum.indexOf('–'));
+	return artistName;
+};
+Connector.isPlaying = () => {
+	return $('.playpause .fas.fa-pause').length;
+};
