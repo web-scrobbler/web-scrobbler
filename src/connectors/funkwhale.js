3ad06b8a6d6219f289dcf3b338c8b5ffbc3c8ffa
alexesprit <alex.esprit@gmail.com>
2020-03-29 04:08:42 +0300

    Update funkwhale connector
    Add support for new UI.
    Add improvements for old UI.
    
    Closes #2241.

--- src/connectors/funkwhale.js
+++ src/connectors/funkwhale.js
@@ -1,9 +1,51 @@
 'use strict';
 
-Connector.playerSelector = '.player';
+const oldPlayerSelector = '.player';
 
-Connector.trackSelector = '.small.header.discrete.link.track';
+setupConnector();
 
-Connector.artistSelector = '.meta > .artist';
+function setupConnector() {
+	const isOldPlayer = $(oldPlayerSelector).length > 0;
 
-Connector.albumSelector = '.meta > .album';
+	if (isOldPlayer) {
+		setupOldPlayer();
+	} else {
+		setupNewPlayer();
+	}
+}
+
+function setupOldPlayer() {
+	Connector.playerSelector = oldPlayerSelector;
+
+	Connector.playButtonSelector = '.controls .play';
+
+	Connector.trackSelector = '.small.header.discrete.link.track';
+
+	Connector.artistSelector = '.meta > .artist';
+
+	Connector.albumSelector = '.meta > .album';
+
+	Connector.currentTimeSelector = '.timer.start';
+
+	Connector.durationSelector = '.timer.total';
+
+	Connector.trackArtSelector = '.player .image img';
+}
+
+function setupNewPlayer() {
+	Connector.playerSelector = '.controls-row';
+
+	Connector.playButtonSelector = '.player-controls .play';
+
+	Connector.trackSelector = '.content .track';
+
+	Connector.artistSelector = '.meta > a:first';
+
+	Connector.albumSelector = '.meta > a:last';
+
+	Connector.currentTimeSelector = '.timer .start';
+
+	Connector.durationSelector = '.timer .total';
+
+	Connector.trackArtSelector = '.controls .image img';
+}
