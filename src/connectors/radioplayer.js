f2b5a62ccf6a194e04c5803681a5e401923b5af9
alexesprit <alex.esprit@gmail.com>
2020-04-08 16:31:47 +0300

    Improve radioplayer connector
    Get artist and track from `iframe` element.
    Use latest ConnectorAPI to get track info.

--- src/connectors/radioplayer.js
+++ src/connectors/radioplayer.js
@@ -1,19 +1,26 @@
 'use strict';
 
-const artistTrackSelectors = [
-	'.song-text', '.scrolling-text'
-];
+const artistSelector = '.playing-now .pod-title';
+const trackSelector = '.playing-now .pod-desc';
 
 Connector.playerSelector = '.radioplayer-head';
 
 Connector.playButtonSelector = '#play';
 
-Connector.getArtistTrack = () => {
-	for (const selector of artistTrackSelectors) {
-		if ($(selector).length > 0) {
-			return Util.splitArtistTrack($(selector).text());
-		}
+// Priority: 1.
+Connector.artistTrackSelector = [
+	'.song-text', '.scrolling-text'
+];
+
+// Priority: 2.
+Connector.getTrackInfo = () => {
+	const radioFeedFrame = $('#radiofeed_iframe').contents();
+	if (radioFeedFrame.length === 0) {
+		return null;
 	}
 
-	return null;
+	const artist = radioFeedFrame.find(artistSelector).text();
+	const track = radioFeedFrame.find(trackSelector).text();
+
+	return { artist, track };
 };
