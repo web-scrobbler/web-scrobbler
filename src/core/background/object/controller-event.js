6a3c130b81e7af9b085093aec333d7630c25a1ab
alexesprit <alex.esprit@gmail.com>
2020-04-15 17:54:57 +0300

    Refactor Controller class
    Move GA and Notifications logic to `extension` module.

---
+++ src/core/background/object/controller-event.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const ControllerReset = 'ControllerReset';
+
+const SongNowPlaying = 'SongNowPlaying';
+
+const SongScrobbled = 'Scrobbled';
+
+const SongUnrecognized = 'SongUnrecognized';
+
+define(() => {
+	return {
+		ControllerReset,
+		SongNowPlaying,
+		SongScrobbled,
+		SongUnrecognized
+	};
+});
