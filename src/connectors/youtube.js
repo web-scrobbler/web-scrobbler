91f38f094b796584718766ef77213c0fdc2ca620
alexesprit <alex.esprit@gmail.com>
2020-04-19 18:36:01 +0300

    Fix youtube connector
    Add more categories to the list of categories.
    Allow scrobbling if category is missing.
    Fix processing "Gaming" and "Education" categories.
    Set an unknown category if fetching is failed.
    
    Closes #2368.
    Closes #2369.

--- src/connectors/youtube.js
+++ src/connectors/youtube.js
@@ -1,27 +1,5 @@
 'use strict';
 
-const CATEGORY_MUSIC = '/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ';
-const CATEGORY_SPORTS = '/channel/UCDbM8yVukVKPWUQSODaw_Mw';
-const CATEGORY_COMEDY = '/channel/UCEgdi0XIXXZ-qJOFPf4JSKw';
-const CATEGORY_ENTERTAINMENT = '/channel/UCi-g4cjqGV7jvU8aeSuj0jQ';
-const CATEGORY_HOWTO_AND_STYLE = '/channel/UC1vGae2Q3oT5MkhhfW8lwjg';
-const CATEGORY_NEWS_AND_POLITICS = '/channel/UCYfdidRxbB8Qhf0Nx7ioOYw';
-const CATEGORY_FILM_AND_ANIMATION = '/channel/UCxAgnFbkxldX6YUEvdcNjnA';
-const CATEGORY_SCIENCE_AND_TECHNOLOGY = '/channel/UCiDF_uaU1V00dAc8ddKvNxA';
-
-const CATEGORY_PENDING = 'YT_DUMMY_CATEGORY_PENDING';
-
-const CATEGORIES = [
-	CATEGORY_MUSIC,
-	CATEGORY_SPORTS,
-	CATEGORY_COMEDY,
-	CATEGORY_ENTERTAINMENT,
-	CATEGORY_HOWTO_AND_STYLE,
-	CATEGORY_NEWS_AND_POLITICS,
-	CATEGORY_FILM_AND_ANIMATION,
-	CATEGORY_SCIENCE_AND_TECHNOLOGY
-];
-
 /**
  * Array of categories allowed to be scrobbled.
  * @type {Array}
@@ -42,6 +20,44 @@ const videoSelector = '.html5-main-video';
 
 const videoTitleSelector = '.html5-video-player .ytp-title-link';
 const channelNameSelector = '#top-row .ytd-channel-name a';
+
+/**
+ * DOM-based category fetching.
+ */
+
+// Dummy category indicates an actual category is being fetched
+const CATEGORY_PENDING = 'YT_DUMMY_CATEGORY_PENDING';
+// Fallback value in case when we cannot fetch a category.
+const CATEGORY_UNKNOWN = 'YT_DUMMY_CATEGORY_UNKNOWN';
+
+const CATEGORY_MUSIC = '/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ';
+const CATEGORY_ENTERTAINMENT = '/channel/UCi-g4cjqGV7jvU8aeSuj0jQ';
+
+const CATEGORIES = [
+	'/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ', // Music
+	'/channel/UCi-g4cjqGV7jvU8aeSuj0jQ', // Entertainment
+	'/channel/UCDbM8yVukVKPWUQSODaw_Mw', // Sports
+	'/channel/UCEgdi0XIXXZ-qJOFPf4JSKw', // Comedy
+	'/channel/UC1vGae2Q3oT5MkhhfW8lwjg', // Howto & Style / People & Blogs
+	'/channel/UCFYJCBaHRzLJrnhRglM3GdA', // Pets & Animals
+	'/channel/UCYfdidRxbB8Qhf0Nx7ioOYw', // News & Politics
+	'/channel/UCHiJaXgDo_JnsfOmSe-HgzA', // Auto & Vehicles
+	'/channel/UCxAgnFbkxldX6YUEvdcNjnA', // Film & Animation
+	'/channel/UCiDF_uaU1V00dAc8ddKvNxA', // Science & Technology
+	'/channel/UCM6FFmRAK_uTICRwyTubV0A', // Nonprofits & Activism
+	'/channel/UCUrY9QznFi4-S3jWihvaBpA', // Travel & Events
+	'/gaming',
+	'/learning',
+];
+
+const ytFormattedString = '.ytd-metadata-row-renderer .yt-formatted-string';
+const gamingSelector = `${ytFormattedString}[href^="/gaming"]`;
+const learningSelector = `${ytFormattedString}[href^="/learning"]`;
+const genericCatSelector = `${ytFormattedString}[href^="/channel/"]`;
+
+const categorySelectors = [
+	genericCatSelector, gamingSelector, learningSelector
+];
 
 let currentVideoDescription = null;
 let artistTrackFromDescription = null;
@@ -112,17 +128,16 @@ Connector.isScrobblingAllowed = () => {
 		return true;
 	}
 
-	const videoId = Connector.getUniqueID();
-	if (videoId) {
-		const videoCategory = getVideoCategory(videoId);
-		if (videoCategory !== null) {
-			return allowedCategories.includes(videoCategory);
-		}
-
+	const videoCategory = getVideoCategory(Connector.getUniqueID());
+	if (!videoCategory) {
 		return false;
 	}
 
-	return true;
+	if (videoCategory === CATEGORY_UNKNOWN) {
+		return true;
+	}
+
+	return allowedCategories.includes(videoCategory);
 };
 
 Connector.applyFilter(MetadataFilter.getYoutubeFilter());
@@ -137,40 +152,47 @@ function setupEventListener() {
  * @return {String} Video category
  */
 function getVideoCategory(videoId) {
-	if (videoId === null) {
+	if (!videoId) {
 		return null;
 	}
 
-	if (!categoryCache.has(videoId)) {
-		/*
-		 * Add dummy category for videoId to prevent
-		 * fetching category multiple times.
-		 */
-		categoryCache.set(videoId, CATEGORY_PENDING);
-
-		fetchCategoryId(videoId).then((category) => {
-			if (category === null) {
-				Util.debugLog(`Failed to resolve category for ${videoId}`, 'warn');
-			}
-
-			categoryCache.set(videoId, category);
-		});
-
-		return null;
+	if (categoryCache.has(videoId)) {
+		return categoryCache.get(videoId);
 	}
 
-	return categoryCache.get(videoId);
+	/*
+	 * Add dummy category for videoId to prevent
+	 * fetching category multiple times.
+	 */
+	categoryCache.set(videoId, CATEGORY_PENDING);
+
+	fetchCategoryId().then((category) => {
+		console.log(`Fetched category for ${videoId}: ${category}`);
+		categoryCache.set(videoId, category);
+	}).catch((err) => {
+		Util.debugLog(`Failed to fetch category for ${videoId}: ${err}`, 'warn');
+		categoryCache.set(videoId, CATEGORY_UNKNOWN);
+	});
+
+	return null;
 }
 
 async function fetchCategoryId() {
 	await fillMoreSection();
 
-	const ytChannelUrls =
-		$('.ytd-metadata-row-renderer .yt-formatted-string[href^="/channel/"]');
-	if (ytChannelUrls.length === 1) {
-		return ytChannelUrls.attr('href');
+	let ytChannelUrls = [];
+	for (const selector of categorySelectors) {
+		ytChannelUrls = $(selector);
+		if (ytChannelUrls.length > 0) {
+			break;
+		}
 	}
 
+	if (ytChannelUrls.length === 0) {
+		return CATEGORY_UNKNOWN;
+	} else if (ytChannelUrls.length === 1) {
+		return ytChannelUrls.attr('href');
+	}
 	for (const data of ytChannelUrls) {
 		const ytChannelUrl = $(data).attr('href');
 		if (CATEGORIES.includes(ytChannelUrl)) {
@@ -178,7 +200,7 @@ async function fetchCategoryId() {
 		}
 	}
 
-	throw new Error('The video has no category URL!');
+	return CATEGORY_UNKNOWN;
 }
 
 async function fillMoreSection() {
@@ -232,6 +254,7 @@ async function readConnectorOptions() {
 	if (await Util.getOption('YouTube', 'scrobbleEntertainmentOnly')) {
 		allowedCategories.push(CATEGORY_ENTERTAINMENT);
 	}
+	Util.debugLog(`Allowed categories: ${allowedCategories.join(', ')}`);
 }
 
 function getVideoDescription() {
