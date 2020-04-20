eae4aa72c14abff1999fd9b6d471c1f9f0e00515
Malachi Soord <inverse.chi@gmail.com>
2020-04-20 22:01:57 +0200

    implement

--- src/connectors/clyp.js
+++ src/connectors/clyp.js
@@ -1,9 +1,49 @@
 'use strict';
 
-Connector.playerSelector = '#player-page';
 
-Connector.artistSelector = '.user-summary-name';
+function setup() {
+	if (isPlayerPage()) {
+		setupPlayerPage();
+	} else if (isCardsPlayerPage()) {
+		setupCardsPlayer();
+	} else {
+		console.warn('Unmapped page found');
+	}
+}
 
-Connector.trackSelector = '.audio-file-title';
+function isPlayerPage() {
+	return $('#player-page').length !== 0;
+}
 
-Connector.isPlaying = () => $('.soundwave-container').first().hasClass('.pause-button');
+function isCardsPlayerPage() {
+	return $('.cards-wrapper, .audio-cards').length !== 0;
+}
+
+function setupCardsPlayer() {
+	Connector.playerSelector = '.cards-wrapper, .audio-cards';
+
+	Connector.getArtist = () => {
+		const container = $('.audio-card').has('.pause-button');
+		return container.find('span.name').text();
+	};
+
+	Connector.getTrack = () => {
+		const container = $('.audio-card').has('.pause-button');
+		return container.find('.card-title-text').text();
+	};
+
+	Connector.isPlaying = () => $('.pause-button.small').length !== 0;
+}
+
+
+function setupPlayerPage() {
+	Connector.playerSelector = '#player-page';
+
+	Connector.artistSelector = '.user-summary-name';
+
+	Connector.trackSelector = '.audio-file-title';
+
+	Connector.isPlaying = () => $('.soundwave-container > .pause-button').length !== 0;
+}
+
+setup();
