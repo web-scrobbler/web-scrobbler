255271fcf12dc2b449bd0f6d13294eb494761841
alexesprit <alex.esprit@gmail.com>
2020-05-01 13:56:34 +0300

    Update pocketcasts connector
    Don't override `getRemainingTime` function.
    Remaining time is always absolute.

--- src/connectors/pocketcasts.js
+++ src/connectors/pocketcasts.js
@@ -1,15 +1,17 @@
 'use strict';
 
 Connector.playerSelector = '.player-controls';
+
 Connector.artistSelector = '.podcast-title';
+
 Connector.trackSelector = '.episode-title';
+
 Connector.playButtonSelector = '.play_button';
+
 Connector.currentTimeSelector = '.current-time';
+
 Connector.remainingTimeSelector = '.time-remaining';
+
 Connector.trackArtSelector = '.player-image img';
 
-const origGetRemainingTime = Connector.getRemainingTime;
-
-// PocketCasts displays time as -MM:SS, so we return the negative
-Connector.getRemainingTime = () => -origGetRemainingTime();
 Connector.isPodcast = () => true;
