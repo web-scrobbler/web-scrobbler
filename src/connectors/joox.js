4612f8af76d262c02bbd0ceb019f5f40cead500e
alexesprit <alex.esprit@gmail.com>
2020-05-16 15:58:37 +0300

    Fix joox connector

--- src/connectors/joox.js
+++ src/connectors/joox.js
@@ -1,25 +1,17 @@
 'use strict';
 
 const playerSelector = '.kIpMXu';
-const artistSelector = `${playerSelector} .fSOgMq a`;
-const trackSelector = '.jlfeQT';
-const trackArtSelector = `${playerSelector} .XVHIE img`;
-const playBtnSelector = '.jEJMuB';
-const concatArtistNames = function() {
-	return $(artistSelector)
-		.map((_, el) => $(el).text())
-		.get()
-		.join(', ');
-};
-const isPlaying = function() {
-	return $(playBtnSelector).find('i:first').hasClass('playerIcon--pause');
-};
-
-// Connector methods
+const artistSelector = `${playerSelector} .kOwKMw a`;
 
 Connector.playerSelector = playerSelector;
-Connector.trackSelector = trackSelector;
-Connector.trackArtSelector = trackArtSelector;
 
-Connector.getArtist = concatArtistNames;
-Connector.isPlaying = isPlaying;
+Connector.trackSelector = '.xXMwE';
+
+Connector.trackArtSelector = `${playerSelector} .XVHIE img`;
+
+Connector.getArtist = () => {
+	const artistNodes = document.querySelectorAll(artistSelector);
+	return Util.joinArtists(Array.from(artistNodes));
+};
+
+Connector.playButtonSelector = '.playerIcon--play';
