e11c6c94701bbac6ce36f4dece8f3714f0ebb653
alexesprit <alex.esprit@gmail.com>
2020-05-16 19:24:28 +0300

    Update youtube-music connector
    Don't use jQuery.
    Get current time and duration from a video element.
    
    Closes #2404.

--- src/connectors/youtube-music.js
+++ src/connectors/youtube-music.js
@@ -1,11 +1,21 @@
 'use strict';
 
+const playButtonSelector = '.ytmusic-player-bar.play-pause-button #icon > svg > g > path';
+const trackArtSelector = '.ytmusic-player-bar.image';
+const trackSelector = 'ytmusic-player-queue-item[selected] .song-title';
+const videoSelector = '.html5-main-video';
+const adSelector = '.ytmusic-player-bar.advertisement';
+
+const playingPath = 'M6 19h4V5H6v14zm8-14v14h4V5h-4z';
+
 Connector.playerSelector = 'ytmusic-player-bar';
+
 Connector.artistSelector = 'ytmusic-player-queue-item[selected] .byline';
-Connector.trackSelector = 'ytmusic-player-queue-item[selected] .song-title';
+
+Connector.trackSelector = trackSelector;
 
 Connector.getTrackArt = () => {
-	const trackArtUrl = $('.ytmusic-player-bar.image').attr('src');
+	const trackArtUrl = Util.extractImageUrlFromSelectors(trackArtSelector);
 	if (trackArtUrl) {
 		return trackArtUrl.substring(0, trackArtUrl.lastIndexOf('='));
 	}
@@ -14,13 +24,23 @@ Connector.getTrackArt = () => {
 
 Connector.albumSelector = '.ytmusic-player-bar .yt-formatted-string.style-scope.yt-simple-endpoint[href*="browse/MPREb_"]';
 
-Connector.timeInfoSelector = '.ytmusic-player-bar.time-info';
-
-Connector.isPlaying = () => {
-	const playingPath = 'M6 19h4V5H6v14zm8-14v14h4V5h-4z';
-	return $('.ytmusic-player-bar.play-pause-button #icon > svg > g > path').attr('d') === playingPath;
+Connector.getCurrentTime = () => {
+	return getVideoProp('currentTime');
 };
 
-Connector.isScrobblingAllowed = () => $('.ytmusic-player-bar.advertisement').is(':hidden');
+Connector.getDuration = () => {
+	return getVideoProp('duration');
+};
+
+Connector.isPlaying = () => {
+	return Util.getAttrFromSelectors(playButtonSelector, 'd') === playingPath;
+};
+
+Connector.isScrobblingAllowed = () => !Util.isElementVisible(adSelector);
 
 Connector.applyFilter(MetadataFilter.getYoutubeFilter());
+
+function getVideoProp(prop) {
+	const videoElement = document.querySelector(videoSelector);
+	return videoElement && videoElement[prop];
+}
