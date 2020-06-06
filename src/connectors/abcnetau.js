72d21545bff3597ebbf5de51143a6018bc323eb2
Malachi Soord <inverse.chi@gmail.com>
2020-06-06 21:30:06 +0200

    Drop jquery

--- src/connectors/abcnetau.js
+++ src/connectors/abcnetau.js
@@ -2,16 +2,18 @@
 
 Connector.playerSelector = '#main-content';
 
-Connector.getArtist = () => {
-	return $('.playingNow div[class*="songArtist"]').text() || $('.view-liveMusicNow .artist').text();
-};
+Connector.artistSelector = '.playingNow div[class*="songArtist"], .view-liveMusicNow .artist';
 
-Connector.getTrack = () => {
-	return $('.playingNow h3').text() || $('.view-liveMusicNow .title').text();
-};
+Connector.trackSelector = '.playingNow h3, .view-liveMusicNow .title';
 
-Connector.getAlbum = () => {
-	return $('.playingNow div[class*="songRelease"]').text() || $('.view-liveMusicNow .release').text();
-};
+Connector.albumSelector = '.playingNow div[class*="songRelease"], .view-liveMusicNow .release';
 
-Connector.isPlaying = () => $('#jwplayerDiv, #radio-player4-player').hasClass('jw-state-playing');
+Connector.isPlaying = () => {
+	const $playerElement = document.querySelector('#jwplayerDiv, #radio-player4-player');
+
+	if (!$playerElement) {
+		return false;
+	}
+
+	return $playerElement.classList.contains('jw-state-playing');
+};
