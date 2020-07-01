dbe610775b9a56802178e0a01285296959cb664b
alexesprit <alex.esprit@gmail.com>
2020-07-01 17:19:46 +0300

    Extend CustomPatterns from CustomStorage

--- src/core/background/storage/custom-patterns.js
+++ src/core/background/storage/custom-patterns.js
@@ -5,39 +5,14 @@
  */
 define((require) => {
 	const BrowserStorage = require('storage/browser-storage');
+	const CustomStorage = require('storage/custom-storage');
 
-	const storage = BrowserStorage.getStorage(BrowserStorage.CUSTOM_PATTERNS);
+	class CustomPatterns extends CustomStorage {
+		/** @override */
+		getStorage() {
+			return BrowserStorage.getStorage(BrowserStorage.CUSTOM_PATTERNS);
+		}
+	}
 
-	return {
-		/**
-		 * Get custom patterns for all connectors.
-		 * @return {Object} Custom URL patterns
-		 */
-		getAllPatterns() {
-			return storage.get();
-		},
-
-		/**
-		 * Update custom patterns and save them to storage.
-		 * @param {String} connectorId Connector ID
-		 * @param {Array} patterns Array of custom URL patterns
-		 */
-		async setPatterns(connectorId, patterns) {
-			const data = await storage.get();
-
-			data[connectorId] = patterns;
-			await storage.set(data);
-		},
-
-		/**
-		 * Remove custom URL patterns for given connector.
-		 * @param {String} connectorId Connector ID
-		 */
-		async resetPatterns(connectorId) {
-			const data = await storage.get();
-
-			delete data[connectorId];
-			await storage.set(data);
-		},
-	};
+	return new CustomPatterns();
 });
