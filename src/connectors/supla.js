3390f87cddd01e3aa2a1f6ff1f5cfc0c27a6f9f1
areee <10089872+areee@users.noreply.github.com>
2020-07-09 20:16:39 +0300

    supla.js: fixed selectors

--- src/connectors/supla.js
+++ src/connectors/supla.js
@@ -1,7 +1,7 @@
 'use strict';
 
-Connector.playerSelector = '#ComponentContainer__Section-sc-1m3wxgx-0';
+Connector.playerSelector = '#RadioHeader__Container-sc-17ofob1-0 iLWJvB';
 
-Connector.artistTrackSelector = '#RadioHeader__WidgetContent-sc-17ofob1-4';
+Connector.artistTrackSelector = '#RadioHeader__WidgetContent-sc-17ofob1-4 irUZCR';
 
-Connector.playButtonSelector = '#RadioHeader__WidgetContentAction-sc-17ofob1-9';
+Connector.playButtonSelector = '#RadioHeader__WidgetContentAction-sc-17ofob1-9 kKpbyx';
