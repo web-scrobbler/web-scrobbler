3db105c7508688bb88b5184939b09032a18854b9
Carsten Petersen <carpet@taox.net>
2020-07-09 18:19:55 +0200

    Add SWR3 connector (#2475)

---
+++ src/connectors/swr3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Connector.playerSelector = '.audioplayer-ui';
+
+Connector.artistSelector = 'dl.audioplayer-title dd:nth-of-type(2)';
+
+Connector.trackSelector = 'dl.audioplayer-title dd:nth-of-type(1)';
+
+Connector.isPlaying = () => Util.getAttrFromSelectors('.audioplayer-control[data-action=play]', 'aria-pressed') === 'true';
