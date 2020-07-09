eb1c24a85841f9e559c9877f92492b0e04ae34f7
areee <10089872+areee@users.noreply.github.com>
2020-07-09 20:21:49 +0300

    supla.js: '.' is the class selector, not '#'

--- src/connectors/supla.js
+++ src/connectors/supla.js
@@ -1,7 +1,7 @@
 'use strict';
 
-Connector.playerSelector = '#RadioHeader__Container-sc-17ofob1-0 iLWJvB';
+Connector.playerSelector = '.RadioHeader__Container-sc-17ofob1-0 iLWJvB';
 
-Connector.artistTrackSelector = '#RadioHeader__WidgetContent-sc-17ofob1-4 irUZCR';
+Connector.artistTrackSelector = '.RadioHeader__WidgetContent-sc-17ofob1-4 irUZCR';
 
-Connector.playButtonSelector = '#RadioHeader__WidgetContentAction-sc-17ofob1-9 kKpbyx';
+Connector.playButtonSelector = '.RadioHeader__WidgetContentAction-sc-17ofob1-9 kKpbyx';
