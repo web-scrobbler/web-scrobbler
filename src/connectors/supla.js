52976b1bacc0f646c9cfcaf5fc6dae641cc81f35
areee <10089872+areee@users.noreply.github.com>
2020-07-10 13:57:25 +0300

    fixed the connector: applyFilter was missing

--- src/connectors/supla.js
+++ src/connectors/supla.js
@@ -10,6 +10,7 @@ Connector.artistTrackSelector = '.RadioHeader__WidgetContent-sc-17ofob1-4.irUZCR
 
 Connector.playButtonSelector = '.r-play-button.r-toggle-playstate-button.r-touch-button';
 
+Connector.applyFilter(filter);
 
 function cleanUpArtist(artist) {
     // Extract an artist title from a `music_note"Artist"` string.
