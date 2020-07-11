56d41035a067a9bfb5a1a1a7aa497e049a19ce74
areee <10089872+areee@users.noreply.github.com>
2020-07-11 13:00:56 +0300

    nrk: when multiple artists, replace '+' with '&' char

--- src/connectors/nrk-radio.js
+++ src/connectors/nrk-radio.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const filter = new MetadataFilter({
+    artist: cleanUpArtist,
+});
+
 Connector.playerSelector = '.app';
 
 Connector.artistTrackSelector = '[data-test="playingEpisodeDesc"]';
 
 Connector.pauseButtonSelector = '[data-test="playPauseMiniplayer"][aria-label="Pause"]';
+
+Connector.applyFilter(filter);
+
+function cleanUpArtist(artist) {
+    return artist.replace('+', '&');
+}
