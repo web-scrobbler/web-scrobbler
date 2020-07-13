c6efbb85aacd1fd589480b6a1b9f725319d0b2bd
areee <10089872+areee@users.noreply.github.com>
2020-07-13 18:06:17 +0300

    supla: simplify the playButtonSelector

--- src/connectors/supla.js
+++ src/connectors/supla.js
@@ -8,7 +8,7 @@ Connector.playerSelector = 'main';
 
 Connector.artistTrackSelector = '.RadioHeader__WidgetContent-sc-17ofob1-4.irUZCR';
 
-Connector.playButtonSelector = '.r-play-button.r-toggle-playstate-button.r-touch-button';
+Connector.playButtonSelector = '.r-play-button';
 
 Connector.applyFilter(filter);
 
