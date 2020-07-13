c01d66a2e2da6820d5bbeececa50deea662c8778
areee <10089872+areee@users.noreply.github.com>
2020-07-13 19:27:06 +0300

    supla: nearly forgot the linting

--- src/connectors/supla.js
+++ src/connectors/supla.js
@@ -6,7 +6,7 @@ const filter = new MetadataFilter({
 
 Connector.playerSelector = 'main';
 
-//Connector.artistTrackSelector = '[class^=RadioHeader__WidgetContent]'; // works with radio channels without the program title ('Groove FM' & 'Anna sen soida')
+// Connector.artistTrackSelector = '[class^=RadioHeader__WidgetContent]'; // works with radio channels without the program title ('Groove FM' & 'Anna sen soida')
 Connector.artistTrackSelector = 'main > div > div > section > div > div:nth-child(2) > div:nth-child(2) > div'; // works with radio channels with the program title (all the rest except the earlier ones)
 
 
