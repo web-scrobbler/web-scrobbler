6c16a393c1a2b69c4a9be15d96ed3a2c716f64d5
heyarne <arne@schlueter.is>
2020-07-17 13:54:57 +0200

    Start fixing gds.fm connector for new page

---
+++ src/connectors/gds.fm.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Connector.artistSelector = '.player-meta__artist';
+Connector.trackSelector = '.player-meta__title';
+
+Connector.playButtonSelector = '.player-meta__icon--play';
+
+Connector.onReady = Connector.onStateChanged;
