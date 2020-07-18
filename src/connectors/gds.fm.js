5ba94128f1090819a3242b0e2cb187c55cad5e57
heyarne <arne@schlueter.is>
2020-07-18 10:17:53 +0200

    Remove commented out code

--- src/connectors/gds.fm.js
+++ src/connectors/gds.fm.js
@@ -7,5 +7,3 @@ Connector.artistSelector = '.player-meta__artist';
 Connector.trackSelector = '.player-meta__title';
 
 Connector.playButtonSelector = '.player-meta__icon--play';
-
-// Connector.onReady = Connector.onStateChanged;
