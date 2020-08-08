ed67a7ba905a52dcbbf50097a000cb82e5af443b
alexesprit <alex.esprit@gmail.com>
2020-08-08 09:03:22 +0300

    Fix emby connector
    Support Jellyfin v10.6.0 and never.
    
    Closes #2515.

--- src/connectors/emby.js
+++ src/connectors/emby.js
@@ -6,14 +6,28 @@ Connector.playerSelector = '.nowPlayingBar';
 
 Connector.artistSelector = '.nowPlayingBarText .nowPlayingBarSecondaryText';
 
-Connector.trackSelector = '.nowPlayingBarText .textActionButton';
+Connector.trackSelector = [
+	// Jellyfin v10.6.0 and newer
+	'.nowPlayingBarText div:nth-child(1) a',
+	// Emby or Jellyfin v10.5.0 and older
+	'.nowPlayingBarText .textActionButton',
+];
 
 Connector.trackArtSelector = trackArtSelector;
 
 Connector.timeInfoSelector = '.nowPlayingBarCenter .nowPlayingBarCurrentTime';
 
 Connector.isPlaying = () => {
-	return $('.nowPlayingBarCenter .playPauseButton i').text() === 'pause';
+	const playButtonLabel = Util.getTextFromSelectors(
+		'.nowPlayingBarCenter .playPauseButton i'
+	);
+	// Emby or Jellyfin v10.5.0 and older
+	if (playButtonLabel) {
+		return playButtonLabel === 'pause';
+	}
+
+	// Jellyfin v10.6.0 and newer
+	return document.querySelector('.playPauseButton .play_arrow') === null;
 };
 
 Connector.getTrackArt = () => {
@@ -36,5 +50,8 @@ Connector.getUniqueID = () => {
 
 Connector.isStateChangeAllowed = () => {
 	// prevents scrobble timer resetting when user changes view while playing a song
-	return Connector.getCurrentTime() === 0 || Connector.getCurrentTime() === Connector.getDuration();
+	return (
+		Connector.getCurrentTime() === 0 ||
+		Connector.getCurrentTime() === Connector.getDuration()
+	);
 };
