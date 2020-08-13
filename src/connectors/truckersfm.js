9917a9f9f3fd166cc269327199edbdcad27f9099
josh <joshuadennisblackman@gmail.com>
2020-08-13 10:41:44 +0100

    Add TruckersFM connector (#2520)

---
+++ src/connectors/truckersfm.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Connector.playerSelector = '.header';
+
+Connector.trackArtSelector = '.player-image > img';
+
+Connector.artistSelector = '.player-artist-text';
+
+Connector.trackSelector = '.player-title-text';
+
+Connector.playButtonSelector = '.pause-btn > .fa-play';
+
+Connector.onReady = Connector.onStateChanged;
