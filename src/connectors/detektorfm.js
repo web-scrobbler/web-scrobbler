dba15cb5decf93678bfba00a5f35bfff5e86386d
Carsten Petersen <carpet@taox.net>
2020-08-28 08:50:11 +0200

    Added trackArtSelector

--- src/connectors/detektorfm.js
+++ src/connectors/detektorfm.js
@@ -6,4 +6,6 @@ Connector.artistSelector = '.playerinfo__artist';
 
 Connector.trackSelector = '.playerinfo__title';
 
+Connector.trackArtSelector = '.playerinfo__cover img';
+
 Connector.isPlaying = () => Util.hasElementClass('.dfm-play', 'is-playing');
