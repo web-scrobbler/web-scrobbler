90512d353ceb27c658175d07feb6387fbb48e3a6
Malachi Soord <inverse.chi@gmail.com>
2020-09-05 20:45:18 +0200

    Rework

--- src/connectors/archive.js
+++ src/connectors/archive.js
@@ -1,18 +1,35 @@
 'use strict';
 
+const artistSelectors = [
+	'.key-val-big a span',
+	'.metadata-definition > dd',
+];
+const trackSelectors = [
+	'.jwrowV2.playing .ttl',
+	'.audio-track-list .selected .track-title',
+];
+
 Connector.currentTimeSelector = '.jw-text-elapsed';
 
 Connector.durationSelector = '.jw-text-duration';
 
-Connector.isPlaying = () => !document.querySelector('video').paused;
+Connector.isPlaying = () => {
+	const videoElement = document.querySelector('video');
+
+	if (videoElement === null) {
+		return false;
+	}
+
+	return !videoElement.paused;
+};
 
 Connector.playerSelector = '#theatre-ia';
 
 Connector.trackArtSelector = '#theatre-ia center > img';
 
 Connector.getArtistTrack = () => {
-	let track = document.querySelector('.jwrowV2.playing .ttl, .audio-track-list .selected .track-title').textContent;
-	const artist = document.querySelector('.key-val-big a span, .metadata-definition > dd').textContent;
+	let track = Util.getTextFromSelectors(trackSelectors);
+	const artist = Util.getTextFromSelectors(artistSelectors);
 
 	const trackParts = track.split('-').map((item) => {
 		return item.trim();
