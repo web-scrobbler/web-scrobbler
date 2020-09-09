f8dd33a7dbc65fa2dda492619dab1254cb88817e
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-09-09 12:24:03 +0800

    Linting

--- src/connectors/recmusic.js
+++ src/connectors/recmusic.js
@@ -6,19 +6,19 @@ const filter = new MetadataFilter({
 
 Connector.playerSelector = '.the-player';
 
-Connector.artistSelector = "a.track-info-artist";
+Connector.artistSelector = 'a.track-info-artist';
 
-Connector.albumSelector = "a.track-info-album";
+Connector.albumSelector = 'a.track-info-album';
 
-Connector.getTrackArt = () => document.querySelector('.track-info-image img').style["background-image"].slice(5, -2);
+Connector.getTrackArt = () => document.querySelector('.track-info-image img').style['background-image'].slice(5, -2);
 
-Connector.currentTimeSelector = ".seekbar-duration:nth-child(2)";
+Connector.currentTimeSelector = '.seekbar-duration:nth-child(2)';
 
-Connector.durationSelector = ".seekbar-duration:last-child";
+Connector.durationSelector = '.seekbar-duration:last-child';
 
-Connector.trackSelector = "p.track-info-title";
+Connector.trackSelector = 'p.track-info-title';
 
-Connector.isPlaying = () => document.querySelector('.button-play').classList.contains("mk-web-icon-stop");
+Connector.isPlaying = () => document.querySelector('.button-play').classList.contains('mk-web-icon-stop');
 
 Connector.applyFilter(filter);
 
@@ -29,23 +29,23 @@ Connector.applyFilter(filter);
  * @return {String} Filtered string
  */
 
-function katakanaHanToZen(text){
-  
-  let katakanaHanToZenMap = {"｡":"。","｢":"「","｣":"」","､":"、","･":"・","ｦ":"ヲ","ｧ":"ァ","ｨ":"ィ","ｩ":"ゥ","ｪ":"ェ","ｫ":"ォ","ｬ":"ャ","ｭ":"ュ","ｮ":"ョ","ｯ":"ッ","ｰ":"ー","ｱ":"ア","ｲ":"イ","ｳ":"ウ","ｴ":"エ","ｵ":"オ","ｶ":"カ","ｷ":"キ","ｸ":"ク","ｹ":"ケ","ｺ":"コ","ｻ":"サ","ｼ":"シ","ｽ":"ス","ｾ":"セ","ｿ":"ソ","ﾀ":"タ","ﾁ":"チ","ﾂ":"ツ","ﾃ":"テ","ﾄ":"ト","ﾅ":"ナ","ﾆ":"ニ","ﾇ":"ヌ","ﾈ":"ネ","ﾉ":"ノ","ﾊ":"ハ","ﾋ":"ヒ","ﾌ":"フ","ﾍ":"ヘ","ﾎ":"ホ","ﾏ":"マ","ﾐ":"ミ","ﾑ":"ム","ﾒ":"メ","ﾓ":"モ","ﾔ":"ヤ","ﾕ":"ユ","ﾖ":"ヨ","ﾗ":"ラ","ﾘ":"リ","ﾙ":"ル","ﾚ":"レ","ﾛ":"ロ","ﾜ":"ワ","ﾝ":"ン"};
-  
-  let textOutput = "";
-  
-  for(let character of text){
-    let charCode = character.charCodeAt();
-    
-    if(charCode >= 65377 && charCode <= 65437){
-      textOutput += katakanaHanToZenMap[character];
-    }else if(charCode == 65438 || charCode == 65439){
-      let lastCharCode = textOutput[textOutput.length - 1].charCodeAt();
-      textOutput = textOutput.slice(0, -1) + String.fromCharCode(lastCharCode + charCode - 65437);
-    }else{
-      textOutput += character;
-    }
-  };
-  return textOutput;
+function katakanaHanToZen(text) {
+
+	const katakanaHanToZenMap = { '｡': '。', '｢': '「', '｣': '」', '､': '、', '･': '・', 'ｦ': 'ヲ', 'ｧ': 'ァ', 'ｨ': 'ィ', 'ｩ': 'ゥ', 'ｪ': 'ェ', 'ｫ': 'ォ', 'ｬ': 'ャ', 'ｭ': 'ュ', 'ｮ': 'ョ', 'ｯ': 'ッ', 'ｰ': 'ー', 'ｱ': 'ア', 'ｲ': 'イ', 'ｳ': 'ウ', 'ｴ': 'エ', 'ｵ': 'オ', 'ｶ': 'カ', 'ｷ': 'キ', 'ｸ': 'ク', 'ｹ': 'ケ', 'ｺ': 'コ', 'ｻ': 'サ', 'ｼ': 'シ', 'ｽ': 'ス', 'ｾ': 'セ', 'ｿ': 'ソ', 'ﾀ': 'タ', 'ﾁ': 'チ', 'ﾂ': 'ツ', 'ﾃ': 'テ', 'ﾄ': 'ト', 'ﾅ': 'ナ', 'ﾆ': 'ニ', 'ﾇ': 'ヌ', 'ﾈ': 'ネ', 'ﾉ': 'ノ', 'ﾊ': 'ハ', 'ﾋ': 'ヒ', 'ﾌ': 'フ', 'ﾍ': 'ヘ', 'ﾎ': 'ホ', 'ﾏ': 'マ', 'ﾐ': 'ミ', 'ﾑ': 'ム', 'ﾒ': 'メ', 'ﾓ': 'モ', 'ﾔ': 'ヤ', 'ﾕ': 'ユ', 'ﾖ': 'ヨ', 'ﾗ': 'ラ', 'ﾘ': 'リ', 'ﾙ': 'ル', 'ﾚ': 'レ', 'ﾛ': 'ロ', 'ﾜ': 'ワ', 'ﾝ': 'ン' };
+
+	let textOutput = '';
+
+	for (const character of text) {
+		const charCode = character.charCodeAt();
+
+		if (charCode >= 65377 && charCode <= 65437) {
+			textOutput += katakanaHanToZenMap[character];
+		} else if (charCode === 65438 || charCode === 65439) {
+			const lastCharCode = textOutput[textOutput.length - 1].charCodeAt();
+			textOutput = textOutput.slice(0, -1) + String.fromCharCode(lastCharCode + charCode - 65437);
+		} else {
+			textOutput += character;
+		}
+	}
+	return textOutput;
 }
