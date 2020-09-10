179a7b278f3758c6efaac8db5c8716470d704155
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-09-10 08:15:21 +0800

    Add Eggs connector

---
+++ src/connectors/eggs.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Connector.getDuration = () => document.querySelector('#aPlayer').duration;
+
+Connector.getCurrentTime = () => document.querySelector('#aPlayer').currentTime;
+
+if (window.location.href.includes('/artist/')) {
+	setupArtistPlayer();
+} else {
+	setupSongPlayer();
+}
+
+function setupArtistPlayer() {
+	Connector.playerSelector = '.m-upload-list ul';
+
+	Connector.getTrackArt = () => document.querySelector('.pause[style*="display: block;"]').closest('.img-album').querySelector('img').src;
+
+	Connector.getArtist = () => document.querySelector('.pause[style*="display: block;"]').closest('li').querySelector('.artist_name').innerText;
+
+	Connector.getTrack = () => document.querySelector('.pause[style*="display: block;"]').closest('.player').dataset.srcname;
+
+	Connector.pauseButtonSelector = '.pause[style*="display: block;"]';
+}
+
+function setupSongPlayer() {
+	Connector.playerSelector = '.player';
+
+	Connector.trackArtSelector = '.img-album img';
+
+	Connector.artistSelector = '.artist_name a';
+
+	Connector.trackSelector = '#js-product-name-0 p';
+
+	Connector.pauseButtonSelector = '.pause';
+}
