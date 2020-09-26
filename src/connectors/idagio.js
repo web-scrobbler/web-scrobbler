dc5757ede7bef25b50c647116060af1ceac6ed51
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-09-26 17:06:44 +0800

    Spring cleaning

--- src/connectors/idagio.js
+++ src/connectors/idagio.js
@@ -10,24 +10,26 @@ Connector.playerSelector = '.player-PlayerBar__bar--2yos_';
 
 Connector.artistSelector = '.player-PlayerInfo__infoEl--2jhHY span:first-child';
 
-Connector.getTrack = () => getCurrentTrack();
+Connector.getTrack = getCurrentTrack;
 
-Connector.getAlbum = () => getCurrentSymphony();
+Connector.getAlbum = getCurrentSymphony;
 
 Connector.currentTimeSelector = '.player-PlayerProgress__progress--2F0qB>span';
 
 Connector.durationSelector = '.player-PlayerProgress__timeTotal--3aHlj span';
 
-Connector.isPlaying = () => document.querySelector(recordingInfoSelector).innerHTML !== 'Sponsor message' && document.querySelector(pauseButtonSelector).innerHTML === 'Pause';
+Connector.isPlaying = () => Util.getTextFromSelectors(pauseButtonSelector) === 'Pause';
+
+Connector.isScrobblingAllowed = () => Util.getTextFromSelectors(recordingInfoSelector) !== 'Sponsor message';
 
 function getCurrentTrack() {
-	if (document.querySelector(symphonySelector)) {
-		return `${document.querySelector(symphonySelector).innerText}: ${document.querySelector(movementSelector).innerText}`;
-	}
+	const symphony = Util.getTextFromSelectors(symphonySelector);
+	const movement = Util.getTextFromSelectors(movementSelector);
+	return `${symphony}: ${movement}`;
 }
 
 function getCurrentSymphony() {
-	if (document.querySelector(symphonySelector)) {
-		return `${document.querySelector(symphonySelector).innerText.split(/ in [A-G]/)[0]} (${document.querySelector(directorSelector).innerText})`;
-	}
+	const symphonyShort = Util.getTextFromSelectors(symphonySelector).split(/ in [A-G]/)[0];
+	const director = Util.getTextFromSelectors(directorSelector);
+	return `${symphonyShort} (${director})`;
 }
