09ca7c725ea436e7dd2282c848037a7a4f3299bf
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-09-30 17:14:07 +0800

    Modify idagio connector

--- src/connectors/idagio.js
+++ src/connectors/idagio.js
@@ -1,35 +1,27 @@
 'use strict';
 
-const symphonySelector = '.player-PlayerInfo__infoEl--2jhHY span:nth-child(3) span';
-const movementSelector = '.player-PlayerInfo__infoEl--2jhHY span span span';
-const directorSelector = '.player-PlayerInfo__recordingInfo--15VMv>span:first-child span';
-const recordingInfoSelector = '.player-PlayerInfo__recordingInfo--15VMv';
-const pauseButtonSelector = '.player-PlayerControls__btn--1r-vy:nth-child(2) .util-IconLabel__component--3Uitr span';
-
 Connector.playerSelector = '.player-PlayerBar__bar--2yos_';
 
 Connector.artistSelector = '.player-PlayerInfo__infoEl--2jhHY span:first-child';
 
-Connector.getTrack = getCurrentTrack;
+Connector.getTrack = () => getCurrentTrack();
 
-Connector.getAlbum = getCurrentSymphony;
+Connector.getAlbum = () => getCurrentSymphony();
 
 Connector.currentTimeSelector = '.player-PlayerProgress__progress--2F0qB>span';
 
 Connector.durationSelector = '.player-PlayerProgress__timeTotal--3aHlj span';
 
-Connector.isPlaying = () => Util.getTextFromSelectors(pauseButtonSelector) === 'Pause';
+Connector.isPlaying = () => Util.getTextFromSelectors('.player-PlayerControls__btn--1r-vy:nth-child(2) .util-IconLabel__component--3Uitr span') === 'Pause';
 
-Connector.isScrobblingAllowed = () => Util.getTextFromSelectors(recordingInfoSelector) !== 'Sponsor message';
+Connector.isScrobblingAllowed = () => Util.getTextFromSelectors('.player-PlayerInfo__recordingInfo--15VMv') !== 'Sponsor message';
 
 function getCurrentTrack() {
-	const symphony = Util.getTextFromSelectors(symphonySelector);
-	const movement = Util.getTextFromSelectors(movementSelector);
-	return `${symphony}: ${movement}`;
+  return Util.getTextFromSelectors(".player-PlayerInfo__infoEl--2jhHY").split(" – ").slice(1).join(" - ").replace(" op. ", ", op. ");
 }
 
 function getCurrentSymphony() {
-	const symphonyShort = Util.getTextFromSelectors(symphonySelector).split(/ in [A-G]/)[0];
-	const director = Util.getTextFromSelectors(directorSelector);
+	const symphonyShort = Util.getTextFromSelectors('.player-PlayerInfo__infoEl--2jhHY span:nth-child(3) span').split(/ in [A-G]/)[0];
+	const director = Util.getTextFromSelectors('.player-PlayerInfo__recordingInfo--15VMv>span:first-child span');
 	return `${symphonyShort} (${director})`;
 }
