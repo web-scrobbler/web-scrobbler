78c33b6774459230eb5b9a5b1e6ebdc88875c755
Vignesh Kumar <xxvignesh@gmail.com>
2020-10-01 21:41:53 +0530

    Add support for Laut.fm

---
+++ src/connectors/laut.fm.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Connector.playerSelector = '.fm-player';
+
+Connector.artistSelector = '.player-display__meta .player-display__meta--artist';
+
+Connector.trackSelector = '.player-display__meta .player-display__meta--title';
+
+Connector.isPlaying = () => $('.fm-player__btn').hasClass('playbutton--playing');
