d9fba19cdd623470ed3149eecbf19d0394862e37
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-10-01 14:20:42 +0800

    Readd constants

--- src/connectors/idagio.js
+++ src/connectors/idagio.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const symphonySelector = '.player-PlayerInfo__infoEl--2jhHY span:nth-child(3) span:first-child';
+const commonNameSelector = '.player-PlayerInfo__infoEl--2jhHY span:nth-child(3) span:nth-child(2)';
+const directorSelector = '.player-PlayerInfo__recordingInfo--15VMv>span:first-child span';
+const trackSelector = '.player-PlayerInfo__infoEl--2jhHY';
+const pauseButtonSelector = '.player-PlayerControls__btn--1r-vy:nth-child(2) .util-IconLabel__component--3Uitr span';
+
 Connector.playerSelector = '.player-PlayerBar__bar--2yos_';
 
 Connector.artistSelector = '.player-PlayerInfo__infoEl--2jhHY span:first-child';
@@ -12,18 +18,18 @@ Connector.currentTimeSelector = '.player-PlayerProgress__progress--2F0qB>span';
 
 Connector.durationSelector = '.player-PlayerProgress__timeTotal--3aHlj span';
 
-Connector.isPlaying = () => Util.getTextFromSelectors('.player-PlayerControls__btn--1r-vy:nth-child(2) .util-IconLabel__component--3Uitr span') === 'Pause';
+Connector.isPlaying = () => Util.getTextFromSelectors(pauseButtonSelector) === 'Pause';
 
 Connector.isScrobblingAllowed = () => Util.getTextFromSelectors('.player-PlayerInfo__recordingInfo--15VMv') !== 'Sponsor message';
 
 function getCurrentTrack() {
-	return Util.getTextFromSelectors('.player-PlayerInfo__infoEl--2jhHY').split(' – ').slice(1).join(': ');
+	return Util.getTextFromSelectors(trackSelector).split(' – ').slice(1).join(': ');
 }
 
 function getCurrentSymphony() {
-	const symphonyShort = Util.getTextFromSelectors('.player-PlayerInfo__infoEl--2jhHY span:nth-child(3) span:first-child').split(/ in [A-G]| op. [0-9]| KV [0-9]/)[0];
-	const commonName = Util.getTextFromSelectors('.player-PlayerInfo__infoEl--2jhHY span:nth-child(3) span:nth-child(2)') || '';
-	const director = removeParenthesis(Util.getTextFromSelectors('.player-PlayerInfo__recordingInfo--15VMv>span:first-child span'));
+	const symphonyShort = Util.getTextFromSelectors(symphonySelector).split(/ in [A-G]| op. [0-9]| KV [0-9]/)[0];
+	const commonName = Util.getTextFromSelectors(commonNameSelector) || '';
+	const director = removeParenthesis(Util.getTextFromSelectors(directorSelector));
 	return `${symphonyShort}${commonName} (${director})`;
 }
 
