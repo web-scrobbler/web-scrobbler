79c69e0be5544248c27499f3ca5f5f46be06cf7f
Vignesh Kumar <xxvignesh@gmail.com>
2020-10-02 16:07:04 +0530

    Remove quotes from the track title

--- src/connectors/laut.fm.js
+++ src/connectors/laut.fm.js
@@ -7,3 +7,11 @@ Connector.artistSelector = '.player-display__meta .player-display__meta--artist'
 Connector.trackSelector = '.player-display__meta .player-display__meta--title';
 
 Connector.isPlaying = () => $('.fm-player__btn').hasClass('playbutton--playing');
+
+function removeEnclosingQuotes(track) {
+	return track.trim().slice(1, -1);
+}
+
+const filter = new MetadataFilter({ track: removeEnclosingQuotes });
+
+Connector.applyFilter(filter);
