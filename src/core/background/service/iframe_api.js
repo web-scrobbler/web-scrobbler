41ca7aea1f03dce6a04f9e36ffe208a999ce90d7
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-10-16 20:28:58 +0800

    Add explanation of changes

--- src/core/background/service/iframe_api.js
+++ src/core/background/service/iframe_api.js
@@ -1,2 +1,4 @@
 /* eslint-disable */
+// Source: https://www.youtube.com/iframe_api
+// Changes: Removed the script loading mechanism that loads from https://s.ytimg.com/yts/jsbin/www-widgetapi-vflI8cWQm/www-widgetapi.js.
 if (!window['YT']) {var YT = {loading: 0,loaded: 0};}if (!window['YTConfig']) {var YTConfig = {'host': 'http://www.youtube.com'};}if (!YT.loading) {YT.loading = 1;(function(){var l = [];YT.ready = function(f) {if (YT.loaded) {f();} else {l.push(f);}};window.onYTReady = function() {YT.loaded = 1;for (var i = 0; i < l.length; i++) {try {l[i]();} catch (e) {}}};YT.setConfig = function(c) {for (var k in c) {if (c.hasOwnProperty(k)) {YTConfig[k] = c[k];}}};var c = document.currentScript;if (c) {var n = c.nonce || c.getAttribute('nonce');}var b = document.getElementsByTagName('script')[0];})();}
