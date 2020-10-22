3e29aea07fa4be463a6cf6015f99c3641c1703ea
alexesprit <alex.esprit@gmail.com>
2020-10-22 13:49:36 +0300

    Fix tunein connector
    Closes #2605.

--- src/connectors/tunein.js
+++ src/connectors/tunein.js
@@ -9,21 +9,32 @@ const STATIONS_SWAP = [
 	'Cat-Country-987', '885-FM-So-Cal',
 ];
 
-Connector.playerSelector = '.player__playerContainer___JEJ2U';
+const playerBar = '[class^=player__playerContainer]';
+const artistTrackSelector = '#playerTitle';
+const stationNameSelector = '[class^=nowPlaying__link]';
+
+Connector.playerSelector = playerBar;
 
 Connector.getArtistTrack = () => {
-	const artistTrackText = $('#playerTitle').text();
-	const swap = shouldSwapArtistTrack();
+	const artistTrackText = Util.getTextFromSelectors(artistTrackSelector);
 
-	return Util.splitArtistTrack(artistTrackText, null, { swap });
+	if (artistTrackText) {
+		const swap = shouldSwapArtistTrack();
+		return Util.splitArtistTrack(artistTrackText, null, { swap });
+	}
+
+	return null;
 };
 
 Connector.trackArtSelector = '#playerArtwork';
 
-Connector.pauseButtonSelector = '#Group-6';
+Connector.pauseButtonSelector = `${playerBar} [data-icon=stop]`;
 
 function shouldSwapArtistTrack() {
-	const stationUrl = $('.nowPlaying__link___2FTVw').attr('href');
+	const stationUrl = Util.getAttrFromSelectors(stationNameSelector, 'href');
+	if (!stationUrl) {
+		return false;
+	}
 
 	for (const station of STATIONS_SWAP) {
 		if (stationUrl.includes(station)) {
