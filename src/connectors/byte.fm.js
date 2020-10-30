096a4a7a83d875cfafe99e6890a1ea222c722626
Alex Laquerre <dev@halhex.ca>
2020-10-30 17:15:32 -0400

    Fix byte.fm connector following update (#2621)

--- src/connectors/byte.fm.js
+++ src/connectors/byte.fm.js
@@ -1,19 +1,9 @@
 'use strict';
 
-Connector.playerSelector = '#player';
+Connector.playerSelector = '.player';
 
 Connector.artistTrackSelector = '.player-current-title';
 
-Connector.getArtistTrack = () => {
-	const text = $(Connector.artistTrackSelector).text();
-	const m = text.match(/ - /g);
-	if (m && m.length === 2) {
-		const arr = text.split(' - ');
-		return { artist: arr[1], track: arr[2] };
-	}
-	return Util.splitArtistTrack(text);
-};
-
-Connector.pauseButtonSelector = '.player-pause';
+Connector.isPlaying = () => Util.getTextFromSelectors('.player-play-button') !== 'play';
 
 Connector.onReady = Connector.onStateChanged;
