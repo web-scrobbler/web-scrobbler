web-scrobbler/web-scrobbler

commit 41f5134d9aab17d059edf2b2d20819a84d0c5d11

Implement the "Scrobble individual tracks from album videos" option

src/connectors/youtube.js
@@ -1,6 +1,12 @@
 'use strict';
 
 /**
+ * Whether to scrobble individual tracks from album videos.
+ * @type {Boolean}
+ */
+let scrobbleIndividualTracks = false;
+
+/**
  * Array of categories allowed to be scrobbled.
  * @type {Array}
  */
@@ -34,11 +40,7 @@ const categoryEntertainment = 'Entertainment';
 let currentVideoDescription = null;
 let artistTrackFromDescription = null;
 
-const trackInfoGetters = [
-	getTrackInfoFromDescription,
-	getTrackInfoFromChapters,
-	getTrackInfoFromTitle,
-];
+let trackInfoGetters = null;
 
 readConnectorOptions();
 setupEventListener();
@@ -46,15 +48,34 @@ setupEventListener();
 Connector.playerSelector = '#content';
 
 Connector.getTrackInfo = () => {
-	let trackInfo = null;
+	let trackInfo = {};
 
 	for (const getter of trackInfoGetters) {
-		trackInfo = getter();
-		if (Util.isArtistTrackEmpty(trackInfo)) {
-			continue;
-		}
+		if (scrobbleIndividualTracks) {
+			const currentTrackInfo = getter();
+			if (!currentTrackInfo) {
+				continue;
+			}
 
-		return trackInfo;
+			if (!trackInfo.artist) {
+				trackInfo.artist = currentTrackInfo.artist;
+			}
+
+			if (!trackInfo.track) {
+				trackInfo.track = currentTrackInfo.track;
+			}
+
+			if (!Util.isArtistTrackEmpty(trackInfo)) {
+				break;
+			}
+		} else {
+			trackInfo = getter();
+			if (Util.isArtistTrackEmpty(trackInfo)) {
+				continue;
+			}
+
+			return trackInfo;
+		}
 	}
 
 	return trackInfo;
@@ -203,6 +224,18 @@ async function fetchCategoryName(videoId) {
  * Asynchronously read connector options.
  */
 async function readConnectorOptions() {
+	if (await Util.getOption('YouTube', 'scrobbleIndividualTracks')) {
+		scrobbleIndividualTracks = true;
+		trackInfoGetters = scrobbleIndividualTracks ? [
+			getTrackInfoFromChapters,
+			getTrackInfoFromDescription,
+			getTrackInfoFromTitle,
+		] : [
+			getTrackInfoFromDescription,
+			getTrackInfoFromChapters,
+			getTrackInfoFromTitle,
+		];
+	}
 	if (await Util.getOption('YouTube', 'scrobbleMusicOnly')) {
 		allowedCategories.push(categoryMusic);
 	}
@@ -229,9 +262,10 @@ function getTrackInfoFromDescription() {
 }
 
 function getTrackInfoFromChapters() {
-	return Util.splitArtistTrack(
-		Util.getTextFromSelectors(chapterNameSelector)
-	);
+	const trackInfo = {};
+	trackInfo.artist = null;
+	trackInfo.track = Util.getTextFromSelectors(chapterNameSelector);
+	return trackInfo;
 }
 
 function getTrackInfoFromTitle() {

src/core/background/storage/options.js
@@ -74,6 +74,7 @@ define((require) => {
 			useShortTrackNames: false,
 		},
 		YouTube: {
+			scrobbleIndividualTracks: false,
 			scrobbleMusicOnly: false,
 			scrobbleEntertainmentOnly: false,
 		},

src/ui/options/index.html
@@ -95,6 +95,10 @@
 								<h5>YouTube</h5>
 								<div class="options-block">
 									<div class="form-check">
+										<input class="form-check-input" type="checkbox" id="yt-scrobble-individual-tracks">
+										<label class="form-check-label" for="yt-scrobble-individual-tracks" i18n="optionYtScrobbleIndividualTracks" i18n-title="optionYtScrobbleIndividualTracksTitle"></label>
+									</div>
+									<div class="form-check">
 										<input class="form-check-input" type="checkbox" id="yt-music-only">
 										<label class="form-check-label" for="yt-music-only" i18n="optionYtMusicOnly" i18n-title="optionYtMusicOnlyTitle"></label>
 									</div>

src/ui/options/options.js
@@ -26,6 +26,7 @@ define((require) => {
 			'#tdl-short-track-names': 'useShortTrackNames',
 		},
 		YouTube: {
+			'#yt-scrobble-individual-tracks': 'scrobbleIndividualTracks',
 			'#yt-music-only': 'scrobbleMusicOnly',
 			'#yt-entertainment-only': 'scrobbleEntertainmentOnly',
 		},

commit 04c298095dc8e7ee5d10d8bb72cde28234620eb6

Fix broken support for artist names when extracting track info from chapter titles
Extend the splitArtistTrack function to support track-only strings
without breaking its API (as well as the underlying splitString
function).  Revert the logic of getTrackInfoFromChapters to the old one.

This bug was introduced in commit 41f5134d9aab17d059edf2b2d20819a84d0c5d11.

src/connectors/youtube.js
@@ -257,10 +257,7 @@ function getTrackInfoFromDescription() {
 }
 
 function getTrackInfoFromChapters() {
-	const trackInfo = {};
-	trackInfo.artist = null;
-	trackInfo.track = Util.getTextFromSelectors(chapterNameSelector);
-	return trackInfo;
+	return Util.splitArtistTrack(Util.getTextFromSelectors(chapterNameSelector), null, false, true);
 }
 
 function getTrackInfoFromTitle() {

src/core/content/util.js
@@ -87,10 +87,11 @@ const Util = {
 	 * @param  {String} str String contains artist and track
 	 * @param  {Array} [separators] Array of separators
 	 * @param  {Boolean} [swap=false] Swap artist and track values
+	 * @param  {Boolean} [allowTrackOnly=false] Allow splitting of a string containing a track name only
 	 * @return {Object} Object contains artist and track fields
 	 */
-	splitArtistTrack(str, separators = null, { swap = false } = {}) {
-		const [artist, track] = this.splitString(str, separators, { swap });
+	splitArtistTrack(str, separators = null, { swap = false } = {}, allowTrackOnly = false) {
+		const [artist, track] = this.splitString(str, separators, { swap }, allowTrackOnly);
 		return { artist, track };
 	},
 
@@ -130,9 +131,10 @@ const Util = {
 	 * @param  {String} str Any string
 	 * @param  {Array} [separators] Array of separators
 	 * @param  {Boolean} [swap=false] Swap values
+	 * @param  {Boolean} [returnStrAsSecond=false] Return the whole string as the second value if there is no separator in it
 	 * @return {Array} Array of strings splitted by separator
 	 */
-	splitString(str, separators, { swap = false } = {}) {
+	splitString(str, separators, { swap = false } = {}, returnStrAsSecond = false) {
 		let first = null;
 		let second = null;
 
@@ -146,6 +148,8 @@ const Util = {
 				if (swap) {
 					[second, first] = [first, second];
 				}
+			} else if (returnStrAsSecond) {
+				return [null, str];
 			}
 		}
 