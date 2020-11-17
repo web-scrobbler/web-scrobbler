275fc78302e2678eaf157755ec695910a44a78ee
Malachi Soord <inverse.chi@gmail.com>
2020-11-17 22:52:21 +0100

    Tidy

--- src/connectors/synology.js
+++ src/connectors/synology.js
@@ -4,7 +4,6 @@ Connector.playerSelector = '.syno-as-control';
 
 Connector.trackSelector = '.info-title';
 
-
 Connector.getTrackInfo = () => {
 	const albumArtistText = document.querySelector('.info-album-artist').textContent;
 	return Util.splitArtistAlbum(albumArtistText, null, { swap: true });
