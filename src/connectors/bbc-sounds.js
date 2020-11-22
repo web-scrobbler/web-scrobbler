cb9fc1afe028f61b4173ace21c513dabfea644fb
alexesprit <alex.esprit@gmail.com>
2020-11-22 11:59:34 +0300

    Improve bbc-sounds connector
    Add support for offline recordings.
    
    Resolves #2637.

--- src/connectors/bbc-sounds.js
+++ src/connectors/bbc-sounds.js
@@ -1,9 +1,49 @@
 'use strict';
 
-Connector.playerSelector = '.radio-main';
+const trackItemSelector = '.sc-c-basic-tile';
 
-Connector.artistSelector = '.sc-c-track__artist';
+setupConnector();
 
-Connector.trackSelector = '.sc-c-track__title';
+function setupConnector() {
+	if (isLiveRadio()) {
+		setupPropertiesForLiveRadio();
+	} else {
+		setupPropertierForOfflineRecord();
+	}
+}
 
-Connector.onReady = Connector.onStateChanged;
+function isLiveRadio() {
+	return document.querySelector(trackItemSelector) === null;
+}
+
+function setupPropertiesForLiveRadio() {
+	Connector.playerSelector = '.radio-main';
+
+	Connector.artistSelector = '.sc-c-track__artist';
+
+	Connector.trackSelector = '.sc-c-track__title';
+}
+
+function setupPropertierForOfflineRecord() {
+	const equalizerIconSelector = '.sc-c-equalizer';
+
+	const artistSelector = '.sc-c-basic-tile__artist';
+	const trackSelector = '.sc-c-basic-tile__title';
+
+	Connector.playerSelector = '.sc-o-scrollable';
+
+	Connector.getArtistTrack = () => {
+		const artistTrackElement = document.querySelector(
+			equalizerIconSelector
+		);
+		if (!artistTrackElement) {
+			return null;
+		}
+
+		const trackItem = artistTrackElement.closest(trackItemSelector);
+		const artist = trackItem.querySelector(artistSelector).textContent;
+		const track = trackItem.querySelector(trackSelector).textContent;
+
+		return { artist, track };
+	};
+}
