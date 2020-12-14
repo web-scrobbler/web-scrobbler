e01d86fc27f725504a81676ff95da34a6aca5460
alexesprit <alex.esprit@gmail.com>
2020-12-14 19:10:34 +0300

    Fix pinguinradio connector
    Closes #2663.

--- src/connectors/pinguinradio.js
+++ src/connectors/pinguinradio.js
@@ -1,19 +1,28 @@
 'use strict';
 
-const filter = MetadataFilter.createFilter({ artist: removeByPrefix });
+const trackInfoSelector = '.stream-title';
+const playButtonSelector = '.playbutton .material-icons';
 
-Connector.playerSelector = 'body';
+Connector.playerSelector = '.audio-player';
 
-Connector.artistSelector = '.artist-name';
+Connector.getArtistTrack = () => {
+	const trackInfoElement = document.querySelector(trackInfoSelector);
+	if (!trackInfoElement) {
+		return null;
+	}
 
-Connector.playButtonSelector = '.fa-play';
+	const [trackElement, , artistElement] = trackInfoElement.childNodes;
+	return {
+		artist: artistElement && artistElement.textContent,
+		track: trackElement && trackElement.textContent,
+	};
+};
 
-Connector.trackArtSelector = '.artist-image img';
+Connector.isPlaying = () => {
+	const playButton = document.querySelector(playButtonSelector);
+	if (playButton) {
+		return playButton.textContent.includes('pause');
+	}
 
-Connector.trackSelector = '.artist-track';
-
-Connector.applyFilter(filter);
-
-function removeByPrefix(text) {
-	return text.replace('by: ', '');
-}
+	return true;
+};
