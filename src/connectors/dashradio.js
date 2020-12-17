ce779d6a6f19cdf4108833dc436c20ef98bddfee
Evie Viau <evie.viau@gmail.com>
2020-12-17 18:41:26 -0500

    Add Dash Radio Connector

---
+++ src/connectors/dashradio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Connector.playerSelector = '#music-dataview-container';
+
+Connector.getArtist = () => {
+    return Util.getTextFromSelectors('#artist-text').substring(3);
+};
+
+Connector.trackSelector = '#title-text';
