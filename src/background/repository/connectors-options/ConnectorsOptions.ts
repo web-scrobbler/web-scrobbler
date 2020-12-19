f2e025521b10d6d2c420dec860bbca9ef69bbef9
alexesprit <alex.esprit@gmail.com>
2020-12-19 21:00:15 +0300

    Add docs

--- src/background/repository/connectors-options/ConnectorsOptions.ts
+++ src/background/repository/connectors-options/ConnectorsOptions.ts
@@ -1,9 +1,24 @@
 export interface ConnectorsOptions<D> {
+	/**
+	 * Return option value for a connector with the given connector ID.
+	 *
+	 * @param connectorId Connector ID
+	 * @param optionKey Option key
+	 *
+	 * @return Option value
+	 */
 	getOption<I extends keyof D, K extends keyof D[I]>(
 		connectorId: I,
 		optionKey: K
 	): Promise<D[I][K]>;
 
+	/**
+	 * Set option value for a connector with the given connector ID.
+	 *
+	 * @param connectorId Connector ID
+	 * @param optionKey Option key
+	 * @param optionValue Option value
+	 */
 	setOption<I extends keyof D, K extends keyof D[I], V extends D[I][K]>(
 		connectorId: I,
 		optionKey: K,
