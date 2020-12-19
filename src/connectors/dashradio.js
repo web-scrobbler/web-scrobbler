46353980605cf52979c395eae025e776999dad28
Evie Viau <evie.viau@gmail.com>
2020-12-19 10:14:50 -0500

    Use MetadataFilter to remove "by" prefix

--- src/connectors/dashradio.js
+++ src/connectors/dashradio.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const filter = MetadataFilter.createFilter({
+	artist: removeByText,
+});
+
 Connector.playerSelector = '#music-dataview-container';
 
-Connector.getArtist = () => {
-	return Util.getTextFromSelectors('#artist-text').substring(3);
-};
+Connector.artistSelector = '#artist-text';
 
 Connector.trackSelector = '#title-text';
+
+Connector.applyFilter(filter);
+
+function removeByText(text) {
+	return text.replace('by ', '');
+}
