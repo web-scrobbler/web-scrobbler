6b835be95af126e6f85c013aabea3f1d1591596d
alexesprit <alex.esprit@gmail.com>
2020-12-20 00:26:09 +0300

    add docs

--- src/background/repository/custom-patterns/CustomUrlPatterns.ts
+++ src/background/repository/custom-patterns/CustomUrlPatterns.ts
@@ -1,5 +1,28 @@
+/**
+ * Repository that provides custom URL patterns for connectors.
+ */
 export interface CustomUrlPatterns {
+	/**
+	 * Get custom URL patterns for a connector with the given ID.
+	 *
+	 * @param connectorId Connector ID
+	 *
+	 * @return Custom URL patterns
+	 */
 	getPatterns(connectorId: string): Promise<string[]>;
+
+	/**
+	 * Set custom URL patterns for a connector with the given ID.
+	 *
+	 * @param connectorId Connector ID
+	 * @param patterns List of custom URL patterns
+	 */
 	setPatterns(connectorId: string, patterns: string[]): Promise<void>;
+
+	/**
+	 * Remove custom URL patterns for a connector with the given ID.
+	 *
+	 * @param connectorId Connector ID
+	 */
 	deletePatterns(connectorId: string): Promise<void>;
 }
