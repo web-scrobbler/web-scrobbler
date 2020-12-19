c864c0295617058f7c3d820a0841762c4c37716b
alexesprit <alex.esprit@gmail.com>
2020-12-19 12:47:48 +0300

    Add test

---
+++ tests/background/repository/custom-patterns/CustomUrlPatternsImpl.spec.ts
@@ -0,0 +1,55 @@
+import { getTestName } from '#/helpers/util';
+import { MockedStorage } from '#/mock/MockedStorage';
+
+import { CustomUrlPatterns } from '@/background/repository/custom-patterns/CustomUrlPatterns';
+import { CustomUrlPatternsData } from '@/background/repository/custom-patterns/CustomUrlPatternsData';
+import { CustomUrlPatternsImpl } from '@/background/repository/custom-patterns/CustomUrlPatternsImpl';
+import { expect } from 'chai';
+
+describe(getTestName(__filename), () => {
+	describe(
+		'test custom URL patterns repository',
+		testCustomUrlPatternsRepository
+	);
+});
+
+function testCustomUrlPatternsRepository() {
+	const repository = createMockedRepository();
+	const connectorId = 'dummyConnectorId';
+	const customUrlPatterns = ['pattern1', 'pattern2'];
+
+	it('should have no patterns for unknown connector', () => {
+		const promise = repository.getPatterns(connectorId);
+		return expect(promise).to.be.eventually.empty;
+	});
+
+	it('should add patterns to unknown connector', () => {
+		const promise = repository.setPatterns(connectorId, customUrlPatterns);
+		return expect(promise).to.be.eventually.fulfilled;
+	});
+
+	it('should have patterns for known connector', () => {
+		const promise = repository.getPatterns(connectorId);
+		return expect(promise).to.be.eventually.deep.equal(customUrlPatterns);
+	});
+
+	it('should remove patterns for known connector', () => {
+		const promise = repository.deletePatterns(connectorId);
+		return expect(promise).to.be.eventually.fulfilled;
+	});
+
+	it('should remove patterns for unknown connector', () => {
+		const promise = repository.deletePatterns(connectorId);
+		return expect(promise).to.be.eventually.fulfilled;
+	});
+
+	it('should have no patterns for removed connector', () => {
+		const promise = repository.getPatterns(connectorId);
+		return expect(promise).to.be.eventually.empty;
+	});
+}
+
+function createMockedRepository(): CustomUrlPatterns {
+	const mockedStorage = new MockedStorage<CustomUrlPatternsData>();
+	return new CustomUrlPatternsImpl(mockedStorage);
+}
