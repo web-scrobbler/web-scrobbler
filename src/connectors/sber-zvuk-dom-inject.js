d1029b88ee8c0787adc5c0a32f4cd2a1d2efacec
UnQuaiz <unquaiz@gmail.com>
2021-02-12 18:36:08 +0800

    fix: event.type checking & codestyle for functions

--- src/connectors/sber-zvuk-dom-inject.js
+++ src/connectors/sber-zvuk-dom-inject.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const getTrackInfo = (track, currentTime) => {
+const getTrackInfo = function(track, currentTime) {
 	const { title, credits, duration, id, image, release_title: album } = track;
 	const trackArt = image.src.replace('{size}', '400x400');
 
@@ -17,38 +17,33 @@ const getTrackInfo = (track, currentTime) => {
 
 const queueEvent = (event) => {
 	let type = null;
-	let trackInfo; let queue;
+	let queue;
 
-	if (event.type === 'update') {
-		switch (event.name) {
-			case 'state':
-				type = event.newValue;
-				queue = event.object.Queue;
-				trackInfo = getTrackInfo(queue.currentTrack, queue.Player.currentTime);
-				break;
-			case 'currentTime':
-			case 'current_idx':
-				queue = event.name === 'current_idx' ? event.object : event.object.Queue;
-				trackInfo = getTrackInfo(queue.currentTrack, queue.Player.currentTime);
-				type = 'currentTime';
-				break;
-			case 'buffered':
-			case 'isLoading':
-			case 'initialPlaying':
-			case 'manually':
-			default:
-				return;
-		}
+	if (event.type !== 'update') {
+		return;
 	}
 
-	if (type) {
-		window.postMessage({
-			sender: 'web-scrobbler',
-			type,
-			trackInfo,
-		}, '*');
+	switch (event.name) {
+		case 'state':
+			type = event.newValue;
+			queue = event.object.Queue;
+			break;
+		case 'currentTime':
+		case 'current_idx':
+			queue = event.name === 'current_idx' ? event.object : event.object.Queue;
+			type = 'currentTime';
+			break;
+		default:
+			return;
 	}
 
+	const trackInfo = getTrackInfo(queue.currentTrack, queue.Player.currentTime);
+
+	window.postMessage({
+		sender: 'web-scrobbler',
+		type,
+		trackInfo,
+	}, '*');
 };
 
 function setupEventListeners() {
