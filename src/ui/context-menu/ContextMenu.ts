fbcce2e2f4df055c44654e4aa50b209b172d1899
alexesprit <alex.esprit@gmail.com>
2021-03-06 17:50:39 +0300

    Add docs

--- src/ui/context-menu/ContextMenu.ts
+++ src/ui/context-menu/ContextMenu.ts
@@ -1,4 +1,11 @@
 export interface ContextMenu {
+	/**
+	 * Add context menu item for a tab with the given tab ID.
+	 *
+	 * @param tabId Tab ID
+	 * @param title Menu item title
+	 * @param onClicked Function that will be executed on the item click
+	 */
 	addContextMenuItem(
 		tabId: number,
 		title: string,
