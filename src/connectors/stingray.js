e6a16cc01199fdfa32c8c01a1780f1cb4c5b98e1
James Harris <james@slideaway.ca>
2021-03-11 21:04:40 -0500

    Add Stingray Music Connector

---
+++ src/connectors/stingray.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Connector.playerSelector = 'player';
+
+Connector.artistSelector = 'cover-with-info.current.cover artist-names div.value';
+
+Connector.trackSelector = 'cover-with-info.current.cover div.title-content > span.title';
+
+Connector.onReady = Connector.onStateChanged;
