106e5bb318e7dd752064f452604e6737fd3df894
James Harris <james@slideaway.ca>
2021-03-11 21:05:18 -0500

    Add CBC Music, Indie88 & Stingray Music Connectors

--- src/core/connectors.js
+++ src/core/connectors.js
@@ -1740,6 +1740,28 @@ const connectors = [{
 	],
 	js: 'connectors/turntable.fm.js',
 	id: 'turntable.fm',
+}, {
+    label: 'Stingray Music',
+    matches: [
+		'*://webplayer.stingray.com/*'
+	],
+    js: 'connectors/stingray.js',
+    id: 'stingray',
+}, {
+    label: 'Indie88',
+    matches: [
+		'*://indie88.com/lean-stream-player/*',
+		'*://cob.leanplayer.com/CINDFM*'
+	],
+    js: 'connectors/indie88.js',
+    id: 'indie88',
+}, {
+    label: 'CBC Music',
+    matches: [
+		'*://www.cbc.ca/listen/cbc-music-playlists*'
+	],
+    js: 'connectors/cbcmusic.js',
+    id: 'cbcmusic',
 }];
 
 define(() => connectors);
