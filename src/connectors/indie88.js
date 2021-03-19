7d5c458e845102108f412ddfc48512daba6d69ea
James Harris <james@slideaway.ca>
2021-03-19 10:36:26 -0400

    Added selectors into arrays

--- src/connectors/indie88.js
+++ src/connectors/indie88.js
@@ -2,10 +2,17 @@
 
 // Indie88 has the track and artist flipped on their main web player. The values are intentionally flipped in this Connector
 
-Connector.playerSelector = 'div.cobrp-player-main, div.mediaplayer-mainbody';
+Connector.playerSelector = [
+				'div.cobrp-player-main',
+				'div.mediaplayer-mainbody',
+];
 
-Connector.artistSelector = 'div.cobrp-player-main div.cobrp-player-artist, div.mediaplayer-mainbody .mediaplayer-snp p#track-info-title span';
+Connector.artistSelector = [
+				'div.cobrp-player-artist',
+				'p#track-info-title span',
+];
 
-Connector.trackSelector = 'div.cobrp-player-main .cobrp-player-song, div.mediaplayer-mainbody .mediaplayer-snp p#track-info-artist span';
-
-Connector.onReady = Connector.onStateChanged;
+Connector.trackSelector = [
+				'.cobrp-player-song',
+				'p#track-info-artist span',
+];
