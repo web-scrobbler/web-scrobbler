9d0038a26533e0738734b6a38ea9ff8eb2a14149
James Harris <james@slideaway.ca>
2021-03-19 09:40:02 -0400

    Add Stingray Music Connector
    Describe the changes you made
    Added new connector for Canadian service Stingray Music
    
    Stingray Music
    This one works well if you're using an ad blocker like uBlock Origin, but I had noted issues with being a track behind when it did ad insertion that I wasn't able to figure out.

--- src/connectors/stingray.js
+++ src/connectors/stingray.js
@@ -1,9 +1,7 @@
 'use strict';
 
 Connector.playerSelector = 'player';
-
 Connector.artistSelector = 'cover-with-info.current.cover artist-names div.value';
-
 Connector.trackSelector = 'cover-with-info.current.cover div.title-content > span.title';
 
 Connector.onReady = Connector.onStateChanged;
