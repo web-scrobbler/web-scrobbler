d0170516a037a2bbd987425ad8e559598b5049f5
James Harris <james@slideaway.ca>
2021-03-19 10:46:33 -0400

    Removed unnecessary onstatechange

---
+++ src/connectors/stingray.js
@@ -0,0 +1,5 @@
+'use strict';
+
+Connector.playerSelector = 'player';
+Connector.artistSelector = 'cover-with-info.current.cover artist-names div.value';
+Connector.trackSelector = 'cover-with-info.current.cover div.title-content > span.title';
