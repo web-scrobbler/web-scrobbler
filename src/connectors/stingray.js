a0ec0c860f1a296dc20ea4aa038fc660f416d223
James Harris <james@slideaway.ca>
2021-03-24 23:01:42 -0400

    Remove Stingray connector from Indie88 branch

--- src/connectors/stingray.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-Connector.playerSelector = 'player';
-Connector.artistSelector = 'cover-with-info.current.cover artist-names div.value';
-Connector.trackSelector = 'cover-with-info.current.cover div.title-content > span.title';
