8964639cd54eda60bc59f477b12f6d686381748d
James Harris <james@slideaway.ca>
2021-03-24 22:58:35 -0400

    Spaces to tab fix

--- src/connectors/indie88.js
+++ src/connectors/indie88.js
@@ -3,16 +3,16 @@
 // Indie88 has the track and artist flipped on their main web player. The values are intentionally flipped in this Connector
 
 Connector.playerSelector = [
-				'div.cobrp-player-main',
-				'div.mediaplayer-mainbody',
+	'div.cobrp-player-main',
+	'div.mediaplayer-mainbody',
 ];
 
 Connector.artistSelector = [
-				'div.cobrp-player-artist',
-				'p#track-info-title span',
+	'div.cobrp-player-artist',
+	'p#track-info-title span',
 ];
 
 Connector.trackSelector = [
-				'.cobrp-player-song',
-				'p#track-info-artist span',
+	'.cobrp-player-song',
+	'p#track-info-artist span',
 ];
