ae8c08b54bcb3e51648b23c3601e1e837fe51a3b
Will Folsom <folsomwg@gmail.com>
2021-03-27 15:05:09 -0400

    Address comments

--- src/connectors/playlistrandomizer.js
+++ src/connectors/playlistrandomizer.js
@@ -1,20 +1,21 @@
 'use strict';
 
 Connector.playerSelector = '.MuiList-root';
-Connector.videoTitleSelector = '.MuiListItemText-primary';
 Connector.trackArtSelector = '.Mui-selected .MuiListItemAvatar-root .MuiAvatar-circle .MuiAvatar-img';
 
 const trackSelector = '.Mui-selected .MuiListItemText-root .MuiListItemText-primary';
 
 Connector.getArtistTrack = () => {
-	let { artist, track } = Util.splitArtistTrack(Util.getTextFromSelectors(trackSelector), '-');
+    let { artist, track } = Util.processYtVideoTitle($(trackSelector).text());
 
-	// Set to the information that we have (probably "Song Title Song Artist" with a space)
+    // Set to some default information that we have (probably "Song Title Song Artist" with a space)
 	// so that the user can edit the info in the extension
 	if (!artist) {
 		artist = Util.getTextFromSelectors(trackSelector);
-		track = Util.getTextFromSelectors(trackSelector);
 	}
+    if (!track) {
+        track = Util.getTextFromSelectors(trackSelector);
+    }
 
 	return { artist, track };
 };
