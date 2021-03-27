41bda878f0beadad19cecf902b5f2f1eb0d9d8ee
Will Folsom <folsomwg@gmail.com>
2021-03-27 15:07:27 -0400

    linting

--- src/connectors/playlistrandomizer.js
+++ src/connectors/playlistrandomizer.js
@@ -6,16 +6,16 @@ Connector.trackArtSelector = '.Mui-selected .MuiListItemAvatar-root .MuiAvatar-c
 const trackSelector = '.Mui-selected .MuiListItemText-root .MuiListItemText-primary';
 
 Connector.getArtistTrack = () => {
-    let { artist, track } = Util.processYtVideoTitle($(trackSelector).text());
+	let { artist, track } = Util.processYtVideoTitle($(trackSelector).text());
 
-    // Set to some default information that we have (probably "Song Title Song Artist" with a space)
+	// Set to some default information that we have (probably "Song Title Song Artist" with a space)
 	// so that the user can edit the info in the extension
 	if (!artist) {
 		artist = Util.getTextFromSelectors(trackSelector);
 	}
-    if (!track) {
-        track = Util.getTextFromSelectors(trackSelector);
-    }
+	if (!track) {
+		track = Util.getTextFromSelectors(trackSelector);
+	}
 
 	return { artist, track };
 };
