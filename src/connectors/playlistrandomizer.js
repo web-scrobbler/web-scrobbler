bbb14f491f44db4be20b3aa4dc7f0de2f46ba9f5
Will Folsom <3690251+willfolsom@users.noreply.github.com>
2021-03-27 15:38:10 -0400

    Add playlist-randomizer connector (#2798)

---
+++ src/connectors/playlistrandomizer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Connector.playerSelector = '.MuiList-root';
+Connector.trackArtSelector = '.Mui-selected .MuiListItemAvatar-root .MuiAvatar-circle .MuiAvatar-img';
+
+const trackSelector = '.Mui-selected .MuiListItemText-root .MuiListItemText-primary';
+
+Connector.getArtistTrack = () => {
+	let { artist, track } = Util.processYtVideoTitle(Util.getTextFromSelectors(trackSelector));
+
+	// Set to some default information that we have (probably "Song Title Song Artist" with a space)
+	// so that the user can edit the info in the extension
+	if (!artist) {
+		artist = Util.getTextFromSelectors(trackSelector);
+	}
+	if (!track) {
+		track = Util.getTextFromSelectors(trackSelector);
+	}
+
+	return { artist, track };
+};
+
+Connector.applyFilter(MetadataFilter.getYoutubeFilter());
