ebb1191e2809ec3400096269dd95567efe52622a
Will Folsom <folsomwg@gmail.com>
2021-03-28 11:54:23 -0400

    Add isScrobblingAllowed handling

--- src/connectors/live365.js
+++ src/connectors/live365.js
@@ -5,4 +5,13 @@ Connector.trackArtSelector = '.player-image';
 Connector.trackSelector = '.track-name';
 Connector.artistSelector = '.track-artist';
 
+const LIVE_365_ARTIST = 'Live365';
+const ADWTAG = 'ADWTAG';
+const ADBREAK = 'Ad Break';
+
 Connector.isPlaying = () => Util.hasElementClass('.playing-indicator', 'is-playing');
+
+Connector.isScrobblingAllowed = () => {
+	const artist = Connector.getArtist();
+	return artist !== LIVE_365_ARTIST && !artist.includes(ADWTAG) && !artist.includes(ADBREAK);
+};
