a13ef19548e196cc6ee15919fe430b7bee97147d
James Harris <james@slideaway.ca>
2021-04-02 11:21:28 -0400

    Added play/pause states

--- src/connectors/indie88.js
+++ src/connectors/indie88.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Indie88 has the track and artist flipped on their main web player. The values are intentionally flipped in setupWebsitePlayer
-
 function setupConnector() {
 	if (isCobrPlayer()) {
 		setupCobrPlayer();
@@ -18,12 +16,19 @@ function setupCobrPlayer() {
 	Connector.playerSelector = 'div.cobrp-player-main';
 	Connector.trackSelector = 'div.cobrp-player-song';
 	Connector.artistSelector = 'div.cobrp-player-artist';
+	Connector.isPlaying = () => {
+		return $('img.cobrp-player-footer-icon').hasClass('mod-pause');
+	};
 }
 
+// Indie88 has the track and artist flipped on their main web player. The values are intentionally flipped in setupWebsitePlayer
 function setupWebsitePlayer() {
 	Connector.playerSelector = 'div.mediaplayer-mainbody';
 	Connector.artistSelector = 'p#track-info-title span';
 	Connector.trackSelector = 'p#track-info-artist span';
+	Connector.isPlaying = () => {
+		return $('a.btn-stop').hasClass('hidden');
+	};
 }
 
 setupConnector();
