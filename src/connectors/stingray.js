17319392144a7607312a933c3d634eee5789157d
James Harris <james@slideaway.ca>
2021-04-03 12:21:16 -0400

    Added album, currentTime, duration and isPlaying

--- src/connectors/stingray.js
+++ src/connectors/stingray.js
@@ -3,3 +3,7 @@
 Connector.playerSelector = 'player';
 Connector.artistSelector = 'cover-with-info.current.cover artist-names div.value';
 Connector.trackSelector = 'cover-with-info.current.cover div.title-content > span.title';
+Connector.albumSelector = 'cover-with-info.current.cover div.album';
+Connector.currentTimeSelector = 'time-progress div.time.duration';
+Connector.durationSelector = 'time-progress div.track.duration';
+Connector.isPlaying = () => $('time-progress div.time.duration').hasClass('ng-star-inserted');
