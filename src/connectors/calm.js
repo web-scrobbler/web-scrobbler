ddedfba66e89086f85feb93076afa92fb9e5544c
Will Folsom <folsomwg@gmail.com>
2021-04-22 07:57:03 -0400

    Use selectors instead of func

--- src/connectors/calm.js
+++ src/connectors/calm.js
@@ -1,17 +1,8 @@
 'use strict';
 
-const currentTimeSelector = '.dBVUFV > .jVXvOE > .eDQGaR > .cwMKJQ';
-const durationSelector = '.dBVUFV > .jVXvOE > .eDQGaR > .eguLQa';
-
 Connector.playerSelector = '.MiniSessionPlayer__PlayerBar-du0okt-4';
 Connector.artistSelector = '.MiniSessionPlayer__Subtitle-du0okt-11';
 Connector.trackSelector = '.MiniSessionPlayer__Title-du0okt-10';
 Connector.trackArtSelector = '.MiniSessionPlayer__IconImage-du0okt-9';
-
-Connector.getCurrentTime = () => getSeconds(currentTimeSelector);
-Connector.getDuration = () => getSeconds(durationSelector);
-
-function getSeconds(selector) {
-	const timeStr = Util.getTextFromSelectors(selector);
-	return Util.stringToSeconds(timeStr);
-}
+Connector.currentTimeSelector = '.dBVUFV > .jVXvOE > .eDQGaR > .cwMKJQ';
+Connector.durationSelector = '.dBVUFV > .jVXvOE > .eDQGaR > .eguLQa';
