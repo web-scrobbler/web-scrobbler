94008341740bb8bf20229a53fad54fa2bffbcbda
Als <capkov.aleksei@gmail.com>
2021-05-21 04:16:32 +0300

    1001tracklists changed its layout a bit. We are now jQuery-less.

--- src/connectors/1001tracklists.js
+++ src/connectors/1001tracklists.js
@@ -3,7 +3,11 @@
 let nextCue;
 let cue;
 let currentTime;
-const mixDuration = Util.stringToSeconds($('.mediaTabItem span').text().split('[')[1].split(']')[0]);
+const mixDuration = Util.stringToSeconds(
+	Util.getTextFromSelectors('.mediaTabItm:not(.hidden) .active')
+		.split('[')[1]
+		.split(']')[0]
+);
 
 Connector.playerSelector = '#playerWidgetFields';
 
@@ -11,10 +15,13 @@ Connector.trackArtSelector = '#artworkLeft';
 
 Connector.albumSelector = '#pageTitle';
 
-Connector.getUniqueID = () => $('.cPlay:first').attr('id');
+Connector.getUniqueID = () => Util.getAttrFromSelectors('.cPlay:first', 'id');
 
 Connector.getArtistTrack = () => {
-	const text = $('.cPlay:first meta[itemprop="name"]').attr('content');
+	const text = Util.getAttrFromSelectors(
+		'.cPlay:first meta[itemprop="name"]',
+		'content'
+	);
 	return Util.splitArtistTrack(text);
 };
 
@@ -34,15 +41,17 @@ Connector.getDuration = () => {
 	}
 };
 
-Connector.isPlaying = () => {
-	return $('#playerWidgetPause').hasClass('fa-pause');
-};
+Connector.isPlaying = () =>
+	Util.hasElementClass('#playerWidgetPause', 'fa-pause');
 
 Connector.isScrobblingAllowed = () => {
-	nextCue = +$('.cPlay:first').nextAll('.topBorder').find('input').eq(0).val();
-	cue = +$('.cPlay:first input').val();
-	currentTime = Util.stringToSeconds($('#playerWidgetCurrentTime').text());
-	const noIDs = $('.cPlay:first').find('.trackFormat .redTxt').length;
+	nextCue = +Util.getAttrFromSelectors(
+		'.cPlay:first ~ .tlpTog:first input',
+		'value'
+	);
+	cue = +Util.getAttrFromSelectors('.cPlay:first input', 'value');
+	currentTime = Util.getSecondsFromSelectors('#playerWidgetCurrentTime');
+	const noIDs = Util.queryElements('.cPlay:first .redTxt').length;
 
 	return noIDs <= 0 && (cue > 0 || nextCue > 0);
 };
