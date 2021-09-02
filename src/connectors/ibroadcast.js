ac3489beee1187350efc900a764e1603d764748d
Ryan <rlayne.ca@gmail.com>
2021-09-02 11:18:48 -0700

    Update ibroadcast.js
    Updated iBroadcast connector for new website UI

--- src/connectors/ibroadcast.js
+++ src/connectors/ibroadcast.js
@@ -2,16 +2,19 @@
 
 Connector.playerSelector = '.mgr-player';
 
-Connector.trackSelector = '.mgr-player-nowplaying .mgr-player-title';
+Connector.trackSelector = '.mgr-player-title';
 
-Connector.artistSelector = '.mgr-player-nowplaying .mgr-player-artist';
+Connector.artistSelector = '.mgr-player-artist';
 
-Connector.albumSelector = '.mgr-player-nowplaying .mgr-player-album';
+Connector.albumSelector = '.mgr-player-album';
 
-Connector.currentTimeSelector = '.mgr-player-top .jp-current-time';
+Connector.currentTimeSelector = '.mgr-player-current-time';
 
-Connector.durationSelector = '.mgr-player-top .jp-duration';
+Connector.durationSelector = '.mgr-player-duration';
 
-Connector.trackArtSelector = '.mgr-player-nowplaying .mgr-player-artwork';
+Connector.trackArtSelector = '.mgr-player-artwork-image';
 
-Connector.isPlaying = () => Util.getAttrFromSelectors('.mgr-player-top .mgr-player-play', 'src') === '/svg/btn-pause.svg';
+Connector.isPlaying = () => Util.hasElementClass(
+	['.mgr-player-play'],
+	'icon-player-pause'
+);
