4fc3d65fa03405eaa2a5f5bff781550d0abbea36
mathwar <57148677+mathwar@users.noreply.github.com>
2021-09-18 21:56:35 +0200

    Fix Horizonte.cl connector (#2989)
    There are multiple elements with .fa-play class on the page.
    To correctly detect the play button status is necessary to add a class 
    to make it a unique combination.
    
    Resolves: #2942

--- src/connectors/horizonte.cl.js
+++ src/connectors/horizonte.cl.js
@@ -6,7 +6,7 @@ Connector.artistSelector = 'p[class="line__clamp_2"]';
 
 Connector.trackSelector = 'h1[class="line__clamp_2"]';
 
-Connector.playButtonSelector = '.fa-play';
+Connector.playButtonSelector = '.button_control .fa-play';
 
 Connector.trackArtSelector = '.np__thumbnail img';
 
