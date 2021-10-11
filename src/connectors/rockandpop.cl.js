11fd299c9584de57d881e715d8654c0d77016749
mathwar <57148677+mathwar@users.noreply.github.com>
2021-10-11 22:54:01 +0200

    Add Radio Rock&Pop Chile connector
    Rock & Pop is a radio station located at 94.1 MHz on the FM dial 
    in Santiago, Chile.

---
+++ src/connectors/rockandpop.cl.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Connector.playerSelector = '#player';
+
+Connector.artistSelector = '#top_player_TopPlayer_spanInfo3';
+
+Connector.trackSelector = '#top_player_TopPlayer_spanInfo2';
+
+Connector.playButtonSelector = '.mm_player_btn_play .fa .fa-play';
+
+Connector.trackArtSelector = '.mm_media-left img';
+
+Connector.currentTimeSelector = '.mm_current-time';
+
+Connector.durationSelector = '.mm_total-time';
