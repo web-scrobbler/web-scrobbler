ec67d6897ddd546fb7354bdb0c538a2587bc073a
James Harris <james@slideaway.ca>
2021-11-12 10:19:06 -0500

    Fix Indie88 connector

--- src/connectors/indie88.js
+++ src/connectors/indie88.js
@@ -9,22 +9,29 @@ function setupConnector() {
 }
 
 function isCobrPlayer() {
-	return document.querySelector('div.cobrp-page-column') !== null;
+	return document.querySelector('article.cobrp-page') !== null;
 }
 
 function setupCobrPlayer() {
-	Connector.playerSelector = 'div.cobrp-player';
+	Connector.playerSelector = 'div.cobrp-player-main';
 	Connector.trackSelector = 'div.cobrp-player-song';
 	Connector.artistSelector = 'div.cobrp-player-artist';
-	Connector.isPlaying = () => $('img.cobrp-player-footer-icon').hasClass('mod-pause');
+	Connector.isPlaying = () => {
+		return Util.hasElementClass(
+			'img.cobrp-player-footer-icon',
+			'mod-pause'
+		);
+	};
 }
 
 // Indie88 has the track and artist flipped on their main web player. The values are intentionally flipped in setupWebsitePlayer
 function setupWebsitePlayer() {
-	Connector.playerSelector = 'div.mediaplayer';
+	Connector.playerSelector = 'div.mediaplayer-mainbody';
 	Connector.artistSelector = 'p#track-info-title span';
 	Connector.trackSelector = 'p#track-info-artist span';
-	Connector.isPlaying = () => $('a.btn-play').hasClass('hidden');
+	Connector.isPlaying = () => {
+		return Util.hasElementClass('a.btn-stop', 'hidden');
+	};
 }
 
 setupConnector();
