9068650c4a3c16813aedce1efa0b58377ed4d501
Julien <fetzu@users.noreply.github.com>
2022-02-07 19:32:18 +0100

    lint: spaces to tabs

--- src/connectors/auborddeleau.radio.js
+++ src/connectors/auborddeleau.radio.js
@@ -13,19 +13,19 @@ function isHomePlayer() {
 }
 
 function setupHomePlayer() {
-    Connector.playerSelector = '.hero-player';
-    Connector.artistSelector = '.rk_widget_currenttrack_table>tbody:nth-child(1)>tr:nth-child(1)>td:nth-child(3)';
-    Connector.trackSelector = '.rk_widget_recenttracks_title';
-    Connector.trackArtSelector = '.rk_widget_currenttrack_table img';
-    Connector.isPlaying = () => Util.hasElementClass('.ci-soundplayer', 'playing');
+	Connector.playerSelector = '.hero-player';
+	Connector.artistSelector = '.rk_widget_currenttrack_table>tbody:nth-child(1)>tr:nth-child(1)>td:nth-child(3)';
+	Connector.trackSelector = '.rk_widget_recenttracks_title';
+	Connector.trackArtSelector = '.rk_widget_currenttrack_table img';
+	Connector.isPlaying = () => Util.hasElementClass('.ci-soundplayer', 'playing');
 }
 
 function setupOnAirPlayer() {
-    Connector.playerSelector = '#player';
-    Connector.artistSelector = '#artiste';
-    Connector.trackSelector = '#titre';
-    Connector.trackArtSelector = '#pochette img';
-    Connector.isPlaying = () => $('#btnStop').css('display') === 'inline-block';
+	Connector.playerSelector = '#player';
+	Connector.artistSelector = '#artiste';
+	Connector.trackSelector = '#titre';
+	Connector.trackArtSelector = '#pochette img';
+	Connector.isPlaying = () => $('#btnStop').css('display') === 'inline-block';
 }
 
 setupConnector();
