f85194238ee44b2983457d2eadc52c0ca400b3ea
fckawe <fckawe@mailbox.org>
2022-02-23 10:11:27 +0100

    Reformatted source code via grunt lint.

--- src/connectors/fluxfm.js
+++ src/connectors/fluxfm.js
@@ -4,7 +4,7 @@ Connector.playerSelector = '.hero-player__headline';
 
 Connector.getArtistTrack = () => {
 	const artistTrack = Util.getTextFromSelectors('.hero-player__title');
-	return Util.splitArtistTrack(artistTrack, null, {swap: true});
+	return Util.splitArtistTrack(artistTrack, null, { swap: true });
 };
 
 Connector.isPlaying = () => {
