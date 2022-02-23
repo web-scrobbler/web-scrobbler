62bb024d3c91c0e148b46754498bdd7608f7549c
fckawe <fckawe@mailbox.org>
2022-02-23 10:06:12 +0100

    FluxFM connector: Artist and title were swapped

--- src/connectors/fluxfm.js
+++ src/connectors/fluxfm.js
@@ -4,7 +4,7 @@ Connector.playerSelector = '.hero-player__headline';
 
 Connector.getArtistTrack = () => {
 	const artistTrack = Util.getTextFromSelectors('.hero-player__title');
-	return Util.splitArtistTrack(artistTrack);
+	return Util.splitArtistTrack(artistTrack, null, {swap: true});
 };
 
 Connector.isPlaying = () => {
