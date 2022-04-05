50f8190e8eb5a2996995dea9e82acddb499605e1
hornbuckle <hornbuckle@gmail.com>
2022-04-04 18:05:28 -0700

    Add connector for hoopladigital.com
    New connector for hoopla, a service provided by local libraries to "borrow" and stream audio

---
+++ src/connectors/hoopladigital.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Connector.playerSelector = '.duration-1000';
+
+Connector.trackSelector = '.text-current > h2';
+
+Connector.getTrackInfo = () => {
+    const artistAlbum = Util.getTextFromSelectors('.text-current > p');
+    return Util.splitArtistAlbum(artistAlbum, ['-'], { swap: true });
+};
+
+Connector.trackArtSelector = '.duration-1000 .max-w-112 > img';
+
+Connector.playButtonSelector = '.duration-1000 button[aria-label=Play]';
+
+Connector.durationSelector = '.duration-1000 .rounded-md.border-gray-400 button > span > span:nth-of-type(3) > span';
