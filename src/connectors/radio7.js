83054ba9182bb381ae6353f189aa55f3778e249d
Malachi Soord <inverse.chi@gmail.com>
2022-04-24 21:55:52 +0200

    Prevent scrobbling radio7 on non-tracks

--- src/connectors/radio7.js
+++ src/connectors/radio7.js
@@ -5,3 +5,7 @@ Connector.playerSelector = '.swiper-intro-inner';
 Connector.artistTrackSelector = '#dziesma';
 
 Connector.pauseButtonSelector = '.amazingaudioplayer-pause';
+
+Connector.isScrobblingAllowed = () => {
+	return Connector.getArtist() !== 'Radio7';
+};
