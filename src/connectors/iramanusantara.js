e2dbe3064d1af4f6e16a3b4d42882424b3d2c269
Malachi Soord <inverse.chi@gmail.com>
2022-04-28 18:53:35 +0200

    Update src/connectors/iramanusantara.js

--- src/connectors/iramanusantara.js
+++ src/connectors/iramanusantara.js
@@ -6,7 +6,7 @@ Connector.artistSelector = '#pl-info-text > h4';
 
 Connector.trackSelector = '#pl-info-text > h3';
 
-// Album title is only available when track is played through the album page 
+// Album title is only available when track is played through the album page
 Connector.albumSelector = '.ad-info-text1 > h1';
 
 Connector.playButtonSelector = '#pl-ctrl-play';
