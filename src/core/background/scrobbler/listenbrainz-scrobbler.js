0c5d2fe57eccf80773b1106b85122361ea6f21bc
badlandspray <92253501+badlandspray@users.noreply.github.com>
2022-06-09 14:09:04 +0000

    Send more additional information to listenbrainz

--- src/core/background/scrobbler/listenbrainz-scrobbler.js
+++ src/core/background/scrobbler/listenbrainz-scrobbler.js
@@ -284,6 +284,14 @@ define((require) => {
 				trackMeta.additional_info.release_artist_name = song.getAlbumArtist();
 			}
 
+			if (song.getUniqueId() && song.metadata.label === "Spotify") {
+				trackMeta.additional_info.spotify_id = song.getUniqueId();
+			}
+
+			if (song.getDuration()) {
+				trackMeta.additional_info.duration = song.getDuration();
+			}
+
 			return trackMeta;
 		}
 	}
