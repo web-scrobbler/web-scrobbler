5bec5dbb54d542d2adf6bb7239960ef69dd52fa4
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2022-06-19 02:05:09 +0800

    Fix width detection bugs

--- src/connectors/dashradio.js
+++ src/connectors/dashradio.js
@@ -1,7 +1,5 @@
 'use strict';
 
-let ww;
-
 addWindowListeners();
 
 const filter = MetadataFilter.createFilter({
@@ -10,24 +8,23 @@ const filter = MetadataFilter.createFilter({
 
 Connector.playerSelector = '#music-dataview-container';
 
-if (ww > 1450) {
-	Connector.artistSelector = '#artist-text';
-	Connector.trackSelector = '#title-text';
-} else {
-	Connector.artistSelector = '#marquee2 > span.artist';
-	Connector.trackSelector = '#marquee1 > span';
-}
-
 Connector.applyFilter(filter);
 
 function removeByText(text) {
 	return text.replace('by ', '');
 }
 
-function addWindowListeners() {
-	addEventListener('load', setWindowWidth);
-	addEventListener('resize', setWindowWidth);
-	function setWindowWidth() {
-		ww = window.innerWidth;
+function setMetadataSelectors() {
+	if (window.innerWidth >= 1450) {
+		Connector.artistSelector = '#artist-text';
+		Connector.trackSelector = '#title-text';
+	} else {
+		Connector.artistSelector = '#marquee2 > span.artist';
+		Connector.trackSelector = '#marquee1 > span';
 	}
+}
+
+function addWindowListeners() {
+	addEventListener('resize', setMetadataSelectors);
+	setMetadataSelectors();
 }
