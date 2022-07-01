6d67d04cea08d8a4ed2c070ac34da1b782b94d15
Evan Mullins <evanrm@gmail.com>
2022-07-01 09:44:42 -0400

    set up new connector file for play.nugs.net

---
+++ src/connectors/nugs.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Connector.playerSelector = 'div[app-player] .player-wrapper';
+
+Connector.playButtonSelector = '.play-toggle';
+
+Connector.pauseButtonSelector = '.pause-toggle';
+
+Connector.artistSelector = '.player-info .artist-name';
+
+Connector.trackSelector = '.player-info .track-title';
+
+Connector.albumSelector = '.player-info .recording-title';
+
+Connector.currentTimeSelector = '.scrubber-time .player-time';
+
+Connector.durationSelector = '.scrubber-time .track-time';
+
+Connector.trackArtSelector = '.player-cover img';
