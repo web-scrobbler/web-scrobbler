5803dc693d32b56872f7a23cd0e98149ef74144e
Californ1a <rk_california@yahoo.com>
2022-07-10 06:09:03 -0400

    add tooltip selector

--- src/connectors/playlist-randomizer.js
+++ src/connectors/playlist-randomizer.js
@@ -2,9 +2,20 @@
 
 Connector.playerSelector = '#root';
 Connector.trackArtSelector = '.Mui-selected .MuiListItemAvatar-root .MuiAvatar-circular .MuiAvatar-img';
-Connector.playButtonSelector = 'button[title="Play"]';
 
 const trackSelector = '.Mui-selected .MuiListItemText-root .MuiListItemText-primary';
+
+Connector.isPlaying = () => {
+	const playButton = document.querySelector('button[title="Play"]');
+	const tooltip = document.querySelector('.MuiTooltip-popper');
+	const tooltipText = tooltip ? tooltip.innerText : '';
+
+	if (playButton || tooltipText.includes('Play')) {
+		return false;
+	}
+
+	return true;
+};
 
 Connector.getArtistTrack = () => {
 	const currentTrack = document.querySelector(trackSelector);
@@ -13,8 +24,7 @@ Connector.getArtistTrack = () => {
 	}
 	let { artist, track } = Util.processYtVideoTitle(currentTrack.firstChild.nodeValue);
 
-	// Set to some default information that we have (probably "Song Title Song Artist" with a space)
-	// so that the user can edit the info in the extension
+	// Set to some default information that we have so that the user can edit the info in the extension
 	if (!artist) {
 		const regex = /^(.*) - Topic$/;
 		artist = currentTrack.lastChild.innerText.replace(regex, '$1');
