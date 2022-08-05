356bc59bf2d50ecde25c7f271c574b47fdcd7680
Malcolm Peterson <mgpetersn@gmail.com>
2022-08-05 17:54:25 -0400

    #3284
    updating due to page redesign

--- src/connectors/epidemicsound.js
+++ src/connectors/epidemicsound.js
@@ -1,11 +1,11 @@
 'use strict';
 
-Connector.playerSelector = '[class*=PlayerUI__content]';
+Connector.playerSelector = '[class="src-mainapp-player-components-___PlayerBar__player___ZJ8j7"]';
 
-Connector.artistSelector = '[class*=CreativesLabel__container]';
+Connector.artistSelector = '[aria-label="creatives"]';
 
-Connector.trackSelector = '[class*=TrackInfo__title]';
+Connector.trackSelector = '[class="src-mainapp-components-___CreativesLabel__container___3sd3f"]';
 
-Connector.timeInfoSelector = '[class*=TimeProgress__container]';
+Connector.timeInfoSelector = '["class=src-mainapp-player-components-___PlayerBar__elapsedTime___3j43S css-7bhp37"]';
 
-Connector.playButtonSelector = '[class*=PlaybackControls__playIcon]';
+Connector.playButtonSelector = '[class="src-mainapp-player-components-___PlaybackControls__button___2_sdi src-mainapp-player-components-___PlaybackControls__playPauseButton___2sy2U"]';
