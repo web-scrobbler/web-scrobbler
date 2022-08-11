6a14dea3f6ae1c65dc56a767e2855a69ec9d2ddf
Malachi Soord <inverse.chi@gmail.com>
2022-08-11 21:29:36 +0200

    Fix lint

--- src/connectors/onlineradiobox.js
+++ src/connectors/onlineradiobox.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// Need to select also .station to cover popup where #top_player_track is 
-// outside of .player.
+// Need to select also .station to cover popup where #top_player_track is outside of .player.
 Connector.playerSelector = ['.player', 'body > .station'];
 
 Connector.artistTrackSelector = '#top_player_track';
