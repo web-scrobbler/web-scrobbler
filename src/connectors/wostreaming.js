d22fbcddf4bbfd415a0d29d4da45c81edd05e9df
D Λ V I D <hornbuckle@gmail.com>
2022-09-13 08:21:42 -0700

    Create wostreaming.js

---
+++ src/connectors/wostreaming.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Connector.playerSelector = '#main';
+
+Connector.artistSelector = 'p[class$=-player__artist]';
+
+Connector.trackSelector = 'h3[class$=-player__song-name]';
+
+Connector.trackArtSelector = 'img[class*=-player__album-cover]';
+
+Connector.isPlaying = () => Util.getAttrFromSelectors('div[class$=-stream-player] span[class$=-stream-player__play]', 'title') === 'Stop';
