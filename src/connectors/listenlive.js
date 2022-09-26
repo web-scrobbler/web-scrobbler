fca4a30f4a47b6833dac1264f39e6a560cdb1a85
david hornbuckle <hornbuckle@gmail.com>
2022-09-26 11:35:37 -0700

    Update listenlive.js

--- src/connectors/listenlive.js
+++ src/connectors/listenlive.js
@@ -6,4 +6,26 @@ Connector.artistSelector = '.nowPlayingCard .song .artist';
 
 Connector.trackSelector = '.nowPlayingCard .song .title';
 
+Connector.getAlbum = () => {
+	const albumText = Util.getTextFromSelectors('.nowPlayingCard .song .album');
+
+	if (albumText && !albumText.includes('{album}')) {
+		return albumText;
+	}
+
+	return null;
+};
+
+Connector.getTrackArt = () => {
+	const trackArtUrl = Util.extractImageUrlFromSelectors('.nowPlayingCard figure img');
+
+	if (trackArtUrl) {
+		return trackArtUrl.replace(/(?<=\/)\d+x\d+/g, '300x300'); // larger image path
+	}
+
+	return null;
+};
+
+Connector.isTrackArtDefault = (url) => url.includes('default');
+
 Connector.isPlaying = () => Util.hasElementClass('#nowPlaying', 'hasSong');
