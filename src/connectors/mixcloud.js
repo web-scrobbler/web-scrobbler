eab15356b7e2769f5938569b43029cd223be206a
david hornbuckle <hornbuckle@gmail.com>
2022-09-26 12:00:04 -0700

    Update mixcloud.js

--- src/connectors/mixcloud.js
+++ src/connectors/mixcloud.js
@@ -4,26 +4,60 @@ const filter = MetadataFilter.createFilter({
 	artist: [removeByPrefix, removeBuySuffix],
 });
 
-Connector.playerSelector = '[class*=playerQueue__PlayerWrapper]';
+Connector.playerSelector = '[class^=playerQueue__PlayerWrapper-]';
 
-Connector.artistSelector = '[class*=PlayerSliderComponent__Artist]';
+const trackInfoSelector = '[class^=PlayerSliderComponent__PlayerTrackInfo-]';
 
-Connector.trackSelector = '[class*=PlayerSliderComponent__Track]';
+Connector.getTrackInfo = () => {
+	const artistTrackElement = document.querySelector(trackInfoSelector);
+	let artistText;
+	let trackText;
+	let trackArtUrl;
+	let currentTimeValue;
+	let remainingTimeValue;
+	let podcastBoolean;
 
-Connector.playButtonSelector = '[aria-label=Play]';
+	if (artistTrackElement && artistTrackElement.hasChildNodes()) {
+		artistText = Util.getTextFromSelectors('[class*=PlayerSliderComponent__Artist-]');
+		trackText = Util.getTextFromSelectors('[class*=PlayerSliderComponent__Track-]');
+		podcastBoolean = false;
+
+	} else if (artistTrackElement && !artistTrackElement.hasChildNodes()) {
+		artistText = Util.getTextFromSelectors('[class^=PlayerControls__ShowOwnerName-]');
+		trackText = Util.getTextFromSelectors('[class*=PlayerControls__ShowTitle-]');
+		trackArtUrl = Util.extractImageUrlFromSelectors('[class^=PlayerControls__ShowPicture-] > img')
+			.replace(/(?<=\/)\d+x\d+(?=\/)/g, '300x300'); // larger image path
+		currentTimeValue = Util.getSecondsFromSelectors('[class^=PlayerSliderComponent__StartTime-]');
+		remainingTimeValue = Util.getSecondsFromSelectors('[class^=PlayerSliderComponent__EndTime-]');
+		podcastBoolean = true;
+	}
+
+	return {
+		artist: artistText,
+		track: trackText,
+		trackArt: trackArtUrl,
+		currentTime: currentTimeValue,
+		duration: -remainingTimeValue + currentTimeValue,
+		isPodcast: podcastBoolean,
+	};
+};
+
+Connector.isPlaying = () => Util.getAttrFromSelectors('[class^=PlayButton__PlayerControl-]', 'aria-label') === 'Pause';
 
 Connector.isStateChangeAllowed = () => {
 	/*
 	 * Mixcloud player hides artist and track elements while seeking the stream,
 	 * and we should not update state in such case.
 	 */
-	return Connector.getArtist() || Connector.getTrack();
+	return Connector.getTrackInfo() && Util.isElementVisible(trackInfoSelector);
 };
+
+Connector.isScrobblingAllowed = () => Connector.isStateChangeAllowed();
 
 Connector.applyFilter(filter);
 
 function removeByPrefix(text) {
-	return text.replace('by ', '');
+	return text.replace(/^by\s/g, '');
 }
 
 function removeBuySuffix(text) {
