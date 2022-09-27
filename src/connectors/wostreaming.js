464036746779d53fd434ef5369355565e0dd664a
david hornbuckle <hornbuckle@gmail.com>
2022-09-26 17:01:58 -0700

    Update wostreaming.js

--- src/connectors/wostreaming.js
+++ src/connectors/wostreaming.js
@@ -8,4 +8,6 @@ Connector.trackSelector = 'h3[class$=-player__song-name]';
 
 Connector.trackArtSelector = 'img[class*=-player__album-cover]';
 
+Connector.isTrackArtDefault = (url) => url.includes('DefaultAlbumArt');
+
 Connector.isPlaying = () => Util.getAttrFromSelectors('div[class$=-stream-player] span[class$=-stream-player__play]', 'title') === 'Stop';
