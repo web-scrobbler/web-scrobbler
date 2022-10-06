5c17d29f527b5c40ef3f15044c15fd133d8bf985
david hornbuckle <hornbuckle@gmail.com>
2022-10-06 14:01:04 -0700

    Update KCRW connector (#3408)
    * Update kcrw.js
    
    * Update connectors.js

--- src/connectors/kcrw.js
+++ src/connectors/kcrw.js
@@ -1,9 +1,30 @@
 'use strict';
 
-Connector.playerSelector = '#player';
+Connector.playerSelector = '.site-player'; // supports main and mini players
 
-Connector.artistSelector = '.episode-name';
+const filter = MetadataFilter.createFilter({
+	track: (text) => text.replace(/[‘’“”]/g, ''), // filter for Today's Top Tune
+});
 
-Connector.trackSelector = '.song-name';
+Connector.getTrackInfo = () => {
+	const metaSelector = '.site-player .meta';
+	const showNameText = Util.getTextFromSelectors(`${metaSelector} .show-name`);
+	const artistText = Util.getTextFromSelectors(`${metaSelector} .episode-name`);
+	const trackText = Util.getTextFromSelectors(`${metaSelector} .song-name`);
 
-Connector.isPlaying = () => Util.hasElementClass('.play', 'active');
+	if (Util.hasElementClass(metaSelector, 'music') && trackText && !artistText.includes('[BREAK]')) {
+		return {
+			artist: artistText,
+			track: trackText,
+		};
+
+	} else if (!Util.hasElementClass(metaSelector, 'music') && artistText && showNameText.includes('Today\'s Top Tune')) {
+		return Util.splitArtistTrack(artistText, [': ']);
+	}
+
+	return null;
+};
+
+Connector.applyFilter(filter);
+
+Connector.isPlaying = () => Util.hasElementClass('button.play', 'active');
