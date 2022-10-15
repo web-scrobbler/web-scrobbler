c920edbd78d5d18c4e766e27f89e447db86e5b55
jbwharris <>
2022-10-15 15:56:36 -0400

    Revised selectors
    Used selectors suggested by @hornbuckle

--- src/connectors/qcindie.js
+++ src/connectors/qcindie.js
@@ -1,7 +1,7 @@
 'use strict';
 // the artist and track info are inverted on this site, values for artist and track are intentionally flipped
 
-Connector.playerSelector = '#content.site-content';
-Connector.trackSelector = '#ReloadThis .col-sm-6:first-child p.artistinfo';
-Connector.artistSelector = '#ReloadThis .col-sm-6:first-child p.trackinfo';
-Connector.isPlaying = () => $('#wp1598381648smallbuttonplay').css('display') === 'none';
+Connector.playerSelector = '#content';
+Connector.artistSelector = '#ReloadThis > div:first-of-type .trackinfo';
+Connector.trackSelector = '#ReloadThis > div:first-of-type .artistinfo';
+Connector.isPlaying = () => Util.isElementVisible('.lunaaudioplayer [id$=smallbuttonpause]');
