72f05256e3224a4304d69d2ed40c7de62957f740
Malachi Soord <inverse.chi@gmail.com>
2022-11-01 21:02:06 +0100

    Update friskyfm.js

--- src/connectors/friskyfm.js
+++ src/connectors/friskyfm.js
@@ -3,4 +3,4 @@ Connector.playerSelector = '#app';
 Connector.trackSelector = '[class*=music-player_metaInner_] .title';
 Connector.artistSelector = '[class*=music-player_metaInner_] .artist';
 Connector.trackArtSelector = 'img[class*=_base-resource-image_image_]';
-Connector.isPlaying = () => Util.getAttrFromSelectors('[class*=music-player_play_] > svg > use', 'xlink\:href') === '#icon-pause';
+Connector.isPlaying = () => Util.getAttrFromSelectors('[class*=music-player_play_] > svg > use', 'xlink:href') === '#icon-pause';
