6e0a71bed6c0ae1cc03469567d9908801be7465a
Malachi Soord <inverse.chi@gmail.com>
2022-11-01 20:56:51 +0100

    Update gotradio.js

--- src/connectors/gotradio.js
+++ src/connectors/gotradio.js
@@ -2,7 +2,7 @@
 
 const filter = MetadataFilter.createFilter({ track: removePrefix });
 
-Connector.isPlaying = () => { return Util.hasElementClass('div#playbtn', 'jp-stopx'); };
+Connector.isPlaying = () => Util.hasElementClass('div#playbtn', 'jp-stopx');
 Connector.playerSelector = '.player-content';
 Connector.trackSelector = 'h1#song';
 Connector.artistSelector = 'h2#artist';
