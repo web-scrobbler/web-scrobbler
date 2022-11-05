5059092853b7010fdd47a795ae880b6617fbdd30
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2022-11-06 03:22:35 +0800

    Remove errors

--- src/connectors/apple-music.js
+++ src/connectors/apple-music.js
@@ -4,15 +4,15 @@ const shadowRoot = document.querySelector('amp-lcd').shadowRoot;
 
 const queryShadowRoot = (selector) => shadowRoot.querySelector(selector);
 
-Connector.getArtist = () => queryShadowRoot('.lcd-meta__secondary .lcd-meta-line__fragment:first-child').textContent;
+Connector.getArtist = () => queryShadowRoot('.lcd-meta__secondary .lcd-meta-line__fragment:first-child')?.textContent;
 
-Connector.getAlbum = () => queryShadowRoot('.lcd-meta__secondary .lcd-meta-line__fragment:last-child').textContent;
+Connector.getAlbum = () => queryShadowRoot('.lcd-meta__secondary .lcd-meta-line__fragment:last-child')?.textContent;
 
-Connector.getCurrentTime = () => queryShadowRoot('.lcd-progress__time--elapsed').textContent;
+Connector.getCurrentTime = () => queryShadowRoot('.lcd-progress__time--elapsed')?.textContent;
 
-Connector.getRemainingTime = () => queryShadowRoot('.lcd-progress__time--remaining').textContent;
+Connector.getRemainingTime = () => queryShadowRoot('.lcd-progress__time--remaining')?.textContent;
 
-Connector.getTrack = () => queryShadowRoot('.lcd-meta__primary-wrapper .lcd-meta-line__fragment').textContent;
+Connector.getTrack = () => queryShadowRoot('.lcd-meta__primary-wrapper .lcd-meta-line__fragment')?.textContent;
 
 Connector.isPlaying = () => Util.getAttrFromSelectors('.playback-play__pause', 'aria-hidden') !== 'true';
 
