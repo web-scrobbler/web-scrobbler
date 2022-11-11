a901cb0fbed94cfd135a9d0893a530d39217bc6b
david hornbuckle <hornbuckle@gmail.com>
2022-11-11 15:04:11 -0800

    Delete wsum.js

--- src/connectors/wsum.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Connector.playerSelector = '.songLineOnNow';
-Connector.playButtonSelector = '.playButton';
-Connector.albumSelector = '.songLineOnNow .songDetail .songAlbum';
-Connector.artistSelector = '.songLineOnNow .songDetail .songArtist';
-Connector.trackSelector = '.songLineOnNow .songDetail .songTitle';
