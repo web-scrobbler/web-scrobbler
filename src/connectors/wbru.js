31da3f45407d7e79efe039994077014f6e396037
jbwharris <>
2022-11-12 21:20:58 -0500

    Fix linter issue with isPlaying

--- src/connectors/wbru.js
+++ src/connectors/wbru.js
@@ -4,4 +4,4 @@ Connector.playerSelector = '.radioco-player';
 Connector.trackSelector = '.player-meta .track-name';
 Connector.artistSelector = '.player-meta .track-artist';
 Connector.trackArtSelector = 'img.current-artwork';
-Connector.isPlaying = () => { return Util.hasElementClass('button.play-button', 'icon-playerstop'); };
+Connector.isPlaying = () => Util.hasElementClass('button.play-button', 'icon-playerstop');
