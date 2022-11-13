27729149db3e0d49e94fb6b9c74c99c224f394f3
jbwharris <>
2022-11-13 13:07:04 -0500

    Remove wbru connector

--- src/connectors/wbru.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Connector.playerSelector = '.radioco-player';
-Connector.trackSelector = '.player-meta .track-name';
-Connector.artistSelector = '.player-meta .track-artist';
-Connector.trackArtSelector = 'img.current-artwork';
-Connector.isPlaying = () => Util.hasElementClass('button.play-button', 'icon-playerstop');
