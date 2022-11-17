4433e6d40dc1109a77cc836126d794490f40c616
david hornbuckle <hornbuckle@gmail.com>
2022-11-17 12:13:51 -0800

    Update Freegal Music connector (#3471)
    * Update freegalmusic.js
    
    * Update connectors.js

--- src/connectors/freegalmusic.js
+++ src/connectors/freegalmusic.js
@@ -1,35 +1,50 @@
 'use strict';
 
+const filter = MetadataFilter.createFilter({
+	artist: extractPrimaryArtist,
+	track: [removeParody, removeRemaster],
+});
+
 Connector.playerSelector = '#player-section';
 
 Connector.pauseButtonSelector = '.fp-playbtn[title=Pause]';
 
 Connector.trackSelector = '.playlist-player ul > li.active-playlist-song';
 
+Connector.durationSelector = '.fp-duration';
+
+Connector.currentTimeSelector = '.fp-elapsed';
+
 Connector.getArtist = () => {
 	const artistTrackText = Util.getTextFromSelectors('.fp-message');
 
 	if (artistTrackText) {
-		return artistTrackText.split(' - ')[0]; // track title may be truncated or missing; only return artist
+		// track title may be truncated or missing; only return artist
+		return artistTrackText.split(' - ')[0];
 	}
 
 	return null;
 };
 
-Connector.durationSelector = '.fp-duration';
-
-Connector.currentTimeSelector = '.fp-elapsed';
-
 Connector.isScrobblingAllowed = () => {
 	return (
 		Connector.getTrack() &&
 		Util.isElementVisible(Connector.playerSelector) &&
-		!Connector.getArtist().includes('Sorry, this song is not available for streaming at this time')
+		!Connector.getArtist().includes('Sorry, this song is not available')
 	);
 };
 
-const filter = MetadataFilter.createFilter({
-	artist: (text) => text.split(/(?<!\d),(?!\s)/g)[0], // only first artist if comma-separated list of contributors
-});
-
 Connector.applyFilter(filter);
+
+function extractPrimaryArtist(text) {
+	// only return first artist if semicolon/comma-separated list of contributors
+	return text.split(/(;|((?<!\d),))(?!\s)/)[0];
+}
+
+function removeParody(text) {
+	return text.replace(/\s?\((Parody|Lyrical Adaption) of.*(\)|\.{3})/i, '');
+}
+
+function removeRemaster(text) {
+	return text.replace(/\s?(\(|\[)[\s\w]*Remaster(ed)?[\s\w]*(\)|\]|\.{3})/gi, '');
+}
