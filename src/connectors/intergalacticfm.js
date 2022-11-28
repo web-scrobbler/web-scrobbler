8f563ceeef31a79bc90986d4c458a76633d92740
Ovidiu Cara <ovidiu92@gmail.com>
2022-11-28 21:35:05 +0100

    Updated connector to Intergalactic FM (#3494)
    * Updated connector to Intergalactic FM
    
    * Update src/connectors/intergalacticfm.js
    
    Co-authored-by: Malachi Soord <inverse.chi@gmail.com>

--- src/connectors/intergalacticfm.js
+++ src/connectors/intergalacticfm.js
@@ -1,12 +1,7 @@
 'use strict';
 
-Connector.playerSelector = '#radio_playing_now';
+Connector.playerSelector = '#player';
 
-Connector.artistSelector = '#radio_playing_now > .media-body > h5 > span';
+Connector.artistTrackSelector = '.track-meta > h5';
 
-Connector.trackSelector =
-	'#radio_playing_now > .media-body h5 > span:nth-child(3)';
-
-Connector.isPlaying = () => {
-	return $('#player1').hasClass('jw-state-playing');
-};
+Connector.isPlaying = () => Util.hasElementClass('.vjs-play-control', 'vjs-playing');
