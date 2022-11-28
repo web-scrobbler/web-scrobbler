5e6bc1f8d70b19bbb2a12def0789ba00f3f62a8c
redstonekasi <kasi@hanneskann.de>
2022-11-28 22:26:56 +0100

    Fix some issues with the uwu radio connector

--- src/connectors/uwu-radio.js
+++ src/connectors/uwu-radio.js
@@ -1,17 +1,4 @@
 'use strict';
 
-// If I understood correctly, these are all just backups in case the Media
-// Session API isn't supported.
 Connector.useMediaSessionApi();
-
-// There shouldn't be any other images ever.
-Connector.trackArtSelector = ".text-center > img";
-Connector.trackSelector = "img ~ div > div:nth-child(1)";
-Connector.artistSelector = "img ~ div > div:nth-child(2)";
-Connector.playerSelector = "img ~ div";
-
-const filter = MetadataFilter.createFilter({
-	artist: (text) => text.replace("by ", ""),
-});
-
-Connector.applyFilter(filter);
+Connector.playerSelector = '#player';
