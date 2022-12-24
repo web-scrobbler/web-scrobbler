af341a99b1ca67e6b2cae26be2bb0efdfd7c13ab
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2022-12-23 19:26:02 -0500

    remove jquery

--- src/connectors/piped.js
+++ src/connectors/piped.js
@@ -2,22 +2,25 @@
 
 Connector.playerSelector = '#app > div';
 
-Connector.artistTrackSelector = 'div.w-full > div:nth-child(1) > div.font-bold.mt-2.text-2xl.break-words';
+Connector.artistTrackSelector =
+	'div.w-full > div:nth-child(1) > div.font-bold.mt-2.text-2xl.break-words';
 
 Connector.getArtistTrack = () => {
-	const text = $(Connector.artistTrackSelector).text();
+	const text = Util.getTextFromSelectors(Connector.artistTrackSelector);
 	return Util.processYtVideoTitle(text);
 };
 
-Connector.getCurrentTime = () => $('.shaka-video').prop('currentTime');
+Connector.getCurrentTime = () =>
+	document.querySelector('.shaka-video').currentTime;
 
-Connector.getDuration = () => $('.shaka-video').prop('duration');
+Connector.getDuration = () => document.querySelector('.shaka-video').duration;
 
 Connector.getUniqueID = () => {
-	const videoUrl = $('[aria-current=page]').attr('href');
+	const videoUrl = Util.getAttrFromSelectors('[aria-current=page]', 'href');
 	return Util.getYtVideoIdFromUrl(videoUrl);
 };
 
-Connector.isPlaying = () => $('.shaka-small-play-button').text() === 'pause';
+Connector.isPlaying = () =>
+	document.querySelector('.shaka-small-play-button').innerText === 'pause';
 
 Connector.applyFilter(MetadataFilter.getYoutubeFilter());
