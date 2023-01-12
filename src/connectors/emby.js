438526eb33dad3312d2111ac465262045572a8fc
4piu <16668056+4piu@users.noreply.github.com>
2023-01-12 11:46:31 -0800

    Update emby.js (#3503)
    * Fix selector for new Emby
    
    * Add album selector
    
    * Fix album selector for Emby v4.7.10
    
    * Fix isPlaying for emby v4.7.10
    
    * Tidy up file
    
    * Fix track title selector
    
    * Remove album selector
    
    * Remove comment and add trailing comma
    
    Co-authored-by: 4piu <16668056@users.noreply.github.com>

--- src/connectors/emby.js
+++ src/connectors/emby.js
@@ -1,37 +1,49 @@
 'use strict';
 
-const trackArtSelector = '.nowPlayingBarInfoContainer .nowPlayingImage';
+Connector.trackArtSelector = [
+	// Emby v4.6.7
+	'.nowPlayingBarInfoContainer .nowPlayingBarImage',
+	'.nowPlayingBarInfoContainer .nowPlayingImage',
+];
 
 Connector.playerSelector = '.nowPlayingBar';
 
-Connector.artistSelector = '.nowPlayingBarText .nowPlayingBarSecondaryText';
+Connector.artistSelector = [
+	// Emby v4.6.7
+	'.nowPlayingBarText .secondaryText',
+	'.nowPlayingBarText .nowPlayingBarSecondaryText',
+];
 
 Connector.trackSelector = [
 	// Jellyfin v10.6.0 and newer
-	'.nowPlayingBarText div:nth-child(1) a',
+	'.nowPlayingBarText div:nth-child(1)',
 	// Emby or Jellyfin v10.5.0 and older
 	'.nowPlayingBarText .textActionButton',
 ];
 
-Connector.trackArtSelector = trackArtSelector;
-
 Connector.timeInfoSelector = '.nowPlayingBarCenter .nowPlayingBarCurrentTime';
 
 Connector.isPlaying = () => {
+	// Emby or Jellyfin v10.5.0 and older
 	const playButtonLabel = Util.getTextFromSelectors(
 		'.nowPlayingBarCenter .playPauseButton i'
 	);
-	// Emby or Jellyfin v10.5.0 and older
 	if (playButtonLabel) {
 		return playButtonLabel === 'pause';
 	}
-
+	// Emby v4.7.10
+	const playButtonText = Util.getTextFromSelectors(
+		'.nowPlayingBarCenter .playPauseButton'
+	);
+	if (playButtonText) {
+		return playButtonText === '\uE034'; // pause button icon
+	}
 	// Jellyfin v10.6.0 and newer
 	return document.querySelector('.playPauseButton .play_arrow') === null;
 };
 
 Connector.getTrackArt = () => {
-	const trackArtUrl = Util.extractImageUrlFromSelectors(trackArtSelector);
+	const trackArtUrl = Util.extractImageUrlFromSelectors(Connector.trackArtSelector);
 	if (trackArtUrl) {
 		return trackArtUrl.replace(/height=\d+/, '');
 	}
@@ -40,7 +52,7 @@ Connector.getTrackArt = () => {
 };
 
 Connector.getUniqueID = () => {
-	const trackArtUrl = Util.extractImageUrlFromSelectors(trackArtSelector);
+	const trackArtUrl = Util.extractImageUrlFromSelectors(Connector.trackArtSelector);
 	if (trackArtUrl) {
 		return /Items\/(\w+)/g.exec(trackArtUrl)[1];
 	}
