22289bcbdce91469729c8db57b52f85045d4b225
Jonathan Gongye <mail@yefoenix.ws>
2023-01-14 05:09:13 +0800

    Enhance Synology Support (#3539)
    * Enhance Synology Support
    
    Add support for some Synology Audiostation webpage without 5000 port. 
    Need the user open a new tab/window for Synology Audiostation.
    
    * fix lint
    
    * Update connectors.js
    
    * Update connectors.js
    
    Co-authored-by: Malachi Soord <inverse.chi@gmail.com>

--- src/core/connectors.js
+++ src/core/connectors.js
@@ -1861,6 +1861,7 @@ const connectors = [{
 	label: 'Synology',
 	matches: [
 		'*://*5000/*',
+		'*://*/?launchApp=SYNO.SDS.AudioStation.Application*',
 	],
 	js: 'connectors/synology.js',
 	id: 'synology',
