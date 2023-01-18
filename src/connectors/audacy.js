85bece1ede409eefa6098929fc617bcc55863fd8
hornbuckle <hornbuckle@gmail.com>
2023-01-18 13:41:52 -0800

    Update audacy.js (#3550)

--- src/connectors/audacy.js
+++ src/connectors/audacy.js
@@ -1,7 +1,37 @@
 'use strict';
 
-Connector.playerSelector = '#radioPlayer';
-Connector.artistSelector = '#showDescription';
-Connector.trackSelector = '#showName';
-Connector.trackArtSelector = '#poster';
-Connector.pauseButtonSelector = '#playButton.stop';
+const audioPlayer = 'div[aria-label="Audio player"]';
+const buttonFullscreen = 'button[aria-label="Open full-screen player"]';
+
+Connector.playerSelector = '#root'; // player not in DOM until initiated by user
+
+Connector.trackSelector = `${audioPlayer} ${buttonFullscreen} span:first-of-type`;
+
+Connector.artistSelector = `${audioPlayer} ${buttonFullscreen} span:nth-of-type(2)`;
+
+Connector.getArtist = () => {
+	const artistStationText = Util.getTextFromSelectors(Connector.artistSelector);
+
+	if (artistStationText === null) {
+		return null;
+	}
+
+	return artistStationText.split(' • ')[0];
+};
+
+Connector.trackArtSelector = `${audioPlayer} ${buttonFullscreen} img`;
+
+Connector.isTrackArtDefault = (url) => url.includes('base64');
+
+Connector.isPlaying = () => {
+	const buttonPlaying = Util.getTextFromSelectors(`${audioPlayer} > div:first-of-type > div:nth-of-type(2) > button:not([aria-label=Like]) svg title`);
+	return buttonPlaying === 'Stop' || buttonPlaying === 'Pause';
+};
+
+Connector.isScrobblingAllowed = () => {
+	return (
+		Connector.getArtist() && !Connector.getArtist().includes('Audacy') &&
+		Connector.getTrack() && !Connector.getTrack().includes('Advertisement') &&
+		Connector.getArtist() !== Connector.getTrack()
+	);
+};
