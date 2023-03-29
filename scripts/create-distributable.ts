2a3fbb18cc245b5333e260e72ad55fc9ee8f39a5
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-03-30 07:12:58 +0800

    Use glob for top level files

--- scripts/create-distributable.ts
+++ scripts/create-distributable.ts
@@ -19,25 +19,13 @@ async function createSrcArchive() {
 
 	archive.pipe(output);
 
-	// TODO: make a glob that gets the top level files and doesnt nuke the system
 	archive
 		.directory('src', 'src')
 		.directory('scripts', 'scripts')
 		.directory('tests', 'tests')
-		.file('.eslintrc.yml', { name: '.eslintrc.yml' })
-		.file('.eslintignore', { name: '.eslintignore' })
-		.file('build.ts', { name: 'build.ts' })
-		.file('manifest.config.ts', { name: 'manifest.config.ts' })
-		.file('vite.configs.ts', { name: 'vite.configs.ts' })
-		.file('vitest.config.ts', { name: 'vitest.config.ts' })
-		.file('package.json', { name: 'package.json' })
-		.file('package-lock.json', { name: 'package-lock.json' })
-		.file('tsconfig.json', { name: 'tsconfig.json' })
-		.file('tsconfig.connectors.json', { name: 'tsconfig.connectors.json' })
-		.file('safari.sh', { name: 'safari.sh' })
-		.file('.prettierignore', { name: '.prettierignore' })
-		.file('LICENSE.md', { name: 'LICENSE.md' })
-		.file('README.md', { name: 'README.md' });
+		.glob('*', {
+			ignore: ['web-scrobbler-*.zip', '*/'],
+		});
 
 	archive.finalize();
 }
