af4998f2fba10b57f1de42abcdbcf8d26b09b515
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-04-01 01:51:46 +0800

    Approach production-usable safari build

--- safari.sh
+++ safari.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+rm -rf build/safarixcode
+mkdir build/safarixcode
 rm -rf build/safari
 mkdir build/safari
-cd build/safari
+cd build/safarixcode
 xcrun safari-web-extension-converter ../safariraw --no-open
-xcodebuild -workspace "./Web Scrobbler/Web Scrobbler.xcodeproj/project.xcworkspace" -scheme "Web Scrobbler (macOS)" build
+xcodebuild -workspace "./Web Scrobbler/Web Scrobbler.xcodeproj/project.xcworkspace" -scheme "Web Scrobbler (macOS)" -configuration Release CONFIGURATION_BUILD_DIR="../../safari" build
