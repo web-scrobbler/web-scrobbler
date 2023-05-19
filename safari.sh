4bf6eb80c9ca55db16895f7baa15dbff4cc49d93
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-05-19 15:48:12 +0800

    Don't sign

--- safari.sh
+++ safari.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 rm -rf build/safari
 mkdir build/safari
 cd .xcode
-xcodebuild -workspace "./Web Scrobbler/Web Scrobbler.xcodeproj/project.xcworkspace" -scheme "Web Scrobbler (macOS)" -configuration Release CONFIGURATION_BUILD_DIR="../../build/safari" build
+xcodebuild -workspace "./Web Scrobbler/Web Scrobbler.xcodeproj/project.xcworkspace" -scheme "Web Scrobbler (macOS)" -configuration Release CONFIGURATION_BUILD_DIR="../../build/safari" build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
