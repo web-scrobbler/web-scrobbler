bb0442848ef1214b91de0aa22f0941ec367831ad
jbwharris <james@slideaway.ca>
2023-06-08 09:48:17 -0400

    Fix RadioParadise connector
    Cleaned up the targeted IDs and added a filter so it wouldn't scrobble the Listener Supported message

--- src/connectors/radioparadise.ts
+++ src/connectors/radioparadise.ts
@@ -2,14 +2,18 @@ export {};
 
 Connector.playerSelector = '.content-wrapper';
 
-Connector.albumSelector =
-	'.now_playing_list.ng-star-inserted:not(.dim) .title .album';
-
-Connector.getArtistTrack = () => {
-	const text = document.title;
-	return Util.splitArtistTrack(text);
-};
+Connector.albumSelector = '#now_playing .album';
+Connector.artistSelector = '#now_playing .artist';
+Connector.trackSelector = '#now_playing .title';
 
 Connector.isPlaying = () => Util.hasElementClass('#play-button', 'active');
 
-Connector.trackArtSelector = '.now_playing_list.ng-star-inserted:not(.dim) img';
+Connector.trackArtSelector = '#now_playing img.album-cover';
+
+Connector.isScrobblingAllowed = () => {
+	return (
+		!Util.getTextFromSelectors('#now_playing .title')?.includes(
+			'Listener-supported'
+		) && !Connector.getArtist()?.startsWith('Commercial-Free')
+	);
+};
