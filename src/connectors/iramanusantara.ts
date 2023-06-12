7475cf80d515439b6ffd58a1149140596cecb7d7
Faris Haidar Zuhdi <faris.haidar@ui.ac.id>
2023-06-12 22:55:14 +0700

    Fix bug for Irama Nusantara connector

--- src/connectors/iramanusantara.ts
+++ src/connectors/iramanusantara.ts
@@ -9,6 +9,4 @@ Connector.trackSelector = '#player-music > div > span:nth-child(1)';
 // Album title is only available when track is played through the album page
 Connector.albumSelector = 'h1:first-of-type';
 
-Connector.playButtonSelector = 'li > div > div:nth-child(2) > button:nth-child(2)';
-
 Connector.trackArtSelector = '#player-music > span > img';
