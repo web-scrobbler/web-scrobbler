5ab8513192f46dd1f4516b48a1c410649965fe08
Fiki Maulana <fikimaul@gmail.com>
2023-08-30 12:06:18 +0700

    fix spotify not send metadata to mediasession

--- src/connectors/stationhead.ts
+++ src/connectors/stationhead.ts
@@ -1,4 +1,10 @@
 export {};
 
+const playerSelector = 
+	'#root>div>div:nth-child(2)>div>div>div>div>div:nth-child(2)>div:nth-child(2)>div';
+
 Connector.useMediaSessionApi();
-Connector.playerSelector = '#root>div>div:nth-child(2)>div>div>div>div>div:nth-child(2)>div:nth-child(2)>div';
+
+Connector.playerSelector = playerSelector;
+Connector.trackSelector = `${playerSelector} p`;
+Connector.artistSelector = `${playerSelector}>div>div>div p`;
