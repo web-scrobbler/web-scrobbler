83336ce469e5e27f3e01b00a293ca81c6a8fa7ab
Pat McKenna <pat@mckenna.rocks>
2023-09-05 20:18:24 +1000

    Ensure debug log mesages are printed in order
    re: https://github.com/web-scrobbler/web-scrobbler/pull/4067#discussion_r1310393413
    
    Introducing an asynchronous check for user preferences within
    `debugLog()` in a previous commit meant that the print order of log
    messages was no longer guaranteed to be the same as the order in which
    each call to `debugLog()` was made. Implementing a queue for log
    messages ensures that they will be printed in the correct order.

--- src/core/content/util.ts
+++ src/core/content/util.ts
@@ -746,27 +746,69 @@ export function injectScriptIntoDocument(scriptUrl: string): void {
 }
 
 /**
+ * Handle async checks for DEBUG_LOGGING_ENABLED option while ensuring
+ * that logs are still printed in a predictable order.
+ */
+class DebugLogQueue {
+  private queue: {text: unknown, logType: DebugLogType}[] = [];
+  private isActive = false;
+
+  /**
+   * Enqueue a log message to be printed.
+   * @param text - Debug message
+   * @param logType - Log type
+   */
+  public push(text: unknown, logType: DebugLogType): void {
+    this.queue.push({text, logType});
+    this.start();
+  }
+
+  /**
+   * Process the queue to print logs in order.
+   */
+  private async start(): Promise<void> {
+    if (this.isActive) return;
+    this.isActive = true;
+
+    try {
+      for (let i = 0; i < 100 && this.queue.length > 0; i++) {
+        const currentMessage = this.queue.shift();
+        if (currentMessage && await this.shouldPrint()) {
+          const logFunc = console[currentMessage.logType];
+
+          if (typeof logFunc !== 'function') {
+            throw new TypeError(`Unknown log type: ${currentMessage.logType}`);
+          }
+
+          const message = `Web Scrobbler: ${currentMessage.text}`;
+          logFunc(message);
+        }
+      }
+      this.isActive = false;
+    } catch(err) {
+      this.isActive = false;
+    }
+  }
+
+  /**
+   * Check if the user has enabled debug logging in the extension options.
+   */
+  private async shouldPrint(): Promise<boolean> {
+    const awaitedOptions = await Options;
+    const debugLoggingEnabled = await awaitedOptions.getOption(awaitedOptions.DEBUG_LOGGING_ENABLED);
+    return !!debugLoggingEnabled;
+  }
+}
+const debugLogQueue = new DebugLogQueue();
+
+/**
  * Print debug message with prefixed "Web Scrobbler" string.
  * @param text - Debug message
  * @param logType - Log type
  */
 /* istanbul ignore next */
-export async function debugLog(text: unknown, logType: DebugLogType = 'log'): Promise<void> {
-	const awaitedOptions = await Options;
-	const loggingEnabled = await awaitedOptions.getOption(awaitedOptions.DEBUG_LOGGING_ENABLED);
-
-	if (!loggingEnabled) {
-		return;
-	}
-
-	const logFunc = console[logType];
-
-	if (typeof logFunc !== 'function') {
-		throw new TypeError(`Unknown log type: ${logType}`);
-	}
-
-	const message = `Web Scrobbler: ${text}`;
-	logFunc(message);
+export function debugLog(text: unknown, logType: DebugLogType = 'log'): void {
+  debugLogQueue.push(text, logType)
 }
 
 /** YouTube section. */
