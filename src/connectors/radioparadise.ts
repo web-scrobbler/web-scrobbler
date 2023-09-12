2c352445fbbddbb208370400085b3c2e52e919a1
jbwharris <james@slideaway.ca>
2023-09-12 09:47:15 -0400

    Prettier consistency changes

--- src/connectors/radioparadise.ts
+++ src/connectors/radioparadise.ts
@@ -1,24 +1,24 @@
 export {};
 
-Connector.playerSelector = "#conductor-wrapper";
+Connector.playerSelector = '#conductor-wrapper';
 
-Connector.albumSelector = "#now_playing .album";
-Connector.artistSelector = ".player-bar .player-artist";
-Connector.trackSelector = ".player-bar .player-title";
+Connector.albumSelector = '#now_playing .album';
+Connector.artistSelector = '.player-bar .player-artist';
+Connector.trackSelector = '.player-bar .player-title';
 
 Connector.isPlaying = () => {
 	return (
-		Util.getAttrFromSelectors("#global-player mat-icon", "title") ===
-		"Pause"
+		Util.getAttrFromSelectors('#global-player mat-icon', 'title') ===
+		'Pause'
 	);
 };
 
-Connector.trackArtSelector = ".player-bar img.player-cover";
+Connector.trackArtSelector = '.player-bar img.player-cover';
 
 Connector.isScrobblingAllowed = () => {
 	return (
-		!Util.getTextFromSelectors(".player-bar .player-title")?.includes(
-			"Listener-supported",
-		) && !Connector.getArtist()?.startsWith("Commercial-Free")
+		!Util.getTextFromSelectors('.player-bar .player-title')?.includes(
+			'Listener-supported',
+		) && !Connector.getArtist()?.startsWith('Commercial-Free')
 	);
 };
