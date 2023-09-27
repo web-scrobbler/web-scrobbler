1945423b5317fbccb65e855f29eb894faf4ea810
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-09-27 17:54:32 +0100

    Fix separator

--- src/connectors/tomorrowland.ts
+++ src/connectors/tomorrowland.ts
@@ -2,7 +2,11 @@ export {};
 
 Connector.playerSelector = '.sidebar-player';
 Connector.getArtistTrack = () =>
-	Util.splitArtistTrack(Util.getTextFromSelectors('.meta-title'), null, true);
+	Util.splitArtistTrack(
+		Util.getTextFromSelectors('.meta-title'),
+		[' ·'],
+		true,
+	);
 
 // might be flaky
 Connector.pauseButtonSelector = '#Group-5';
