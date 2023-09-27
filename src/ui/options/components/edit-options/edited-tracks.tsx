9c3cfbcd616226b91411ecbd18df82db1d96881c
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-09-27 01:07:28 +0100

    Fix warnings

--- src/ui/options/components/edit-options/edited-tracks.tsx
+++ src/ui/options/components/edit-options/edited-tracks.tsx
@@ -81,11 +81,12 @@ function TrackInfo(props: {
 				class={styles.deleteEditButton}
 				onClick={(event) => {
 					event.stopPropagation();
+					const key = props.key;
 					props.mutate((e) => {
 						if (!e) {
 							return e;
 						}
-						delete e[props.key];
+						delete e[key];
 						localCache.set(e);
 						return {
 							...e,
