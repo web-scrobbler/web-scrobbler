cdd5a5c0c9aabf89a470b7d2c0362b3c037fd352
James Harris <james@slideaway.ca>
2023-09-28 15:33:40 -0400

    Fix QCIndie Connector (#4156)

--- src/connectors/qcindie.ts
+++ src/connectors/qcindie.ts
@@ -1,6 +1,10 @@
 export {};
 
-Connector.playerSelector = '.p3-wrapper';
-Connector.artistSelector = '.p3-artistInfo';
-Connector.trackSelector = '.p3-title';
-Connector.isPlaying = () => Util.hasElementClass('#ppBtn', 'playing');
+// artist and track info are intentionally inverted, as they're backwards on the site
+
+Connector.playerSelector = '.site-content';
+Connector.artistSelector = '#ReloadThis .col-sm-6:first-of-type .trackinfo';
+Connector.trackSelector = '#ReloadThis .col-sm-6:first-of-type .artistinfo';
+Connector.trackArtSelector =
+	'#ReloadThis .col-sm-6:first-of-type .imagediv img';
+Connector.playButtonSelector = '.play-pause-button .fa-play';
