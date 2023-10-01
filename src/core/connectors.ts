b043c940291a1b2a42e8dfe64fdc51f96ad2627f
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-10-01 21:18:37 +0100

    Run prettier fix

--- src/core/connectors.ts
+++ src/core/connectors.ts
@@ -2172,8 +2172,8 @@ export default <ConnectorMeta[]>[
 		matches: ['*://blend.io/*'],
 		js: 'blend.js',
 		id: 'blend',
-  },
-  {
+	},
+	{
 		label: 'Radio Italia',
 		matches: ['*://www.radioitalia.it/*'],
 		js: 'radio-italia.js',
@@ -2214,11 +2214,11 @@ export default <ConnectorMeta[]>[
 		matches: ['*://play.rtl.it/live/*'],
 		js: 'radiofreccia.js',
 		id: 'radiofreccia',
-  },
-  {
+	},
+	{
 		label: 'khinsider',
 		matches: ['*://downloads.khinsider.com/*'],
 		js: 'khinsider.js',
 		id: 'khinsider',
-  },
+	},
 ];
