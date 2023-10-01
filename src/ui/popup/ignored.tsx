5009ea28b18e283dd57c7c0b75808780747d244f
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-10-01 22:37:09 +0200

    Overhaul popup (#4145)

---
+++ src/ui/popup/ignored.tsx
@@ -0,0 +1,34 @@
+import { ManagerTab } from '@/core/storage/wrapper';
+import { Resource, createMemo } from 'solid-js';
+import styles from './popup.module.scss';
+import { t } from '@/util/i18n';
+import { PopupAnchor } from '../components/util';
+import CancelScheduleSend from '@suid/icons-material/CancelScheduleSendOutlined';
+import ClonedSong from '@/core/object/cloned-song';
+
+export default function Ignored(props: { tab: Resource<ManagerTab> }) {
+	const clonedSong = createMemo(() => {
+		const cloneable = props.tab()?.song;
+		if (cloneable) {
+			return new ClonedSong(cloneable, -1);
+		}
+	});
+	return (
+		<div class={styles.alertPopup}>
+			<CancelScheduleSend class={styles.bigIcon} />
+			<h1>{t('ignoredHeader')}</h1>
+			<p>
+				{t('ignoredDesc', [
+					clonedSong()?.getArtist() ?? '????',
+					clonedSong()?.getTrack() ?? '????',
+				])}
+			</p>
+			<PopupAnchor
+				href="https://support.last.fm/t/track-filter-list-unknown-scrobbles/776"
+				title={t('ignoredDescReadMore')}
+			>
+				{t('ignoredDescReadMore')}
+			</PopupAnchor>
+		</div>
+	);
+}
