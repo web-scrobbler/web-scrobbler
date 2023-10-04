ee65278dbcee8dfdb87bcc5be60f4bb2500b9ffb
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-10-04 19:25:15 +0100

    Add interface to edit blocklist

---
+++ src/ui/options/components/edit-options/blocked-channels.tsx
@@ -0,0 +1,127 @@
+import { t } from '@/util/i18n';
+import {
+	For,
+	Setter,
+	createEffect,
+	createResource,
+	createSignal,
+} from 'solid-js';
+import * as BrowserStorage from '@/core/storage/browser-storage';
+import styles from '../components.module.scss';
+import Delete from '@suid/icons-material/DeleteOutlined';
+import { ExportBlocklist, ImportBlocklist, ViewBlocklist } from './util';
+import { ModalType } from '../navigator';
+import { ConnectorMeta } from '@/core/connectors';
+import { Blocklists } from '@/core/storage/wrapper';
+
+const blocklistStorage = BrowserStorage.getStorage(BrowserStorage.BLOCKLISTS);
+const [blocklists, { mutate }] = createResource(
+	blocklistStorage.get.bind(blocklistStorage),
+);
+const [connector, setConnector] = createSignal<ConnectorMeta>();
+
+/**
+ * Component that allows the user to see, import, and export connector blocklists.
+ */
+export default function BlockedChannels(props: {
+	setActiveModal: Setter<ModalType>;
+	modal: HTMLDialogElement | undefined;
+	connector: ConnectorMeta;
+}) {
+	createEffect(() => {
+		setConnector(props.connector);
+	});
+
+	return (
+		<>
+			<h3>{t('optionsBlockedSources')}</h3>
+			<p>{t('optionsBlockedSourcesDesc')}</p>
+			<div class={styles.buttonContainer}>
+				<ViewBlocklist
+					setActiveModal={props.setActiveModal}
+					modal={props.modal}
+					type={'blocklist'}
+				/>
+				<ExportBlocklist
+					blocklistWrapper={blocklistStorage}
+					connector={props.connector}
+					filename={`${props.connector.id}-blocklist.json`}
+				/>
+				<ImportBlocklist
+					blocklistWrapper={blocklistStorage}
+					connector={props.connector}
+					mutate={mutate}
+				/>
+			</div>
+		</>
+	);
+}
+
+/**
+ * Component that shows all the currently registered blocked channels and allows the user to unblock them.
+ * To be displayed in a modal.
+ */
+export function BlocklistModal() {
+	return (
+		<>
+			<h1>
+				{t(
+					'optionsBlockedSourcesPopupTitle',
+					Object.keys(
+						blocklists()?.[connector()?.id ?? ''] ?? {},
+					).length.toString(),
+				)}
+			</h1>
+			<ul class={styles.optionList}>
+				<For
+					each={Object.entries(
+						blocklists()?.[connector()?.id ?? ''] ?? {},
+					)}
+				>
+					{([channel]) => (
+						<ChannelInfo
+							channel={channel}
+							connector={connector()}
+							mutate={mutate}
+						/>
+					)}
+				</For>
+			</ul>
+		</>
+	);
+}
+
+/**
+ * Component that shows a blocked channel and allows the user to delete it.
+ */
+function ChannelInfo(props: {
+	channel: string;
+	connector: ConnectorMeta | undefined;
+	mutate: Setter<Blocklists | null | undefined>;
+}) {
+	return (
+		<li class={styles.deleteListing}>
+			<button
+				class={styles.deleteEditButton}
+				onClick={(event) => {
+					event.stopPropagation();
+					const channel = props.channel;
+					const connector = props.connector;
+					props.mutate((e) => {
+						if (!e || !connector || !e[connector.id]) {
+							return e;
+						}
+						delete e[connector.id][channel];
+						blocklistStorage.set(e);
+						return {
+							...e,
+						};
+					});
+				}}
+			>
+				<Delete />
+			</button>
+			<span>{props.channel}</span>
+		</li>
+	);
+}
