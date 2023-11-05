d9835863373297dc95cc1a72afc8729a9c0f7952
Reuben <reuben_p@yahoo.com>
2023-11-05 17:45:47 +1000

    add getOriginUrl to retrive url for track when on player page for audioaddict sites

--- src/connectors/radiotunes.ts
+++ src/connectors/radiotunes.ts
@@ -1,3 +1,4 @@
+
 export {};
 
 const playerBar = '#webplayer-region';
@@ -12,9 +13,11 @@ const filter = MetadataFilter.createFilter({ artist: removeTrailingDash });
 
 Connector.playerSelector = playerBar;
 
-Connector.artistSelector = '.artist-name';
+const artistSelector = '.artist-name';
+Connector.artistSelector = artistSelector;
 
-Connector.trackSelector = '.track-name';
+const trackSelector = '.track-name';
+Connector.trackSelector = trackSelector;
 
 Connector.playButtonSelector = '.icon-play';
 
@@ -43,3 +46,23 @@ Connector.applyFilter(filter);
 function removeTrailingDash(text: string) {
 	return text.replace(/\s-\s$/, '');
 }
+
+Connector.getOriginUrl = () => {
+	var ta = Util.getTextFromSelectors(artistSelector);
+	var tt = Util.getTextFromSelectors(trackSelector);
+	const pta = Util.getTextFromSelectors('.now-playing-component__artist');
+	const ptt = Util.getTextFromSelectors('.now-playing-component__title');
+	if (ta != null) {
+		ta = ta.trim();
+		if (ta.endsWith(' -')) {
+			ta = ta.substring(0, ta.length-2);
+		}
+	}
+	// 	compare with player to make sure we are still on the page where the track originates
+	if (ta == pta && tt == ptt){
+		const url = new URL(document.location.href);
+		return url.origin + Util.getAttrFromSelectors('.now-playing-component__title', 'href');
+	};
+	return document.location.href;
+	// + ta + tt + (ta == pta && tt == ptt) + pta + ptt;
+};
