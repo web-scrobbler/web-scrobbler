ad05e7702a2b8ade32fc5986a1d9c7ea535f36b3
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2023-11-08 22:57:47 +0300

    Add an artist filter for Rekt Network
    - Extract original artist if it is Mixtape

--- src/connectors/rekt.network.ts
+++ src/connectors/rekt.network.ts
@@ -6,3 +6,14 @@ Connector.playerSelector = 'body';
 Connector.artistTrackSelector = '#npTitle';
 
 Connector.playButtonSelector = '#playerPlay';
+
+const filter = MetadataFilter.createFilter({
+	artist: onlyArtist,
+});
+
+Connector.applyFilter(filter);
+
+function onlyArtist(artist: string) {
+	const chunks = artist.split('\n');
+	return chunks[chunks.length - 1];
+}
