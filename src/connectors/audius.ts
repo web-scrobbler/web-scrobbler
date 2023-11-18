f787d36e72b2dbcdd527fe46cf5099bf52000567
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2023-11-18 22:56:03 +0300

    Add Audius connector

---
+++ src/connectors/audius.ts
@@ -0,0 +1,16 @@
+export {};
+
+Connector.playerSelector = '._playBarContentWrapper_1sp1c_10';
+
+Connector.playButtonSelector = '[aria-label="play track"]';
+
+Connector.artistSelector = '._artistName_1nv4o_48';
+
+Connector.trackSelector = '._trackTitle_1nv4o_47';
+
+Connector.durationSelector = '.Scrubber-module_timestampEnd__yPsor';
+
+Connector.currentTimeSelector = '.Scrubber-module_timestampStart__a43VN';
+
+Connector.trackArtSelector =
+	'[aria-label="View currently playing track"] div div:nth-child(2)';
