f0a21b0043f5f2e8b841b5355e8596fd6fb6272d
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2023-11-19 16:48:55 +0300

    Handle default artwork

--- src/connectors/q-dance.ts
+++ src/connectors/q-dance.ts
@@ -8,6 +8,8 @@ Connector.trackSelector = '.audioplayer-nowplaying__track';
 
 Connector.trackArtSelector = '.audioplayer-nowplaying__image img';
 
+Connector.isTrackArtDefault = (url) => url?.endsWith('0.jpg');
+
 Connector.isPlaying = () =>
 	Util.hasElementClass(
 		'.audioplayer-controls__icon',
