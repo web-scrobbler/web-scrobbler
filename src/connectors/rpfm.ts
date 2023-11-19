225398a839faa3fb7f04e327e5b98b87bdb90183
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2023-11-19 04:02:04 +0300

    Reduce complexity

--- src/connectors/rpfm.ts
+++ src/connectors/rpfm.ts
@@ -1,30 +1,42 @@
 export {};
 
+interface TrackInfo {
+	[key: string]: string | number | null | undefined;
+}
+
+interface Field {
+	[key: string]: [
+		string,
+		(
+			selectors: string | string[],
+		) => (string | null) | (number | undefined),
+	];
+}
+
+const fields: Field = {
+	track: ['.radio-current-track', Util.getTextFromSelectors],
+	artist: ['.radio-current-artist', Util.getTextFromSelectors],
+	duration: ['.track-time-duration', Util.getSecondsFromSelectors],
+	currentTime: ['.track-time-position', Util.getSecondsFromSelectors],
+	trackArt: ['.radio-current-cover', Util.extractImageUrlFromSelectors],
+};
+
 Connector.playerSelector = '.app-player';
 
-Connector.getArtist = () =>
-	Util.getTextFromSelectors(`${getPlayerSelector()} .radio-current-artist`);
-
-Connector.getTrack = () =>
-	Util.getTextFromSelectors(`${getPlayerSelector()} .radio-current-track`);
-
-Connector.getTrackArt = () =>
-	Util.extractImageUrlFromSelectors(
-		`${getPlayerSelector()} .radio-current-cover`,
-	);
-
-Connector.getDuration = () =>
-	Util.getSecondsFromSelectors(`${getPlayerSelector()} .track-time-duration`);
-
-Connector.getCurrentTime = () =>
-	Util.getSecondsFromSelectors(`${getPlayerSelector()} .track-time-position`);
+Connector.getTrackInfo = () => {
+	const data: TrackInfo = {};
+	const context = getCurrentContext();
+	for (const field in fields) {
+		const [selector, fn] = fields[field];
+		data[field] = fn(`${context} ${selector}`);
+	}
+	return data;
+};
 
 Connector.isPlaying = () =>
 	Util.getDataFromSelectors('.app-player', 'status') === 'play';
 
-Connector.isStateChangeAllowed = () => Connector.getArtist() !== null;
-
-function getPlayerSelector(): string {
+function getCurrentContext(): string {
 	const single = Util.hasElementClass(
 		Connector.playerSelector,
 		'single-player',
