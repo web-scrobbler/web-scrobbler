716bcc0aaff8acf0ce5b7b6720461045d023ad8f
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2023-11-20 14:03:07 +0300

    Add J1 Radio connector

---
+++ src/connectors/aiircom.ts
@@ -0,0 +1,53 @@
+export {};
+
+interface TrackInfo {
+	[key: string]: string | null | number | undefined;
+}
+
+interface Field {
+	[key: string]: [
+		string,
+		(
+			selectors: string | string[],
+		) => (string | null) | (number | undefined),
+	];
+}
+
+const fields: Field = {
+	artist: ['span', Util.getTextFromSelectors],
+	track: ['strong', Util.getTextFromSelectors],
+	trackArt: ['img', Util.extractImageUrlFromSelectors],
+};
+
+const PAUSE_ICONS = ['#icon-pause', '#icon-stop'];
+
+Connector.playerSelector = '#playbar';
+
+Connector.getTrackInfo = () => {
+	const context: string = getCurrentContext();
+	const trackInfo: TrackInfo = {};
+	for (const field in fields) {
+		const [selector, fn] = fields[field];
+		trackInfo[field] = fn(`${context} ${selector}`);
+	}
+	return trackInfo;
+};
+
+Connector.isPlaying = () => {
+	const href =
+		Util.getAttrFromSelectors(
+			'[data-player-target="playButtonIcon"]',
+			'href',
+		) ?? '';
+	return PAUSE_ICONS.includes(href);
+};
+
+Connector.isPodcast = () => Util.isElementVisible('[role="navigation"]');
+
+function getCurrentContext() {
+	const isVisible = Util.isElementVisible(
+		'[data-player-target="playBarSecondaryContainer"]',
+	);
+	const kind = isVisible ? 'secondary' : 'primary';
+	return `.c-playbar__${kind}`;
+}
