84bd6209a2e73cce6eda9ae8edd966d830340a4d
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2023-11-28 23:12:54 +0300

    Update RadioFrance connector (#4321)
    * Use local artwork
    
    * Use `currentTime` and `duration`
    
    * Add support for podcasts
    
    * Add a filter for artist
    
    * Add support for more players
    
    * Revert "Add a filter for artist"
    
    This reverts commit 881b34f1629d544f5f2aa69fbd0b79dd335d10bf.
    
    * Rename `trackAsArtist` to `artistAsTrack`
    
    * Handle podcasts playing

--- src/connectors/radiofrance.ts
+++ src/connectors/radiofrance.ts
@@ -1,17 +1,36 @@
 export {};
 
-const PLAY_BUTTON = '[data-testid="mainButton"]';
-
-const BUTTON_STATE_TEXT = 'STOP';
+const SEPARATOR = ' \u2022 ';
 
 Connector.playerSelector = '#player';
 
-Connector.playButtonSelector = PLAY_BUTTON;
+Connector.isPlaying = () => {
+	const text = Util.getTextFromSelectors('[data-testid="mainButton"]') ?? '';
+	return ['STOP', 'PAUSE'].includes(text);
+};
 
-Connector.isPlaying = () =>
-	Util.getTextFromSelectors(PLAY_BUTTON) === BUTTON_STATE_TEXT;
+Connector.isPodcast = () => Util.isElementVisible('.Seekbar input');
 
 Connector.getArtistTrack = () => {
-	const track = Util.getTextFromSelectors('section > div.Line > p > span');
-	return Util.splitArtistTrack(track, [' \u2022 ']);
+	const artist = Util.getTextFromSelectors('.Metadata .firstline');
+	const track = Util.getTextFromSelectors('.Metadata .secondline');
+	if (!hasSeparator(artist) && !hasSeparator(track)) {
+		return { artist, track };
+	}
+	if (hasSeparator(track)) {
+		return Util.splitArtistTrack(track, [SEPARATOR]);
+	}
+	const [artistAsTrack] = Util.splitString(artist, [SEPARATOR], true);
+	return { artist: track, track: artistAsTrack };
 };
+
+Connector.getTrackArt = () =>
+	Util.getAttrFromSelectors('.Cover picture source', 'srcset');
+
+Connector.currentTimeSelector = '.time span:nth-child(1)';
+
+Connector.durationSelector = '.time span:nth-child(3)';
+
+function hasSeparator(text: string | null): boolean {
+	return Boolean(text?.includes(SEPARATOR));
+}
