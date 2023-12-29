403775ba5b1b90f3f621da850d9faafad702fa0f
Fiki Maulana <fikimaul@gmail.com>
2023-12-29 22:12:16 +0700

    Fix joox not scrobbling (#4364)
    * Fix joox not scrobbling
    
    * fix lint issue

--- src/connectors/joox.ts
+++ src/connectors/joox.ts
@@ -1,23 +1,9 @@
 export {};
 
-const playerSelector = '.kIpMXu';
-const artistSelector = `${playerSelector} .kOwKMw a`;
-
 Connector.useMediaSessionApi();
 
-Connector.playerSelector = playerSelector;
-
-Connector.trackSelector = '.xXMwE';
-
-Connector.trackArtSelector = `${playerSelector} .XVHIE img`;
-
-Connector.getArtist = () => {
-	const artistNodes = document.querySelectorAll(artistSelector);
-	return Util.joinArtists(Array.from(artistNodes));
-};
+Connector.playerSelector = '#__next > div:nth-child(2) > div:nth-child(2)';
 
 Connector.currentTimeSelector = '#currentTime';
-
-Connector.durationSelector = '.GeFxq';
 
 Connector.playButtonSelector = '.playerIcon--play';
