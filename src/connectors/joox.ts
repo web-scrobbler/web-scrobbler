c5f10d92fe42a5c3b36510d6a7ca63da77fbb780
Fiki Maulana <fikimaul@gmail.com>
2023-12-29 20:04:07 +0700

    fix lint issue

--- src/connectors/joox.ts
+++ src/connectors/joox.ts
@@ -2,7 +2,7 @@ export {};
 
 Connector.useMediaSessionApi();
 
-Connector.playerSelector =  '#__next > div:nth-child(2) > div:nth-child(2)';
+Connector.playerSelector = '#__next > div:nth-child(2) > div:nth-child(2)';
 
 Connector.currentTimeSelector = '#currentTime';
 
