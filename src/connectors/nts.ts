b03205c046e4496e42bd7a926bcf86bbb1a1242a
Harrison Wilde <harrisondwilde@outlook.com>
2024-01-31 17:36:18 +0000

    Update src/connectors/nts.ts
    Co-authored-by: yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>

--- src/connectors/nts.ts
+++ src/connectors/nts.ts
@@ -2,31 +2,9 @@ export {};
 
 Connector.playerSelector = '.page-live-tracks__body';
 
-Connector.getArtistTrack = () => {
-	let artist = null;
-	let track = null;
+Connector.artistSelector = '.page-live-tracks__list li:first-child .page-live-tracks__artist-title';
 
-	const trackinfo = document.querySelector(
-		'.page-live-tracks__list li:first-child',
-	);
-
-	if (trackinfo) {
-		const artistElement = trackinfo.querySelector(
-			'.page-live-tracks__artist-title',
-		);
-		const trackElement = trackinfo.querySelector(
-			'.page-live-tracks__song-title',
-		);
-		if (artistElement) {
-			artist = artistElement.textContent || '';
-		}
-		if (trackElement) {
-			track = trackElement.textContent || '';
-		}
-	}
-
-	return { artist, track };
-};
+Connector.trackSelector = '.page-live-tracks__list li:first-child .page-live-tracks__song-title';
 
 Connector.scrobbleInfoLocationSelector = '.page-live-tracks__title';
 Connector.scrobbleInfoStyle = {
