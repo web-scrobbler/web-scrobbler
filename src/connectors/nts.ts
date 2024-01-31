076dc587c80f5b89251cb447f003b99547f5bad6
HarrisonWilde <harrisondwilde@outlook.com>
2024-01-31 18:48:10 +0000

    Fix formatting with prettier

--- src/connectors/nts.ts
+++ src/connectors/nts.ts
@@ -2,9 +2,11 @@ export {};
 
 Connector.playerSelector = '.page-live-tracks__body';
 
-Connector.artistSelector = '.page-live-tracks__list li:first-child .page-live-tracks__artist-title';
+Connector.artistSelector =
+	'.page-live-tracks__list li:first-child .page-live-tracks__artist-title';
 
-Connector.trackSelector = '.page-live-tracks__list li:first-child .page-live-tracks__song-title';
+Connector.trackSelector =
+	'.page-live-tracks__list li:first-child .page-live-tracks__song-title';
 
 Connector.scrobbleInfoLocationSelector = '.page-live-tracks__title';
 Connector.scrobbleInfoStyle = {
