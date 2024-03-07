53e5908dbe0dff0fc129276a8521891fc7f5f244
Omar Saleem <ofsaleem@users.noreply.github.com>
2024-03-07 06:02:16 -0500

    First attempt at azuracast.ts

---
+++ src/connectors/azuracast.ts
@@ -0,0 +1,12 @@
+Connector.useMediaSessionsApi();
+
+Connector.playerSelector = '.radio-player-widget';
+
+Connector.artistSelector = '.now-playing-details .now-playing-main div.now-playing-artist';
+
+Connector.trackSelector = '.now-playing-title';
+
+Connector.trackArtSelector = '.now-playing-art .album-art .album-art;
+
+Connector.isPlaying = () => Util.getAttrFromSelectors('.radio-controls .btn', 'title') == 'Play';
+
