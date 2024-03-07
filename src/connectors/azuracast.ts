28402c9a475298f5980634dc6daef64a0330fd66
Omar Saleem <ofsaleem@users.noreply.github.com>
2024-03-07 06:03:14 -0500

    missed an end '

--- src/connectors/azuracast.ts
+++ src/connectors/azuracast.ts
@@ -6,7 +6,7 @@ Connector.artistSelector = '.now-playing-details .now-playing-main div.now-playi
 
 Connector.trackSelector = '.now-playing-title';
 
-Connector.trackArtSelector = '.now-playing-art .album-art .album-art;
+Connector.trackArtSelector = '.now-playing-art .album-art .album-art';
 
 Connector.isPlaying = () => Util.getAttrFromSelectors('.radio-controls .btn', 'title') == 'Play';
 
