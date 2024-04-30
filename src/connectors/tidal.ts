a3d91f47fe1a370ecfb1fc5d4680b4c68d98f58f
Jacob MacDonald <jaccarmac@gmail.com>
2024-04-30 18:14:20 -0500

    Fix the location of TIDAL's first-choice track selector. (#4675)
    This changed slightly again. The `-medium` sounds suspiciously
    viewport-dependent, but I cannot get the DOM to change by manipulating my
    window.

--- src/connectors/tidal.ts
+++ src/connectors/tidal.ts
@@ -10,7 +10,7 @@ Connector.scrobblingDisallowedReason = () =>
 	Util.queryElements(Connector.playButtonSelector) ? null : 'ElementMissing';
 
 Connector.trackSelector = [
-	'#nowPlaying div.react-tabs__tab-panel--selected > div > div:nth-child(1) > div:nth-child(1) > wave-text:nth-child(2)',
+	'#nowPlaying div.react-tabs__tab-panel--selected > div > div:nth-child(1) > div:nth-child(1) > .wave-text-description-medium',
 	`${Connector.playerSelector} div[data-test="footer-track-title"]`,
 ];
 
