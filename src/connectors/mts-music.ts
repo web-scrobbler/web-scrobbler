c1aaa67167ea9b5708d6ea234f0d6330c9c811f5
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-05-09 15:44:55 +0300

    run prettier on MTS music connector

--- src/connectors/mts-music.ts
+++ src/connectors/mts-music.ts
@@ -1,19 +1,20 @@
 export {};
 
 // Selects previous node to .Toastify
-Connector.playerSelector = "div:has(+ .Toastify)"
-Connector.artistSelector = "[data-testid=player-song-info] [data-testid=player-song-author]"
-Connector.trackSelector = "[data-testid=player-song-info] [itemprop=name]"
-Connector.trackArtSelector = "[data-testid=player-song-icon] img"
-Connector.currentTimeSelector = "[data-testid=player-current-time]"
-Connector.durationSelector = "[data-testid=player-full-time]"
+Connector.playerSelector = 'div:has(+ .Toastify)';
+Connector.artistSelector =
+	'[data-testid=player-song-info] [data-testid=player-song-author]';
+Connector.trackSelector = '[data-testid=player-song-info] [itemprop=name]';
+Connector.trackArtSelector = '[data-testid=player-song-icon] img';
+Connector.currentTimeSelector = '[data-testid=player-current-time]';
+Connector.durationSelector = '[data-testid=player-full-time]';
 
-const playButtonSelector = "[data-testid=player-play]"
+const playButtonSelector = '[data-testid=player-play]';
 
 Connector.isPlaying = () => {
-  let playButton = document.querySelector(playButtonSelector);
-  if (playButton === null) return false 
+	let playButton = document.querySelector(playButtonSelector);
+	if (playButton === null) return false;
 
-  let imageCSS = window.getComputedStyle(playButton).backgroundImage
-  return imageCSS.includes("pause")
-}
+	let imageCSS = window.getComputedStyle(playButton).backgroundImage;
+	return imageCSS.includes('pause');
+};
