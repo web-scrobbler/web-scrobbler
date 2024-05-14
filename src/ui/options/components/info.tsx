eadebe6c36de5f034ef1aca1a0a1fa20a77798dc
Malachi Soord <inverse.chi@gmail.com>
2024-05-14 20:58:41 +0200

    Add version to about (#4707)

--- src/ui/options/components/info.tsx
+++ src/ui/options/components/info.tsx
@@ -1,4 +1,5 @@
 import { currentChangelog, t } from '@/util/i18n';
+import { getExtensionVersion } from '@/util/util-browser';
 
 /**
  * Component that shows basic information about the extension
@@ -10,6 +11,9 @@ export default function InfoComponent() {
 			<p>{t('aboutExtensionDesc')}</p>
 			{/* eslint-disable-next-line */}
 			<p innerHTML={t('aboutChangelog', currentChangelog())}></p>
+			<h2>{t('versionTitle')}</h2>
+			{/* eslint-disable-next-line */}
+			<p innerHTML={t('versionText', getExtensionVersion())}></p>
 			<h2>{t('contributorsTitle')}</h2>
 			{/* eslint-disable-next-line */}
 			<p innerHTML={t('contributorsText')}></p>
