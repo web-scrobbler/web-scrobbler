3af236b159328683e908f9d4d55654c5f3e16ce8
jbwharris <james@slideaway.ca>
2024-07-19 10:14:43 -0400

    New Basspistol Radio connector

--- src/core/connectors.ts
+++ src/core/connectors.ts
@@ -2449,4 +2449,10 @@ export default <ConnectorMeta[]>[
 		js: 'chirpradio.js',
 		id: 'chirpradio',
 	},
+	{
+		label: 'Basspistol Radio',
+		matches: ['*://*.basspistol.com/*'],
+		js: 'basspistol.js',
+		id: 'basspistol',
+	},
 ];
