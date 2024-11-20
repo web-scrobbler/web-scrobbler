378078fc6b00d9f049581759340ff05374261cb0
Malachi Soord <inverse.chi@gmail.com>
2024-11-20 20:54:32 +0100

    Fix code lint

--- src/connectors/somafm.ts
+++ src/connectors/somafm.ts
@@ -9,9 +9,10 @@ Connector.trackSelector =
 	'tr.songHistoryLine:nth-child(1) > td:nth-child(2) > div:nth-child(1)';
 
 Connector.isPlaying = () => {
-
 	// making sure selector is not null
-	let playerSelector = document.querySelector('.playbackController__info') ?? document.createElement('div');
+	let playerSelector =
+		document.querySelector('.playbackController__info') ??
+		document.createElement('div');
 
 	// player is playing if it has channel name AND extra div for track info as children
 	return playerSelector.childElementCount === 2;
