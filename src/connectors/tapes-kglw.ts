086a4e2c7641790244f226f704582e6a17d2d60f
Malachi Soord <inverse.chi@gmail.com>
2024-12-25 22:51:28 +0100

    fix lint

--- src/connectors/tapes-kglw.ts
+++ src/connectors/tapes-kglw.ts
@@ -6,15 +6,13 @@ Connector.getArtist = () => {
 	return 'King Gizzard & The Lizard Wizard';
 };
 
-Connector.trackSelector =
-	'#now-playing > #now-playing-track-title';
+Connector.trackSelector = '#now-playing > #now-playing-track-title';
 
-Connector.albumSelector =
-	'#now-playing > #now-playing-show-date';
+Connector.albumSelector = '#now-playing > #now-playing-show-date';
 
 Connector.durationSelector = '#now-playing > .now-playing-duration';
 
-Connector.currentTimeSelector =	'#now-playing > .now-playing-elapsed';
+Connector.currentTimeSelector = '#now-playing > .now-playing-elapsed';
 
 Connector.isPlaying = () => {
 	return document
