4c9f1788d987fa7b3112db0a53e52fe1a12b1fd3
xemles <13338602+xemles@users.noreply.github.com>
2025-02-16 20:47:47 +0100

    Fixed onlyhit.ts

--- src/connectors/onlyhit.ts
+++ src/connectors/onlyhit.ts
@@ -1,20 +1,14 @@
 export {};
 
 Connector.playerSelector = '#audio-player';
-
 Connector.trackSelector = '#player-track-title';
 
-Connector.artistSelector = '#player-track-artist';
-
-Connector.playButtonSelector = '#play-button';
+Connector.getArtist = () => {
+    const artistText = document.querySelector('#player-track-artist')?.textContent ?? '';
+    return artistText.includes(' • ') ? artistText.split(' • ')[0] : artistText;
+};
 
 Connector.isPlaying = () => {
     const playButton = document.querySelector('#play-button');
     return playButton?.querySelector('path')?.getAttribute('d') === 'M6 6h12v12H6z';
 };
-
-Connector.artistTrackSeparator = '•';
-
-Connector.applyFilter(MetadataFilter.createFilter({
-    artist: [{ source: /\s*•.*$/, target: '' }],
-}));
