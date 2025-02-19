0499c8225a663a2a51b88546dc943f6c3952b330
Malachi Soord <inverse.chi@gmail.com>
2025-02-19 20:28:43 +0100

    fix lint

--- src/connectors/onlyhit.ts
+++ src/connectors/onlyhit.ts
@@ -4,11 +4,14 @@ Connector.playerSelector = '#audio-player';
 Connector.trackSelector = '#player-track-title';
 
 Connector.getArtist = () => {
-    const artistText = document.querySelector('#player-track-artist')?.textContent ?? '';
-    return artistText.includes(' • ') ? artistText.split(' • ')[0] : artistText;
+	const artistText =
+		document.querySelector('#player-track-artist')?.textContent ?? '';
+	return artistText.includes(' • ') ? artistText.split(' • ')[0] : artistText;
 };
 
 Connector.isPlaying = () => {
-    const playButton = document.querySelector('#play-button');
-    return playButton?.querySelector('path')?.getAttribute('d') === 'M6 6h12v12H6z';
+	const playButton = document.querySelector('#play-button');
+	return (
+		playButton?.querySelector('path')?.getAttribute('d') === 'M6 6h12v12H6z'
+	);
 };
