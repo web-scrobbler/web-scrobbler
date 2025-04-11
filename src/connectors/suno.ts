4ca57deebd1bbcd05972910d180679ad834b32f8
Tạ Quang Khôi <khoi0941@gmail.com>
2025-04-12 01:37:07 +0700

    Use singlequote

--- src/connectors/suno.ts
+++ src/connectors/suno.ts
@@ -2,14 +2,14 @@ export {};
 
 Connector.playerSelector = 'div:has(audio)';
 
-Connector.artistSelector = `a[aria-label^="Playbar: Artist"]`;
+Connector.artistSelector = 'a[aria-label^="Playbar: Artist"]';
 
-Connector.trackSelector = `a[aria-label^="Playbar: Title"]`;
+Connector.trackSelector = 'a[aria-label^="Playbar: Title"]';
 
 Connector.trackArtSelector = 'img[aria-label^="Playbar: Cover image"]';
 
-Connector.currentTimeSelector = `span[aria-label^="Playbar: Duration for"]`;
+Connector.currentTimeSelector = 'span[aria-label^="Playbar: Duration for"]';
 
-Connector.durationSelector = `span[aria-label^="Playbar: Duration for"]`;
+Connector.durationSelector = 'span[aria-label^="Playbar: Duration for"]';
 
-Connector.playButtonSelector = `button[aria-label^="Playbar: Play button"]`;
+Connector.playButtonSelector = 'button[aria-label^="Playbar: Play button"]';
