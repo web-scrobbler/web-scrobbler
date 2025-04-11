6daee86bf1c2263c114015b480d81db31f6b1b57
Tạ Quang Khôi <khoi0941@gmail.com>
2025-04-12 01:33:19 +0700

    Add suno.ts

---
+++ src/connectors/suno.ts
@@ -0,0 +1,15 @@
+export {};
+
+Connector.playerSelector = 'div:has(audio)';
+
+Connector.artistSelector = `a[aria-label^="Playbar: Artist"]`;
+
+Connector.trackSelector = `a[aria-label^="Playbar: Title"]`;
+
+Connector.trackArtSelector = 'img[aria-label^="Playbar: Cover image"]';
+
+Connector.currentTimeSelector = `span[aria-label^="Playbar: Duration for"]`;
+
+Connector.durationSelector = `span[aria-label^="Playbar: Duration for"]`;
+
+Connector.playButtonSelector = `button[aria-label^="Playbar: Play button"]`;
