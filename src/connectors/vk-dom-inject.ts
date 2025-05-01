f0240c13d3f0d477816ad0385668208964db8eac
Huterenok <clash.of.clans23916@gmail.com>
2025-05-01 19:53:34 +0300

    fix: VK connector album identification

--- src/connectors/vk-dom-inject.ts
+++ src/connectors/vk-dom-inject.ts
@@ -31,6 +31,14 @@ if ('cleanup' in window && typeof window.cleanup === 'function') {
 	interface Window {
 		ap: {
 			_currentAudio?: string[];
+			_currentPlaylist?: {
+				_ref: {
+					// Get album title
+					getTitle: () => string;
+					// Get type of album: "playlist" or "album"
+					getType: () => string;
+				};
+			};
 			_impl: { _currentAudioEl?: { currentTime: string } };
 			isPlaying: () => boolean;
 			subscribers: {
@@ -41,10 +49,29 @@ if ('cleanup' in window && typeof window.cleanup === 'function') {
 	}
 
 	function sendUpdateEvent(type: string) {
-		const audioObject = (window as unknown as Window).ap._currentAudio;
+		const ctx = (window as unknown as Window).ap;
+
+		const audioObject = ctx._currentAudio;
+
 		if (!audioObject) {
 			return;
 		}
+
+		const albumType = ctx._currentPlaylist?._ref.getType();
+		const title = ctx._currentPlaylist?._ref.getTitle();
+		const album = (() => {
+			switch (albumType) {
+				case 'album':
+					// Аlbums in VK usually contain the correct title
+					return title || '';
+				case 'playlist':
+					// But playlists can have different formats that may
+					// contain unnecessary information
+					return title ? cleanPlaylistName(title) : '';
+				default:
+					return '';
+			}
+		})();
 
 		const currentTime = (window as unknown as Window).ap._impl
 			._currentAudioEl?.currentTime;
@@ -74,6 +101,7 @@ if ('cleanup' in window && typeof window.cleanup === 'function') {
 					currentTime,
 					trackArt,
 					track,
+					album,
 					duration: audioObject[INFO_DURATION],
 					uniqueID: `${audioObject[INFO_OWNER_ID]}_${audioObject[INFO_ID]}`,
 					artist: audioObject[INFO_ARTIST],
@@ -117,3 +145,36 @@ if ('cleanup' in window && typeof window.cleanup === 'function') {
 		);
 	};
 })();
+
+/**
+ * Clean playlist name specific to VK
+ * Many playlists in VK have structure like:
+ *	1. "Artist - Album name"
+ *	2. "Artist - Album name: Disk Name"
+ *  3. "Artist - Album name Disk Number: Disk Name"
+ *
+ * @param playlist - Playlist name with possible unnecessary information
+ * @returns Cleaned playlist name
+ */
+function cleanPlaylistName(playlist: string): string {
+	// Clean unnecessary information in parentheses (for example, "(2021, Longinus Recordings)")
+	let cleaned = playlist.replace(/\(.*?\)/g, '');
+
+	// Remove Artist part
+	const artistSeparators = ['-', '–', '—'];
+	for (const sep of artistSeparators) {
+		if (cleaned.includes(sep)) {
+			cleaned = cleaned.split(sep)[1];
+			break;
+		}
+	}
+	// Remove {Disk Name} part
+	if (cleaned.includes(':')) {
+		cleaned = cleaned.split(':')[0];
+	}
+
+	// Remove Disk Number part: "Disc 20", "Disk 1", "CD 2", "Part 3"
+	cleaned = cleaned.replace(/\b(Disc|Disk|CD|Part)\s*\d+\b/gi, '');
+
+	return cleaned.trim();
+}
