88cd45994ab27dc37090c435a2371bba1d218afe
Malachi Soord <inverse.chi@gmail.com>
2025-08-17 00:53:04 +0200

    fix lint of connector

--- src/connectors/vas3kradio.ts
+++ src/connectors/vas3kradio.ts
@@ -5,10 +5,12 @@ function setup() {
 	Connector.playButtonSelector = 'button.radio-control-play-button';
 	Connector.trackSelector = '.now-playing-title';
 	Connector.artistSelector = '.now-playing-artist';
-	Connector.currentTimeSelector = '.time-display-played'
-	Connector.durationSelector = '.time-display-total'
-	Connector.isPlaying = () => 
-		document.getElementsByClassName("radio-control-play-button")[0].getAttribute("title") === 'Stop'
+	Connector.currentTimeSelector = '.time-display-played';
+	Connector.durationSelector = '.time-display-total';
+	Connector.isPlaying = () =>
+		document
+			.getElementsByClassName('radio-control-play-button')[0]
+			.getAttribute('title') === 'Stop';
 }
 
 setup();
