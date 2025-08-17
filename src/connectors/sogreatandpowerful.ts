e007e0ebb5df3d1b32d067e62a0e00ecc6f7e98a
VINXIS <oykxf2@gmail.com>
2025-08-17 09:23:56 -0600

    add getTimeInfo connector

--- src/connectors/sogreatandpowerful.ts
+++ src/connectors/sogreatandpowerful.ts
@@ -8,3 +8,17 @@ Util.bindListeners(
 	['playing', 'pause', 'timeupdate'],
 	Connector.onStateChanged,
 );
+
+Connector.getTimeInfo = () => {
+	const audioElems = Util.queryElements(['audio']);
+	if (!audioElems) {
+		return null;
+	}
+
+	const audioElement = audioElems[0];
+	if (!audioElement || !(audioElement instanceof HTMLAudioElement)) {
+		return null;
+	}
+	const { duration, currentTime } = audioElement;
+	return { duration, currentTime };
+};
