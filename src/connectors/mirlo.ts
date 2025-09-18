db61f888aa1600792990148e4875af2bac06f1be
Jacob MacDonald <jaccarmac@gmail.com>
2025-09-18 14:45:35 -0500

    interpolate, fixing whitespace

--- src/connectors/mirlo.ts
+++ src/connectors/mirlo.ts
@@ -1,9 +1,9 @@
 export {};
 
 Connector.playerSelector = '#player';
-Connector.trackSelector = '#player #player-track-title';
-Connector.artistSelector = '#player #player-artist-name';
-Connector.albumSelector = '#player #player-trackGroup-title';
-Connector.trackArtSelector = '#player img';
+Connector.trackSelector = `${Connector.playerSelector} #player-track-title`;
+Connector.artistSelector = `${Connector.playerSelector} #player-artist-name`;
+Connector.albumSelector = `${Connector.playerSelector} #player-trackGroup-title`;
+Connector.trackArtSelector = `${Connector.playerSelector} img`;
 Connector.isPlaying = () =>
-	Util.getAttrFromSelectors('#player .play-button', 'aria-label') !== 'Play';
+	Util.getAttrFromSelectors(`${Connector.playerSelector} .play-button`, 'aria-label') !== 'Play';
