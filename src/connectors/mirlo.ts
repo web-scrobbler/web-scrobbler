f1a995b7c3bc613a4293896383c9a205c31b5870
Jacob MacDonald <jaccarmac@gmail.com>
2025-09-18 14:52:59 -0500

    break out `Connector.playButtonSelector` to lint-satisfy

--- src/connectors/mirlo.ts
+++ src/connectors/mirlo.ts
@@ -5,5 +5,9 @@ Connector.trackSelector = `${Connector.playerSelector} #player-track-title`;
 Connector.artistSelector = `${Connector.playerSelector} #player-artist-name`;
 Connector.albumSelector = `${Connector.playerSelector} #player-trackGroup-title`;
 Connector.trackArtSelector = `${Connector.playerSelector} img`;
+Connector.playButtonSelector = `${Connector.playerSelector} .play-button`
 Connector.isPlaying = () =>
-	Util.getAttrFromSelectors(`${Connector.playerSelector} .play-button`, 'aria-label') !== 'Play';
+	Util.getAttrFromSelectors(
+		Connector.playButtonSelector,
+		'aria-label',
+	) !== 'Play';
