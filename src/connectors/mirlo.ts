ac6107713e5ee405c2f172e42c30f75470f6357a
Jacob MacDonald <jaccarmac@gmail.com>
2025-09-18 14:58:00 -0500

    prettierfix

--- src/connectors/mirlo.ts
+++ src/connectors/mirlo.ts
@@ -5,9 +5,7 @@ Connector.trackSelector = `${Connector.playerSelector} #player-track-title`;
 Connector.artistSelector = `${Connector.playerSelector} #player-artist-name`;
 Connector.albumSelector = `${Connector.playerSelector} #player-trackGroup-title`;
 Connector.trackArtSelector = `${Connector.playerSelector} img`;
-Connector.playButtonSelector = `${Connector.playerSelector} .play-button`
+Connector.playButtonSelector = `${Connector.playerSelector} .play-button`;
 Connector.isPlaying = () =>
-	Util.getAttrFromSelectors(
-		Connector.playButtonSelector,
-		'aria-label',
-	) !== 'Play';
+	Util.getAttrFromSelectors(Connector.playButtonSelector, 'aria-label') !==
+	'Play';
