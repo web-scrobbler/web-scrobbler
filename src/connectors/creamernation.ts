cdc493cf9c967e97e4e27a531ab016ac225ae549
Jacob MacDonald <jaccarmac@gmail.com>
2025-09-21 22:28:10 -0500

    clean up

--- src/connectors/creamernation.ts
+++ src/connectors/creamernation.ts
@@ -7,11 +7,7 @@ Connector.trackSelector = '#player-track-title';
 
 Connector.albumSelector = '#now-playing-bar #web-scrobbler-album';
 
-Connector.albumArtSelector = '#player-art-img';
+Connector.trackArtSelector = '#player-art-img';
 
 Connector.isPlaying = () =>
-	document.querySelector('#play-pause-btn')?.classList.contains('playing');
-
-Connector.id = 'creamer-nation';
-Connector.label = 'CREAMER NATION';
-Connector.match = 'creamer.nation';
+	Util.hasElementClass('#play-pause-btn', 'playing')
