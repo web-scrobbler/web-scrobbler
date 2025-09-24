f126cc087c9fe6e95d5519ec7752b94b76d93e6a
Ezra Ashenafi <eazash22@gmail.com>
2025-09-24 17:14:48 +0300

    Add support for WQXR radio

---
+++ src/connectors/wqxr.ts
@@ -0,0 +1,12 @@
+export {};
+
+Connector.playerSelector = '.is-current-sound'
+Connector.artistTrackSelector = '.text-crawl-scroll'
+
+Connector.isPlaying = () => Util.hasElementClass('.is-current-sound', 'is-playing')
+Connector.getArtistTrack = () => {
+	const artistTrack = Util.getTextFromSelectors(Connector.artistTrackSelector)
+	if (artistTrack) {
+		return	Util.splitArtistTrack(artistTrack.split(' - ', 2)[1], [', '])
+	}
+}
