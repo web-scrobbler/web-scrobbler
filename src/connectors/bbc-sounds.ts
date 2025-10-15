845475ccfe3de17c368a9cfeb1719bee660054b5
Bruno Gangemi <bruno@lien.digital>
2025-10-15 19:32:06 +0100

    Fixed formatting issues

--- src/connectors/bbc-sounds.ts
+++ src/connectors/bbc-sounds.ts
@@ -3,40 +3,48 @@ export {};
 Connector.playerSelector = '[data-testid="rail-recent_tracks"]';
 
 Connector.isPlaying = () => {
-  const btn = document.querySelector('[data-testid="play_pause_button"]');
-  return btn?.getAttribute('aria-label')?.toUpperCase() === 'PAUSE';
+	const btn = document.querySelector('[data-testid="play_pause_button"]');
+	return btn?.getAttribute('aria-label')?.toUpperCase() === 'PAUSE';
 };
 
 Connector.scrobblingDisallowedReason = () => {
-  const firstTrack = document.querySelector('[data-testid="rail-recent_tracks"] li:nth-child(2)');
-  const hasNowPlaying = firstTrack?.textContent?.toLowerCase().includes('now playing');
-  return hasNowPlaying ? null : 'Other';
+	const firstTrack = document.querySelector(
+		'[data-testid="rail-recent_tracks"] li:nth-child(2)',
+	);
+	const hasNowPlaying = firstTrack?.textContent
+		?.toLowerCase()
+		.includes('now playing');
+	return hasNowPlaying ? null : 'Other';
 };
 
 Connector.getArtistTrack = () => {
-  const firstTrack = document.querySelector('[data-testid="rail-recent_tracks"] li:nth-child(2)');
-  
-  if (!firstTrack?.textContent?.toLowerCase().includes('now playing')) {
-    return null;
-  }
+	const firstTrack = document.querySelector(
+		'[data-testid="rail-recent_tracks"] li:nth-child(2)',
+	);
 
-  const cleanText = (firstTrack as HTMLElement).innerText
-    .replace(/(^\d\.|\nnow playing)/gi, '')
-    .trim();
-  
-  const parts = cleanText.split('\n');
-  const track = parts[0]?.trim();
-  const artist = parts[1]?.trim();
-  
-  return artist && track ? { artist, track } : null;
+	if (!firstTrack?.textContent?.toLowerCase().includes('now playing')) {
+		return null;
+	}
+
+	const cleanText = (firstTrack as HTMLElement).innerText
+		.replace(/(^\d\.|\nnow playing)/gi, '')
+		.trim();
+
+	const parts = cleanText.split('\n');
+	const track = parts[0]?.trim();
+	const artist = parts[1]?.trim();
+
+	return artist && track ? { artist, track } : null;
 };
 
 Connector.getTrackArt = () => {
-  const firstTrack = document.querySelector('[data-testid="rail-recent_tracks"] li:nth-child(2)');
-  
-  if (!firstTrack?.textContent?.toLowerCase().includes('now playing')) {
-    return null;
-  }
-  
-  return firstTrack.querySelector('img')?.src || null;
+	const firstTrack = document.querySelector(
+		'[data-testid="rail-recent_tracks"] li:nth-child(2)',
+	);
+
+	if (!firstTrack?.textContent?.toLowerCase().includes('now playing')) {
+		return null;
+	}
+
+	return firstTrack.querySelector('img')?.src || null;
 };
