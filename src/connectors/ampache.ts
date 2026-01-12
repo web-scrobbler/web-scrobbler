a24d3e82dd60673739eef04a1b699719888fc822
Gabi <calingabriel89@gmail.com>
2026-01-12 13:24:25 +0200

    add ampache

---
+++ src/connectors/ampache.ts
@@ -0,0 +1,14 @@
+export { };
+
+Connector.playerSelector = '#webplayer';
+
+Connector.artistSelector = '#webplayer > div.playing_info > div.playing_artist > a';
+
+Connector.trackSelector = '#webplayer > div.playing_info > div.playing_title > a';
+
+// Album is not displayed in this version of Ampache
+// Connector.albumSelector = '';
+
+Connector.trackArtSelector = '#jp_poster_0';
+
+Connector.currentTimeSelector = '#jp_container_1 > div > div.jp-gui > div > div.jp-current-time';
--- src/core/connectors.ts
+++ src/core/connectors.ts
@@ -2720,4 +2720,11 @@ export default <ConnectorMeta[]>[
 		js: 'teleplay.js',
 		id: 'teleplay',
 	},
+	{
+		label: 'Ampache',
+		matches: ['*://192.168.1.100:8087/*'],
+		js: 'ampache.js',
+		id: 'ampache',
+		usesBlocklist: true,
+	},
 ];
