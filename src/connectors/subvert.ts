03a307297f69b1fb03d2bd5dd10bcc8e924a41bc
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2026-01-28 18:02:33 -0500

    Fixed methodology for time, bug on some tracks

--- src/connectors/subvert.ts
+++ src/connectors/subvert.ts
@@ -11,53 +11,16 @@ Connector.playerSelector = player;
 
 Connector.playButtonSelector = `${player} .playPauseButton`;
 
-function parseTime(timeSelector: string): number | undefined {
-	const timeString = document.querySelector(timeSelector) as HTMLElement;
+Connector.durationSelector = desktopDurationSelector;
 
-	if (!timeString) {
-		return undefined;
-	}
-
-	let timeParts = timeString.innerText.split(':');
-	let timeInSeconds = timeParts.reduce((acc, number, index) => {
-		let parsedNum = 0;
-		let mult = 60;
-		let finalNum = 0;
-		if (Number.isNaN(Number(number))) {
-			return acc;
-		} else {
-			parsedNum = Number(number.trim());
-		}
-		if (index === 0 && timeParts.length === 3) {
-			// account for hours
-			finalNum = parsedNum * mult * mult;
-		} else if (
-			(index === 1 && timeParts.length === 3) ||
-			(index === 0 && timeParts.length === 2)
-		) {
-			// account for minutes
-			finalNum = parsedNum * mult;
-		} else {
-			finalNum = parsedNum;
-		}
-		return acc + finalNum;
-	}, 0);
-
-	return timeInSeconds;
-}
-
-Connector.getCurrentTime = () => {
-	return parseTime(desktopCurrentTimeSelector);
-};
-
-Connector.getDuration = () => {
-	return parseTime(desktopDurationSelector);
-};
+Connector.currentTimeSelector = desktopCurrentTimeSelector;
 
 Connector.getArtistTrack = () => {
 	let track = Util.getTextFromSelectors(trackSelector);
 	let artist = Util.getTextFromSelectors(artistSelector);
-
+	if (track) {
+		track = track.split('\n')[0].trim();
+	}
 	let titleParts = document.title.split('–');
 	if (!artist && titleParts.length === 3) {
 		artist = titleParts[1].trim();
