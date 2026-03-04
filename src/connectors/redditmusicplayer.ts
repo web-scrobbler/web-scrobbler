72246718f9acddf3a8cf3e9ec79c789278608659
Rob9315 <dev.robk@gmail.com>
2026-03-04 19:52:59 +0100

    fix lints

--- src/connectors/redditmusicplayer.ts
+++ src/connectors/redditmusicplayer.ts
@@ -2,11 +2,10 @@ export {};
 
 Connector.playerSelector = ['main+div>div+div', 'main>div>div:last-child'];
 
-Connector.currentTimeSelector = Connector.playerSelector[0] + '>div>span';
-Connector.remainingTimeSelector =
-	Connector.playerSelector[0] + '>div>span:last-child';
+Connector.currentTimeSelector = `${Connector.playerSelector[0]}>div>span`;
+Connector.remainingTimeSelector = `${Connector.playerSelector[0]}>div>span:last-child`;
 
-Connector.trackSelector = Connector.playerSelector[1] + ' h2';
+Connector.trackSelector = `${Connector.playerSelector[1]} h2`;
 
 Connector.getArtistTrack = () => {
 	const text = Util.getTextFromSelectors(Connector.trackSelector)?.replace(
@@ -16,7 +15,7 @@ Connector.getArtistTrack = () => {
 	return Util.splitArtistTrack(text);
 };
 
-Connector.pauseButtonSelector = Connector.playerSelector[0] + ' .lucide-pause';
+Connector.pauseButtonSelector = `${Connector.playerSelector[0]} .lucide-pause`;
 
 Connector.getUniqueID = () => {
 	const videoUrl = Util.getAttrFromSelectors('iframe', 'src');
