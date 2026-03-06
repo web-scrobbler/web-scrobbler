87bd11dfc8e41fe96d566eedd6028b406d2d193c
Rob9315 <dev.robk@gmail.com>
2026-03-06 18:29:42 +0100

    fix blocsonic connector

--- src/connectors/blocsonic.ts
+++ src/connectors/blocsonic.ts
@@ -1,11 +1,26 @@
 export {};
 
-Connector.playerSelector = '#sp-container';
+Connector.playerSelector = '#bloc-player-audio-container+div';
 
-Connector.pauseButtonSelector = '.spi-pause-alt';
+Connector.pauseButtonSelector = `${Connector.playerSelector} button[aria-label="Click to Play"]`;
 
-Connector.artistTrackSelector = '.sp-title';
+Connector.trackSelector = `${Connector.playerSelector} h2>strong`;
 
-Connector.currentTimeSelector = '.sp-time-position';
+Connector.artistSelector = `${Connector.playerSelector} h2>a`;
 
-Connector.durationSelector = '.sp-length';
+const getTimes = () => {
+	const PlayerTimeSelector = '[class*=_PlayerTime_]';
+	return document.querySelector(PlayerTimeSelector)?.textContent.split(' / ');
+};
+
+Connector.getCurrentTime = () => {
+	return Util.stringToSeconds(getTimes()?.[0]);
+};
+
+Connector.getDuration = () => {
+	return Util.stringToSeconds(getTimes()?.[1]);
+};
+
+Connector.trackArtSelector = `${Connector.playerSelector} img`;
+
+Connector.albumSelector = `${Connector.playerSelector} h3>a`;
