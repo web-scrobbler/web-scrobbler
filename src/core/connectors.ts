d9fe458d2b67ee7e013ccef93f744132e0a7ba5d
Rob9315 <dev.robk@gmail.com>
2026-03-08 20:53:27 +0100

    Add radiosaw connector (#5869)
    * add radiosaw connector
    
    * hotfix

--- src/core/connectors.ts
+++ src/core/connectors.ts
@@ -2765,4 +2765,10 @@ export default <ConnectorMeta[]>[
 		js: 'kiss.js',
 		id: 'kiss',
 	},
+	{
+		label: 'Radio SAW',
+		matches: ['*://www.radiosaw.de/*'],
+		js: 'radiosaw.js',
+		id: 'radiosaw',
+	},
 ];
