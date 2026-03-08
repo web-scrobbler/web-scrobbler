dbbb56f89292053dd82b93675661408051a65479
Rob9315 <dev.robk@gmail.com>
2026-03-08 20:40:37 +0100

    hotfix

--- src/connectors/radiosaw.ts
+++ src/connectors/radiosaw.ts
@@ -4,6 +4,6 @@ Connector.playerSelector = '.radio-player';
 
 Connector.artistTrackSelector = '.radio-player__song';
 
-Connector.trackArtSelector = '.responsive-image__img';
+Connector.trackArtSelector = '.radio-player__cover .responsive-image__img';
 
 Connector.pauseButtonSelector = '.radio-player__toggle-button .fa-pause';
