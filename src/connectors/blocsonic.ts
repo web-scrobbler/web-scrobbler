3bd0e9cfb361b71f8b11b0837420d5732739b191
rob9315 <dev.robk@gmail.com>
2026-03-08 19:05:34 +0100

    apply simplification

--- src/connectors/blocsonic.ts
+++ src/connectors/blocsonic.ts
@@ -8,18 +8,7 @@ Connector.trackSelector = `${Connector.playerSelector} h2>strong`;
 
 Connector.artistSelector = `${Connector.playerSelector} h2>a`;
 
-const getTimes = () => {
-	const PlayerTimeSelector = '[class*=_PlayerTime_]';
-	return document.querySelector(PlayerTimeSelector)?.textContent.split(' / ');
-};
-
-Connector.getCurrentTime = () => {
-	return Util.stringToSeconds(getTimes()?.[0]);
-};
-
-Connector.getDuration = () => {
-	return Util.stringToSeconds(getTimes()?.[1]);
-};
+Connector.timeInfoSelector = '[class*=_PlayerTime_]';
 
 Connector.trackArtSelector = `${Connector.playerSelector} img`;
 
