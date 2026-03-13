672b1352cde46364428606170c2d4629bade9eb5
Rob9315 <dev.robk@gmail.com>
2026-03-13 20:34:27 +0100

    add data_collection_permissions and update @types/webextension-polyfill

--- manifest.config.ts
+++ manifest.config.ts
@@ -96,6 +96,12 @@ export const firefoxManifest: Manifest.WebExtensionManifest = {
 	browser_specific_settings: {
 		gecko: {
 			id: '{799c0914-748b-41df-a25c-22d008f9e83f}',
+			data_collection_permissions: {
+				required: [
+					"browsingActivity",
+					"websiteContent",
+				],
+			},
 		},
 	},
 
