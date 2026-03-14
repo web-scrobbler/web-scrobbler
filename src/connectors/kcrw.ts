21d691a60b0980b872b1dbc2a4dfbd08a8d3c2d8
Malcolm Peterson <mgpetersn@gmail.com>
2026-03-13 21:46:21 -0400

    fix : refactor workaround for scrobbling on shows/episode pages
    there is currently no way to scrobble from the miniplayer / site player as we once did due to the artist name not being in the DOM at all (instead it is the DJ's name). Instead we can scrobble playlists from episode pages for the time being.

--- src/connectors/kcrw.ts
+++ src/connectors/kcrw.ts
@@ -1,71 +1,70 @@
 export {};
 
 const filter = MetadataFilter.createFilter({
-	artist: [removeShowName, replaceSmartQuotes],
+	artist: [replaceSmartQuotes],
 	track: [
-		removeSmartQuotes,
 		replaceSmartQuotes,
 		MetadataFilter.removeVersion,
 		MetadataFilter.removeCleanExplicit,
 	],
 });
-
-Connector.playerSelector = '.site-player'; // supports main and mini players
+// we are using fuzzy matching for the classes in this connector because I think they are generated at run time and thus can potentially change on each build. This would make the only consistent part the suffix of the class name, ex: "AudioPlayer_audioController".
+Connector.playerSelector = '[class*="AudioPlayer_audioController"]';
 
 Connector.getTrackInfo = () => {
-	const metaSelector = '.site-player .meta';
-	const timeSelector = '.site-player .duration';
-	const showNameText = Util.getTextFromSelectors(
-		`${metaSelector} .show-name`,
+	// Tracklist on this page are paginated. When the page is initially loaded this will click the button and it keep clicking "Show More" on each play cycle until all tracks are displayed and the "Show More" button is no longer present. This is necessary because as the tracks play there will be a possiblity that the currently playing track is not in the DOM and thus the connector won't be able to get the track info.
+	const showMoreButton = document.querySelector<HTMLElement>(
+		'[class*="EmbeddedTracklist_pagingShowMore"]',
 	);
-	const artistText = Util.getTextFromSelectors(
-		`${metaSelector} .episode-name`,
+	if (showMoreButton) {
+		showMoreButton.click();
+		return null;
+	}
+	// .shows/... page(s), ex: https://www.kcrw.com/shows/francesca-harding/stories/a-playlist-of-high-energy-indie-delectable-disco-and-groovy-ethio-jazz
+	// the tracklist shows the currently playing track with an SVG playing indicator inside its prefix element
+	const playingItem = document.querySelector(
+		'[class*="EmbeddedTracklist_tracklistItem"]:has([class*="Tracklist_prefix"] svg)',
 	);
-	const trackText = Util.getTextFromSelectors(`${metaSelector} .song-name`);
 
-	if (
-		Util.hasElementClass(metaSelector, 'music') &&
-		trackText &&
-		artistText !== '[BREAK]'
-	) {
-		return {
-			artist: artistText,
-			track: trackText,
-		};
+	Util.debugLog(`playingItem: ${playingItem}`);
+	Util.debugLog(`playingItem innerHTML: ${playingItem?.innerHTML}`);
+
+	if (!playingItem) {
+		return null;
 	}
 
-	if (
-		!Util.hasElementClass(metaSelector, 'music') &&
-		artistText &&
-		showNameText === "Today's Top Tune"
-	) {
-		const artistTrackSplit = artistText.split(/\s+[-\u2013]\s+/);
-		return {
-			artist: artistTrackSplit[0],
-			track: artistTrackSplit[1],
-			currentTime: Util.getSecondsFromSelectors(
-				`${timeSelector} .progress`,
-			),
-			duration: Util.getSecondsFromSelectors(`${timeSelector} .total`),
-		};
+	const trackEl = playingItem.querySelector(
+		'[class*="Tracklist_trackTitle"]',
+	);
+	const artistEl = playingItem.querySelector('[class*="Tracklist_info"]');
+	const trackText =
+		(trackEl as HTMLElement | null)?.innerText?.trim() ?? null;
+	const artistText =
+		(artistEl as HTMLElement | null)?.innerText?.trim() ?? null;
+
+	Util.debugLog(`trackText: ${trackText}`);
+	Util.debugLog(`artistText: ${artistText}`);
+
+	// There may need to be some sort of check here for BREAK if/when it is possible to suppport scrobbling from player.
+	// ex. if (trackText === "BREAK" || artistText === "BREAK") {
+	//     return null;
+	// }
+	if (!trackText) {
+		return null;
 	}
 
-	return null;
+	return {
+		artist: artistText,
+		track: trackText,
+	};
 };
 
-Connector.isPlaying = () => Util.hasElementClass('button.play', 'active');
+Connector.isPlaying = () => {
+	const audio = document.querySelector<HTMLAudioElement>('audio');
+	return audio ? !audio.paused : false;
+};
 
 Connector.applyFilter(filter);
-
-function removeShowName(text: string) {
-	// filter for Today's Top Tune
-	return text.replace(/Today('|\u2019)s Top Tune:\s+/, '');
-}
-
-function removeSmartQuotes(text: string) {
-	// filter for Today's Top Tune
-	return text.replace(/^[\u2018\u201c](.+)[\u2019\u201d](?=\s\(|$)/, '$1');
-}
 
 function replaceSmartQuotes(text: string) {
 	return text.replace(/[\u2018\u2019]/g, "'").replace(/[\u201c\u201d]/g, '"');
