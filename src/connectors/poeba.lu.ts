51e159e89de3bbd40f359bbf43309d68d4f9a9c2
Aleksandr Ponimaskin <aleksandr.ponimaskin@zalando.de>
2026-04-05 00:35:49 +0200

    Remove comment from poeba.lu

--- src/connectors/poeba.lu.ts
+++ src/connectors/poeba.lu.ts
@@ -2,9 +2,6 @@ export {};
 
 /**
  * Connector for the Bandcamp embedded player on poeba.lu.
- *
- * The embedded player runs inside an iframe at bandcamp.com/EmbeddedPlayer/...
- * State: the root #player element gets the class "playing" when audio is playing.
  */
 
 Connector.playerSelector = '#player';
