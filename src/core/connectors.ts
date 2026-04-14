8420aacd3a6b74f3525f6fde05b222448ed1c4b5
Alex <ponimaskin@gmail.com>
2026-04-14 14:15:07 +0200

    Fix tabs indentation

--- src/core/connectors.ts
+++ src/core/connectors.ts
@@ -38,7 +38,7 @@ export default <ConnectorMeta[]>[
 		allFrames: true,
 	},
 	{
-	        label: 'poeba.lu (embedded bandcamp player only)',
+		label: 'poeba.lu (embedded bandcamp player only)',
 		matches: ['*://bandcamp.com/EmbeddedPlayer/*'],
 		js: 'poeba.lu.js',
 		id: 'poeba.lu',
