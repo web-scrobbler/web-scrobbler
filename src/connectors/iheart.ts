73bf9d31812921a012bdff1408ab5b3d55b79f81
ホーンバックル <hornbuckle@gmail.com>
2026-04-15 14:05:56 -0700

    Fix iHeart connector

--- src/connectors/iheart.ts
+++ src/connectors/iheart.ts
@@ -1,19 +1,44 @@
 export {};
 
 const playerBar = '[data-test=player-container]';
-const controlBar = '[data-test=controls-container]';
+const controlBar = '[data-test=player-controls]';
+const artistSelector = `${playerBar} [data-test=description-link]`;
 
 Connector.playerSelector = playerBar;
 
-Connector.pauseButtonSelector = `${controlBar} button[data-test-state=PLAYING]`;
+Connector.isPodcast = () =>
+	Util.isElementVisible(
+		`${playerBar} [data-test=progress-bar-player-control]`,
+	);
 
-Connector.artistSelector = `${playerBar} [data-test=line-text]:nth-child(3)`;
+Connector.getArtistTrack = () => {
+	const artist = Util.getTextFromSelectors(artistSelector);
+	let trackSelector = `${playerBar} [data-test=title-link]`;
 
-Connector.trackSelector = `${playerBar} [data-test=line-text]:nth-child(2)`;
+	if (Connector.isPodcast()) {
+		trackSelector = `${playerBar} [data-test=subtitle-link]`;
+	}
 
-Connector.scrobblingDisallowedReason = () => {
-	const track = Connector.getTrack();
-	return track && !track.startsWith('Thanks for listening')
-		? null
-		: 'FilteredTag';
+	return { artist, track: Util.getTextFromSelectors(trackSelector) };
+};
+
+Connector.getTrackArt = () => {
+	const artist = Util.getTextFromSelectors(artistSelector);
+	const trackArtSelector = `${playerBar} img[alt="${artist}"]`;
+	const trackArtUrl = Util.extractImageUrlFromSelectors(trackArtSelector);
+
+	return trackArtUrl?.replace(/(?<=fit%28)\d{3}%2C\d{1,3}(?=%29)/, '328%2C0'); // larger image
+};
+
+Connector.isTrackArtDefault = (url) => url?.includes('assets.streams');
+
+Connector.isPlaying = () => {
+	const playButtonSvg = `${controlBar} button[data-test=player-play-button] svg`;
+	const svgLabel = Util.getAttrFromSelectors(playButtonSvg, 'aria-label');
+
+	if (Connector.isPodcast()) {
+		return svgLabel === 'Pause';
+	}
+
+	return svgLabel === 'Stop';
 };
