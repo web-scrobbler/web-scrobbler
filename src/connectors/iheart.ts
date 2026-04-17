f9b590564ca81426dd923ec25cfbad054869e373
ホーンバックル <hornbuckle@gmail.com>
2026-04-17 15:20:18 -0700

    Update iHeart connector
    Add playlist support

--- src/connectors/iheart.ts
+++ src/connectors/iheart.ts
@@ -7,9 +7,7 @@ const artistSelector = `${playerBar} [data-test=description-link]`;
 Connector.playerSelector = playerBar;
 
 Connector.isPodcast = () =>
-	Util.isElementVisible(
-		`${playerBar} [data-test=progress-bar-player-control]`,
-	);
+	Util.isElementVisible(`${playerBar} [data-test=forward-30-player-button]`);
 
 Connector.getArtistTrack = () => {
 	const artist = Util.getTextFromSelectors(artistSelector);
@@ -19,7 +17,9 @@ Connector.getArtistTrack = () => {
 		trackSelector = `${playerBar} [data-test=subtitle-link]`;
 	}
 
-	return { artist, track: Util.getTextFromSelectors(trackSelector) };
+	const track = Util.getTextFromSelectors(trackSelector);
+
+	return { artist, track };
 };
 
 Connector.getTrackArt = () => {
@@ -27,7 +27,7 @@ Connector.getTrackArt = () => {
 	const trackArtSelector = `${playerBar} img[alt="${artist}"]`;
 	const trackArtUrl = Util.extractImageUrlFromSelectors(trackArtSelector);
 
-	return trackArtUrl?.replace(/(?<=fit%28)\d{3}%2C\d{1,3}(?=%29)/, '328%2C0'); // larger image
+	return trackArtUrl?.replace(/(?<=fit%28)\d{3}%2C\d{1,3}(?=%29)/, '400%2C0'); // larger image
 };
 
 Connector.isTrackArtDefault = (url) => url?.includes('assets.streams');
@@ -36,8 +36,8 @@ Connector.isPlaying = () => {
 	const playButtonSvg = `${controlBar} button[data-test=player-play-button] svg`;
 	const svgLabel = Util.getAttrFromSelectors(playButtonSvg, 'aria-label');
 
-	if (Connector.isPodcast()) {
-		return svgLabel === 'Pause';
+	if (Util.isElementVisible('[data-test=next-player-button]')) {
+		return svgLabel === 'Pause'; // podcasts and playlists
 	}
 
 	return svgLabel === 'Stop';
