e80b84170ad7ed396dad21426cf6666f8195f232
ホーンバックル <hornbuckle@gmail.com>
2026-04-18 11:00:25 -0700

    Update iHeart connector
    Add delay and track current time/duration support

--- src/connectors/iheart.ts
+++ src/connectors/iheart.ts
@@ -1,8 +1,12 @@
+import type { ArtistTrackInfo } from '@/core/types';
+
 export {};
 
 const playerBar = '[data-test=player-container]';
 const controlBar = '[data-test=player-controls]';
 const artistSelector = `${playerBar} [data-test=description-link]`;
+let artistTrack: ArtistTrackInfo | null = null;
+let timeout: ReturnType<typeof setTimeout> | undefined;
 
 Connector.playerSelector = playerBar;
 
@@ -10,17 +14,19 @@ Connector.isPodcast = () =>
 	Util.isElementVisible(`${playerBar} [data-test=forward-30-player-button]`);
 
 Connector.getArtistTrack = () => {
-	const artist = Util.getTextFromSelectors(artistSelector);
-	let trackSelector = `${playerBar} [data-test=title-link]`;
+	const newArtistTrack = getArtistTrack();
 
-	if (Connector.isPodcast()) {
-		trackSelector = `${playerBar} [data-test=subtitle-link]`;
+	if (isArtistTrackValid(newArtistTrack)) {
+		artistTrack = newArtistTrack;
+		renewTimeout();
 	}
 
-	const track = Util.getTextFromSelectors(trackSelector);
-
-	return { artist, track };
+	return artistTrack;
 };
+
+Connector.currentTimeSelector = `${playerBar} span:has(+ [data-test=progress-bar-player-control])`;
+
+Connector.durationSelector = `${playerBar} [data-test=progress-bar-player-control] + span`;
 
 Connector.getTrackArt = () => {
 	const artist = Util.getTextFromSelectors(artistSelector);
@@ -30,7 +36,7 @@ Connector.getTrackArt = () => {
 	return trackArtUrl?.replace(/(?<=fit%28)\d{3}%2C\d{1,3}(?=%29)/, '400%2C0'); // larger image
 };
 
-Connector.isTrackArtDefault = (url) => url?.includes('assets.streams');
+Connector.isTrackArtDefault = (url) => url?.includes('assets');
 
 Connector.isPlaying = () => {
 	const playButtonSvg = `${controlBar} button[data-test=player-play-button] svg`;
@@ -42,3 +48,32 @@ Connector.isPlaying = () => {
 
 	return svgLabel === 'Stop';
 };
+
+function getArtistTrack() {
+	const artist = Util.getTextFromSelectors(artistSelector);
+	let trackSelector = `${playerBar} [data-test=title-link]`;
+
+	if (Connector.isPodcast()) {
+		trackSelector = `${playerBar} [data-test=subtitle-link]`;
+	}
+
+	const track = Util.getTextFromSelectors(trackSelector);
+
+	return { artist, track };
+}
+
+function isArtistTrackValid(artistTrack: ArtistTrackInfo) {
+	if (!artistTrack.track) {
+		return false;
+	}
+
+	return true;
+}
+
+function renewTimeout() {
+	clearTimeout(timeout);
+	timeout = setTimeout(() => {
+		artistTrack = null;
+		Connector.onStateChanged();
+	}, 10000); // 10 second delay to mitigate frequent empty state on some stations
+}
