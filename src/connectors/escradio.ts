c667330eea8246f42a28f1f5080e1f2cc93c05ad
Rob9315 <dev.robk@gmail.com>
2026-04-18 20:21:43 +0200

    remove playbutton selector

--- src/connectors/escradio.ts
+++ src/connectors/escradio.ts
@@ -2,7 +2,6 @@ export {};
 
 Connector.playerSelector = '#play';
 
-Connector.playButtonSelector = '#playplaybutton[style*="visibility: visible"]';
 Connector.pauseButtonSelector = '#playstopbutton[style*="visibility: visible"]';
 
 Connector.artistSelector = '#playartist';
