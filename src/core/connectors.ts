8670fb5a8a2b7e5db79cf914665e801633c6fbd5
Rob9315 <dev.robk@gmail.com>
2026-04-21 23:51:54 +0200

    github webui is kinda bad for this stuff

--- src/core/connectors.ts
+++ src/core/connectors.ts
@@ -2807,7 +2807,7 @@ export default <ConnectorMeta[]>[
 		js: 'meloradio.js',
 		id: 'meloradio',
 	},
-  {
+	{
 		label: 'The Indie Beat Television',
 		matches: ['*://tv.theindiebeat.fm/*'],
 		js: 'theindiebeat-owncast.js',
