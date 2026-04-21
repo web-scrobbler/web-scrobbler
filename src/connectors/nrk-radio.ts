a47f2072ea5f10f2ee6e852679bc7e8b5e844603
Rob9315 <dev.robk@gmail.com>
2026-04-21 21:11:08 +0200

    add fullscreen selector and track art

--- src/connectors/nrk-radio.ts
+++ src/connectors/nrk-radio.ts
@@ -4,11 +4,13 @@ const filter = MetadataFilter.createFilter({
 	artist: cleanUpArtist,
 });
 
-Connector.playerSelector = '#mini-player';
+Connector.playerSelector = ['#mini-player', '#fullscreen-dialog'];
 
-Connector.artistTrackSelector = `${Connector.playerSelector} [class*="currentlyPlaying"] .nrk-font-footnote`;
+Connector.artistTrackSelector = `${Connector.playerSelector[0]} [class*="currentlyPlaying"] .nrk-font-footnote`;
 
-Connector.pauseButtonSelector = `${Connector.playerSelector} .nrk-button .nrk-media-pause`;
+Connector.pauseButtonSelector = `${Connector.playerSelector[0]} .nrk-button .nrk-media-pause`;
+
+Connector.trackArtSelector = `${Connector.playerSelector[1]} [class*="_image"] img`;
 
 Connector.applyFilter(filter);
 
