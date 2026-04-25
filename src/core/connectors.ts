8772d64133e254fc46be509a255c4c25148d21e3
Rob9315 <dev.robk@gmail.com>
2026-04-25 20:49:09 +0200

    add support for escradio.com (#5965)
    * add support for escradio.com
    
    * remove playbutton selector

--- src/core/connectors.ts
+++ src/core/connectors.ts
@@ -2807,4 +2807,10 @@ export default <ConnectorMeta[]>[
 		js: 'meloradio.js',
 		id: 'meloradio',
 	},
+	{
+		label: 'escradio',
+		matches: ['*://www.escradio.com/player/*'],
+		js: 'escradio.js',
+		id: 'escradio',
+	},
 ];
