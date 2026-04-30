efc03ffbd1cbff5556224245288455934fe8b31c
ホーンバックル <hornbuckle@gmail.com>
2026-04-30 13:18:27 -0700

    Fix KEXP connector

--- src/connectors/kexp.ts
+++ src/connectors/kexp.ts
@@ -1,26 +1,26 @@
 export {};
 
 const filter = MetadataFilter.createFilter({
-	artist: replaceSmartQuotes,
-	track: replaceSmartQuotes,
-	album: replaceSmartQuotes,
+	artist: MetadataFilter.replaceSmartQuotes,
+	track: MetadataFilter.replaceSmartQuotes,
+	album: MetadataFilter.replaceSmartQuotes,
 });
 
 Connector.playerSelector = '.Player';
 
-Connector.artistTrackSelector = '.Player-title';
+Connector.getArtistTrack = () => {
+	const artistTrack = Util.getTextFromSelectors('.Player-title');
+
+	return Util.splitArtistTrack(artistTrack, [' \u2022 '], true);
+};
 
 Connector.albumSelector = '.Player-album';
 
-Connector.trackArtSelector = '.Player-coverImage';
+Connector.trackArtSelector = '.Player-coverImage.image-is-loaded';
 
-Connector.isTrackArtDefault = (url) => url?.includes('default');
+Connector.isTrackArtDefault = (url) => url?.includes('canonical');
 
 Connector.isPlaying = () =>
 	Util.hasElementClass(Connector.playerSelector, 'Player--playing');
 
 Connector.applyFilter(filter);
-
-function replaceSmartQuotes(text: string) {
-	return text.replace(/[\u2018\u2019]/g, "'").replace(/[\u201c\u201d]/g, '"');
-}
