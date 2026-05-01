5b9fbee2a596338fe73c8092eeec52ee50169ed3
demiters <demiters@users.noreply.github.com>
2026-05-01 16:59:57 +0300

    Fix Star FM connector

--- src/connectors/starfm.ts
+++ src/connectors/starfm.ts
@@ -5,6 +5,4 @@ Connector.playerSelector = 'div.tv3-radio-online-player-controls';
 Connector.artistSelector = 'div.js-tv3-radio-online-currently-live-artist';
 Connector.trackSelector = 'div.js-tv3-radio-online-currently-live-song';
 
-Connector.isPlaying = () =>
-	document.querySelector('i.bi-play-fill')?.classList.contains('invisible') ??
-	false;
+Connector.isPlaying = () => Util.hasElementClass('i.bi-play-fill', 'invisible');
