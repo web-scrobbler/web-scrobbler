9f2ced247634a879903e9dc77801f6b3177690e1
hornbuckle <6808988+hornbuckle@users.noreply.github.com>
2026-05-05 19:14:14 -0700

    Update Bandcamp Embed with filter

--- src/connectors/bandcamp-embedded.ts
+++ src/connectors/bandcamp-embedded.ts
@@ -1,5 +1,11 @@
 export {};
 
+const filter = MetadataFilter.createFilter({
+	artist: MetadataFilter.replaceSmartQuotes,
+	track: MetadataFilter.replaceSmartQuotes,
+	album: MetadataFilter.replaceSmartQuotes,
+});
+
 Connector.playerSelector = '#player';
 
 Connector.artistSelector = '#artist';
@@ -12,7 +18,7 @@ Connector.getTrackArt = () => {
 	const trackArtUrl = Util.extractImageUrlFromSelectors('#infolayer .art');
 
 	if (trackArtUrl) {
-		return trackArtUrl.replace(/(?<=_)\d{1,2}(?=\.jpg)/, '10'); // larger image
+		return trackArtUrl.replace(/(?<=_)\d{1,2}(?=\.jpg)/, '5'); // larger image
 	}
 
 	return null;
@@ -24,3 +30,5 @@ Connector.durationSelector = '#totaltime';
 
 Connector.isPlaying = () =>
 	Util.hasElementClass(Connector.playerSelector, 'playing');
+
+Connector.applyFilter(filter);
