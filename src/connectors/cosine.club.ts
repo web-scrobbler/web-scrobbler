5e8dc9bea218d2977edd4194e6088084683caf84
Rob9315 <dev.robk@gmail.com>
2026-05-11 14:15:35 +0200

    fix cosine.club

--- src/connectors/cosine.club.ts
+++ src/connectors/cosine.club.ts
@@ -4,17 +4,11 @@ Connector.playerSelector = '#tracks-container';
 
 Connector.getArtistTrack = () => {
 	const trackElement = document.querySelector(
-		'div[data-playing="true"] span',
+		'div[data-playing="true"] .track-name',
 	);
 	return trackElement
 		? Util.splitArtistTrack(trackElement.textContent)
 		: null;
 };
 
-Connector.isPlaying = () => {
-	const currentTrack = document.querySelector('div[data-playing="true"]');
-	const svg = currentTrack?.querySelector('svg');
-	return svg?.getAttribute('aria-label') === 'pause';
-};
-
-setInterval(Connector.onStateChanged, 1000);
+Connector.pauseButtonSelector = 'div[data-playing="true"]';
