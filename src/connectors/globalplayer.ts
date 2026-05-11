3d6efbf5ebad246589ce40416309376cfce8bf8e
Rob9315 <dev.robk@gmail.com>
2026-05-11 12:35:19 +0200

    change moderator segment detection

--- src/connectors/globalplayer.ts
+++ src/connectors/globalplayer.ts
@@ -4,10 +4,13 @@ Connector.playerSelector = '.globalplayer';
 
 const playbarSelector = '.globalplayer [data-testid="playbar"]';
 
-Connector.trackSelector = `${playbarSelector} [data-testid="show-info-title"]>:first-child`;
-Connector.artistSelector = `${playbarSelector} [data-testid="show-info-title"]>:last-child`;
+const showInfoTitleSelector = `${playbarSelector} [data-testid="show-info-title"]`;
+Connector.trackSelector = `${showInfoTitleSelector}>:first-child`;
+Connector.artistSelector = `${showInfoTitleSelector}>:last-child`;
 Connector.pauseButtonSelector = `${playbarSelector} button[data-testid="play-pause-button"][aria-pressed="true"]`;
 Connector.trackArtSelector = `${playbarSelector} img`;
 
 Connector.scrobblingDisallowedReason = () =>
-	Connector.getTrack() === Connector.getArtist() ? 'FilteredTag' : null;
+	document.querySelector(showInfoTitleSelector)?.childElementCount === 1
+		? 'FilteredTag'
+		: null;
