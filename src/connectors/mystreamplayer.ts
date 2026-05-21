069b3386209a685af6fa9b8f3d7e9f530ea70ace
hornbuckle <6808988+hornbuckle@users.noreply.github.com>
2026-05-20 22:31:19 -0700

    Update MyStreamPlayer connector

--- src/connectors/mystreamplayer.ts
+++ src/connectors/mystreamplayer.ts
@@ -1,7 +1,27 @@
 export {};
 
-Connector.playerSelector = 'header.player';
-Connector.artistSelector = '#artist';
+const filter = MetadataFilter.createFilter({
+	track: [
+		MetadataFilter.removeRemastered,
+		MetadataFilter.removeVersion,
+		MetadataFilter.removeLive,
+	],
+});
+
+Connector.playerSelector = '.jp-jplayer ~ .player';
+
 Connector.trackSelector = '#song';
-Connector.trackArtSelector = 'img.songimg';
+
+Connector.getArtist = () => {
+	const artistElement = document.querySelector('#artist');
+
+	if (artistElement) {
+		return artistElement.firstChild?.textContent;
+	}
+
+	return null;
+};
+
 Connector.isPlaying = () => Util.hasElementClass('#playbtn', 'jp-stopx');
+
+Connector.applyFilter(filter);
