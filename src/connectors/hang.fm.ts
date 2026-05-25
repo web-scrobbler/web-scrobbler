01893c2fb5a1fb76af611ed8b7d7a38bbcc7f4c6
Ell Bradshaw <ell@cincodenada.com>
2026-05-25 14:53:20 -0700

    Prefix selectors with #NowPlaying, add pauseButton

--- src/connectors/hang.fm.ts
+++ src/connectors/hang.fm.ts
@@ -1,11 +1,13 @@
 export {};
 
-const timeDiv = 'button > span > div:last-child';
+const playerDisplay = '#NowPlaying button > span'
+const timeDiv = '${playerDisplay} > div:last-child';
 
 Connector.playerSelector = '#NowPlaying';
 
-Connector.artistTrackSelector =
-	'button > span > div:first-child > span:first-child';
+Connector.pauseButtonSelector = '#NowPlaying button';
+
+Connector.artistTrackSelector = `${playerDisplay} > div:first-child > span:first-child`;
 
 Connector.currentTimeSelector = `${timeDiv} > div:first-child`;
 Connector.durationTimeSelector = `${timeDiv} > div:last-child`;
