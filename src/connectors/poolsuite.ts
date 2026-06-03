3d90eca2b4e21a91e33387e2a0ae0d2860d427fe
Michael Edelstone <michael.edelstone@gmail.com>
2026-06-02 21:02:52 -0500

    fixes lint errors

--- src/connectors/poolsuite.ts
+++ src/connectors/poolsuite.ts
@@ -4,15 +4,18 @@ Connector.playerSelector = 'body';
 Connector.useTabAudibleApi();
 
 Connector.getArtistTrack = () => {
-  const title = navigator.mediaSession.metadata?.title ?? '';
-  const artist = navigator.mediaSession.metadata?.artist ?? '';
+	const title = navigator.mediaSession.metadata?.title ?? '';
+	const artist = navigator.mediaSession.metadata?.artist ?? '';
 
-  if (title === 'Poolsuite ON AIR') {
-    const text = document.querySelector('h2.font-everyday')?.textContent ?? '';
-    return Util.processSoundCloudTrack(text);
-  }
+	if (title === 'Poolsuite ON AIR') {
+		const text =
+			document.querySelector('h2.font-everyday')?.textContent ?? '';
+		return Util.processSoundCloudTrack(text);
+	}
 
-  const result = Util.processSoundCloudTrack(title);
-  if (result.artist && result.track) return result;
-  return { artist, track: title };
+	const result = Util.processSoundCloudTrack(title);
+	if (result.artist && result.track) {
+		return result;
+	}
+	return { artist, track: title };
 };
