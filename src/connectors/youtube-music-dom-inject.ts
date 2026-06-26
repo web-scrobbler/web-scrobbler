c1a9c12ab085a440fb87073e4a2cdfdcac25dcaa
Jose Garcia <jose@jdgarcia.dev>
2026-06-26 01:05:50 -0700

    Only postMessage if metadata or playbackState actually changed

--- src/connectors/youtube-music-dom-inject.ts
+++ src/connectors/youtube-music-dom-inject.ts
@@ -16,21 +16,44 @@ if ('cleanup' in window && typeof window.cleanup === 'function') {
 	(window as unknown as { cleanup: () => void }).cleanup();
 }
 
+type MessagePayload = {
+	sender: 'web-scrobbler';
+	playbackState: MediaSessionPlaybackState;
+	metadata: {
+		title?: string;
+		artist?: string;
+		artwork?: readonly MediaImage[];
+		album?: string;
+	};
+};
+
 (window as unknown as { cleanup: () => void }).cleanup = (() => {
+	let previousPayload: MessagePayload | null = null;
+
 	let pollInterval: number | null = window.setInterval(() => {
-		window.postMessage(
-			{
-				sender: 'web-scrobbler',
-				playbackState: navigator.mediaSession.playbackState,
-				metadata: {
-					title: navigator.mediaSession.metadata?.title,
-					artist: navigator.mediaSession.metadata?.artist,
-					artwork: navigator.mediaSession.metadata?.artwork,
-					album: navigator.mediaSession.metadata?.album,
-				},
+		const payload: MessagePayload = {
+			sender: 'web-scrobbler',
+			playbackState: navigator.mediaSession.playbackState,
+			metadata: {
+				title: navigator.mediaSession.metadata?.title,
+				artist: navigator.mediaSession.metadata?.artist,
+				artwork: navigator.mediaSession.metadata?.artwork,
+				album: navigator.mediaSession.metadata?.album,
 			},
-			'*',
-		);
+		};
+
+		if (
+			!previousPayload ||
+			payload.playbackState !== previousPayload.playbackState ||
+			payload.metadata.title !== previousPayload.metadata.title ||
+			payload.metadata.artist !== previousPayload.metadata.artist ||
+			payload.metadata.artwork !== previousPayload.metadata.artwork ||
+			payload.metadata.album !== previousPayload.metadata.album
+		) {
+			window.postMessage(payload, '*');
+
+			previousPayload = payload;
+		}
 	}, 1000);
 
 	return () => {
