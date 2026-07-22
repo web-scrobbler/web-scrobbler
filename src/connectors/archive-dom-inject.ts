b627b8a28a371f1b8632cccf8e19194189d40fea
sorusari343 <127366704+sorusari343@users.noreply.github.com>
2026-07-21 23:14:47 -0700

    fix archive.org connector (#5984)

---
+++ src/connectors/archive-dom-inject.ts
@@ -0,0 +1,37 @@
+export {};
+
+/**
+ * This script runs in non-isolated environment(internet archive itself)
+ * for accessing navigator variables on Firefox
+ *
+ * * Script is run as an IIFE to ensure variables are scoped, as in the event
+ * of extension reload/update a new script will have to override the current one.
+ *
+ * Script starts by calling window.cleanup to cleanup any potential previous script.
+ *
+ * @returns a cleanup function that cleans up event listeners and similar for a future overriding script.
+ */
+
+if ('cleanup' in window && typeof window.cleanup === 'function') {
+	(window as unknown as { cleanup: () => void }).cleanup();
+}
+
+(window as unknown as { cleanup: () => void }).cleanup = (() => {
+	const sendData = () => {
+		window.postMessage(
+			{
+				sender: 'web-scrobbler',
+				state: {
+					isPlaying: jwplayer().getState() === 'playing',
+					getDuration: jwplayer().getDuration(),
+					getTrack: jwplayer().getPlaylistItem().title,
+					getArtist: jwplayer().getPlaylistItem().artist,
+					getPlaylist: jwplayer().getPlaylist(),
+				},
+			},
+			'*',
+		);
+	};
+	jwplayer().on('play', sendData);
+	jwplayer().on('pause', sendData);
+})();
--- src/connectors/archive.ts
+++ src/connectors/archive.ts
@@ -3,17 +3,11 @@ export {};
 /**
  * Example links to debug and test the connector:
  *
- * https://archive.org/details/AH013_sarin_sunday_-_the_lonely_hike
+ * https://archive.org/details/dystopiaq029
  * Full album
  *
  * https://archive.org/details/AH003_corwin_trails_-_corwin_trails
  * Full album with numeric prefixes
- *
- * https://archive.org/details/lp_everybody-knows-this-is-nowhere_neil-young-crazy-horse-robin-lane
- * Full album with artist suffixed in track names
- *
- * https://archive.org/details/dont-lie-beets-produce-ny-mix
- * Single track
  */
 
 const numericTrackRegex = /^\d+\w+/;
@@ -29,19 +23,11 @@ function removeNumericPrefixes(track: string) {
 }
 
 function hasAllTracksNumericPrefix() {
-	const tracks = getTracksElementShadowDom();
-	if (tracks === null) {
-		return false;
-	}
+	const playlist = state?.getPlaylist;
 
-	const trackTitles = tracks.querySelectorAll('.track .track-title');
-
-	if (trackTitles.length === 0) {
-		return false;
-	}
 	let hasAllTracksNumericPrefix = true;
-	for (const trackTitle of trackTitles) {
-		if (!numericTrackRegex.test(trackTitle?.textContent?.trim() ?? '')) {
+	for (const track of playlist) {
+		if (!numericTrackRegex.test(track.title.trim() ?? '')) {
 			hasAllTracksNumericPrefix = false;
 			break;
 		}
@@ -50,49 +36,11 @@ function hasAllTracksNumericPrefix() {
 	return hasAllTracksNumericPrefix;
 }
 
-function getTracksElementShadowDom() {
-	const tracksElement = document.querySelector('play-av');
-
-	if (tracksElement === null) {
-		return null;
-	}
-
-	return tracksElement.shadowRoot;
-}
-
 Connector.applyFilter(filter);
 
-Connector.artistSelector = '.item-details-metadata > dl > dd a';
+Connector.albumArtistSelector = '.item-details-metadata > dl > dd a';
 
 Connector.albumSelector = '.item-title';
-
-Connector.currentTimeSelector = '.jw-text-elapsed';
-
-Connector.durationSelector = '.jw-text-duration';
-
-Connector.isPlaying = () => {
-	const videoElement = document.querySelector('video');
-
-	if (videoElement === null) {
-		return false;
-	}
-
-	return !videoElement.paused;
-};
-
-Connector.getTrack = () => {
-	const tracksElement = document.querySelector('play-av');
-
-	if (tracksElement === null) {
-		return null;
-	}
-
-	const trackElements = tracksElement.shadowRoot
-		? tracksElement.shadowRoot.querySelector('.track.selected .track-title')
-		: null;
-
-	return trackElements ? trackElements.textContent.trim() : null;
-};
 
 Connector.getTrackArt = () => {
 	const theaterElement = document.querySelector('ia-music-theater');
@@ -104,4 +52,18 @@ Connector.getTrackArt = () => {
 	return theaterElement.querySelector('img')?.getAttribute('src');
 };
 
-Connector.playerSelector = '#theatre-ia';
+Connector.injectScript('connectors/archive-dom-inject.js');
+
+let state = null;
+Connector.onScriptEvent = (event) => {
+	state = event.data.state;
+	Connector.onStateChanged();
+};
+
+Connector.isPlaying = () => state?.isPlaying;
+
+Connector.getDuration = () => state?.getDuration;
+
+Connector.getTrack = () => state?.getTrack;
+
+Connector.getArtist = () => state?.getArtist || Connector.getAlbumArtist();
