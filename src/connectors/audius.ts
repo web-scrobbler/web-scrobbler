b81be3c6459e484a48257800193f00dec8cf3771
Rob9315 <dev.robk@gmail.com>
2026-08-20 00:49:28 +0200

    change splitting behaviour

--- src/connectors/audius.ts
+++ src/connectors/audius.ts
@@ -4,29 +4,18 @@ Connector.playerSelector = '[class*="playBar"]';
 
 Connector.playButtonSelector = `${Connector.playerSelector} [aria-label="play track"]`;
 
-Connector.artistSelector = `${Connector.playerSelector} [class*="_artistName_"]`;
-
-Connector.trackSelector = `${Connector.playerSelector} [class*="_trackTitle_"]`;
+const artistSelector = `${Connector.playerSelector} [class*="_artistName_"]`;
+const trackSelector = `${Connector.playerSelector} [class*="_trackTitle_"]`;
+Connector.getArtistTrack = () => {
+	const track = Util.getTextFromSelectors(trackSelector);
+	const artistTrack = Util.splitArtistTrack(track, [' - ']);
+	artistTrack.track ??= track;
+	artistTrack.artist ??= Util.getTextFromSelectors(artistSelector);
+	return artistTrack;
+};
 
 Connector.durationSelector = `${Connector.playerSelector} [class*="timestampEnd"]`;
 
 Connector.currentTimeSelector = `${Connector.playerSelector} [class*="timestampStart"]`;
 
 Connector.trackArtSelector = '[class*="coverArtLink"] img';
-
-const filter = MetadataFilter.createFilter({
-	track: trimArtist,
-});
-
-Connector.applyFilter(filter);
-
-function trimArtist(track: string): string {
-	const separator = ' - ';
-	const chunks = track.split(separator);
-	const artist = Connector.getArtist();
-	const trackArtist = chunks.shift();
-	if (artist?.toLowerCase() === trackArtist?.toLowerCase()) {
-		return chunks.join(separator);
-	}
-	return track;
-}
