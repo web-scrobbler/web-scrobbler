c01f704ddb51f8ff9ce35e75a7efc32e2cd7bfb3
Rob9315 <dev.robk@gmail.com>
2026-08-31 01:54:23 +0200

    add popup support

--- src/connectors/hang.fm.ts
+++ src/connectors/hang.fm.ts
@@ -1,5 +1,10 @@
 export {};
 
+// Hang.FM connector
+// two sources for playing information supported:
+// - stage view
+// - popup view on navigation, for example going back to the home page
+
 const filter = MetadataFilter.createFilter({
 	artist: trimTrailingSeparator,
 });
@@ -11,17 +16,29 @@ function trimTrailingSeparator(text: string) {
 const playerDisplay = '#NowPlaying button > span';
 const timeDiv = `${playerDisplay} > div:last-child`;
 
-Connector.playerSelector = 'div:has(> #NowPlaying)';
+// listen on all updates on the entire page (for navigation)
+Connector.playerSelector = '.App';
 
-Connector.pauseButtonSelector = '#NowPlaying button';
+// this element doesn't exist in the popup if nothing is playing
+const popupImageWrapper = 'video+div div:has(>button[id^=radix-])';
+
+Connector.pauseButtonSelector = ['#NowPlaying button', popupImageWrapper];
 
 Connector.getArtistTrack = () => {
 	const elems = Util.queryElements(
 		`${playerDisplay} > div:first-child > span:first-child`,
 	);
 
-	if (!elems) {
-		return null;
+	if (!elems?.length) {
+		// take from popup if stage view not available
+		const popupElements = document.querySelectorAll(
+			`${popupImageWrapper} >div>div:last-child [speed]>:first-child`,
+		);
+		if (popupElements.length != 2) return null;
+		const [title, artist] = Array.from(popupElements).map(
+			(elem) => elem.textContent,
+		);
+		return { artist, track: title };
 	}
 
 	for (const elem of elems) {
@@ -42,11 +59,30 @@ Connector.getArtistTrack = () => {
 	}
 };
 
-Connector.currentTimeSelector = `${timeDiv} > div:first-child`;
-Connector.durationSelector = `${timeDiv} > div:last-child`;
+Connector.currentTimeSelector = [
+	`${timeDiv} > div:first-child`,
+	`${popupImageWrapper}+div>div:last-child>:first-child`,
+];
+Connector.durationSelector = [
+	`${timeDiv} > div:last-child`,
+	`${popupImageWrapper}+div>div:last-child>:last-child`,
+];
 
-Connector.loveButtonSelector =
-	'button img[alt="Approve"]:not([src*="pressed"])';
-Connector.unloveButtonSelector = 'button img[alt="Approve"][src*="pressed"]';
+// image behind the stage / click surface in the popup
+Connector.trackArtSelector = [
+	'div:has(>#NowPlaying)>div>img[src*="artwork-cdn.7static.com"]',
+	`${popupImageWrapper}>button[id^=radix-]>img`,
+];
+
+// the like button in stage(bottom)/popup. the dislike button doesn't map to our binary metric
+// the star button would be a better fit but that's in a submenu in the stage view.
+Connector.loveButtonSelector = [
+	'button img[alt="Approve"]:not([src*="pressed"])',
+	`${popupImageWrapper} button.like:not(.active)`,
+];
+Connector.unloveButtonSelector = [
+	'button img[alt="Approve"][src*="pressed"]',
+	`${popupImageWrapper} button.like.active`,
+];
 
 Connector.applyFilter(filter);
