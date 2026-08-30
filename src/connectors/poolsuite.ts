24e31fd19083583276ea822e1fe87b1f7aece14d
Michael Edelstone <michael.edelstone@gmail.com>
2026-08-30 10:50:00 -0500

    Fix Poolsuite metadata parsing for most common track formats (#6081)
    * fix(poolsuite): correctly parse artist/track metadata for all three track formats
    
    * fixes lint errors

--- src/connectors/poolsuite.ts
+++ src/connectors/poolsuite.ts
@@ -1,5 +1,21 @@
 export {};
 
 Connector.playerSelector = 'body';
-Connector.useMediaSessionApi();
 Connector.useTabAudibleApi();
+
+Connector.getArtistTrack = () => {
+	const title = navigator.mediaSession.metadata?.title ?? '';
+	const artist = navigator.mediaSession.metadata?.artist ?? '';
+
+	if (title === 'Poolsuite ON AIR') {
+		const text =
+			document.querySelector('h2.font-everyday')?.textContent ?? '';
+		return Util.processSoundCloudTrack(text);
+	}
+
+	const result = Util.processSoundCloudTrack(title);
+	if (result.artist && result.track) {
+		return result;
+	}
+	return { artist, track: title };
+};
